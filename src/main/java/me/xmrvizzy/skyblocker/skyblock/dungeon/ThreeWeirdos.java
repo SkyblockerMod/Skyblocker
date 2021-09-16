4b6b75bec702b3f1fcfe7a0104647ad22b1d91df
ExternalTime <84183548+ExternalTime@users.noreply.github.com>
2021-09-16 19:40:58 +0200

    Refactored three weirdos solver

--- src/main/java/me/xmrvizzy/skyblocker/skyblock/dungeon/ThreeWeirdos.java
+++ src/main/java/me/xmrvizzy/skyblocker/skyblock/dungeon/ThreeWeirdos.java
@@ -5,29 +5,29 @@ import net.minecraft.entity.decoration.ArmorStandEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ThreeWeirdos {
+
+    private static final MinecraftClient client = MinecraftClient.getInstance();
+    private static final Pattern answer = Pattern.compile("^§e\\[NPC] §c([A-Z][a-z]+)§f: (?:The reward is(?: not in my chest!|n't in any of our chests\\.)|My chest (?:doesn't have the reward we are all telling the truth\\.|has the reward and I'm telling the truth!)|At least one of them is lying, and the reward is not in [A-Z][a-z]+'s chest\\.|Both of them are telling the truth\\. Also, [A-Z][a-z]+ has the reward in their chest\\.)$");
+
     public static void process(String message) {
-        MinecraftClient client = MinecraftClient.getInstance();
-        if (client.player == null && client.world == null) return;
+        assert client.player != null;
+        assert client.world != null;
 
-        String[] solutions = {
-                "The reward is not in my chest!",
-                "At least one of them is lying, and the reward is not in",
-                "My chest doesn't have the reward. We are all telling the truth.",
-                "My chest has the reward and I'm telling the truth!",
-                "The reward isn't in any of our chests.",
-                "Both of them are telling the truth. Also,"
-        };
+        Matcher matcher = answer.matcher(message);
+        if (!matcher.matches())
+            return;
 
-        for (String s : solutions) {
-            if (message.contains(s)) {
-                String npc = message.substring(message.indexOf("]") + 4, message.indexOf(":") - 2);
-                client.world.getEntitiesByClass(ArmorStandEntity.class, client.player.getBoundingBox().expand(3), entity -> {
-                    if (entity.hasCustomName() && entity.getCustomName().getString().contains(npc))
-                        return true;
-                    return false;
-                }).forEach(entity -> entity.setCustomName(Text.of(Formatting.GREEN + npc)));
-            }
-        }
+        String npcName = matcher.group(1);
+        client.world.getEntitiesByClass(
+            ArmorStandEntity.class,
+            client.player.getBoundingBox().expand(3),
+            entity -> entity.hasCustomName() && entity.getCustomName().getString().contains(npcName)
+        ).forEach(
+            entity -> entity.setCustomName(Text.of(Formatting.GREEN + npcName))
+        );
     }
 }
