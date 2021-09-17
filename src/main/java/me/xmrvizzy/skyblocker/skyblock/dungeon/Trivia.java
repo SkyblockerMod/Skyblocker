dbd37d5c29882c6df3f48c8139e95ea8e6efbf5c
ExternalTime <84183548+ExternalTime@users.noreply.github.com>
2021-09-17 14:50:18 +0200

    Updated the number of fairy souls in trivia solver

--- src/main/java/me/xmrvizzy/skyblocker/skyblock/dungeon/Trivia.java
+++ src/main/java/me/xmrvizzy/skyblocker/skyblock/dungeon/Trivia.java
@@ -59,11 +59,11 @@ public class Trivia {
         answers.put("What is the status of Goldor?", new String[]{"Wither Soldier"});
         answers.put("What is the status of Storm?", new String[]{"Elementalist"});
         answers.put("What is the status of Necron?", new String[]{"Wither Lord"});
-        answers.put("How many total Fairy Souls are there?", new String[]{"220 Fairy Souls"});
+        answers.put("How many total Fairy Souls are there?", new String[]{"227 Fairy Souls"});
         answers.put("How many Fairy Souls are there in Spider's Den?", new String[]{"19 Fairy Souls"});
         answers.put("How many Fairy Souls are there in The End?", new String[]{"12 Fairy Souls"});
         answers.put("How many Fairy Souls are there in The Barn?", new String[]{"7 Fairy Souls"});
-        answers.put("How many Fairy Souls are there in Mushroom Desert?", new String[]{"8 Fairy Souls"});
+        answers.put("How many Fairy Souls are there in Mushroom Desert?", new String[]{"13 Fairy Souls"});
         answers.put("How many Fairy Souls are there in Blazing Fortress?", new String[]{"19 Fairy Souls"});
         answers.put("How many Fairy Souls are there in The Park?", new String[]{"11 Fairy Souls"});
         answers.put("How many Fairy Souls are there in Jerry's Workshop?", new String[]{"5 Fairy Souls"});
