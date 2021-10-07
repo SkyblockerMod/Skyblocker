b1c7beb812ec6af83cbc200037634731cb9cf9c6
ExternalTime <84183548+ExternalTime@users.noreply.github.com>
2021-10-07 19:45:52 +0200

    Made SkyblockerMod formatting more consistent

--- src/main/java/me/xmrvizzy/skyblocker/SkyblockerMod.java
+++ src/main/java/me/xmrvizzy/skyblocker/SkyblockerMod.java
@@ -10,35 +10,35 @@ import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.MinecraftClient;
 
 public class SkyblockerMod implements ClientModInitializer {
-	public static final String NAMESPACE = "skyblocker";
-	private static int TICKS = 0;
+    public static final String NAMESPACE = "skyblocker";
+    private static int TICKS = 0;
 
-	@Override
-	public void onInitializeClient() {
-		HotbarSlotLock.init();
-		SkyblockerConfig.init();
-		PriceInfoTooltip.init();
-	}
+    @Override
+    public void onInitializeClient() {
+        HotbarSlotLock.init();
+        SkyblockerConfig.init();
+        PriceInfoTooltip.init();
+    }
 
 
-	public static void onTick() {
-		MinecraftClient client = MinecraftClient.getInstance();
-		if (client == null) return;
+    public static void onTick() {
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (client == null) return;
 
-		TICKS++;
-		if (TICKS % 4 == 0)
-			try {
-				if(Utils.isDungeons){
-					DungeonBlaze.DungeonBlaze();
-				}
-			}catch(Exception e) {
-				//System.out.println("Blazesolver: " + e);
-			}
-		if (TICKS % 20 == 0) {
-			if (client.world != null && !client.isInSingleplayer())
-				Utils.sbChecker();
+        TICKS++;
+        if (TICKS % 4 == 0)
+            try {
+                if (Utils.isDungeons) {
+                    DungeonBlaze.DungeonBlaze();
+                }
+            } catch (Exception e) {
+                //System.out.println("Blazesolver: " + e);
+            }
+        if (TICKS % 20 == 0) {
+            if (client.world != null && !client.isInSingleplayer())
+                Utils.sbChecker();
 
-			TICKS = 0;
-		}
-	}
+            TICKS = 0;
+        }
+    }
 }
