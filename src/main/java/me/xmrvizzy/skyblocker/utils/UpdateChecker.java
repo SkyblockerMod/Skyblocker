7cf1a059304507defd304477790d8f35ee18a393
TacoMonkey <75862693+TacoMonkey11@users.noreply.github.com>
2022-02-18 17:07:00 -0500

    chucking it into a thread...

--- src/main/java/me/xmrvizzy/skyblocker/utils/UpdateChecker.java
+++ src/main/java/me/xmrvizzy/skyblocker/utils/UpdateChecker.java
@@ -17,33 +17,38 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UpdateChecker {
+    public static boolean shouldUpdate = false;
+    public static Pattern pattern = Pattern.compile("v(\\d+)\\.(\\d+)\\.(\\d+)");
+    public static Pattern localPattern = Pattern.compile("(\\d+)\\.(\\d+)\\.(\\d+)");
+    public static Matcher matcher;
+    public static VersionNumber localVersion = null;
+    public static VersionNumber latestVersion = null;
     public static boolean shouldUpdate(){
-        Pattern pattern = Pattern.compile("v(\\d+)\\.(\\d+)\\.(\\d+)");
-        Pattern localPattern = Pattern.compile("(\\d+)\\.(\\d+)\\.(\\d+)");
-        Matcher matcher;
-        VersionNumber localVersion = null;
-        VersionNumber latestVersion = null;
-        boolean shouldUpdate = false;
         if (SkyblockerConfig.get().general.enableUpdateNotification){
-            try{
-                URL url = new URL("https://api.modrinth.com/v2/project/skyblocker-liap/version");
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try{
+                        URL url = new URL("https://api.modrinth.com/v2/project/skyblocker-liap/version");
 
-                InputStreamReader reader = new InputStreamReader(url.openStream());
-                JsonObject versionJson = new Gson().fromJson(reader, JsonElement.class).getAsJsonArray().get(0).getAsJsonObject();
-                matcher = pattern.matcher(versionJson.get("version_number").getAsString());
-                if (matcher.find()){
-                    latestVersion = VersionNumber.parse(matcher.group(1) + "." + matcher.group(2) + "." + matcher.group(3));
-                }
-                matcher = localPattern.matcher(FabricLoader.getInstance().getModContainer(SkyblockerMod.NAMESPACE).get().getMetadata().getVersion().getFriendlyString());
-                if (matcher.find()){
-                    localVersion = VersionNumber.parse(matcher.group(1) + "." + matcher.group(2) + "." + matcher.group(3));
-                }
-                if (localVersion != null && latestVersion != null)
-                    if (localVersion.compareTo(latestVersion) < 0) shouldUpdate = true;
+                        InputStreamReader reader = new InputStreamReader(url.openStream());
+                        JsonObject versionJson = new Gson().fromJson(reader, JsonElement.class).getAsJsonArray().get(0).getAsJsonObject();
+                        matcher = pattern.matcher(versionJson.get("version_number").getAsString());
+                        if (matcher.find()){
+                            latestVersion = VersionNumber.parse(matcher.group(1) + "." + matcher.group(2) + "." + matcher.group(3));
+                        }
+                        matcher = localPattern.matcher(FabricLoader.getInstance().getModContainer(SkyblockerMod.NAMESPACE).get().getMetadata().getVersion().getFriendlyString());
+                        if (matcher.find()){
+                            localVersion = VersionNumber.parse(matcher.group(1) + "." + matcher.group(2) + "." + matcher.group(3));
+                        }
+                        if (localVersion != null && latestVersion != null)
+                            if (localVersion.compareTo(latestVersion) < 0) shouldUpdate = true;
 
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
         }
         return shouldUpdate;
     }
