48ce8068ffd576edd3ea692aad4e6ac898894c4a
ExternalTime <84183548+ExternalTime@users.noreply.github.com>
2022-03-08 15:14:27 +0100

    Changed chat listeners to use the new api

--- src/main/java/me/xmrvizzy/skyblocker/chat/filters/ImplosionFilter.java
+++ src/main/java/me/xmrvizzy/skyblocker/chat/filters/ImplosionFilter.java
@@ -1,15 +1,15 @@
 package me.xmrvizzy.skyblocker.chat.filters;
 
-import me.xmrvizzy.skyblocker.chat.ChatListener;
+import me.xmrvizzy.skyblocker.chat.ChatFilterResult;
 import me.xmrvizzy.skyblocker.config.SkyblockerConfig;
 
-public class ImplosionFilter extends ChatFilter {
+public class ImplosionFilter extends SimpleChatFilter {
     public ImplosionFilter() {
         super("^Your Implosion hit " + NUMBER + " enem(?:y|ies) for " + NUMBER + " damage\\.$");
     }
 
     @Override
-    public boolean isEnabled() {
+    public ChatFilterResult state() {
         return SkyblockerConfig.get().messages.hideImplosion;
     }
 }
--- src/main/java/me/xmrvizzy/skyblocker/skyblock/api/ApiKeyListener.java
+++ src/main/java/me/xmrvizzy/skyblocker/skyblock/api/ApiKeyListener.java
@@ -1,24 +1,28 @@
 package me.xmrvizzy.skyblocker.skyblock.api;
 
 import me.shedaniel.autoconfig.AutoConfig;
-import me.xmrvizzy.skyblocker.chat.ChatListener;
+import me.xmrvizzy.skyblocker.chat.ChatFilterResult;
+import me.xmrvizzy.skyblocker.chat.ChatPatternListener;
 import me.xmrvizzy.skyblocker.config.SkyblockerConfig;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 
-public class ApiKeyListener extends ChatListener {
+import java.util.regex.Matcher;
+
+public class ApiKeyListener extends ChatPatternListener {
     public ApiKeyListener() {
         super("^Your new API key is (.*)$");
     }
 
     @Override
-    public boolean isEnabled() {
-        return true;
+    protected ChatFilterResult state() {
+        return null;
     }
 
     @Override
-    public boolean onMessage(String[] groups) {
-        SkyblockerConfig.get().general.apiKey = groups[1];
+    protected boolean onMatch(Text message, Matcher matcher) {
+        SkyblockerConfig.get().general.apiKey = matcher.group(1);
         AutoConfig.getConfigHolder(SkyblockerConfig.class).save();
         MinecraftClient.getInstance().player.sendMessage(new TranslatableText("skyblocker.api.got_key"), false);
         return false;
