1c26538ae79739edd6ae87e310c6bcaec2885a02
LifeIsAParadox <LifeIsAParadox@users.noreply.github.com>
2022-03-08 19:23:49 +0100

    update to 1.18.2

--- src/main/java/me/xmrvizzy/skyblocker/mixin/HandledScreenMixin.java
+++ src/main/java/me/xmrvizzy/skyblocker/mixin/HandledScreenMixin.java
@@ -47,7 +47,7 @@ public abstract class HandledScreenMixin extends Screen {
 
     @Inject(at = @At("HEAD"), method = "keyPressed", cancellable = true)
     public void keyPressed(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> cir) {
-        if (keyCode != 256 && !MinecraftClient.getInstance().options.keyInventory.matchesKey(keyCode, scanCode)){
+        if (keyCode != 256 && !MinecraftClient.getInstance().options.inventoryKey.matchesKey(keyCode, scanCode)){
             if (WikiLookup.wikiLookup.matchesKey(keyCode, scanCode)) WikiLookup.openWiki(focusedSlot);
         }
     }
