57376158f295adc84001cb17c65da65eca489649
Fix3dll <serhanduzce@gmail.com>
2022-03-09 01:33:08 +0300

    change: AutopetFilterTest

--- src/test/java/me/xmrvizzy/skyblocker/chat/filters/AutopetFilterTest.java
+++ src/test/java/me/xmrvizzy/skyblocker/chat/filters/AutopetFilterTest.java
@@ -2,9 +2,6 @@ package me.xmrvizzy.skyblocker.chat.filters;
 
 import me.xmrvizzy.skyblocker.chat.ChatPatternListenerTest;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertLinesMatch;
-
-import java.util.List;
 
 class AutopetFilterTest extends ChatPatternListenerTest<AutopetFilter> {
     public AutopetFilterTest() {
@@ -13,9 +10,6 @@ class AutopetFilterTest extends ChatPatternListenerTest<AutopetFilter> {
 
     @Test
     void testAutopet() {
-        List<String> expected = List.of(listener.pattern.toString());
-        List<String> actual = List.of("§cAutopet §eequipped your §7[Lvl 85] §6Tiger§e! §a§lVIEW RULE");
-
-        assertLinesMatch(expected, actual);
+        assertMatches("§cAutopet §eequipped your §7[Lvl 85] §6Tiger§e! §a§lVIEW RULE");
     }
 }
