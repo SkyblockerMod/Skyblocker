be9bf6d47cc64cf5de6a7e33dda24b69fe7c56e7
TacoMonkey <75862693+TacoMonkey11@users.noreply.github.com>
2022-03-19 18:08:57 -0400

    Simplify callback

--- src/main/java/me/xmrvizzy/skyblocker/utils/events/SkyblockJoinCallback.java
+++ src/main/java/me/xmrvizzy/skyblocker/utils/events/SkyblockJoinCallback.java
@@ -2,16 +2,13 @@ package me.xmrvizzy.skyblocker.utils.events;
 
 import net.fabricmc.fabric.api.event.Event;
 import net.fabricmc.fabric.api.event.EventFactory;
-import net.fabricmc.fabric.api.networking.v1.PacketSender;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.util.ActionResult;
 
 public interface SkyblockJoinCallback {
     Event<SkyblockJoinCallback> EVENT = EventFactory.createArrayBacked(SkyblockJoinCallback.class,
-            (listeners) -> (handler, sender, client) -> {
+            (listeners) -> () -> {
                 for (SkyblockJoinCallback listener : listeners) {
-                    ActionResult result = listener.join(handler, sender, client);
+                    ActionResult result = listener.join();
 
                     if(result != ActionResult.PASS) {
                         return result;
@@ -20,5 +17,5 @@ public interface SkyblockJoinCallback {
                 return ActionResult.PASS;
             });
 
-    ActionResult join(ClientPlayNetworkHandler handler, PacketSender sender, MinecraftClient client);
+    ActionResult join();
 }
