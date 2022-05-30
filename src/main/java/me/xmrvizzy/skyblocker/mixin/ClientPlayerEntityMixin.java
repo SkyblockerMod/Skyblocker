3a1a1427b838fbfb992c0eab48d4282aa42ea1e3
Trương Hoàng Long <longtruong2411@gmail.com>
2022-05-30 17:12:43 +0200

    remove dead code from original mod

--- src/main/java/me/xmrvizzy/skyblocker/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/me/xmrvizzy/skyblocker/mixin/ClientPlayerEntityMixin.java
@@ -9,7 +9,6 @@ import net.minecraft.client.world.ClientWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ClientPlayerEntity.class)
@@ -17,14 +16,6 @@ public abstract class ClientPlayerEntityMixin extends AbstractClientPlayerEntity
 
     public ClientPlayerEntityMixin(ClientWorld world, GameProfile profile) {
         super(world, profile);
-    }
-
-    @Inject(method = "sendChatMessage", at = @At("HEAD"), cancellable = true)
-    public void sendChatMessage(String message, CallbackInfo ci) {
-        String[] split = message.toLowerCase().split(" ");
-        if (split.length > 0 && split[0].contentEquals("/skb")) {
-            ci.cancel();
-        }
     }
 
     @Inject(method = "dropSelectedItem", at = @At("HEAD"), cancellable = true)
