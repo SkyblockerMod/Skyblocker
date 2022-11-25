175c7734f335aaa8ef1bacee6e8e4d2407ca1a5e
Spencer <75862693+TacoMonkey11@users.noreply.github.com>
2022-11-25 07:45:39 -0500

    Make update notification translation cleaner

--- src/main/java/me/xmrvizzy/skyblocker/utils/UpdateChecker.java
+++ src/main/java/me/xmrvizzy/skyblocker/utils/UpdateChecker.java
@@ -6,9 +6,6 @@ import com.google.gson.JsonObject;
 import me.xmrvizzy.skyblocker.SkyblockerMod;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.text.ClickEvent;
-import net.minecraft.text.HoverEvent;
-import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.util.VersionNumber;
 
@@ -19,50 +16,36 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UpdateChecker {
-    public static boolean shouldUpdate = false;
     public static Pattern pattern = Pattern.compile("v(\\d+)\\.(\\d+)\\.(\\d+)");
     public static Pattern localPattern = Pattern.compile("(\\d+)\\.(\\d+)\\.(\\d+)");
     public static Matcher matcher;
     public static VersionNumber localVersion = null;
     public static VersionNumber latestVersion = null;
-    public static boolean shouldUpdate(){
-        if (SkyblockerMod.getInstance().CONFIG.general.enableUpdateNotification()){
-            new Thread(() -> {
-                try{
-                    URL url = new URL("https://api.modrinth.com/v2/project/skyblocker-liap/version");
-
-                    InputStreamReader reader = new InputStreamReader(url.openStream());
-                    JsonObject versionJson = new Gson().fromJson(reader, JsonElement.class).getAsJsonArray().get(0).getAsJsonObject();
-                    matcher = pattern.matcher(versionJson.get("version_number").getAsString());
-                    if (matcher.find()){
-                        latestVersion = VersionNumber.parse(matcher.group(1) + "." + matcher.group(2) + "." + matcher.group(3));
-                    }
-                    matcher = localPattern.matcher(FabricLoader.getInstance().getModContainer(SkyblockerMod.NAMESPACE).get().getMetadata().getVersion().getFriendlyString());
-                    if (matcher.find()){
-                        localVersion = VersionNumber.parse(matcher.group(1) + "." + matcher.group(2) + "." + matcher.group(3));
-                    }
-                    if (localVersion != null && latestVersion != null)
-                        if (localVersion.compareTo(latestVersion) < 0) shouldUpdate = true;
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }).start();
-        }
-        return shouldUpdate;
-    }
 
     public static void init(){
-        SkyblockEvents.JOIN.register(() -> {
-            if (shouldUpdate()) {
-                MutableText linkMessage = Text.translatable("skyblocker.update.update_message");
-                MutableText linkMessageEnding = Text.translatable("skyblocker.update.update_message_end");
-                MutableText link = Text.translatable("skyblocker.update.update_link");
-                MutableText hoverText = Text.translatable("skyblocker.update.hover_text");
-                linkMessage.append(link.styled(style -> style.withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://modrinth.com/mod/skyblocker-liap/versions")).withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, hoverText)))).append(linkMessageEnding);
+        if (!SkyblockerMod.getInstance().CONFIG.general.enableUpdateNotification()) return;
+        try {
+            URL url = new URL("https://api.modrinth.com/v2/project/skyblocker-liap/version");
 
-                MinecraftClient.getInstance().player.sendMessage(linkMessage, false);
+            InputStreamReader reader = new InputStreamReader(url.openStream());
+            JsonObject versionJson = new Gson().fromJson(reader, JsonElement.class).getAsJsonArray().get(0).getAsJsonObject();
+            matcher = pattern.matcher(versionJson.get("version_number").getAsString());
+            if (matcher.find()){
+                latestVersion = VersionNumber.parse(matcher.group(1) + "." + matcher.group(2) + "." + matcher.group(3));
+            }
+            matcher = localPattern.matcher(FabricLoader.getInstance().getModContainer(SkyblockerMod.NAMESPACE).get().getMetadata().getVersion().getFriendlyString());
+            if (matcher.find()){
+                localVersion = VersionNumber.parse(matcher.group(1) + "." + matcher.group(2) + "." + matcher.group(3));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        SkyblockEvents.JOIN.register(() -> {
+            if (localVersion != null && latestVersion != null) {
+                System.out.println(localVersion.compareTo(latestVersion) == -1);
+                if (localVersion.compareTo(latestVersion) == -1) MinecraftClient.getInstance().player.sendMessage(Text.translatable("skyblocker.update.update_message"), false);
             }
         });
+
     }
 }
