5e1339416ae119e124199b51f0d43a3fd3791fdf
Julienraptor01 <julienraptor01@gmail.com>
2023-04-02 20:43:31 +0200

    make sure it does not contain but instead match
    thanks for the help @AzureAaron
    
    Co-Authored-By: Aaron <51387595+AzureAaron@users.noreply.github.com>

--- src/main/java/me/xmrvizzy/skyblocker/skyblock/quicknav/QuickNav.java
+++ src/main/java/me/xmrvizzy/skyblocker/skyblock/quicknav/QuickNav.java
@@ -42,7 +42,7 @@ public class QuickNav {
         if (itemData.nbt.length() > 2) nbtString += "," + itemData.nbt;
         nbtString += "}";
         return new QuickNavButton(id,
-                screenTitle.contains(buttonInfo.uiTitle),
+                screenTitle.equals(buttonInfo.uiTitle),
                 buttonInfo.clickEvent,
                 ItemStack.fromNbt(StringNbtReader.parse(nbtString))
         );
