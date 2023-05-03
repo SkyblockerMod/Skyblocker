fca101c2d22c682856f7097a1c013c59d0850e0d
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2023-05-03 17:35:43 -0400

    Migrated client tick and container mixins to ClientTickEvents and ScreenEvents

--- src/main/java/me/xmrvizzy/skyblocker/SkyblockerMod.java
+++ src/main/java/me/xmrvizzy/skyblocker/SkyblockerMod.java
@@ -1,15 +1,28 @@
 package me.xmrvizzy.skyblocker;
 
+import me.xmrvizzy.skyblocker.chat.ChatMessageListener;
+import me.xmrvizzy.skyblocker.config.SkyblockerConfig;
 import me.xmrvizzy.skyblocker.container.ContainerSolverManager;
 import me.xmrvizzy.skyblocker.discord.DiscordRPCManager;
 import me.xmrvizzy.skyblocker.skyblock.BackpackPreview;
+import me.xmrvizzy.skyblocker.skyblock.HotbarSlotLock;
 import me.xmrvizzy.skyblocker.skyblock.StatusBarTracker;
+import me.xmrvizzy.skyblocker.skyblock.api.StatsCommand;
 import me.xmrvizzy.skyblocker.skyblock.dungeon.DungeonBlaze;
 import me.xmrvizzy.skyblocker.skyblock.dwarven.DwarvenHud;
+import me.xmrvizzy.skyblocker.skyblock.item.PriceInfoTooltip;
+import me.xmrvizzy.skyblocker.skyblock.item.WikiLookup;
+import me.xmrvizzy.skyblocker.skyblock.itemlist.ItemRegistry;
 import me.xmrvizzy.skyblocker.utils.Scheduler;
+import me.xmrvizzy.skyblocker.utils.UpdateChecker;
 import me.xmrvizzy.skyblocker.utils.Utils;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
 
-public class SkyblockerMod {
+import java.awt.*;
+
+public class SkyblockerMod implements ClientModInitializer {
     public static final String NAMESPACE = "skyblocker";
     private static final SkyblockerMod instance = new SkyblockerMod();
 
@@ -29,7 +42,23 @@ public class SkyblockerMod {
         return instance;
     }
 
-    public void onTick() {
+    @Override
+    public void onInitializeClient() {
+        ClientTickEvents.END_CLIENT_TICK.register(this::tick);
+        HotbarSlotLock.init();
+        SkyblockerConfig.init();
+        PriceInfoTooltip.init();
+        WikiLookup.init();
+        ItemRegistry.init();
+        ContainerSolverManager.init();
+        StatsCommand.init();
+        DwarvenHud.init();
+        ChatMessageListener.init();
+        UpdateChecker.init();
+        DiscordRPCManager.init();
+    }
+
+    public void tick(MinecraftClient client) {
         scheduler.tick();
     }
 }
