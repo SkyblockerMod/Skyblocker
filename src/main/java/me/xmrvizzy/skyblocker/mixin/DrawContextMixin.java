9e142941684bb40fe48f7f68bcd456894d3acd57
Aaron <51387595+AzureAaron@users.noreply.github.com>
2023-06-07 13:13:24 -0400

    Fix drill fuel Z layering

--- src/main/java/me/xmrvizzy/skyblocker/mixin/DrawContextMixin.java
+++ src/main/java/me/xmrvizzy/skyblocker/mixin/DrawContextMixin.java
@@ -5,6 +5,8 @@ import java.util.regex.Pattern;
 
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,12 +18,15 @@ import me.xmrvizzy.skyblocker.utils.ItemUtils;
 import me.xmrvizzy.skyblocker.utils.Utils;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.math.ColorHelper;
 
 @Mixin(DrawContext.class)
 public abstract class DrawContextMixin {
+	@Shadow @Final private MatrixStack matrices;
+	
     @Inject(method = "drawItemInSlot(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/item/ItemStack;IILjava/lang/String;)V", at = @At("HEAD"))
     public void skyblocker$renderItemBar(TextRenderer textRenderer, ItemStack stack, int x, int y, @Nullable String countOverride, CallbackInfo ci) {
 
@@ -44,13 +49,17 @@ public abstract class DrawContextMixin {
                         }
                         
                         DrawContext context = ((DrawContext) (Object) this);
-
+                        matrices.push();
+                        matrices.translate(0f, 0f, 200f);
                         RenderSystem.disableDepthTest();
+                        
                         float hue = Math.max(0.0F, 1.0F - (max - current) / max);
                         int width = Math.round(current / max * 13.0F);
                         Color color = Color.getHSBColor(hue / 3.0F, 1.0F, 1.0F);
                         context.fill(x + 2, y + 13, x + 15, y + 15, 0xFF000000);
                         context.fill(x + 2, y + 13, x + 2 + width, y + 14, ColorHelper.Argb.getArgb(color.getAlpha(), color.getRed(), color.getGreen(), color.getBlue()));
+                        
+                        matrices.pop();
                         RenderSystem.enableDepthTest();
                     }
                 }
