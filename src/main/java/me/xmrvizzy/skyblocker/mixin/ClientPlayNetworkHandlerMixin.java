c04fde4d99b93d68be1c641c650370d1571ce050
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2023-07-25 13:59:27 +0800

    Hide spammed warnings

--- src/main/java/me/xmrvizzy/skyblocker/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/me/xmrvizzy/skyblocker/mixin/ClientPlayNetworkHandlerMixin.java
@@ -1,24 +1,35 @@
 package me.xmrvizzy.skyblocker.mixin;
 
+import com.llamalad7.mixinextras.injector.WrapWithCondition;
 import me.xmrvizzy.skyblocker.skyblock.FishingHelper;
-import net.minecraft.client.MinecraftClient;
+import me.xmrvizzy.skyblocker.utils.Utils;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.packet.s2c.play.PlaySoundS2CPacket;
-import org.spongepowered.asm.mixin.Final;
+import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public abstract class ClientPlayNetworkHandlerMixin {
-    @Shadow
-    @Final
-    private MinecraftClient client;
-
     @Inject(method = "onPlaySound", at = @At("RETURN"))
-    private void skyblockmod_onPlaySound(PlaySoundS2CPacket packet, CallbackInfo ci) {
-        FishingHelper.onSound(client, packet);
+    private void skyblocker$onPlaySound(PlaySoundS2CPacket packet, CallbackInfo ci) {
+        FishingHelper.onSound(packet);
+    }
+
+    @WrapWithCondition(method = "onEntityPassengersSet", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;)V"))
+    private boolean skyblocker$cancelEntityPassengersWarning(Logger instance, String msg) {
+        return !Utils.isOnHypixel();
+    }
+
+    @WrapWithCondition(method = "onPlayerList", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;)V"))
+    private boolean skyblocker$cancelPlayerListWarning(Logger instance, String format, Object arg) {
+        return !Utils.isOnHypixel();
+    }
+
+    @WrapWithCondition(method = "onTeam", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;[Ljava/lang/Object;)V"))
+    private boolean skyblocker$cancelTeamWarning(Logger instance, String format, Object... arg) {
+        return !Utils.isOnHypixel();
     }
 }
