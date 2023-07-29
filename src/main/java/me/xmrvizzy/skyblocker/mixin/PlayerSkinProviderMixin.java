9a784067b9921ce4b61ef309eb197843ea0c19d6
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2023-07-29 12:24:10 +0800

    Refactor PlayerSkinProviderMixin

--- src/main/java/me/xmrvizzy/skyblocker/mixin/PlayerSkinProviderMixin.java
+++ src/main/java/me/xmrvizzy/skyblocker/mixin/PlayerSkinProviderMixin.java
@@ -1,27 +1,20 @@
 package me.xmrvizzy.skyblocker.mixin;
 
-import java.util.concurrent.ExecutorService;
-
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
 import me.xmrvizzy.skyblocker.utils.Utils;
 import net.minecraft.client.texture.PlayerSkinProvider;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(PlayerSkinProvider.class)
 public class PlayerSkinProviderMixin {
-
-	@Redirect(method = "loadSkin(Lcom/mojang/authlib/GameProfile;Lnet/minecraft/client/texture/PlayerSkinProvider$SkinTextureAvailableCallback;Z)V", at = @At(value = "INVOKE", target = "Ljava/util/concurrent/ExecutorService;execute(Ljava/lang/Runnable;)V", remap = false))
-	private void skyblocker$removeInvalidBase64LogSpam(ExecutorService executor, Runnable runnable) {
-		executor.execute(() -> {
-			try {
-				runnable.run();
-			} catch (Throwable t) {
-				if (!(t instanceof IllegalArgumentException) || !Utils.isOnHypixel()) {
-					t.printStackTrace();
-				}
-			}
-		});
-	}
+    @ModifyVariable(method = "loadSkin(Lcom/mojang/authlib/GameProfile;Lnet/minecraft/client/texture/PlayerSkinProvider$SkinTextureAvailableCallback;Z)V", at = @At("STORE"))
+    private Runnable skyblocker$removeInvalidBase64LogSpam(Runnable runnable) {
+        return Utils.isOnHypixel() ? () -> {
+            try {
+                runnable.run();
+            } catch (IllegalArgumentException ignored) {
+            }
+        } : runnable;
+    }
 }
