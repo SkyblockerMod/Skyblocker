813271ea198f0f683677d349396eab017a778964
msg-programs <msgdoesstuff@gmail.com>
2023-08-13 14:09:29 +0200

    Make padding from screen edges larger

--- src/main/java/me/xmrvizzy/skyblocker/skyblock/tabhud/util/ScreenConst.java
+++ src/main/java/me/xmrvizzy/skyblocker/skyblock/tabhud/util/ScreenConst.java
@@ -5,7 +5,7 @@ import me.xmrvizzy.skyblocker.config.SkyblockerConfig;
 public class ScreenConst {
     public static final int WIDGET_PAD = 5;
     public static final int WIDGET_PAD_HALF = 3;
-    private static final int SCREEN_PAD_BASE = 10;
+    private static final int SCREEN_PAD_BASE = 20;
 
     public static int getScreenPad() {
         return (int) ((1f/((float)SkyblockerConfig.get().general.tabHud.tabHudScale/100f) * SCREEN_PAD_BASE));
