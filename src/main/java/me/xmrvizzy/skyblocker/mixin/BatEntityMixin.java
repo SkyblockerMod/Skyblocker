b32d3128b9b772a431bafc809f8c0892c7638282
Kevin <92656833+kevinthegreat1@users.noreply.github.com>
2023-09-08 15:29:10 -0400

    Remove Unique annotation

--- src/main/java/me/xmrvizzy/skyblocker/mixin/BatEntityMixin.java
+++ src/main/java/me/xmrvizzy/skyblocker/mixin/BatEntityMixin.java
@@ -14,7 +14,6 @@ public abstract class BatEntityMixin extends AmbientEntity {
         super(entityType, world);
     }
 
-    @Unique
     @Override
     public void onRemoved() {
         super.onRemoved();
