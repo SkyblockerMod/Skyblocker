8c88eb79c83b2ab33eb4b53759e41fc6d8170e8f
Aaron <51387595+AzureAaron@users.noreply.github.com>
2023-09-16 21:09:10 -0400

    Locraw filter fix (#314)
    * Fixed locraw being filtered wrongly in some situations
    
    * Fix locraw filter

--- src/main/java/me/xmrvizzy/skyblocker/utils/Utils.java
+++ src/main/java/me/xmrvizzy/skyblocker/utils/Utils.java
@@ -49,7 +49,7 @@ public class Utils {
     private static String map = "";
     private static long clientWorldJoinTime = 0;
     private static boolean sentLocRaw = false;
-    private static long lastLocRaw = 0;
+    private static boolean canSendLocRaw = false;
 
     public static boolean isOnHypixel() {
         return isOnHypixel;
@@ -293,10 +293,10 @@ public class Utils {
     private static void updateLocRaw() {
         if (isOnSkyblock) {
             long currentTime = System.currentTimeMillis();
-            if (!sentLocRaw && currentTime > clientWorldJoinTime + 1000 && currentTime > lastLocRaw + 15000) {
+            if (!sentLocRaw && canSendLocRaw && currentTime > clientWorldJoinTime + 1000) {
                 MessageScheduler.INSTANCE.sendMessageAfterCooldown("/locraw");
                 sentLocRaw = true;
-                lastLocRaw = currentTime;
+                canSendLocRaw = false;
             }
         } else {
             resetLocRawInfo();
@@ -323,7 +323,11 @@ public class Utils {
                 if (locRaw.has("map")) {
                     map = locRaw.get("map").getAsString();
                 }
-                return !sentLocRaw;
+                
+                boolean shouldFilter = !sentLocRaw;
+                sentLocRaw = false;
+                
+                return shouldFilter;
             }
         }
         return true;
@@ -331,6 +335,7 @@ public class Utils {
 
     private static void resetLocRawInfo() {
         sentLocRaw = false;
+        canSendLocRaw = true;
         server = "";
         gameType = "";
         locationRaw = "";
