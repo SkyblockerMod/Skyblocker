bc22e75e2eac6526cd7dc8ec99f32469ee4cc5d7
alexia <me@alexia.lol>
2023-10-07 17:29:29 +0200

    Address further review comments

--- src/main/java/me/xmrvizzy/skyblocker/utils/ItemUtils.java
+++ src/main/java/me/xmrvizzy/skyblocker/utils/ItemUtils.java
@@ -19,7 +19,22 @@ import java.util.regex.Pattern;
 public class ItemUtils {
     private final static Pattern WHITESPACES = Pattern.compile("^\\s*$");
 
-    public record Durability(int current, int max) {
+    public static List<Text> getTooltip(ItemStack item) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        return client.player == null || item == null ? Collections.emptyList() : item.getTooltip(client.player, TooltipContext.Default.BASIC);
+    }
+
+    public static List<String> getTooltipStrings(ItemStack item) {
+        List<Text> lines = getTooltip(item);
+        List<String> list = new ArrayList<>();
+
+        for (Text line : lines) {
+            String string = line.getString();
+            if (!WHITESPACES.matcher(string).matches())
+                list.add(string);
+        }
+
+        return list;
     }
 
     @Nullable
@@ -69,29 +84,14 @@ public class ItemUtils {
         return null;
     }
 
-    public static List<Text> getTooltip(ItemStack item) {
-        MinecraftClient client = MinecraftClient.getInstance();
-        return client.player == null || item == null ? Collections.emptyList() : item.getTooltip(client.player, TooltipContext.Default.BASIC);
-    }
-
-    public static List<String> getTooltipStrings(ItemStack item) {
-        List<Text> lines = getTooltip(item);
-        List<String> list = new ArrayList<>();
-
-        for (Text line : lines) {
-            String string = line.getString();
-            if (!WHITESPACES.matcher(string).matches())
-                list.add(string);
-        }
-
-        return list;
-    }
-
     public static ItemStack getSkyblockerStack() {
         try {
             return ItemStack.fromNbt(StringNbtReader.parse("{id:\"minecraft:player_head\",Count:1,tag:{SkullOwner:{Id:[I;-300151517,-631415889,-1193921967,-1821784279],Properties:{textures:[{Value:\"e3RleHR1cmVzOntTS0lOOnt1cmw6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZDdjYzY2ODc0MjNkMDU3MGQ1NTZhYzUzZTA2NzZjYjU2M2JiZGQ5NzE3Y2Q4MjY5YmRlYmVkNmY2ZDRlN2JmOCJ9fX0=\"}]}}}}"));
         } catch (CommandSyntaxException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public record Durability(int current, int max) {
     }
 }
