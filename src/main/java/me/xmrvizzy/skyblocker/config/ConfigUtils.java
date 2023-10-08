0e251fd4cba79bf1578dafe2947e7c9ca28294c3
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2023-10-07 23:53:27 -0400

    Fix indentation

--- src/main/java/me/xmrvizzy/skyblocker/config/ConfigUtils.java
+++ src/main/java/me/xmrvizzy/skyblocker/config/ConfigUtils.java
@@ -12,7 +12,7 @@ import java.util.function.Function;
 
 public class ConfigUtils {
 	public static final Function<Formatting, String> FORMATTING_TO_STRING = formatting -> StringUtils.capitalize(formatting.getName().replaceAll("_", " "));
-    public static final ValueFormatter<Float> FLOAT_TWO_FORMATTER = value -> Text.literal(String.format("%,.2f", value).replaceAll("[\u00a0\u202F]", " "));
+	public static final ValueFormatter<Float> FLOAT_TWO_FORMATTER = value -> Text.literal(String.format("%,.2f", value).replaceAll("[\u00a0\u202F]", " "));
 
 	public static BooleanControllerBuilder createBooleanController(Option<Boolean> opt) {
 		return BooleanControllerBuilder.create(opt).yesNoFormatter().coloured(true);
