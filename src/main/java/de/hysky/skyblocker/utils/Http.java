158782dabf00a009d7a431f20d4cb2227ffc4298
Aaron <51387595+AzureAaron@users.noreply.github.com>
2023-10-28 00:50:26 -0400

    Use Api Redirect (#388)
    Redirects to api.azureaaron.net

--- src/main/java/de/hysky/skyblocker/utils/Http.java
+++ src/main/java/de/hysky/skyblocker/utils/Http.java
@@ -26,7 +26,7 @@ import net.minecraft.SharedConstants;
  */
 public class Http {
 	private static final String NAME_2_UUID = "https://api.minecraftservices.com/minecraft/profile/lookup/name/";
-	private static final String HYPIXEL_PROXY = "https://api.azureaaron.net/hypixel/";
+	private static final String HYPIXEL_PROXY = "https://hysky.de/api/hypixel/";
 	private static final String USER_AGENT = "Skyblocker/" + SkyblockerMod.VERSION + " (" + SharedConstants.getGameVersion().getName() + ")";
 	private static final HttpClient HTTP_CLIENT = HttpClient.newBuilder()
 			.connectTimeout(Duration.ofSeconds(10))
