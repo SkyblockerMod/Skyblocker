59f7642cda0b1014149b16304ed149382f3e2bdf
Aaron <51387595+AzureAaron@users.noreply.github.com>
2023-11-28 16:24:09 -0500

    1.20.3-pre4

--- src/main/java/de/hysky/skyblocker/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/de/hysky/skyblocker/mixin/ClientPlayNetworkHandlerMixin.java
@@ -37,8 +37,8 @@ public abstract class ClientPlayNetworkHandlerMixin {
         return !Utils.isOnHypixel();
     }
 
-    @WrapWithCondition(method = "onPlayerList", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;)V", remap = false))
-    private boolean skyblocker$cancelPlayerListWarning(Logger instance, String format, Object arg) {
+    @WrapWithCondition(method = "onPlayerList", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V", remap = false))
+    private boolean skyblocker$cancelPlayerListWarning(Logger instance, String format, Object arg1, Object arg2) {
         return !Utils.isOnHypixel();
     }
 
