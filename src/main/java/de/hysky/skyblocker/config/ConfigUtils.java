4b194f8fc191720ee07b349904a75d33dafd0021
Kevin <92656833+kevinthegreat1@users.noreply.github.com>
2023-12-15 15:47:18 -0500

    Migrate to upstreamed EnumDropdownController (#447)

--- src/main/java/de/hysky/skyblocker/config/ConfigUtils.java
+++ src/main/java/de/hysky/skyblocker/config/ConfigUtils.java
@@ -1,9 +1,7 @@
 package de.hysky.skyblocker.config;
 
 import dev.isxander.yacl3.api.Option;
-import dev.isxander.yacl3.api.controller.BooleanControllerBuilder;
-import dev.isxander.yacl3.api.controller.EnumControllerBuilder;
-import dev.isxander.yacl3.api.controller.ValueFormatter;
+import dev.isxander.yacl3.api.controller.*;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import org.apache.commons.lang3.StringUtils;
@@ -11,7 +9,7 @@ import org.apache.commons.lang3.StringUtils;
 import java.util.function.Function;
 
 public class ConfigUtils {
-	public static final Function<Formatting, String> FORMATTING_TO_STRING = formatting -> StringUtils.capitalize(formatting.getName().replaceAll("_", " "));
+	public static final ValueFormatter<Formatting> FORMATTING_FORMATTER = formatting -> Text.literal(StringUtils.capitalize(formatting.getName().replaceAll("_", " ")));
 	public static final ValueFormatter<Float> FLOAT_TWO_FORMATTER = value -> Text.literal(String.format("%,.2f", value).replaceAll("[\u00a0\u202F]", " "));
 
 	public static BooleanControllerBuilder createBooleanController(Option<Boolean> opt) {
@@ -21,5 +19,19 @@ public class ConfigUtils {
 	@SuppressWarnings("unchecked")
 	public static <E extends Enum<E>> EnumControllerBuilder<E> createEnumCyclingListController(Option<E> opt) {
 		return EnumControllerBuilder.create(opt).enumClass((Class<E>) opt.binding().defaultValue().getClass());
+	}
+
+	/**
+	 * Creates a factory for {@link EnumDropdownControllerBuilder}s with the given function for converting enum constants to texts.
+	 * Use this if a custom formatter function for an enum is needed.
+	 * Use it like this:
+	 * <pre>{@code Option.<MyEnum>createBuilder().controller(ConfigUtils.getEnumDropdownControllerFactory(MY_CUSTOM_ENUM_TO_TEXT_FUNCTION))}</pre>
+	 *
+	 * @param formatter The function used to convert enum constants to texts used for display, suggestion, and validation
+	 * @param <E>       the enum type
+	 * @return a factory for {@link EnumDropdownControllerBuilder}s
+	 */
+	public static <E extends Enum<E>> Function<Option<E>, ControllerBuilder<E>> getEnumDropdownControllerFactory(ValueFormatter<E> formatter) {
+		return opt -> EnumDropdownControllerBuilder.create(opt).formatValue(formatter);
 	}
 }
