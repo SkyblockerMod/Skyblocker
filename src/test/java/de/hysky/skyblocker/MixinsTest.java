15864375102291012ffb662c13d2ad5a1254f21f
Aaron <51387595+AzureAaron@users.noreply.github.com>
2024-01-02 02:22:07 -0500

    Audit Mixins

---
+++ src/test/java/de/hysky/skyblocker/MixinsTest.java
@@ -0,0 +1,27 @@
+package de.hysky.skyblocker;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.spongepowered.asm.mixin.MixinEnvironment;
+import org.spongepowered.asm.mixin.transformer.IMixinTransformer;
+
+import net.minecraft.Bootstrap;
+import net.minecraft.SharedConstants;
+
+public class MixinsTest {
+
+	@BeforeAll
+	public static void setupEnvironment() {
+		SharedConstants.createGameVersion();
+		Bootstrap.initialize();
+	}
+
+	@Test
+	public void auditMixins() {
+		//Ensure that the transformer is active so that the Mixins can be audited
+		assert MixinEnvironment.getCurrentEnvironment().getActiveTransformer() instanceof IMixinTransformer;
+
+		//If this fails check the report to get the full stack trace
+		MixinEnvironment.getCurrentEnvironment().audit();
+	}
+}
