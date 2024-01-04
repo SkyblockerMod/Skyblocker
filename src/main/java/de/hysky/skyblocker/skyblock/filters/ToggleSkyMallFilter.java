4dc26a13ced11281a4da74bd52d00ef36c0d03d5
Aaron <51387595+AzureAaron@users.noreply.github.com>
2024-01-04 01:56:29 -0500

    Toggle Sky Mall Filter (#475)

---
+++ src/main/java/de/hysky/skyblocker/skyblock/filters/ToggleSkyMallFilter.java
@@ -0,0 +1,16 @@
+package de.hysky.skyblocker.skyblock.filters;
+
+import de.hysky.skyblocker.config.SkyblockerConfigManager;
+import de.hysky.skyblocker.utils.chat.ChatFilterResult;
+
+public class ToggleSkyMallFilter extends SimpleChatFilter {
+
+	public ToggleSkyMallFilter() {
+		super("^§8§oYou can disable this messaging by toggling Sky Mall in your /hotm!$");
+	}
+
+	@Override
+	protected ChatFilterResult state() {
+		return SkyblockerConfigManager.get().messages.hideToggleSkyMall;
+	}
+}
