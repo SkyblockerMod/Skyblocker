db778f5f2b92c9697c9df38a8e056392c70252ed
Kevin <92656833+kevinthegreat1@users.noreply.github.com>
2024-01-11 03:43:41 +0800

    Use vanilla translations (#479)

--- src/main/java/de/hysky/skyblocker/utils/JoinWorldPlaceholderScreen.java
+++ src/main/java/de/hysky/skyblocker/utils/JoinWorldPlaceholderScreen.java
@@ -3,10 +3,7 @@ package de.hysky.skyblocker.utils;
 import net.minecraft.text.Text;
 
 public final class JoinWorldPlaceholderScreen extends BasePlaceholderScreen {
-
-    private static final String SCREEN_TITLE = "Joining World Screen";
-
     public JoinWorldPlaceholderScreen() {
-        super(Text.literal(SCREEN_TITLE));
+        super(Text.translatable("connect.joining"));
     }
 }
