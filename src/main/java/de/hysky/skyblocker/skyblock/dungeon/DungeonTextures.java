1af64a2311e291d969e42bc231065ddcd3a16c0c
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2024-01-13 18:14:36 -0500

    Refactor

--- src/main/java/de/hysky/skyblocker/skyblock/dungeon/DungeonTextures.java
+++ src/main/java/de/hysky/skyblocker/skyblock/dungeon/DungeonTextures.java
@@ -1,18 +1,16 @@
 package de.hysky.skyblocker.skyblock.dungeon;
 
+import de.hysky.skyblocker.SkyblockerMod;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.util.Identifier;
 
 public class DungeonTextures {
     public static void init() {
-        FabricLoader.getInstance()
-            .getModContainer("skyblocker")
-            .ifPresent(container -> ResourceManagerHelper.registerBuiltinResourcePack(
-                new Identifier("skyblocker", "recolored_dungeon_items"),
-                container,
+        ResourceManagerHelper.registerBuiltinResourcePack(
+                new Identifier(SkyblockerMod.NAMESPACE, "recolored_dungeon_items"),
+                SkyblockerMod.SKYBLOCKER_MOD,
                 ResourcePackActivationType.NORMAL
-            ));
+        );
     }
 }
