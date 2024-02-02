443753ad8ed3e9e98e70881a733003a9b6386499
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2024-02-01 22:35:25 -0500

    Add silverfish board parsing

--- src/main/java/de/hysky/skyblocker/skyblock/dungeon/puzzle/IceFill.java
+++ src/main/java/de/hysky/skyblocker/skyblock/dungeon/puzzle/IceFill.java
@@ -1,9 +1,32 @@
 package de.hysky.skyblocker.skyblock.dungeon.puzzle;
 
+import de.hysky.skyblocker.config.SkyblockerConfigManager;
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
+import net.minecraft.client.MinecraftClient;
+
 // 1: 15, 69, 7
 // 2: 15, 70, 12
 // 3: 15, 71, 19
 // ice -> packed_ice
 // polished andesite
-public class IceFill {
+public class IceFill extends DungeonPuzzle {
+    public static final IceFill INSTANCE = new IceFill();
+
+    public IceFill() {
+        super("ice-fill", "ice-path");
+    }
+
+    public static void init() {}
+
+    @Override
+    public void tick(MinecraftClient client) {
+        if (!SkyblockerConfigManager.get().locations.dungeons.solveIceFill) {
+            return;
+        }
+    }
+
+    @Override
+    public void render(WorldRenderContext context) {
+
+    }
 }
--- src/main/java/de/hysky/skyblocker/skyblock/dungeon/puzzle/Silverfish.java
+++ src/main/java/de/hysky/skyblocker/skyblock/dungeon/puzzle/Silverfish.java
@@ -1,22 +1,195 @@
 package de.hysky.skyblocker.skyblock.dungeon.puzzle;
 
+import com.mojang.brigadier.Command;
+import de.hysky.skyblocker.SkyblockerMod;
+import de.hysky.skyblocker.config.SkyblockerConfigManager;
+import de.hysky.skyblocker.debug.Debug;
+import de.hysky.skyblocker.skyblock.dungeon.secrets.DungeonManager;
+import de.hysky.skyblocker.skyblock.dungeon.secrets.Room;
+import de.hysky.skyblocker.utils.Constants;
+import de.hysky.skyblocker.utils.render.RenderHelper;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.mob.SilverfishEntity;
+import net.minecraft.util.DyeColor;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
+import org.joml.Vector2i;
+import org.joml.Vector2ic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 
 //right: 7, 66, 8
 //left: 23, 66, 8
 //right back: 7, 66, 24
 //polished andesite
 public class Silverfish extends DungeonPuzzle {
-    public Silverfish() {
-        super("Silverfish", "ice-silverfish-room");
+    private static final Logger LOGGER = LoggerFactory.getLogger(Silverfish.class);
+    public static final Silverfish INSTANCE = new Silverfish();
+    private static final float[] LIME_COLOR_COMPONENTS = DyeColor.LIME.getColorComponents();
+    private final boolean[][] silverfishBoard = new boolean[17][17];
+    private Vector2ic silverfishPos;
+    private final List<Vector2ic> silverfishPath = new ArrayList<>();
+
+    private Silverfish() {
+        super("silverfish", "ice-silverfish-room");
+        if (Debug.debugEnabled()) {
+            ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(literal(SkyblockerMod.NAMESPACE).then(literal("dungeons").then(literal("puzzle").then(literal(puzzleName)
+                    .then(literal("printBoard").executes(context -> {
+                        context.getSource().sendFeedback(Constants.PREFIX.get().append(boardToString(silverfishBoard)));
+                        return Command.SINGLE_SUCCESS;
+                    })).then(literal("printPath").executes(context -> {
+                        context.getSource().sendFeedback(Constants.PREFIX.get().append(silverfishPath.toString()));
+                        return Command.SINGLE_SUCCESS;
+                    }))
+            )))));
+        }
+    }
+
+    public static void init() {}
+
+    private static String boardToString(boolean[][] silverfishBoard) {
+        StringBuilder sb = new StringBuilder();
+        for (boolean[] row : silverfishBoard) {
+            sb.append("\n");
+            for (boolean cell : row) {
+                sb.append(cell ? '#' : '.');
+            }
+        }
+        return sb.toString();
     }
 
     @Override
     public void tick(MinecraftClient client) {
+        if (!SkyblockerConfigManager.get().locations.dungeons.solveSilverfish || client.world == null || !DungeonManager.isCurrentRoomMatched()) {
+            return;
+        }
+        Room room = DungeonManager.getCurrentRoom();
+
+        boolean boardChanged = false;
+        BlockPos.Mutable pos = new BlockPos.Mutable(23, 67, 24);
+        for (int row = 0; row < silverfishBoard.length; pos.move(silverfishBoard[row].length, 0, -1), row++) {
+            for (int col = 0; col < silverfishBoard[row].length; pos.move(Direction.WEST), col++) {
+                boolean isBlock = !client.world.getBlockState(room.relativeToActual(pos)).isAir();
+                if (silverfishBoard[row][col] != isBlock) {
+                    silverfishBoard[row][col] = isBlock;
+                    boardChanged = true;
+                }
+            }
+        }
+
+        BlockPos blockPos = room.relativeToActual(new BlockPos(16, 16, 16));
+        List<SilverfishEntity> entities = client.world.getEntitiesByClass(SilverfishEntity.class, Box.of(Vec3d.ofCenter(blockPos), 32, 32, 32), silverfishEntity -> true);
+        if (entities.isEmpty()) {
+            return;
+        }
+        BlockPos newSilverfishBlockPos = room.actualToRelative(entities.get(0).getBlockPos());
+        Vector2ic newSilverfishPos = new Vector2i(23 - newSilverfishBlockPos.getX(), 24 - newSilverfishBlockPos.getZ());
+        if (newSilverfishPos.x() < 0 || newSilverfishPos.x() >= 17 || newSilverfishPos.y() < 0 || newSilverfishPos.y() >= 17) {
+            return;
+        }
+        boolean silverfishChanged = !newSilverfishPos.equals(silverfishPos);
+        if (silverfishChanged) {
+            silverfishPos = newSilverfishPos;
+        }
+        if (silverfishChanged || boardChanged) {
+            solve();
+        }
+    }
+
+    private void solve() {
+        if (silverfishPos == null) {
+            return;
+        }
+        Set<Vector2ic> visited = new HashSet<>();
+        Queue<List<Vector2ic>> queue = new ArrayDeque<>();
+        queue.add(List.of(silverfishPos));
+        while (!queue.isEmpty()) {
+            List<Vector2ic> path = queue.poll();
+            Vector2ic pos = path.get(path.size() - 1);
+            if (pos.equals(8, 0)) {
+                silverfishPath.clear();
+                silverfishPath.addAll(path);
+                return;
+            }
+
+            Vector2i posMutable = new Vector2i(pos);
+            while (posMutable.x() < 17 && !silverfishBoard[posMutable.x()][posMutable.y()]) {
+                posMutable.add(1, 0);
+            }
+            posMutable.add(-1, 0);
+            if (!visited.contains(posMutable)) {
+                ArrayList<Vector2ic> newPath = new ArrayList<>(path);
+                newPath.add(new Vector2i(posMutable));
+                queue.add(newPath);
+                visited.add(posMutable);
+            }
+
+            posMutable.set(pos);
+            while (posMutable.x() >= 0 && !silverfishBoard[posMutable.x()][posMutable.y()]) {
+                posMutable.add(-1, 0);
+            }
+            posMutable.add(1, 0);
+            if (!visited.contains(posMutable)) {
+                ArrayList<Vector2ic> newPath = new ArrayList<>(path);
+                newPath.add(new Vector2i(posMutable));
+                queue.add(newPath);
+                visited.add(posMutable);
+            }
+
+            posMutable.set(pos);
+            while (posMutable.y() < 17 && !silverfishBoard[posMutable.x()][posMutable.y()]) {
+                posMutable.add(0, 1);
+            }
+            posMutable.add(0, -1);
+            if (!visited.contains(posMutable)) {
+                ArrayList<Vector2ic> newPath = new ArrayList<>(path);
+                newPath.add(new Vector2i(posMutable));
+                queue.add(newPath);
+                visited.add(posMutable);
+            }
+
+            posMutable.set(pos);
+            while (posMutable.y() >= 0 && !silverfishBoard[posMutable.x()][posMutable.y()]) {
+                posMutable.add(0, -1);
+            }
+            posMutable.add(0, 1);
+            if (!visited.contains(posMutable)) {
+                ArrayList<Vector2ic> newPath = new ArrayList<>(path);
+                newPath.add(new Vector2i(posMutable));
+                queue.add(newPath);
+                visited.add(posMutable);
+            }
+        }
     }
 
     @Override
     public void render(WorldRenderContext context) {
+        if (!SkyblockerConfigManager.get().locations.dungeons.solveSilverfish || !DungeonManager.isCurrentRoomMatched() || silverfishPath.isEmpty()) {
+            return;
+        }
+        Room room = DungeonManager.getCurrentRoom();
+        BlockPos.Mutable pos = new BlockPos.Mutable();
+        for (int i = 0; i < silverfishPath.size() - 1; i++) {
+            Vec3d start = Vec3d.ofCenter(room.relativeToActual(pos.set(23 - silverfishPath.get(i).x(), 67, 24 - silverfishPath.get(i).y())));
+            Vec3d end = Vec3d.ofCenter(room.relativeToActual(pos.set(23 - silverfishPath.get(i + 1).x(), 67, 24 - silverfishPath.get(i + 1).y())));
+            RenderHelper.renderLinesFromPoints(context, new Vec3d[]{start, end}, LIME_COLOR_COMPONENTS, 1f, 5f, true);
+        }
+    }
+
+    @Override
+    public void reset() {
+        super.reset();
+        for (boolean[] silverfishBoardRow : silverfishBoard) {
+            Arrays.fill(silverfishBoardRow, false);
+        }
+        silverfishPos = null;
     }
 }
