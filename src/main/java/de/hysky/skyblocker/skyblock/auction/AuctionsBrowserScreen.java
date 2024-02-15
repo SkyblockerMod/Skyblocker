2fd7b9925011e5a68ccc5bbd3d16d1ca442ebd65
vicisacat <victor.branchu@gmail.com>
2024-02-15 15:39:48 +0100

    Scrollbar yay

--- src/main/java/de/hysky/skyblocker/skyblock/auction/AuctionsBrowserScreen.java
+++ src/main/java/de/hysky/skyblocker/skyblock/auction/AuctionsBrowserScreen.java
@@ -2,11 +2,14 @@ package de.hysky.skyblocker.skyblock.auction;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import de.hysky.skyblocker.SkyblockerMod;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.client.sound.PositionedSoundInstance;
+import net.minecraft.client.texture.Sprite;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
@@ -15,6 +18,7 @@ import net.minecraft.screen.GenericContainerScreenHandler;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.screen.slot.SlotActionType;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.util.Colors;
 import net.minecraft.util.Identifier;
@@ -22,11 +26,18 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Supplier;
 
-@SuppressWarnings("DataFlowIssue")
+@SuppressWarnings("DataFlowIssue") // For this.client and stuff
 public class AuctionsBrowserScreen extends Screen {
 
     private static final Identifier BACKGROUND_TEXTURE = new Identifier(SkyblockerMod.NAMESPACE,"textures/gui/auctions_gui/browser/background.png");
+    private static final Identifier SCROLLER_TEXTURE = new Identifier("container/creative_inventory/scroller");
+
+    private static final Identifier up_arrow_tex = new Identifier(SkyblockerMod.NAMESPACE, "up_arrow_even"); // Put them in their own fields to avoid object allocation on each frame
+    private static final Identifier down_arrow_tex = new Identifier(SkyblockerMod.NAMESPACE, "down_arrow_even");
+    public static final Supplier<Sprite> UP_ARROW = () -> MinecraftClient.getInstance().getGuiAtlasManager().getSprite(up_arrow_tex);
+    public static final Supplier<Sprite> DOWN_ARROW = () -> MinecraftClient.getInstance().getGuiAtlasManager().getSprite(down_arrow_tex);
     private boolean waitingForServer = true;
 
     // <editor-fold desc="Boring handled screen stuff">
@@ -52,6 +63,11 @@ public class AuctionsBrowserScreen extends Screen {
     private final PlayerInventory playerInventory;
     private final List<Slot> auctionedItems = new ArrayList<>(24);
     private @Nullable Slot hoveredSlot = null;
+    private int prevPageSlotId = -1;
+    private int nextPageSlotId = -1;
+
+    private int currentPage = 1;
+    private int totalPages = 1;
 
     // WIDGETS
     private SortWidget sortWidget;
@@ -90,28 +106,60 @@ public class AuctionsBrowserScreen extends Screen {
                 auctionedItems.add(handler.slots.get(i*9 + j));
             }
         }
+        nextPageSlotId = -1;
+        prevPageSlotId = -1;
+
+        boolean pageParsed = false;
         for (int i = (handler.getRows() - 1) * 9; i < handler.getRows() * 9; i++) {
             Slot slot = handler.slots.get(i);
             ItemStack stack = slot.getStack();
-            if (slot.hasStack() && stack.isOf(Items.HOPPER) && stack.getName().getString().toLowerCase().contains("sort")) {
+            if (!slot.hasStack()) continue;
+            String stackName = stack.getName().getString().toLowerCase();
+            if (stack.isOf(Items.HOPPER) && stackName.contains("sort")) {
                 sortWidget.setSlotId(i);
                 List<Text> tooltip = stack.getTooltip(client.player, TooltipContext.BASIC);
                 int ordinal = getOrdinal(tooltip);
                 sortWidget.setCurrent(SortWidget.Option.get(ordinal));
-            } else if (slot.hasStack() && stack.getName().getString().toLowerCase().contains("bin filter")) {
+            } else if (stackName.contains("bin filter")) {
                 auctionTypeWidget.setSlotId(i);
                 List<Text> tooltip = stack.getTooltip(client.player, TooltipContext.BASIC);
                 int ordinal = getOrdinal(tooltip);
                 auctionTypeWidget.setCurrent(AuctionTypeWidget.Option.get(ordinal));
-            } else if (slot.hasStack() && stack.isOf(Items.ENDER_EYE) && stack.getName().getString().toLowerCase().contains("item tier")) {
+            } else if (stack.isOf(Items.ENDER_EYE) && stackName.contains("item tier")) {
                 rarityWidget.setSlotId(i);
                 List<Text> tooltip = stack.getTooltip(client.player, TooltipContext.BASIC);
                 int ordinal = getOrdinal(tooltip);
                 String split = tooltip.get(ordinal+2).getString().substring(2);
                 rarityWidget.setText(tooltip.subList(1, tooltip.size()-3), split);
-
+            } else if (stack.isOf(Items.ARROW) && stackName.contains("previous page")) {
+                prevPageSlotId = i;
+                if (!pageParsed) {
+                    parsePage(stack);
+                    pageParsed = true;
+                }
+            } else if (stack.isOf(Items.ARROW) && stackName.contains("next page")) {
+                nextPageSlotId = i;
+                if (!pageParsed) {
+                    parsePage(stack);
+                    pageParsed = true;
+                }
             }
         }
+        if (!pageParsed) {
+            currentPage = 1;
+            totalPages = 1;
+        }
+    }
+
+    private void parsePage(ItemStack stack) {
+        List<Text> tooltip = stack.getTooltip(client.player, TooltipContext.BASIC);
+        String str = tooltip.get(1).getString().trim();
+        str = str.substring(1, str.length() - 1); // remove parentheses
+        String[] parts = str.split("/"); // split the string
+        try {
+            currentPage = Integer.parseInt(parts[0]); // parse current page
+            totalPages = Integer.parseInt(parts[1]); // parse total
+        } catch (NumberFormatException ignored) {}
     }
 
     private static int getOrdinal(List<Text> tooltip) {
@@ -208,6 +256,25 @@ public class AuctionsBrowserScreen extends Screen {
         context.drawText(this.textRenderer, playerInventory.getDisplayName(), 8, 187-94, 0x404040, false);
         if (highlightSlotX != -1) HandledScreen.drawSlotHighlight(context, highlightSlotX, highlightSlotY, 0);
 
+        // Scrollbar
+        if (prevPageSlotId != -1) {
+            if (onScrollbarTop(mouseX, mouseY))
+                context.drawSprite(159, 13, 0, 6, 3, UP_ARROW.get());
+            else context.drawSprite(159, 13, 0, 6, 3, UP_ARROW.get(), 0.54f, 0.54f, 0.54f, 1);
+        }
+
+        if (nextPageSlotId != -1) {
+            if (onScrollbarBottom(mouseX, mouseY))
+                context.drawSprite(159, 72, 0, 6, 3, DOWN_ARROW.get());
+            else context.drawSprite(159, 72, 0, 6, 3, DOWN_ARROW.get(), 0.54f, 0.54f, 0.54f, 1);
+        }
+        context.drawText(textRenderer, String.format("%d/%d", currentPage, totalPages), 99, 6, Colors.GRAY, false);
+        if (totalPages <= 1)
+            context.drawGuiTexture(SCROLLER_TEXTURE, 156, 18, 12, 15);
+        else
+            context.drawGuiTexture(SCROLLER_TEXTURE, 156, (int) (18 + (float)(currentPage-1)/(totalPages-1)*37), 12, 15);
+
+
         matrices.pop();
 
         if (this.hoveredSlot != null) {
@@ -216,6 +283,38 @@ public class AuctionsBrowserScreen extends Screen {
         }
 
         RenderSystem.enableDepthTest();
+    }
+
+    private boolean onScrollbarTop(int mouseX, int mouseY) {
+        int localX = mouseX - x;
+        int localY = mouseY - y;
+        return localX > 154 && localX < 169 && localY > 11 && localY < 44;
+    }
+    private boolean onScrollbarBottom(int mouseX, int mouseY) {
+        int localX = mouseX - x;
+        int localY = mouseY - y;
+        return localX > 154 && localX < 169 && localY > 43 && localY < 75;
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        if (super.mouseClicked(mouseX, mouseY, button)) return true;
+        if (waitingForServer) return false;
+        if (onScrollbarTop((int)mouseX, (int) mouseY) && prevPageSlotId != -1) {
+            clickAndWaitForServer(prevPageSlotId);
+            playClickSound();
+            return true;
+        }
+        if (onScrollbarBottom((int)mouseX, (int) mouseY) && nextPageSlotId != -1) {
+            clickAndWaitForServer(nextPageSlotId);
+            playClickSound();
+            return true;
+        }
+        return false;
+    }
+
+    public static void playClickSound() {
+        MinecraftClient.getInstance().getSoundManager().play(PositionedSoundInstance.master(SoundEvents.UI_BUTTON_CLICK, 1.0f));
     }
 
     @Override
