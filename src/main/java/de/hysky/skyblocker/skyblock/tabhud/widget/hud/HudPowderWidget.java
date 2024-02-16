347b9688a83ce2170836eea9e9540d1a421d3312
Ghost <66458557+Ghost-3@users.noreply.github.com>
2024-02-16 18:28:25 +0300

    Show the diff in the Powders widget

--- src/main/java/de/hysky/skyblocker/skyblock/tabhud/widget/hud/HudPowderWidget.java
+++ src/main/java/de/hysky/skyblocker/skyblock/tabhud/widget/hud/HudPowderWidget.java
@@ -1,16 +1,40 @@
 package de.hysky.skyblocker.skyblock.tabhud.widget.hud;
 
+import de.hysky.skyblocker.skyblock.dwarven.DwarvenHud;
 import de.hysky.skyblocker.skyblock.tabhud.util.Ico;
 import de.hysky.skyblocker.skyblock.tabhud.widget.Widget;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 
+import java.util.Locale;
+
 // this widget shows the status of the king's commissions.
 // (dwarven mines and crystal hollows)
 // USE ONLY WITH THE DWARVEN HUD!
 
 public class HudPowderWidget extends Widget {
+
+    /**
+     * current value of Mithril Powder
+     */
+    private static int mithrilPowder = 0;
+    /**
+     * current value of Gemstone Powder
+     */
+    private static int gemstonePowder = 0;
+    /**
+     * the difference between the previous and current value of Mithril Powder
+     */
+    private static int mithrilPowderDif = 0;
+    /**
+     * the difference between the previous and current value of Gemstone Powder
+     */
+    private static int gemstonePowderDif = 0;
+    /**
+     * The initial value of the timer for the difference update delay countdown.
+     */
+    private static long startTime = System.currentTimeMillis();
 
     private static final MutableText TITLE = Text.literal("Powders").formatted(Formatting.DARK_AQUA,
             Formatting.BOLD);
@@ -30,11 +54,56 @@ public class HudPowderWidget extends Widget {
         super(TITLE, Formatting.DARK_AQUA.getColorValue());
     }
 
+    /**
+     * Converts a string with a number and commas between digits to an integer value.
+     * @param str a string with a number and commas between digits
+     * @return integer value
+     */
+    private static int parsePowder(String str) {
+        return Integer.parseInt(str.replace(",", ""));
+    }
+
+    /**
+     * Converts Powder and difference values to a string and adds commas to the digits of the numbers.
+     * @param powder the value of Mithril or Gemstone Powder
+     * @param diff the difference between the previous and current value of Mithril or Gemstone Powder
+     * @return formatted string
+     */
+    private static String formatPowderString(int powder, int diff) {
+        if (diff == 0) return String.format(Locale.US, "%,d", powder);
+
+        String difString = String.format(Locale.US, "%s%,d", diff > 0 ? "+" : "", diff);
+        return String.format(Locale.US, "%,d (%s)", powder, difString);
+    }
+
+    /**
+     * Updates Powders and difference values when Powder values change or every 2 seconds.
+     */
+    private static void updatePowders(){
+        long elapsedTime = System.currentTimeMillis() - startTime;
+
+        int newMithrilPowder = parsePowder(DwarvenHud.mithrilPowder);
+        int newGemstonePowder = parsePowder(DwarvenHud.gemStonePowder);
+
+        if (newMithrilPowder != mithrilPowder || newGemstonePowder != gemstonePowder || elapsedTime > 2000) {
+            startTime = System.currentTimeMillis();
+
+            mithrilPowderDif = newMithrilPowder - mithrilPowder;
+            gemstonePowderDif = newGemstonePowder - gemstonePowder;
+
+            mithrilPowder = newMithrilPowder;
+            gemstonePowder = newGemstonePowder;
+        }
+    }
 
     @Override
     public void updateContent() {
-        this.addSimpleIcoText(Ico.MITHRIL, "Mithril:", Formatting.AQUA, 46);
-        this.addSimpleIcoText(Ico.AMETHYST_SHARD, "Gemstone:", Formatting.DARK_PURPLE, 47);
+        updatePowders();
+        String mithrilPowderString = formatPowderString(mithrilPowder, mithrilPowderDif);
+        String gemstonePowderString = formatPowderString(gemstonePowder, gemstonePowderDif);
+
+        this.addSimpleIcoText(Ico.MITHRIL, "Mithril: ", Formatting.AQUA, mithrilPowderString);
+        this.addSimpleIcoText(Ico.AMETHYST_SHARD, "Gemstone: ", Formatting.DARK_PURPLE, gemstonePowderString);
     }
 
 }
