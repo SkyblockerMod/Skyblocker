cc00542e78fd87e0b554ab2a74d1cb193feabbb2
Aaron <51387595+AzureAaron@users.noreply.github.com>
2024-02-18 02:40:37 -0500

    Migrate to HudRenderEvents
    The Dwarven HUD & End HUD will be moved later due to the tab hud doing
    stuff with the z value.

--- src/main/java/de/hysky/skyblocker/skyblock/dwarven/CrystalsHud.java
+++ src/main/java/de/hysky/skyblocker/skyblock/dwarven/CrystalsHud.java
@@ -2,11 +2,11 @@ package de.hysky.skyblocker.skyblock.dwarven;
 
 import de.hysky.skyblocker.SkyblockerMod;
 import de.hysky.skyblocker.config.SkyblockerConfigManager;
+import de.hysky.skyblocker.events.HudRenderEvents;
 import de.hysky.skyblocker.utils.Utils;
 import de.hysky.skyblocker.utils.scheduler.Scheduler;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.util.math.MatrixStack;
@@ -34,7 +34,7 @@ public class CrystalsHud {
                         .then(ClientCommandManager.literal("crystals")
                                 .executes(Scheduler.queueOpenScreenCommand(CrystalsHudConfigScreen::new))))));
 
-        HudRenderCallback.EVENT.register((context, tickDelta) -> {
+        HudRenderEvents.BEFORE_CHAT.register((context, tickDelta) -> {
             if (!SkyblockerConfigManager.get().locations.dwarvenMines.crystalsHud.enabled
                     || CLIENT.player == null
                     || !visible) {
@@ -65,7 +65,7 @@ public class CrystalsHud {
         //and set position and scale
         MatrixStack matrices = context.getMatrices();
         matrices.push();
-        matrices.translate(hudX, hudY, 200f);
+        matrices.translate(hudX, hudY, 0f);
         matrices.scale(scale, scale, 0f);
 
         //draw map texture
