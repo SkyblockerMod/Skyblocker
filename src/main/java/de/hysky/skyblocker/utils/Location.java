f3fe77aee71a7a830b2b5fa91786078547c19ae6
Ghost <66458557+Ghost-3@users.noreply.github.com>
2024-02-18 05:06:54 +0300

    Better location management

---
+++ src/main/java/de/hysky/skyblocker/utils/Location.java
@@ -0,0 +1,87 @@
+package de.hysky.skyblocker.utils;
+
+/**
+ * All Skyblock locations
+ */
+public enum Location {
+    /**
+     * mode: dynamic
+     */
+    PRIVATE_ISLAND,
+    /**
+     * mode: garden
+     */
+    GARDEN,
+    /**
+     * mode: hub
+     */
+    HUB,
+    /**
+     * mode: farming_1
+     */
+    THE_FARMING_ISLAND,
+    /**
+     * mode: foraging_1
+     */
+    THE_PARK,
+    /**
+     * mode: combat_1
+     */
+    SPIDERS_DEN,
+    /**
+     * mode: combat_2
+     */
+    BLAZING_FORTRESS,
+    /**
+     * mode: combat_3
+     */
+    THE_END,
+    /**
+     * mode: crimson_isle
+     */
+    CRIMSON_ISLE,
+    /**
+     * mode: mining_1
+     */
+    GOLD_MINE,
+    /**
+     * mode: mining_2
+     */
+    DEEP_CAVERNS,
+    /**
+     * mode: mining_3
+     */
+    DWARVEN_MINES,
+    /**
+     * mode: dungeon_hub
+     */
+    DUNGEON_HUB,
+    /**
+     * mode: winter
+     */
+    WINTER_ISLAND,
+    /**
+     * mode: rift
+     */
+    THE_RIFT,
+    /**
+     * mode: dark_auction
+     */
+    DARK_AUCTION,
+    /**
+     * mode: crystal_hollows
+     */
+    CRYSTAL_HOLLOWS,
+    /**
+     * mode: dungeon
+     */
+    DUNGEON,
+    /**
+     * mode: kuudra
+     */
+    KUUDRAS_HOLLOW,
+    /**
+     * Unknown Skyblock location
+     */
+    UNKNOWN
+}
