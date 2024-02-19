08639001f90f0f23b2189f9723522ed76c218846
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2024-02-19 14:25:13 -0500

    Fix discoveries index

--- src/main/java/de/hysky/skyblocker/skyblock/tabhud/widget/DungeonSecretWidget.java
+++ src/main/java/de/hysky/skyblocker/skyblock/tabhud/widget/DungeonSecretWidget.java
@@ -1,16 +1,19 @@
 package de.hysky.skyblocker.skyblock.tabhud.widget;
 
 import de.hysky.skyblocker.skyblock.tabhud.util.Ico;
+import de.hysky.skyblocker.skyblock.tabhud.util.PlayerListMgr;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+
+import java.util.regex.Pattern;
 
 // this widget shows info about the secrets of the dungeon
 
 public class DungeonSecretWidget extends Widget {
 
-    private static final MutableText TITLE = Text.literal("Discoveries").formatted(Formatting.DARK_PURPLE,
-            Formatting.BOLD);
+    private static final MutableText TITLE = Text.literal("Discoveries").formatted(Formatting.DARK_PURPLE, Formatting.BOLD);
+    private static final Pattern DISCOVERIES = Pattern.compile("Discoveries: (\\d+)");
 
     public DungeonSecretWidget() {
         super(TITLE, Formatting.DARK_PURPLE.getColorValue());
@@ -18,9 +21,12 @@ public class DungeonSecretWidget extends Widget {
 
     @Override
     public void updateContent() {
-        this.addSimpleIcoText(Ico.CHEST, "Secrets:", Formatting.YELLOW, 31);
-        this.addSimpleIcoText(Ico.SKULL, "Crypts:", Formatting.YELLOW, 32);
-
+        if (PlayerListMgr.regexAt(31, DISCOVERIES) != null) {
+            this.addSimpleIcoText(Ico.CHEST, "Secrets:", Formatting.YELLOW, 32);
+            this.addSimpleIcoText(Ico.SKULL, "Crypts:", Formatting.YELLOW, 33);
+        } else {
+            this.addSimpleIcoText(Ico.CHEST, "Secrets:", Formatting.YELLOW, 31);
+            this.addSimpleIcoText(Ico.SKULL, "Crypts:", Formatting.YELLOW, 32);
+        }
     }
-
 }
