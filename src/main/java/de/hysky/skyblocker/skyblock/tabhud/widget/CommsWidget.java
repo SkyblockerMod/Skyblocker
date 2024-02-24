a0d216514ff64efcb463249c8c279ef7bd75b214
Ghost <66458557+Ghost-3@users.noreply.github.com>
2024-02-24 18:24:15 +0300

    Added functionality from HUD widgets

--- src/main/java/de/hysky/skyblocker/skyblock/tabhud/widget/CommsWidget.java
+++ src/main/java/de/hysky/skyblocker/skyblock/tabhud/widget/CommsWidget.java
@@ -3,10 +3,14 @@ package de.hysky.skyblocker.skyblock.tabhud.widget;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import de.hysky.skyblocker.config.SkyblockerConfig;
+import de.hysky.skyblocker.config.SkyblockerConfigManager;
 import de.hysky.skyblocker.skyblock.tabhud.util.Colors;
 import de.hysky.skyblocker.skyblock.tabhud.util.Ico;
 import de.hysky.skyblocker.skyblock.tabhud.util.PlayerListMgr;
+import de.hysky.skyblocker.skyblock.tabhud.widget.component.Component;
 import de.hysky.skyblocker.skyblock.tabhud.widget.component.IcoTextComponent;
+import de.hysky.skyblocker.skyblock.tabhud.widget.component.PlainTextComponent;
 import de.hysky.skyblocker.skyblock.tabhud.widget.component.ProgressComponent;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
@@ -29,8 +33,26 @@ public class CommsWidget extends Widget {
         super(TITLE, Formatting.DARK_AQUA.getColorValue());
     }
 
+    private static ProgressComponent getFancyComponent(String name, String progress) {
+        if (progress.equals("DONE")) {
+            return new ProgressComponent(Ico.BOOK, Text.of(name), Text.of(progress), 100f, Colors.pcntToCol(100));
+        } else {
+            float pcnt = Float.parseFloat(progress.substring(0, progress.length() - 1));
+            return new ProgressComponent(Ico.BOOK, Text.of(name), pcnt, Colors.pcntToCol(pcnt));
+        }
+    }
+
+    private static PlainTextComponent getComponent(String name, String progress) {
+        float pcnt = Float.parseFloat(progress.substring(0, progress.length() - 1));
+        return new PlainTextComponent(Text.literal(name + ": ").append(Text.literal(progress)
+                .formatted(Colors.hypixelProgressColor(pcnt))));
+    }
+
     @Override
     public void updateContent() {
+        SkyblockerConfig.DwarvenHudStyle style = SkyblockerConfigManager.get().locations.dwarvenMines.dwarvenHud.style;
+        boolean isFancy = style == SkyblockerConfig.DwarvenHudStyle.FANCY;
+
         for (int i = 50; i <= 53; i++) {
             Matcher m = PlayerListMgr.regexAt(i, COMM_PATTERN);
             // end of comms found?
@@ -41,18 +63,14 @@ public class CommsWidget extends Widget {
                 break;
             }
 
-            ProgressComponent pc;
-
             String name = m.group("name");
             String progress = m.group("progress");
 
-            if (progress.equals("DONE")) {
-                pc = new ProgressComponent(Ico.BOOK, Text.of(name), Text.of(progress), 100f, Colors.pcntToCol(100));
-            } else {
-                float pcnt = Float.parseFloat(progress.substring(0, progress.length() - 1));
-                pc = new ProgressComponent(Ico.BOOK, Text.of(name), pcnt, Colors.pcntToCol(pcnt));
-            }
-            this.addComponent(pc);
+            Component component;
+            if (isFancy) component = getFancyComponent(name, progress);
+            else component = getComponent(name, progress);
+
+            this.addComponent(component);
         }
     }
 
--- src/main/java/de/hysky/skyblocker/skyblock/tabhud/widget/PowderWidget.java
+++ src/main/java/de/hysky/skyblocker/skyblock/tabhud/widget/PowderWidget.java
@@ -3,14 +3,43 @@ package de.hysky.skyblocker.skyblock.tabhud.widget;
 
 import de.hysky.skyblocker.skyblock.tabhud.util.Ico;
 
+import de.hysky.skyblocker.skyblock.tabhud.util.PlayerListMgr;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
 
 // this widget shows how much mithril and gemstone powder you have
 // (dwarven mines and crystal hollows)
 
 public class PowderWidget extends Widget {
+    /**
+     * American number format instance
+     */
+    private static final NumberFormat NUMBER_FORMAT = NumberFormat.getInstance(Locale.US);
+    /**
+     * current value of Mithril Powder
+     */
+    private static int mithrilPowder = 0;
+    /**
+     * current value of Gemstone Powder
+     */
+    private static int gemstonePowder = 0;
+    /**
+     * the difference between the previous and current value of Mithril Powder
+     */
+    private static int mithrilPowderDiff = 0;
+    /**
+     * the difference between the previous and current value of Gemstone Powder
+     */
+    private static int gemstonePowderDiff = 0;
+    /**
+     * The initial value of the timer for the difference update delay countdown.
+     */
+    private static long startTime = System.currentTimeMillis();
 
     private static final MutableText TITLE = Text.literal("Powders").formatted(Formatting.DARK_AQUA,
             Formatting.BOLD);
@@ -19,11 +48,61 @@ public class PowderWidget extends Widget {
         super(TITLE, Formatting.DARK_AQUA.getColorValue());
     }
 
+    /**
+     * Converts a string with a number and commas between digits to an integer value.
+     *
+     * @param str a string with a number and commas between digits
+     * @return integer value
+     */
+    private static int parsePowder(String str) {
+        if (str == null) return 0;
+        try {
+            return NUMBER_FORMAT.parse(str.split(": ")[1]).intValue();
+        } catch (ParseException e) {
+            return 0;
+        }
+    }
+
+    /**
+     * Converts Powder and difference values to a string and adds commas to the digits of the numbers.
+     *
+     * @param powder the value of Mithril or Gemstone Powder
+     * @param diff   the difference between the previous and current value of Mithril or Gemstone Powder
+     * @return formatted string
+     */
+    private static String formatPowderString(int powder, int diff) {
+        if (diff == 0) return NUMBER_FORMAT.format(powder);
+        return NUMBER_FORMAT.format(powder) + (diff > 0 ? " (+" : " (") + NUMBER_FORMAT.format(diff) + ")";
+    }
+
+    /**
+     * Updates Powders and difference values when Powder values change or every 2 seconds.
+     */
+    private static void updatePowders() {
+        long elapsedTime = System.currentTimeMillis() - startTime;
+
+        int newMithrilPowder = parsePowder(PlayerListMgr.strAt(46));
+        int newGemstonePowder = parsePowder(PlayerListMgr.strAt(47));
+
+        if (newMithrilPowder != mithrilPowder || newGemstonePowder != gemstonePowder || elapsedTime > 2000) {
+            startTime = System.currentTimeMillis();
+
+            mithrilPowderDiff = newMithrilPowder - mithrilPowder;
+            gemstonePowderDiff = newGemstonePowder - gemstonePowder;
+
+            mithrilPowder = newMithrilPowder;
+            gemstonePowder = newGemstonePowder;
+        }
+    }
+
     @Override
     public void updateContent() {
-        this.addSimpleIcoText(Ico.MITHRIL, "Mithril:", Formatting.AQUA, 46);
-        this.addSimpleIcoText(Ico.AMETHYST_SHARD, "Gemstone:", Formatting.DARK_PURPLE, 47);
+        updatePowders();
+        String mithrilPowderString = formatPowderString(mithrilPowder, mithrilPowderDiff);
+        String gemstonePowderString = formatPowderString(gemstonePowder, gemstonePowderDiff);
 
+        this.addSimpleIcoText(Ico.MITHRIL, "Mithril: ", Formatting.AQUA, mithrilPowderString);
+        this.addSimpleIcoText(Ico.AMETHYST_SHARD, "Gemstone: ", Formatting.DARK_PURPLE, gemstonePowderString);
     }
 
 }
