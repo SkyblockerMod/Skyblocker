d5d14d6336431e0d23e723d15ac39d62ac92c388
Aaron <51387595+AzureAaron@users.noreply.github.com>
2024-03-14 16:52:00 -0400

    Revert "Fix Mixin incompatibility with MixinPlugin"
    This reverts commit aa163f14dfdada71cf8be7900d5f5167e5a2d9cb.

--- src/main/java/de/hysky/skyblocker/compatibility/MixinPlugin.java
+++ src/main/java/de/hysky/skyblocker/compatibility/MixinPlugin.java
@@ -2,7 +2,6 @@ package de.hysky.skyblocker.compatibility;
 
 import net.fabricmc.loader.api.FabricLoader;
 import org.objectweb.asm.tree.ClassNode;
-import org.spongepowered.asm.mixin.MixinEnvironment;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
@@ -11,7 +10,6 @@ import java.util.Set;
 
 public class MixinPlugin implements IMixinConfigPlugin {
 	private static final boolean OPTIFABRIC_LOADED = FabricLoader.getInstance().isModLoaded("optifabric");
-	private static final boolean FCWLS_LOADED = FabricLoader.getInstance().isModLoaded("forcecloseworldloadingscreen");
 
 	@Override
 	public void onLoad(String mixinPackage) {
@@ -26,16 +24,7 @@ public class MixinPlugin implements IMixinConfigPlugin {
 	@Override
 	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
 		//OptiFabric Compatibility
-		if (OPTIFABRIC_LOADED && mixinClassName.endsWith("WorldRendererMixin")) return false;
-
-		//Force Close World Screen Compatibility
-		if (FCWLS_LOADED && mixinClassName.endsWith("MinecraftClientScreenMixin")) {
-			String mixinCodeSource = MixinEnvironment.class.getProtectionDomain().getCodeSource().getLocation().toString();
-
-			return !mixinCodeSource.contains("0.12.5+mixin.0.8.5");
-		}
-
-        return true;
+        return !mixinClassName.endsWith("WorldRendererMixin") || !OPTIFABRIC_LOADED;
     }
 
 	@Override
