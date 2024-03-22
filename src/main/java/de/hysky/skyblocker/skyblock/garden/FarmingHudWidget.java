e3a7e09b26096f8a2d249ac850041155da1c64bd
vicisacat <victor.branchu@gmail.com>
2024-03-22 22:10:24 +0100

    fix it for real this time it's all kevin's fault

--- src/main/java/de/hysky/skyblocker/skyblock/garden/FarmingHudWidget.java
+++ src/main/java/de/hysky/skyblocker/skyblock/garden/FarmingHudWidget.java
@@ -7,6 +7,7 @@ import de.hysky.skyblocker.skyblock.tabhud.widget.component.PlainTextComponent;
 import de.hysky.skyblocker.skyblock.tabhud.widget.component.ProgressComponent;
 import de.hysky.skyblocker.utils.ItemUtils;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
@@ -49,18 +50,19 @@ public class FarmingHudWidget extends Widget {
         update();
     }
 
-    @SuppressWarnings("DataFlowIssue")
     @Override
     public void updateContent() {
         ItemStack icon = client.player == null ? Ico.HOE: FARMING_TOOLS.getOrDefault(ItemUtils.getItemId(client.player.getMainHandStack()), Ico.HOE);
         addSimpleIcoText(icon, "Counter: ", Formatting.YELLOW, FarmingHud.NUMBER_FORMAT.format(FarmingHud.counter()));
         addSimpleIcoText(icon, "Crops/min: ", Formatting.YELLOW, FarmingHud.NUMBER_FORMAT.format((int) FarmingHud.cropsPerMinute() / 100 * 100));
         addSimpleIcoText(icon, "Blocks/s: ", Formatting.YELLOW, Integer.toString(FarmingHud.blockBreaks()));
+        //noinspection DataFlowIssue
         addComponent(new ProgressComponent(Ico.LANTERN, Text.literal("Farming Level: "), FarmingHud.farmingXpPercentProgress(), Formatting.GOLD.getColorValue()));
         addSimpleIcoText(Ico.LIME_DYE, "Farming XP/h: ", Formatting.YELLOW, FarmingHud.NUMBER_FORMAT.format((int) FarmingHud.farmingXpPerHour()));
 
-        double yaw = client.getCameraEntity().getYaw();
-        double pitch = client.getCameraEntity().getPitch();
+        Entity cameraEntity = client.getCameraEntity();
+        double yaw = cameraEntity == null ? 0.0d: cameraEntity.getYaw();
+        double pitch = cameraEntity == null ? 0.0d: cameraEntity.getPitch();
         addComponent(new PlainTextComponent(Text.literal("Yaw: " + String.format("%.3f", MathHelper.wrapDegrees(yaw))).formatted(Formatting.YELLOW)));
         addComponent(new PlainTextComponent(Text.literal("Pitch: " + String.format("%.3f", MathHelper.wrapDegrees(pitch))).formatted(Formatting.YELLOW)));
     }
