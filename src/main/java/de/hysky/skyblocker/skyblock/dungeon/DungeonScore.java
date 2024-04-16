732ab7e8ef193ce108f2c64206a0b0a4e111f132
Fluboxer <36457056+Fluboxer@users.noreply.github.com>
2024-04-17 02:46:25 +0300

    Update src/main/java/de/hysky/skyblocker/skyblock/dungeon/DungeonScore.java
    Co-authored-by: Kevin <92656833+kevinthegreat1@users.noreply.github.com>

--- src/main/java/de/hysky/skyblocker/skyblock/dungeon/DungeonScore.java
+++ src/main/java/de/hysky/skyblocker/skyblock/dungeon/DungeonScore.java
@@ -114,7 +114,7 @@ public class DungeonScore {
 			sent270 = true;
 		}
 
-		if (!sentCrypts && score >= SCORE_CONFIG.dungeonCryptsMessageThreshold && (5 > getCrypts())) {
+		if (!sentCrypts && score >= SCORE_CONFIG.dungeonCryptsMessageThreshold && getCrypts() < 5) {
 			if (SCORE_CONFIG.enableDungeonCryptsMessage) {
 				MessageScheduler.INSTANCE.sendMessageAfterCooldown("/pc " + Constants.PREFIX.get().getString() + SCORE_CONFIG.dungeonCryptsMessage.replaceAll("\\[crypts]", String.valueOf(getCrypts())));
 			}
