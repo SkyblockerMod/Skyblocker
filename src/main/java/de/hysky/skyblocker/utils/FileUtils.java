abf64d39fd9bf39da3fb1373efc606942ff62400
Kevin <92656833+kevinthegreat1@users.noreply.github.com>
2024-04-22 00:30:35 -0400

    Fix update repository (#663)

--- src/main/java/de/hysky/skyblocker/utils/FileUtils.java
+++ src/main/java/de/hysky/skyblocker/utils/FileUtils.java
@@ -1,29 +1,35 @@
 package de.hysky.skyblocker.utils;
 
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-
-import org.slf4j.Logger;
-
-import com.mojang.logging.LogUtils;
+import java.util.stream.Stream;
 
 public class FileUtils {
 	private static final Logger LOGGER = LogUtils.getLogger();
 
 	public static void recursiveDelete(Path dir) throws IOException {
-		if (Files.isDirectory(dir) && !Files.isSymbolicLink(dir)) {
-			Files.list(dir).forEach(child -> {
-				try {
-					recursiveDelete(child);
-				} catch (Exception e) {
-					LOGGER.error("[Skyblocker] Encountered an exception while deleting a file! Path: {}", child.toAbsolutePath(), e);
-				}
-			});
+		if (!Files.exists(dir)) {
+			return;
 		}
 
-		if (!Files.isWritable(dir)) {
-			dir.toFile().setWritable(true);
+		if (Files.isDirectory(dir) && !Files.isSymbolicLink(dir)) {
+			try (Stream<Path> stream = Files.list(dir)) {
+				stream.forEach(child -> {
+					try {
+						recursiveDelete(child);
+					} catch (Exception e) {
+						LOGGER.error("[Skyblocker] Encountered an exception while deleting a file. Path: {}", child.toAbsolutePath(), e);
+					}
+				});
+			}
+		}
+
+		if (!Files.isWritable(dir) && !dir.toFile().setWritable(true)) {
+			LOGGER.error("[Skyblocker] Failed to make file writable. Path: {}", dir.toAbsolutePath());
 		}
 
 		Files.delete(dir);
@@ -31,7 +37,7 @@ public class FileUtils {
 
 	/**
 	 * Replaces any characters that do not match the regex: [^a-z0-9_.-]
-	 * 
+	 *
 	 * @implNote Designed to convert a file path to an {@link net.minecraft.util.Identifier}
 	 */
 	public static String normalizePath(Path path) {
