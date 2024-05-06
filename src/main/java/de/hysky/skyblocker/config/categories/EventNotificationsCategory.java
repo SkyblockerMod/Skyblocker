c4688d4f5f43432114a9e3cc635140c6b6f565bb
viciscat <51047087+viciscat@users.noreply.github.com>
2024-05-06 19:54:03 +0200

    clean or something idk

--- src/main/java/de/hysky/skyblocker/config/categories/EventNotificationsCategory.java
+++ src/main/java/de/hysky/skyblocker/config/categories/EventNotificationsCategory.java
@@ -12,7 +12,6 @@ import net.minecraft.text.Text;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 public class EventNotificationsCategory {
 
