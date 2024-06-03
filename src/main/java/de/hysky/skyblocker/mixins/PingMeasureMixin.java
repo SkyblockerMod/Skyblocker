9d19f2f99fa951428d8abbcfb400e3e0adc13d1a
olim <bobq4582@gmail.com>
2024-06-03 21:18:55 +0100

    add ping measure and improve mastery
    mastery now counts down to when to release the bow

---
+++ src/main/java/de/hysky/skyblocker/mixins/PingMeasureMixin.java
@@ -0,0 +1,23 @@
+package de.hysky.skyblocker.mixins;
+
+import de.hysky.skyblocker.skyblock.crimson.dojo.DojoManager;
+import de.hysky.skyblocker.utils.Location;
+import de.hysky.skyblocker.utils.Utils;
+import net.minecraft.client.network.PingMeasurer;
+import net.minecraft.network.packet.s2c.query.PingResultS2CPacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PingMeasurer.class)
+public class PingMeasureMixin {
+
+    @Inject(method = "onPingResult", at = @At("RETURN"))
+    private void skyblocker$onPingResult(PingResultS2CPacket packet, CallbackInfo ci) {
+        if (Utils.getLocation() == Location.CRIMSON_ISLE) {
+            long ping = System.currentTimeMillis() - packet.startTime();
+            DojoManager.onPingResult(ping);
+        }
+    }
+}
