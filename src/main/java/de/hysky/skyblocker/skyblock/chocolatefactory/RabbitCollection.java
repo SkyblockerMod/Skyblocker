8142f57c7395ea8efda3984b87ca139509407e97
Rime <81419447+Emirlol@users.noreply.github.com>
2024-06-19 12:12:05 +0300

    Increment cached rabbit counts when a new rabbit is found

--- src/main/java/de/hysky/skyblocker/skyblock/chocolatefactory/RabbitCollection.java
+++ src/main/java/de/hysky/skyblocker/skyblock/chocolatefactory/RabbitCollection.java
@@ -9,6 +9,7 @@ import de.hysky.skyblocker.utils.NEURepoManager;
 import de.hysky.skyblocker.utils.ProfileUtils;
 import it.unimi.dsi.fastutil.objects.Object2ReferenceMap;
 import it.unimi.dsi.fastutil.objects.Object2ReferenceOpenHashMap;
+import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.screen.slot.Slot;
@@ -22,6 +23,9 @@ import org.slf4j.LoggerFactory;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RabbitCollection extends TooltipAdder {
 	private static final Object2ReferenceMap<String, RabbitRarity> RABBITS = new Object2ReferenceOpenHashMap<>(500); //There's roughly 460 rabbits so far
@@ -30,6 +34,8 @@ public class RabbitCollection extends TooltipAdder {
 	private static final byte CF_HOPPITY_COLLECTION_SLOT = 50;
 	//The slot in chocolate collection screen
 	private static final byte HC_HOPPITY_COLLECTION_SLOT = 4;
+	private static final Pattern RABBIT_FOUND_PATTERN = Pattern.compile("§D§LHOPPITY'S HUNT §7You found (?:§\\S)+(.*?) (?:§\\S)+\\((?:§\\S)+([^§]+).*");
+	private static RabbitRarity lastRarity = null;
 
 	public RabbitCollection() {
 		super("^Chocolate Factory|\\(\\d+/\\d+\\) Hoppity's Collection$", Integer.MIN_VALUE);
@@ -38,6 +44,25 @@ public class RabbitCollection extends TooltipAdder {
 	public static void init() {
 		readJson();
 		SkyblockEvents.JOIN.register(RabbitCollection::apiRequest);
+		//These 2 messages are sent one after the other when a rabbit is found
+		//The found rabbit can also be a duplicate rabbit, so we have to check if the next message says "NEW RABBIT!"
+		ClientReceiveMessageEvents.GAME.register(((message, overlay) -> {
+			String str = message.getString();
+			Matcher matcher = RABBIT_FOUND_PATTERN.matcher(str);
+			if (matcher.matches()) {
+				try {
+					lastRarity = RabbitRarity.fromString(matcher.group(2).toLowerCase(Locale.ROOT));
+				} catch (IllegalArgumentException e) {
+					LOGGER.warn("[Skyblocker Rabbit Collection] Unknown rabbit rarity: {} - report this to Skyblocker!", matcher.group(1));
+					lastRarity = null;
+				}
+				return;
+			}
+			if (str.startsWith("§d§lNEW RABBIT!") && lastRarity != null) {
+				lastRarity.incrementCollected();
+				lastRarity = null;
+			}
+		}));
 //		ApiAuthentication.TOKEN_REQUEST_CALLBACK.register(RabbitCollection::apiRequest);
 	}
 
@@ -70,7 +95,7 @@ public class RabbitCollection extends TooltipAdder {
 			reader.beginObject(); // Open up the `rarities` object
 			while (reader.hasNext()) {
 				final String key = reader.nextName(); //Read the current rarity key
-				final RabbitRarity rarity = RabbitRarity.fromJsonKey(key);
+				final RabbitRarity rarity = RabbitRarity.fromString(key);
 				reader.beginObject(); // Open up the current rarity object
 				reader.nextName(); // Skip the `rabbits` key
 				reader.beginArray(); // Open up the `rabbits` array
@@ -143,20 +168,20 @@ public class RabbitCollection extends TooltipAdder {
 		@Override
 		public String toString() {
 			return switch (this) {
-				case COMMON -> "Common Rabbits";
-				case UNCOMMON -> "Uncommon Rabbits";
-				case RARE -> "Rare Rabbits";
-				case EPIC -> "Epic Rabbits";
+				case COMMON    -> "Common Rabbits";
+				case UNCOMMON  -> "Uncommon Rabbits";
+				case RARE      -> "Rare Rabbits";
+				case EPIC      -> "Epic Rabbits";
 				case LEGENDARY -> "Legendary Rabbits";
-				case MYTHIC -> "Mythic Rabbits";
-				case DIVINE -> "Divine Rabbits";
+				case MYTHIC    -> "Mythic Rabbits";
+				case DIVINE    -> "Divine Rabbits";
 			};
 		}
 
 		// This is to avoid creating a new array every time we iterate over the values
 		public static final @Unmodifiable List<RabbitRarity> entries = Arrays.asList(values());
 
-		public static RabbitRarity fromJsonKey(String key) {
+		public static RabbitRarity fromString(String key) {
 			return switch (key) { // @formatter:off
 				case "common"    -> COMMON;
 				case "uncommon"  -> UNCOMMON;
