981b5a258377367b620f6f4ed05f9b37acac2b47
Rime <81419447+Emirlol@users.noreply.github.com>
2024-06-22 12:50:27 +0300

    Annotate Profile's fields properly

--- src/main/java/de/hysky/skyblocker/utils/Profile.java
+++ src/main/java/de/hysky/skyblocker/utils/Profile.java
@@ -1,18 +1,19 @@
 package de.hysky.skyblocker.utils;
 
 import com.google.gson.JsonObject;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Represents a skyblock profile.
  */
 public class Profile {
-	public final String uuid;
-	public final JsonObject jsonData;
+	public final @NotNull String uuid;
+	public final @NotNull JsonObject jsonData;
 	private boolean selected;
 	//This is used for the API call cooldown, but the value is not updated as creating new Profile objects is easier than getting those from the cache and updating them
 	public final long lastUpdate;
 
-	public Profile(String uuid, JsonObject jsonData, boolean selected, long lastUpdate) {
+	public Profile(@NotNull String uuid, @NotNull JsonObject jsonData, boolean selected, long lastUpdate) {
 		this.uuid = uuid;
 		this.jsonData = jsonData;
 		this.selected = selected;
