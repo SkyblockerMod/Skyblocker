ef38d4fb61304c6c2676c0150f280b6452f2cdf4
viciscat <51047087+viciscat@users.noreply.github.com>
2024-07-02 14:56:41 +0200

    fix tab hud showing outside skyblock

--- src/main/java/de/hysky/skyblocker/skyblock/tabhud/screenbuilder/ScreenMaster.java
+++ src/main/java/de/hysky/skyblocker/skyblock/tabhud/screenbuilder/ScreenMaster.java
@@ -156,6 +156,7 @@ public class ScreenMaster {
         SkyblockEvents.LOCATION_CHANGE.register(location -> ScreenBuilder.positionsNeedsUpdating = true);
 
         HudRenderEvents.BEFORE_CHAT.register((context, tickDelta) -> {
+            if (!Utils.isOnSkyblock()) return;
             MinecraftClient client = MinecraftClient.getInstance();
             Window window = client.getWindow();
             float scale = SkyblockerConfigManager.get().uiAndVisuals.tabHud.tabHudScale / 100f;
