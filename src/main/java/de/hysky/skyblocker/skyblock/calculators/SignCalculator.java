40443cdc320ba09953cc7a6ffc464d0c15345d45
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2024-07-18 14:35:56 +0800

    Refactor indents

--- src/main/java/de/hysky/skyblocker/skyblock/calculators/SignCalculator.java
+++ src/main/java/de/hysky/skyblocker/skyblock/calculators/SignCalculator.java
@@ -11,57 +11,57 @@ import java.text.NumberFormat;
 import java.util.Locale;
 
 public class SignCalculator {
-	private static final MinecraftClient CLIENT = MinecraftClient.getInstance();
-	private static final NumberFormat FORMATTER = NumberFormat.getInstance(Locale.US);
+    private static final MinecraftClient CLIENT = MinecraftClient.getInstance();
+    private static final NumberFormat FORMATTER = NumberFormat.getInstance(Locale.US);
 
-	private static String lastInput;
-	private static double output;
+    private static String lastInput;
+    private static double output;
 
-	public static void renderCalculator(DrawContext context, String message, int renderX, int renderY) {
-		if (SkyblockerConfigManager.get().uiAndVisuals.inputCalculator.requiresEquals && !message.startsWith("=")) {
-			output = -1;
-			lastInput = message;
-			return;
-		}
-		if (message.startsWith("=")) {
-			message = message.substring(1);
-		}
-		//only update output if new input
-		if (!message.equals(lastInput)) { //
-			try {
-				output = Calculator.calculate(message);
-			} catch (Exception e) {
-				output = -1;
-			}
-		}
+    public static void renderCalculator(DrawContext context, String message, int renderX, int renderY) {
+        if (SkyblockerConfigManager.get().uiAndVisuals.inputCalculator.requiresEquals && !message.startsWith("=")) {
+            output = -1;
+            lastInput = message;
+            return;
+        }
+        if (message.startsWith("=")) {
+            message = message.substring(1);
+        }
+        //only update output if new input
+        if (!message.equals(lastInput)) { //
+            try {
+                output = Calculator.calculate(message);
+            } catch (Exception e) {
+                output = -1;
+            }
+        }
 
-		render(context, message, renderX, renderY);
+        render(context, message, renderX, renderY);
 
-		lastInput = message;
-	}
+        lastInput = message;
+    }
 
-	public static String getNewValue(Boolean isPrice) {
-		if (output == -1) {
-			//if mode is not activated or just invalid equation return what the user typed in
-			return lastInput;
-		}
+    public static String getNewValue(Boolean isPrice) {
+        if (output == -1) {
+            //if mode is not activated or just invalid equation return what the user typed in
+            return lastInput;
+        }
 
-		//price can except decimals and exponents
-		if (isPrice) {
-			return String.valueOf(output);
-		}
-		//amounts want an integer number so round
-		return Long.toString(Math.round(output));
-	}
+        //price can except decimals and exponents
+        if (isPrice) {
+            return String.valueOf(output);
+        }
+        //amounts want an integer number so round
+        return Long.toString(Math.round(output));
+    }
 
-	private static void render(DrawContext context, String input, int renderX, int renderY) {
-		Text text;
-		if (output == -1) {
-			text = Text.translatable("skyblocker.config.uiAndVisuals.inputCalculator.invalidEquation").formatted(Formatting.RED);
-		} else {
-			text = Text.literal(input + " = " + FORMATTER.format(output)).formatted(Formatting.GREEN);
-		}
+    private static void render(DrawContext context, String input, int renderX, int renderY) {
+        Text text;
+        if (output == -1) {
+            text = Text.translatable("skyblocker.config.uiAndVisuals.inputCalculator.invalidEquation").formatted(Formatting.RED);
+        } else {
+            text = Text.literal(input + " = " + FORMATTER.format(output)).formatted(Formatting.GREEN);
+        }
 
-		context.drawCenteredTextWithShadow(CLIENT.textRenderer, text, renderX, renderY, 0xFFFFFFFF);
-	}
+        context.drawCenteredTextWithShadow(CLIENT.textRenderer, text, renderX, renderY, 0xFFFFFFFF);
+    }
 }
