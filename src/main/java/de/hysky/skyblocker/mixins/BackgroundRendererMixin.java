6fc1e05083c5ff835920a522ab364f6fdde0921f
Hunter Lane <hunterlane6299@gmail.com>
2024-07-26 20:11:38 -0700

    message and code style changes

--- src/main/java/de/hysky/skyblocker/mixins/BackgroundRendererMixin.java
+++ src/main/java/de/hysky/skyblocker/mixins/BackgroundRendererMixin.java
@@ -1,5 +1,6 @@
 package de.hysky.skyblocker.mixins;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.systems.RenderSystem;
 import de.hysky.skyblocker.config.SkyblockerConfigManager;
 import de.hysky.skyblocker.config.configs.CrimsonIsleConfig;
@@ -8,7 +9,6 @@ import net.minecraft.block.enums.CameraSubmersionType;
 import net.minecraft.client.render.BackgroundRenderer;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.FogShape;
-import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -22,19 +22,16 @@ public abstract class BackgroundRendererMixin {
      * This sets it to be the same distance as what you would see in the overworld (every other skyblock island)
      */
     @Inject(method = "applyFog", at = @At("RETURN"))
-    private static void applyFogModifyDistance(Camera camera, BackgroundRenderer.FogType fogType, float viewDistance, boolean thickFog, float tickDelta, CallbackInfo info) {
+    private static void applyFogModifyDistance(CallbackInfo ci, @Local(argsOnly = true) Camera camera, @Local(argsOnly = true) BackgroundRenderer.FogType fogType, @Local(argsOnly = true, ordinal = 0) float viewDistance, @Local(argsOnly = true) boolean thickFog) {
         final CameraSubmersionType cameraSubmersionType = camera.getSubmersionType();
         CrimsonIsleConfig config = SkyblockerConfigManager.get().crimsonIsle;
 
-        if (Utils.isOnSkyblock()
-                && config.extendNetherFog
-                && cameraSubmersionType == CameraSubmersionType.NONE && thickFog) {
-
+        if (Utils.isOnSkyblock() && config.extendNetherFog && cameraSubmersionType == CameraSubmersionType.NONE && thickFog) {
             float start;
             if (fogType == BackgroundRenderer.FogType.FOG_SKY) {
                 start = 0.0f;
             } else {
-                start = viewDistance - MathHelper.clamp(viewDistance / 10.0f, 4.0f, 64.0f);
+                start = viewDistance - Math.clamp(viewDistance / 10.0f, 4.0f, 64.0f);
             }
 
             RenderSystem.setShaderFogStart(start);
