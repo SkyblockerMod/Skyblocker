1d95773ad9502320b5dc59c619d87225c158b49b
Aaron <51387595+AzureAaron@users.noreply.github.com>
2024-08-02 01:39:24 -0400

    Rare Dye Drop Special Effect!

---
+++ src/main/java/de/hysky/skyblocker/skyblock/special/DyeSpecialEffects.java
@@ -0,0 +1,56 @@
+package de.hysky.skyblocker.skyblock.special;
+
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import de.hysky.skyblocker.config.SkyblockerConfigManager;
+import de.hysky.skyblocker.skyblock.itemlist.ItemRepository;
+import de.hysky.skyblocker.utils.Utils;
+import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.item.ItemStack;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.text.Text;
+
+public class DyeSpecialEffects {
+	private static final Logger LOGGER = LogUtils.getLogger();
+	private static final MinecraftClient CLIENT = MinecraftClient.getInstance();
+	private static final Pattern DROP_PATTERN = Pattern.compile("WOW! (?:\\[[A-Z+]+\\] )?(?<player>[A-Za-z0-9_]+) found (?<dye>[A-Za-z ]+ Dye)!");
+
+	static void init() {
+		ClientReceiveMessageEvents.GAME.register(DyeSpecialEffects::displayDyeDropEffect);
+	}
+
+	private static void displayDyeDropEffect(Text message, boolean overlay) {
+		if (Utils.isOnSkyblock() && SkyblockerConfigManager.get().general.specialEffects.rareDyeDropEffects && !overlay) {
+			try {
+				String stringForm = message.getString();
+				Matcher matcher = DROP_PATTERN.matcher(stringForm);
+
+				if (matcher.matches() && matcher.group("player").equals(CLIENT.getSession().getUsername())) {
+					ItemStack stack = findDyeStack(matcher.group("dye"));
+
+					if (!stack.isEmpty()) {
+						CLIENT.particleManager.addEmitter(CLIENT.player, ParticleTypes.TRIAL_SPAWNER_DETECTION, 30);
+						CLIENT.gameRenderer.showFloatingItem(stack);
+					}
+				}
+			} catch (Exception e) {
+				LOGGER.error("[Skyblocker Special Effects] An unexpected exception was encountered!", e);
+			}
+		}
+	}
+
+	private static ItemStack findDyeStack(String dyeName) {
+		Optional<ItemStack> dye = ItemRepository.getItemsStream()
+				.filter(stack -> stack.getName().getString().equals(dyeName))
+				.findFirst();
+
+		return dye.orElse(ItemStack.EMPTY);
+	}
+}
