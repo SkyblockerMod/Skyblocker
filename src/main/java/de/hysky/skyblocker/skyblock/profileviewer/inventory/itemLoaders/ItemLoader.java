a4fb4dddd6372e2daf61177db577722d4057ab5b
Aaron <51387595+AzureAaron@users.noreply.github.com>
2024-08-11 04:59:01 -0400

    Proper Item DFU for PV
    This is the result of wayy too much functional programming with DFU (or
    not enough!)

--- src/main/java/de/hysky/skyblocker/skyblock/profileviewer/inventory/itemLoaders/ItemLoader.java
+++ src/main/java/de/hysky/skyblocker/skyblock/profileviewer/inventory/itemLoaders/ItemLoader.java
@@ -5,31 +5,19 @@ import com.google.gson.JsonParser;
 import com.mojang.serialization.JsonOps;
 
 import de.hysky.skyblocker.skyblock.PetCache;
-import de.hysky.skyblocker.skyblock.itemlist.ItemRepository;
 import de.hysky.skyblocker.skyblock.profileviewer.ProfileViewerScreen;
 import de.hysky.skyblocker.skyblock.profileviewer.inventory.Pet;
 import de.hysky.skyblocker.skyblock.tabhud.util.Ico;
 import de.hysky.skyblocker.utils.ItemUtils;
-import de.hysky.skyblocker.utils.NEURepoManager;
-import de.hysky.skyblocker.utils.TextTransformer;
-import io.github.moulberry.repo.data.NEUItem;
+import de.hysky.skyblocker.utils.datafixer.LegacyItemStackFixer;
 import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.*;
-import net.minecraft.datafixer.fix.ItemIdFix;
-import net.minecraft.datafixer.fix.ItemInstanceTheFlatteningFix;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.*;
-import net.minecraft.registry.Registries;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.Util;
 
 import java.io.ByteArrayInputStream;
 import java.util.*;
-import java.util.stream.Collectors;
-
-import static de.hysky.skyblocker.skyblock.itemlist.ItemRepository.getItemStack;
 
 public class ItemLoader {
 
@@ -38,107 +26,42 @@ public class ItemLoader {
         List<ItemStack> itemList = new ArrayList<>();
 
         for (int i = 0; i < containerContent.size(); i++) {
-            if (containerContent.getCompound(i).getInt("id") == 0) {
+            NbtCompound nbt = containerContent.getCompound(i);
+            if (nbt.getInt("id") == 0) {
                 itemList.add(ItemStack.EMPTY);
                 continue;
             }
 
-            NbtCompound nbttag = containerContent.getCompound(i).getCompound("tag");
-            NbtCompound extraAttributes = nbttag.getCompound("ExtraAttributes");
-            String internalName = extraAttributes.getString("id");
-            if (internalName.equals("PET")) {
-                PetCache.PetInfo petInfo = PetCache.PetInfo.CODEC.parse(JsonOps.INSTANCE, JsonParser.parseString(extraAttributes.getString("petInfo"))).getOrThrow();
+            ItemStack stack = LegacyItemStackFixer.fixLegacyStack(nbt);
+
+            if (stack.isEmpty()) {
+            	ItemStack fallback = Ico.BARRIER.copy();
+
+            	fallback.set(DataComponentTypes.CUSTOM_NAME, Text.literal("Error: " + nbt.getCompound("tag").getCompound("ExtraAttributes").getString("id")));
+                itemList.add(fallback);
+
+                continue;
+            }
+
+            String itemId = stack.getSkyblockId();
+            NbtCompound customData = ItemUtils.getCustomData(stack);
+
+            if (itemId.equals("PET")) {
+                PetCache.PetInfo petInfo = PetCache.PetInfo.CODEC.parse(JsonOps.INSTANCE, JsonParser.parseString(customData.getString("petInfo"))).getOrThrow();
                 Pet pet = new Pet(petInfo);
                 itemList.add(pet.getIcon());
                 continue;
             }
 
-            Identifier itemId = identifierFromOldId(containerContent.getCompound(i).getInt("id"), containerContent.getCompound(i).getInt("Damage"));
-
-            ItemStack stack;
-            if (itemId.toString().equals("minecraft:air")) {
-                ItemStack itemStack = getItemStack(internalName);
-                stack = itemStack != null ? itemStack.copy() : ItemStack.EMPTY;
-            } else {
-                stack = new ItemStack(Registries.ITEM.get(itemId));
-            }
-
-            if (stack.isEmpty() || stack.getItem().equals(Ico.BARRIER.getItem())) {
-                // Last ditch effort to find item in NEU REPO
-                Map<String, NEUItem> items = NEURepoManager.NEU_REPO.getItems().getItems();
-                stack = items.values().stream()
-                        .filter(j -> Formatting.strip(j.getSkyblockItemId()).equals(Formatting.strip(internalName).replace(":", "-")))
-                        .findFirst()
-                        .map(NEUItem::getSkyblockItemId)
-                        .map(ItemRepository::getItemStack)
-                        .map(ItemStack::copy)
-                        .orElse(Ico.BARRIER.copy());
-
-
-                if (stack.getName().getString().contains("barrier")) {
-                    stack.set(DataComponentTypes.CUSTOM_NAME, Text.literal("Err: " + internalName));
-                    itemList.add(stack);
-                    continue;
-                }
-            }
-
-            // Custom Data
-            NbtCompound customData = new NbtCompound();
-
-            // Add Skyblock Item Id
-            customData.put(ItemUtils.ID, NbtString.of(internalName));
-
-
-            // Item Name
-            stack.set(DataComponentTypes.CUSTOM_NAME, TextTransformer.fromLegacy(nbttag.getCompound("display").getString("Name")));
-
-            // Lore
-            NbtList loreList = nbttag.getCompound("display").getList("Lore", 8);
-            stack.set(DataComponentTypes.LORE, new LoreComponent(loreList.stream()
-                    .map(NbtElement::asString)
-                    .map(TextTransformer::fromLegacy)
-                    .collect(Collectors.toList())));
-
-            // add skull texture
-            NbtList texture = nbttag.getCompound("SkullOwner").getCompound("Properties").getList("textures", 10);
-            if (!texture.isEmpty()) {
-                stack.set(DataComponentTypes.PROFILE, new ProfileComponent(Optional.of(internalName), Optional.of(UUID.fromString(nbttag.getCompound("SkullOwner").get("Id").asString())), ItemUtils.propertyMapWithTexture(texture.getCompound(0).getString("Value"))));
-            }
-
-            // Colour
-            if (nbttag.getCompound("display").contains("color")) {
-                int color = nbttag.getCompound("display").getInt("color");
-                stack.set(DataComponentTypes.DYED_COLOR, new DyedColorComponent(color, false));
-            }
-
-            // add enchantment glint
-            if (nbttag.getKeys().contains("ench")) {
-                stack.set(DataComponentTypes.ENCHANTMENT_GLINT_OVERRIDE, true);
-            }
-
-            // Hide weapon damage and other useless info
-            stack.set(DataComponentTypes.ATTRIBUTE_MODIFIERS, new AttributeModifiersComponent(List.of(), false));
-
-            // Set Count
-            stack.setCount(containerContent.getCompound(i).getInt("Count"));
-
             // Attach an override for Aaron's Mod so that these ItemStacks will work with the mod's features even when not in Skyblock
-            extraAttributes.put("aaron-mod", Util.make(new NbtCompound(), comp -> comp.putBoolean("alwaysDisplaySkyblockInfo", true)));
-
-            stack.set(DataComponentTypes.CUSTOM_DATA, NbtComponent.of(extraAttributes));
+            if (stack.contains(DataComponentTypes.CUSTOM_DATA)) {
+                customData.put("aaron-mod", Util.make(new NbtCompound(), comp -> comp.putBoolean("alwaysDisplaySkyblockInfo", true)));
+            }
 
             itemList.add(stack);
         }
 
         return itemList;
-    }
-
-    private static Identifier identifierFromOldId(int id, int damage) {
-        try {
-            return damage != 0 ? Identifier.of(ItemInstanceTheFlatteningFix.getItem(ItemIdFix.fromId(id), damage)) : Identifier.of(ItemIdFix.fromId(id));
-        } catch (Exception e) {
-            return Identifier.of("air");
-        }
     }
 
     private static NbtList decompress(JsonObject data) {
