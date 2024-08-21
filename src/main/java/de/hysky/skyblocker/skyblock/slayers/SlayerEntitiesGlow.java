94dee39190a4e3fbd27141faf3d4dcdd5a24e443
nmccullagh <narhanael64@gmail.com>
2024-08-21 02:22:13 +0100

    [Pv & SlayerHighlight] Fixes and improvements@

--- src/main/java/de/hysky/skyblocker/skyblock/slayers/SlayerEntitiesGlow.java
+++ src/main/java/de/hysky/skyblocker/skyblock/slayers/SlayerEntitiesGlow.java
@@ -1,6 +1,10 @@
 package de.hysky.skyblocker.skyblock.slayers;
 
 import de.hysky.skyblocker.utils.SlayerUtils;
+import de.hysky.skyblocker.utils.render.RenderHelper;
+import de.hysky.skyblocker.utils.scheduler.Scheduler;
+import it.unimi.dsi.fastutil.objects.Object2BooleanOpenHashMap;
+import it.unimi.dsi.fastutil.objects.Object2ObjectMap;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
@@ -12,6 +16,8 @@ import net.minecraft.util.math.Box;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 public class SlayerEntitiesGlow {
     private static final Map<String, String> SLAYER_MINI_NAMES = Map.ofEntries(
@@ -47,6 +53,12 @@ public class SlayerEntitiesGlow {
 
     private static final Set<UUID> MOBS_TO_GLOW = new HashSet<>();
 
+    /**
+     * ARMORSTAND_TO_MOBS_TO_GLOW tracks if an armor stand already has an associated mob entity. This is used for trying to dedupe glows,
+     * where an armor stand has detected multiple candidates as its associated mob entity -  in a vain attempt to reduce the amount of false positives
+     */
+    private static final ConcurrentHashMap<UUID, UUID> ARMORSTAND_TO_MOBS_TO_GLOW = new ConcurrentHashMap<>();
+
     public static boolean shouldGlow(UUID entityUUID) {
         return MOBS_TO_GLOW.contains(entityUUID);
     }
@@ -80,13 +92,21 @@ public class SlayerEntitiesGlow {
      * @param armorStand  the entity that contains the display name of the Slayer (mini)boss
      */
     private static MobEntity findClosestMobEntity(Class<? extends MobEntity> entityClass, ArmorStandEntity armorStand) {
-        return armorStand.getWorld().getEntitiesByClass(entityClass, armorStand.getDimensions(null)
-                        .getBoxAt(armorStand.getPos()).expand(1.5), entity ->
-                        !entity.isDead() && entity.age > armorStand.age - 4 && entity.age < armorStand.age + 4)
+        List<MobEntity> mobEntities = armorStand.getWorld().getEntitiesByClass(entityClass, armorStand.getDimensions(null)
+                        .getBoxAt(armorStand.getPos()).expand(0.3f, 1.5f, 0.3f), entity -> !entity.isDead())
                 .stream()
                 .filter(SlayerEntitiesGlow::isValidSlayerMob)
-                .min(Comparator.comparingDouble((MobEntity e) -> e.distanceTo(armorStand)))
-                .orElse(null);
+                .sorted(Comparator.comparingDouble(e -> e.squaredDistanceTo(armorStand)))
+                .collect(Collectors.toList());
+
+        return switch (mobEntities.size()) {
+            case 0 -> null;
+            case 1 -> mobEntities.getFirst();
+            default -> mobEntities.stream()
+                    .filter(entity -> entity.age > armorStand.age - 4 && entity.age < armorStand.age + 4)
+                    .findFirst()
+                    .orElse(mobEntities.getFirst());
+        };
     }
 
     /**
@@ -107,16 +127,48 @@ public class SlayerEntitiesGlow {
         Class<? extends MobEntity> entityClass = SLAYER_MOB_TYPE.get(slayerType);
         if (entityClass != null) {
             MobEntity closestEntity = findClosestMobEntity(entityClass, armorStand);
-            if (closestEntity != null) MOBS_TO_GLOW.add(closestEntity.getUuid());
+            if (closestEntity != null) {
+               UUID uuid =  ARMORSTAND_TO_MOBS_TO_GLOW.putIfAbsent(armorStand.getUuid(), closestEntity.getUuid());
+                if (uuid != null && closestEntity.getUuid() != uuid && closestEntity.age < 80) {
+                    Scheduler.INSTANCE.schedule(() -> recalculateMobGlow(armorStand, entityClass, uuid), 30, true);
+                }
+                MOBS_TO_GLOW.add(closestEntity.getUuid());
+            }
+        }
+    }
+
+    /**
+     *  This method attempts self-correct by finding the true slayer mob if there's 2 candidates
+     * @param armorStand the armor stand we know to be a slayer mob
+     * @param entityClass the java class of the entity we know the armor stand to belong to
+     * @param oldUUID the uuid of the first detected slayer mob
+     */
+    private static void recalculateMobGlow(ArmorStandEntity armorStand, Class<? extends MobEntity> entityClass, UUID oldUUID) {
+        MobEntity entity = findClosestMobEntity(entityClass, armorStand);
+        if (entity.getUuid() != oldUUID) {
+            RenderHelper.runOnRenderThread(() -> {
+                MOBS_TO_GLOW.add(entity.getUuid());
+                MOBS_TO_GLOW.remove(ARMORSTAND_TO_MOBS_TO_GLOW.put(armorStand.getUuid(), entity.getUuid()));
+            });
+
         }
     }
 
     public static void onEntityDeath(@Nullable Entity entity) {
-        if (entity != null && entity.getUuid() != null) MOBS_TO_GLOW.remove(entity.getUuid());
+        if (entity != null && entity.getUuid() != null) {
+            MOBS_TO_GLOW.remove(entity.getUuid());
+        }
+    }
+
+    public static void cleanupArmorstand(@Nullable ArmorStandEntity entity) {
+        if (entity != null && entity.getUuid() != null) {
+           ARMORSTAND_TO_MOBS_TO_GLOW.remove(entity.getUuid());
+        }
     }
 
     private static void clearGlow() {
         MOBS_TO_GLOW.clear();
+        ARMORSTAND_TO_MOBS_TO_GLOW.clear();
     }
 
 }
