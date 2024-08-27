3b07d982cc6b6c20de7649437e2948df7cd5c440
Rime <81419447+Emirlol@users.noreply.github.com>
2024-08-27 08:01:05 +0300

    Allow mouse keys to work with wardrobe

--- src/main/java/de/hysky/skyblocker/skyblock/WardrobeKeybinds.java
+++ src/main/java/de/hysky/skyblocker/skyblock/WardrobeKeybinds.java
@@ -5,11 +5,16 @@ import de.hysky.skyblocker.config.SkyblockerConfigManager;
 import de.hysky.skyblocker.utils.container.RegexContainerMatcher;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenKeyboardEvents;
+import net.fabricmc.fabric.api.client.screen.v1.ScreenMouseEvents;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.option.KeyBinding;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.screen.slot.SlotActionType;
 import org.lwjgl.glfw.GLFW;
+
+import java.util.function.Predicate;
 
 @SuppressWarnings("unused")
 public class WardrobeKeybinds extends RegexContainerMatcher {
@@ -23,26 +28,34 @@ public class WardrobeKeybinds extends RegexContainerMatcher {
 	public static void init() {
 		ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
 			if (!(screen instanceof HandledScreen<?> handledScreen) || !INSTANCE.test(handledScreen) || !INSTANCE.isEnabled() || client.interactionManager == null) return;
-			ScreenKeyboardEvents.allowKeyPress(handledScreen).register((ignored, keyCode, scanCode, modifiers) -> {
-				boolean found = false;
-				int i;
-				for (i = 0; i < client.options.hotbarKeys.length; i++) {
-					if (client.options.hotbarKeys[i].matchesKey(keyCode, scanCode)) {
-						found = true;
-						break;
-					}
-				}
-				if (!found) return true;
-				// The items start from the 5th row in the inventory. The i number we have is the column, so we have to offset it by 4 rows to get the correct row
-				i += 9 * 4;
-				ItemStack itemStack = handledScreen.getScreenHandler().getSlot(i).getStack();
-				// Check if the item in the slot is a swap/unequip item before going further.
-				// This prevents usage when the inventory hasn't loaded fully or when the slot pressed is locked (which would be meaningless to click)
-				if (!itemStack.isOf(Items.PINK_DYE) && !itemStack.isOf(Items.LIME_DYE)) return true;
-				client.interactionManager.clickSlot(handledScreen.getScreenHandler().syncId, i, GLFW.GLFW_MOUSE_BUTTON_1, SlotActionType.PICKUP, client.player);
-				return false;
-			});
+			ScreenKeyboardEvents.allowKeyPress(handledScreen).register((ignored, keyCode, scanCode, modifiers) ->
+					allowInput(client, handledScreen, keybinding -> keybinding.matchesKey(keyCode, scanCode))
+			);
+			ScreenMouseEvents.allowMouseClick(handledScreen).register((ignored, mouseX, mouseY, button) ->
+					allowInput(client, handledScreen, keybinding -> keybinding.matchesMouse(button))
+			);
 		});
+	}
+
+	private static boolean allowInput(MinecraftClient client, HandledScreen<?> handledScreen, Predicate<KeyBinding> predicate) {
+		boolean found = false;
+		int i;
+		for (i = 0; i < client.options.hotbarKeys.length; i++) {
+			if (predicate.test(client.options.hotbarKeys[i])) {
+				found = true;
+				break;
+			}
+		}
+		if (!found) return true;
+		// The items start from the 5th row in the inventory. The i number we have is the column in the first row, so we have to offset it by 4 rows to get the 5th row, which is where the items start.
+		i += 9 * 4;
+		ItemStack itemStack = handledScreen.getScreenHandler().getSlot(i).getStack();
+		// Check if the item in the slot is a swap/unequip item before going further.
+		// This prevents usage when the inventory hasn't loaded fully or when the slot pressed is locked (which would be meaningless to click)
+		if (!itemStack.isOf(Items.PINK_DYE) && !itemStack.isOf(Items.LIME_DYE)) return true;
+		assert client.interactionManager != null;
+		client.interactionManager.clickSlot(handledScreen.getScreenHandler().syncId, i, GLFW.GLFW_MOUSE_BUTTON_1, SlotActionType.PICKUP, client.player);
+		return false;
 	}
 
 	@Override
