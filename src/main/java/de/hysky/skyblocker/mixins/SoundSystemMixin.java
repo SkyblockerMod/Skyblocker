c709230d75178178eb0821a55f19e217c74c057a
7azeemm <hazemfarah26@gmail.com>
2024-09-05 08:29:29 +0100

    fix miniboss alert

--- src/main/java/de/hysky/skyblocker/mixins/SoundSystemMixin.java
+++ src/main/java/de/hysky/skyblocker/mixins/SoundSystemMixin.java
@@ -17,8 +17,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(SoundSystem.class)
 public class SoundSystemMixin {
 
-    @Inject(method = "play(Lnet/minecraft/client/sound/SoundInstance;)V", at = @At("HEAD"), cancellable = true)
-    private void onPlay(SoundInstance soundInstance, CallbackInfo ci) {
+    @Inject(method = "play(Lnet/minecraft/client/sound/SoundInstance;)V", at = @At("TAIL"))
+    private void onPlayTail(SoundInstance soundInstance, CallbackInfo ci) {
         if(SkyblockerConfigManager.get().slayers.miniBossSpawnAlert && Utils.isOnSkyblock() && soundInstance.getSound() != null && soundInstance.getId().equals(Identifier.ofVanilla("entity.generic.explode"))){
             if (Utils.isInSlayerQuest() && soundInstance.getPitch() == 9 / 7f && soundInstance.getVolume() == 0.6f) {
                 //Checks if MiniBoss is within a radius of the client's location
@@ -27,15 +27,17 @@ public class SoundSystemMixin {
                 }
             }
         }
+    }
 
-        if(Utils.isInTheEnd() && SkyblockerConfigManager.get().otherLocations.end.muteEndermanSounds) {
+    @Inject(method = "play(Lnet/minecraft/client/sound/SoundInstance;)V", at = @At("HEAD"), cancellable = true)
+    private void onPlayHead(SoundInstance soundInstance, CallbackInfo ci) {
+        if (Utils.isInTheEnd() && SkyblockerConfigManager.get().otherLocations.end.muteEndermanSounds) {
             // Check if the sound identifier matches any Enderman sound identifiers
             if (soundInstance.getId().equals(SoundEvents.ENTITY_ENDERMAN_AMBIENT.getId()) ||
                     soundInstance.getId().equals(SoundEvents.ENTITY_ENDERMAN_DEATH.getId()) ||
                     soundInstance.getId().equals(SoundEvents.ENTITY_ENDERMAN_HURT.getId()) ||
                     soundInstance.getId().equals(SoundEvents.ENTITY_ENDERMAN_SCREAM.getId()) ||
                     soundInstance.getId().equals(SoundEvents.ENTITY_ENDERMAN_STARE.getId())) {
-
                 // Cancel the playback of Enderman sounds
                 ci.cancel();
             }
