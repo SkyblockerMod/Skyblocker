0ef339b506839c641df5e47bcecebc697fd4a09e
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2024-09-22 12:58:52 -0400

    Update health and speed bar

--- src/main/java/de/hysky/skyblocker/skyblock/StatusBarTracker.java
+++ src/main/java/de/hysky/skyblocker/skyblock/StatusBarTracker.java
@@ -2,10 +2,11 @@ package de.hysky.skyblocker.skyblock;
 
 import de.hysky.skyblocker.config.SkyblockerConfigManager;
 import de.hysky.skyblocker.utils.Location;
+import de.hysky.skyblocker.utils.RegexUtils;
 import de.hysky.skyblocker.utils.Utils;
+import de.hysky.skyblocker.utils.scheduler.Scheduler;
 import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 
@@ -13,143 +14,147 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StatusBarTracker {
-    private static final Pattern STATUS_HEALTH = Pattern.compile("§[6c](\\d+(,\\d\\d\\d)*)/(\\d+(,\\d\\d\\d)*)❤(?:(\\+§c(\\d+(,\\d\\d\\d)*). *)| *)");
-    private static final Pattern DEFENSE_STATUS = Pattern.compile("§a(\\d+(,\\d\\d\\d)*)§a❈ Defense *");
-    private static final Pattern MANA_USE = Pattern.compile("§b-(\\d+(,\\d\\d\\d)*) Mana \\(§\\S+(?:\\s\\S+)* *");
-    private static final Pattern MANA_STATUS = Pattern.compile("§b(\\d+(,\\d\\d\\d)*)/(\\d+(,\\d\\d\\d)*)✎ (?:Mana|§3(\\d+(,\\d\\d\\d)*)ʬ) *");
+	private static final Pattern STATUS_HEALTH = Pattern.compile("§[6c](?<health>[0-9,]+)/(?<max>[0-9,]+)❤ *(?<healing>\\+§c([0-9,]+). *)?");
+	private static final Pattern DEFENSE_STATUS = Pattern.compile("§a(\\d+(,\\d\\d\\d)*)§a❈ Defense *");
+	private static final Pattern MANA_USE = Pattern.compile("§b-(\\d+(,\\d\\d\\d)*) Mana \\(§\\S+(?:\\s\\S+)* *");
+	private static final Pattern MANA_STATUS = Pattern.compile("§b(\\d+(,\\d\\d\\d)*)/(\\d+(,\\d\\d\\d)*)✎ (?:Mana|§3(\\d+(,\\d\\d\\d)*)ʬ) *");
 
-    private Resource health = new Resource(100, 100, 0);
-    private Resource mana = new Resource(100, 100, 0);
-    private Resource speed = new Resource(100, 400, 0);
-    private int defense = 0;
+	private final MinecraftClient client = MinecraftClient.getInstance();
+	private Resource health = new Resource(100, 100, 0);
+	private Resource mana = new Resource(100, 100, 0);
+	private Resource speed = new Resource(100, 400, 0);
+	private int defense = 0;
 
-    public void init() {
-        ClientReceiveMessageEvents.ALLOW_GAME.register(this::allowOverlayMessage);
-        ClientReceiveMessageEvents.MODIFY_GAME.register(this::onOverlayMessage);
-    }
+	public void init() {
+		ClientReceiveMessageEvents.ALLOW_GAME.register(this::allowOverlayMessage);
+		ClientReceiveMessageEvents.MODIFY_GAME.register(this::onOverlayMessage);
+		Scheduler.INSTANCE.scheduleCyclic(this::tick, 1);
+	}
 
-    public Resource getHealth() {
-        return this.health;
-    }
+	public Resource getHealth() {
+		return this.health;
+	}
 
-    public Resource getMana() {
-        return this.mana;
-    }
+	public Resource getMana() {
+		return this.mana;
+	}
 
-    public int getDefense() {
-        return this.defense;
-    }
+	public int getDefense() {
+		return this.defense;
+	}
 
-    public Resource getSpeed() {
-        updateSpeed();
-        return this.speed;
-    }
+	public Resource getSpeed() {
+		return this.speed;
+	}
 
-    private int parseInt(Matcher m, int group) {
-        return Integer.parseInt(m.group(group).replace(",", ""));
-    }
+	private void tick() {
+		if (client == null || client.player == null) return;
+		updateHealth(health.value, health.max, health.overflow);
+		updateSpeed();
+	}
 
-    private void updateMana(Matcher m) {
-        int value = parseInt(m, 1);
-        int max = parseInt(m, 3);
-        int overflow = m.group(5) == null ? 0 : parseInt(m, 5);
-        this.mana = new Resource(value, max, overflow);
-    }
+	private void updateMana(Matcher m) {
+		int value = RegexUtils.parseIntFromMatcher(m, 1);
+		int max = RegexUtils.parseIntFromMatcher(m, 3);
+		int overflow;
+		overflow = m.group(5) == null ? 0 : RegexUtils.parseIntFromMatcher(m, 5);
+		this.mana = new Resource(value, max, overflow);
+	}
 
-    private void updateSpeed() {
-        // Black cat and racing helm are untested - I don't have the money to test atm, but no reason why they shouldn't work
-        var player = MinecraftClient.getInstance().player;
-        int value = (int) (player.isSprinting() ? (player.getMovementSpeed() / 1.3f) * 1000 : player.getMovementSpeed() * 1000);
-        int max = 400; // hardcoded limit (except for with cactus knife, black cat, snail, racing helm, young drag)
-        if (player.getMainHandStack().getName().getString().contains("Cactus Knife") && Utils.getLocation() == Location.GARDEN) {
-            max = 500;
-        }
-        Iterable<ItemStack> armor = player.getArmorItems();
-        int youngDragCount = 0;
-        for (ItemStack armorPiece : armor) {
-            if (armorPiece.getName().getString().contains("Racing Helmet")) {
-                max = 500;
-            } else if (armorPiece.getName().getString().contains("Young Dragon")) {
-                youngDragCount++;
-            }
-        }
-        if (youngDragCount == 4) {
-            max = 500;
-        }
+	private void updateSpeed() {
+		// Black cat and racing helm are untested - I don't have the money to test atm, but no reason why they shouldn't work
+		assert client.player != null;
+		int value = (int) (client.player.isSprinting() ? (client.player.getMovementSpeed() / 1.3f) * 1000 : client.player.getMovementSpeed() * 1000);
+		int max = 400; // hardcoded limit (except for with cactus knife, black cat, snail, racing helm, young drag)
+		if (client.player.getMainHandStack().getName().getString().contains("Cactus Knife") && Utils.getLocation() == Location.GARDEN) {
+			max = 500;
+		}
+		Iterable<ItemStack> armor = client.player.getArmorItems();
+		int youngDragCount = 0;
+		for (ItemStack armorPiece : armor) {
+			if (armorPiece.getName().getString().contains("Racing Helmet")) {
+				max = 500;
+			} else if (armorPiece.getName().getString().contains("Young Dragon")) {
+				youngDragCount++;
+			}
+		}
+		if (youngDragCount == 4) {
+			max = 500;
+		}
 
-        PetCache.PetInfo pet = PetCache.getCurrentPet();
-        if (pet != null) {
-            if (pet.type().contains("BLACK_CAT")) {
-                max = 500;
-            } else if (pet.type().contains("SNAIL")) {
-                max = 100;
-            }
-        }
-        this.speed = new Resource(value, max, 0);
-    }
+		PetCache.PetInfo pet = PetCache.getCurrentPet();
+		if (pet != null) {
+			if (pet.type().contains("BLACK_CAT")) {
+				max = 500;
+			} else if (pet.type().contains("SNAIL")) {
+				max = 100;
+			}
+		}
+		this.speed = new Resource(value, max, 0);
+	}
 
-    private void updateHealth(Matcher m) {
-        int value = parseInt(m, 1);
-        int max = parseInt(m, 3);
-        int overflow = Math.max(0, value - max);
-        if (MinecraftClient.getInstance() != null && MinecraftClient.getInstance().player != null) {
-            ClientPlayerEntity player = MinecraftClient.getInstance().player;
-            value = (int) (player.getHealth() * max / player.getMaxHealth());
-            overflow = (int) (player.getAbsorptionAmount() * max / player.getMaxHealth());
-        }
-        this.health = new Resource(Math.min(value, max), max, Math.min(overflow, max));
-    }
+	private void updateHealth(Matcher matcher) {
+		int health = RegexUtils.parseIntFromMatcher(matcher, "health");
+		int max = RegexUtils.parseIntFromMatcher(matcher, "max");
+		updateHealth(health, max, Math.max(0, health - max));
+	}
 
-    private String reset(String str, Matcher m) {
-        str = str.substring(m.end());
-        m.reset(str);
-        return str;
-    }
+	private void updateHealth(int value, int max, int overflow) {
+		if (client != null && client.player != null) {
+			value = (int) (client.player.getHealth() * max / client.player.getMaxHealth());
+			overflow = (int) (client.player.getAbsorptionAmount() * max / client.player.getMaxHealth());
+		}
+		health = new Resource(Math.min(value, max), max, Math.min(overflow, max));
+	}
 
-    private boolean allowOverlayMessage(Text text, boolean overlay) {
-        onOverlayMessage(text, overlay);
-        return true;
-    }
+	private String reset(String str, Matcher m) {
+		str = str.substring(m.end());
+		m.reset(str);
+		return str;
+	}
 
-    private Text onOverlayMessage(Text text, boolean overlay) {
-        if (!overlay || !Utils.isOnSkyblock() || !SkyblockerConfigManager.get().uiAndVisuals.bars.enableBars || Utils.isInTheRift()) {
-            return text;
-        }
-        return Text.of(update(text.getString(), SkyblockerConfigManager.get().chat.hideMana));
-    }
+	private boolean allowOverlayMessage(Text text, boolean overlay) {
+		onOverlayMessage(text, overlay);
+		return true;
+	}
 
-    public String update(String actionBar, boolean filterManaUse) {
-        var sb = new StringBuilder();
-        Matcher matcher = STATUS_HEALTH.matcher(actionBar);
-        if (!matcher.lookingAt())
-            return actionBar;
-        updateHealth(matcher);
-        if (matcher.group(5) != null) {
-            sb.append("§c❤");
-            sb.append(matcher.group(5));
-        }
-        actionBar = reset(actionBar, matcher);
-        if (matcher.usePattern(MANA_STATUS).lookingAt()) {
-            defense = 0;
-            updateMana(matcher);
-            actionBar = reset(actionBar, matcher);
-        } else {
-            if (matcher.usePattern(DEFENSE_STATUS).lookingAt()) {
-                defense = parseInt(matcher, 1);
-                actionBar = reset(actionBar, matcher);
-            } else if (filterManaUse && matcher.usePattern(MANA_USE).lookingAt()) {
-                actionBar = reset(actionBar, matcher);
-            }
-            if (matcher.usePattern(MANA_STATUS).find()) {
-                updateMana(matcher);
-                matcher.appendReplacement(sb, "");
-            }
-        }
-        matcher.appendTail(sb);
-        String res = sb.toString().trim();
-        return res.isEmpty() ? null : res;
-    }
+	private Text onOverlayMessage(Text text, boolean overlay) {
+		if (!overlay || !Utils.isOnSkyblock() || !SkyblockerConfigManager.get().uiAndVisuals.bars.enableBars || Utils.isInTheRift()) {
+			return text;
+		}
+		return Text.of(update(text.getString(), SkyblockerConfigManager.get().chat.hideMana));
+	}
 
-    public record Resource(int value, int max, int overflow) {
-    }
+	public String update(String actionBar, boolean filterManaUse) {
+		var sb = new StringBuilder();
+		Matcher matcher = STATUS_HEALTH.matcher(actionBar);
+		if (!matcher.lookingAt()) return actionBar;
+		updateHealth(matcher);
+		if (matcher.group("healing") != null) {
+			sb.append("§c❤");
+			sb.append(matcher.group("healing"));
+		}
+		actionBar = reset(actionBar, matcher);
+		if (matcher.usePattern(MANA_STATUS).lookingAt()) {
+			defense = 0;
+			updateMana(matcher);
+			actionBar = reset(actionBar, matcher);
+		} else {
+			if (matcher.usePattern(DEFENSE_STATUS).lookingAt()) {
+				defense = RegexUtils.parseIntFromMatcher(matcher, 1);
+				actionBar = reset(actionBar, matcher);
+			} else if (filterManaUse && matcher.usePattern(MANA_USE).lookingAt()) {
+				actionBar = reset(actionBar, matcher);
+			}
+			if (matcher.usePattern(MANA_STATUS).find()) {
+				updateMana(matcher);
+				matcher.appendReplacement(sb, "");
+			}
+		}
+		matcher.appendTail(sb);
+		String res = sb.toString().trim();
+		return res.isEmpty() ? null : res;
+	}
+
+	public record Resource(int value, int max, int overflow) {}
 }
--- src/main/java/de/hysky/skyblocker/utils/RegexUtils.java
+++ src/main/java/de/hysky/skyblocker/utils/RegexUtils.java
@@ -9,14 +9,14 @@ public class RegexUtils {
 	/**
 	 * @return An OptionalLong of the first group in the matcher, or an empty OptionalLong if the matcher doesn't find anything.
 	 */
-	public static OptionalLong getLongFromMatcher(Matcher matcher) {
-		return getLongFromMatcher(matcher, matcher.hasMatch() ? matcher.end() : 0);
+	public static OptionalLong findLongFromMatcher(Matcher matcher) {
+		return findLongFromMatcher(matcher, matcher.hasMatch() ? matcher.end() : 0);
 	}
 
 	/**
 	 * @return An OptionalLong of the first group in the matcher, or an empty OptionalLong if the matcher doesn't find anything.
 	 */
-	public static OptionalLong getLongFromMatcher(Matcher matcher, int startingIndex) {
+	public static OptionalLong findLongFromMatcher(Matcher matcher, int startingIndex) {
 		if (!matcher.find(startingIndex)) return OptionalLong.empty();
 		return OptionalLong.of(Long.parseLong(matcher.group(1).replace(",", "")));
 	}
@@ -24,31 +24,39 @@ public class RegexUtils {
 	/**
 	 * @return An OptionalInt of the first group in the matcher, or an empty OptionalInt if the matcher doesn't find anything.
 	 */
-	public static OptionalInt getIntFromMatcher(Matcher matcher) {
-		return getIntFromMatcher(matcher, matcher.hasMatch() ? matcher.end() : 0);
+	public static OptionalInt findIntFromMatcher(Matcher matcher) {
+		return findIntFromMatcher(matcher, matcher.hasMatch() ? matcher.end() : 0);
 	}
 
 	/**
 	 * @return An OptionalInt of the first group in the matcher, or an empty OptionalInt if the matcher doesn't find anything.
 	 */
-	public static OptionalInt getIntFromMatcher(Matcher matcher, int startingIndex) {
+	public static OptionalInt findIntFromMatcher(Matcher matcher, int startingIndex) {
 		if (!matcher.find(startingIndex)) return OptionalInt.empty();
-		return OptionalInt.of(Integer.parseInt(matcher.group(1).replace(",", "")));
+		return OptionalInt.of(parseIntFromMatcher(matcher, 1));
+	}
+
+	public static int parseIntFromMatcher(Matcher matcher, int group) {
+		return Integer.parseInt(matcher.group(group).replace(",", ""));
+	}
+
+	public static int parseIntFromMatcher(Matcher matcher, String group) {
+		return Integer.parseInt(matcher.group(group).replace(",", ""));
 	}
 
 	/**
 	 * @return An OptionalDouble of the first group in the matcher, or an empty OptionalDouble if the matcher doesn't find anything.
 	 * @implNote Assumes the decimal separator is `.`
 	 */
-	public static OptionalDouble getDoubleFromMatcher(Matcher matcher) {
-		return getDoubleFromMatcher(matcher, matcher.hasMatch() ? matcher.end() : 0);
+	public static OptionalDouble findDoubleFromMatcher(Matcher matcher) {
+		return findDoubleFromMatcher(matcher, matcher.hasMatch() ? matcher.end() : 0);
 	}
 
 	/**
 	 * @return An OptionalDouble of the first group in the matcher, or an empty OptionalDouble if the matcher doesn't find anything.
 	 * @implNote Assumes the decimal separator is `.`
 	 */
-	public static OptionalDouble getDoubleFromMatcher(Matcher matcher, int startingIndex) {
+	public static OptionalDouble findDoubleFromMatcher(Matcher matcher, int startingIndex) {
 		if (!matcher.find(startingIndex)) return OptionalDouble.empty();
 		return OptionalDouble.of(Double.parseDouble(matcher.group(1).replace(",", "")));
 	}
