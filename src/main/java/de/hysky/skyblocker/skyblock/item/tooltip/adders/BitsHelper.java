a5f47de4b84756f975517f6a2c73acfc97e367e0
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2024-09-26 11:07:19 -0400

    Clean up fields

--- src/main/java/de/hysky/skyblocker/skyblock/item/tooltip/adders/BitsHelper.java
+++ src/main/java/de/hysky/skyblocker/skyblock/item/tooltip/adders/BitsHelper.java
@@ -36,6 +36,108 @@ public class BitsHelper extends SimpleContainerSolver implements TooltipAdder {
 	private static final NumberFormat DECIMAL_FORMAT = NumberFormat.getInstance(Locale.US);
 	private static final String LOGS_PREFIX = "[Skyblocker Bits Helper] ";
 
+	//region Constants
+	/**
+	 * List of items that sell well. No, it is not automated, but that stuff is unlikely to change unless some update
+	 * Definition of "good selling item": 300+ daily sales, 12+ hourly sales
+	 * I would personally also demand good price per item as filling up all ah slots with catalyst would suck
+	 * But newer players would find a good use of it so ima keep my preferences away
+	 * Actual on 14.08.2024
+	 */
+	private static final List<String> GOOD_SELLING_ITEMS = List.of(
+			"KAT_FLOWER",
+			"KAT_BOUQUET",
+			"HEAT_CORE",
+			"ULTIMATE_CARROT_CANDY_UPGRADE",
+			"TALISMAN_ENRICHMENT_SWAPPER",
+			"GOD_POTION_2",
+			"KISMET_FEATHER",
+			"MATRIARCH_PARFUM",
+			"AUTOPET_RULES_2"   // it barely made it here
+	);
+
+	/**
+	 * SKYBLOCK_ID, Bits Price
+	 * Yes I just hardcoded it. Why work smart when you can work hard?
+	 * Those are only from submenus (categories) as normal item is easy to parse
+	 * It probably could've been extracted from neu repo or something
+	 * Actual on 14.08.2024
+	 */
+	private static final Object2IntMap<String> CAT_KAT = Util.make(new Object2IntOpenHashMap<>(), map -> {
+		map.put("KAT_FLOWER", 500);
+		map.put("KAT_BOUQUET", 2500);
+	});
+
+	private static final Object2IntMap<String> CAT_UPGRADE_COMPONENTS = Util.make(new Object2IntOpenHashMap<>(), map -> {
+		map.put("HEAT_CORE", 3000);
+		map.put("HYPER_CATALYST_UPGRADE", 300);
+		map.put("ULTIMATE_CARROT_CANDY_UPGRADE", 8000);
+		map.put("COLOSSAL_EXP_BOTTLE_UPGRADE", 1200);
+		map.put("JUMBO_BACKPACK_UPGRADE", 4000);
+		map.put("MINION_STORAGE_EXPANDER", 1500);
+	});
+
+	private static final Object2IntMap<String> CAT_SACKS = Util.make(new Object2IntOpenHashMap<>(), map -> {
+		map.put("POCKET_SACK_IN_A_SACK", 8000);
+		map.put("LARGE_DUNGEON_SACK", 14000);
+		map.put("RUNE_SACK", 14000);
+		map.put("FLOWER_SACK", 14000);
+		map.put("DWARVEN_MINES_SACK", 14000);
+		map.put("CRYSTAL_HOLLOWS_SACK", 14000);
+	});
+
+	private static final Object2IntMap<String> CAT_ABIPHONE = Util.make(new Object2IntOpenHashMap<>(), map -> {
+		map.put("TRIO_CONTACTS_ADDON", 6450);
+		map.put("ABICASE_SUMSUNG_1", 15000);
+		map.put("ABICASE_SUMSUNG_2", 25000);
+		map.put("ABICASE_REZAR", 26000);
+		map.put("ABICASE_BLUE_AQUA", 17000);
+		map.put("ABICASE_BLUE_BLUE", 17000);
+		map.put("ABICASE_BLUE_GREEN", 17000);
+		map.put("ABICASE_BLUE_RED", 17000);
+		map.put("ABICASE_BLUE_YELLOW", 17000);
+	});
+
+	private static final Object2IntMap<String> CAT_DYES = Util.make(new Object2IntOpenHashMap<>(), map -> {
+		map.put("DYE_PURE_WHITE", 250000);
+		map.put("DYE_PURE_BLACK", 250000);
+	});
+
+	private static final Object2IntMap<String> CAT_ENCHANTS = Util.make(new Object2IntOpenHashMap<>(), map -> {
+		map.put("ENCHANTMENT_HECATOMB_1", 6000);
+		map.put("ENCHANTMENT_EXPERTISE_1", 4000);
+		map.put("ENCHANTMENT_COMPACT_1", 4000);
+		map.put("ENCHANTMENT_CULTIVATING_1", 4000);
+		map.put("ENCHANTMENT_CHAMPION_1", 4000);
+		map.put("ENCHANTMENT_TOXOPHILITE_1", 4000);
+	});
+
+	private static final Object2IntMap<String> CAT_ENRICHMENTS = Util.make(new Object2IntOpenHashMap<>(), map -> {
+		map.put("TALISMAN_ENRICHMENT_SWAPPER", 200);
+		map.put("TALISMAN_ENRICHMENT_WALK_SPEED", 5000);
+		map.put("TALISMAN_ENRICHMENT_INTELLIGENCE", 5000);
+		map.put("TALISMAN_ENRICHMENT_CRITICAL_DAMAGE", 5000);
+		map.put("TALISMAN_ENRICHMENT_CRITICAL_CHANCE", 5000);
+		map.put("TALISMAN_ENRICHMENT_STRENGTH", 5000);
+		map.put("TALISMAN_ENRICHMENT_DEFENSE", 5000);
+		map.put("TALISMAN_ENRICHMENT_HEALTH", 5000);
+		map.put("TALISMAN_ENRICHMENT_MAGIC_FIND", 5000);
+		map.put("TALISMAN_ENRICHMENT_FEROCITY", 5000);
+		map.put("TALISMAN_ENRICHMENT_SEA_CREATURE_CHANCE", 5000);
+		map.put("TALISMAN_ENRICHMENT_ATTACK_SPEED", 5000);
+	});
+
+	private static final Map<String, Map<String, Integer>> CATEGORIES = Util.make(new HashMap<>(), map -> {
+		map.put("Kat Items", CAT_KAT);
+		map.put("Upgrade Components", CAT_UPGRADE_COMPONENTS);
+		map.put("Sacks", CAT_SACKS);
+		map.put("Abiphone Supershop", CAT_ABIPHONE);
+		map.put("Dyes", CAT_DYES);
+		map.put("Stacking Enchants", CAT_ENCHANTS);
+		map.put("Enrichments", CAT_ENRICHMENTS);
+	});
+	//endregion
+
 	public static final BitsHelper INSTANCE = new BitsHelper();
 
 	private BitsHelper() {
@@ -97,7 +199,6 @@ public class BitsHelper extends SimpleContainerSolver implements TooltipAdder {
 			innerMapGreen = categoryOutput.get(outerKey + "GREEN");
 
 			if (innerMap != null) {
-				//                LOGGER.info(LOGS_PREFIX + "innerMap STATE: {}, {}", innerMap.left(), innerMap.rightLong());	// debug
 				if (isGreen) {  // this is here so green highlighted category won't show yellow stuff
 					itemID = innerMapGreen.left();
 					coinsPerBit = innerMapGreen.rightLong();
@@ -107,7 +208,6 @@ public class BitsHelper extends SimpleContainerSolver implements TooltipAdder {
 				}
 			}
 		}
-		//        LOGGER.info(LOGS_PREFIX + "itemID and coinsPerBit: {}, {}", itemID, coinsPerBit);	// debug
 		ItemStack foundItemStack = ItemRepository.getItemStack(itemID);
 		if (itemID.isEmpty()) {   // a bit dirty, but basically if itemID is empty then it is normal item and NOT category
 			lines.add(Text.empty()
@@ -184,12 +284,11 @@ public class BitsHelper extends SimpleContainerSolver implements TooltipAdder {
 			if (CATEGORY_PATTERN.matcher(ItemUtils.concatenateLore(ItemUtils.getLore(stack))).find()) {
 				String catName = stack.getName().getString();
 				ObjectLongImmutablePair<String> categoryResults = calculateBestInCategory(stack);
-				//                LOGGER.info(LOGS_PREFIX + "TRIGGERED OUTER CODE FOR PROCESSING: {}", catName);	// debug
 
 				String itemID = categoryResults.left();
 				long coinsPerBit = categoryResults.rightLong();
 
-				if (sellingItems.contains(itemID) && (coinsPerBit > bestCoinsPerBitSelling)) {
+				if (GOOD_SELLING_ITEMS.contains(itemID) && (coinsPerBit > bestCoinsPerBitSelling)) {
 					bestCoinsPerBitSelling = coinsPerBit;
 					bestSlotIndexSelling = entry.getIntKey();
 				}
@@ -215,7 +314,7 @@ public class BitsHelper extends SimpleContainerSolver implements TooltipAdder {
 
 			long coinsPerBit = Math.round(itemCost / bitsCost);
 
-			if (sellingItems.contains(itemId) && (coinsPerBit > bestCoinsPerBitSelling)) {
+			if (GOOD_SELLING_ITEMS.contains(itemId) && (coinsPerBit > bestCoinsPerBitSelling)) {
 				bestCoinsPerBitSelling = coinsPerBit;
 				bestSlotIndexSelling = entry.getIntKey();
 			}
@@ -235,14 +334,12 @@ public class BitsHelper extends SimpleContainerSolver implements TooltipAdder {
 		categoryOutput.put(catName, ObjectLongImmutablePair.of("", 0L));
 		categoryOutput.put(catName + "GREEN", ObjectLongImmutablePair.of("", 0L));
 		Object2LongMap<String> categoryResults = processCategory(stack);
-		//        LOGGER.info(LOGS_PREFIX + "TRIGGERED INNER CODE FOR PROCESSING: {}", catName);	// debug
-		//        LOGGER.info(LOGS_PREFIX + "categoryResults: {}", categoryResults.toString());	// debug
 
 		for (Map.Entry<String, Long> categoryEntry : categoryResults.object2LongEntrySet()) {
 			String itemID = categoryEntry.getKey();
 			long coinsPerBit = categoryEntry.getValue();
 
-			if (sellingItems.contains(itemID) && (coinsPerBit > bestCoinsPerBitSelling)) {
+			if (GOOD_SELLING_ITEMS.contains(itemID) && (coinsPerBit > bestCoinsPerBitSelling)) {
 				categoryOutput.put(catName + "GREEN", ObjectLongImmutablePair.of(itemID, coinsPerBit));
 				bestCoinsPerBitSelling = coinsPerBit;
 			}
@@ -252,18 +349,15 @@ public class BitsHelper extends SimpleContainerSolver implements TooltipAdder {
 				bestCoinsPerBitAll = coinsPerBit;
 			}
 		}
-		//        LOGGER.info(LOGS_PREFIX + "CURRENT STATE OF categoryOutput: {}, {}", categoryOutput.get(catName).left(), categoryOutput.get(catName).rightLong());	// debug
 		return categoryOutput.get(catName);
 	}
 
 	private Object2LongMap<String> processCategory(ItemStack stack) {
 		String categoryName = stack.getName().getString();
-		//      LOGGER.info(LOGS_PREFIX + "Detected category name: {}", categoryName);	// debug
 
-		if (categories.containsKey(categoryName)) {
-			//          LOGGER.info(LOGS_PREFIX + "Key matched for: {}", categoryName);	// debug
+		if (CATEGORIES.containsKey(categoryName)) {
 			Object2LongMap<String> results = new Object2LongOpenHashMap<>();
-			Map<String, Integer> category = categories.get(categoryName);
+			Map<String, Integer> category = CATEGORIES.get(categoryName);
 
 			for (Map.Entry<String, Integer> entry : category.entrySet()) {
 				String itemID = entry.getKey();
@@ -274,7 +368,6 @@ public class BitsHelper extends SimpleContainerSolver implements TooltipAdder {
 			}
 			return results;
 		} else if (categoryName.contains("Fuel Blocks")) {
-			//          LOGGER.info(LOGS_PREFIX + "Fuel Blocks code triggered"); // so that thing gets cheaper if player has blaze slayer 9
 			String itemID = "INFERNO_FUEL_BLOCK";    // but I don't know if only 1x offer of 64x offer gets discount too
 			int[] itemBitsPrice = {75, 3600};   // if only 1x gets discount then it doesn't matter as x64 would be ALWAYS better even with it
 			double itemCost = ItemUtils.getItemPrice(itemID).keyDouble();   // TLDR: need blaze slayer 9 players to show their prices
@@ -287,107 +380,6 @@ public class BitsHelper extends SimpleContainerSolver implements TooltipAdder {
 		}
 		return new Object2LongOpenHashMap<>();
 	}
-
-	/**
-	 * List of items that sell well. No, it is not automated, but that stuff is unlikely to change unless some update
-	 * Definition of "good selling item": 300+ daily sales, 12+ hourly sales
-	 * I would personally also demand good price per item as filling up all ah slots with catalyst would suck
-	 * But newer players would find a good use of it so ima keep my preferences away
-	 * Actual on 14.08.2024
-	 */
-	private final List<String> sellingItems = Arrays.asList(
-			"KAT_FLOWER",
-			"KAT_BOUQUET",
-			"HEAT_CORE",
-			"ULTIMATE_CARROT_CANDY_UPGRADE",
-			"TALISMAN_ENRICHMENT_SWAPPER",
-			"GOD_POTION_2",
-			"KISMET_FEATHER",
-			"MATRIARCH_PARFUM",
-			"AUTOPET_RULES_2"   // it barely made it here
-	);
-
-	/**
-	 * SKYBLOCK_ID, Bits Price
-	 * Yes I just hardcoded it. Why work smart when you can work hard?
-	 * Those are only from submenus (categories) as normal item is easy to parse
-	 * It probably could've been extracted from neu repo or something
-	 * Actual on 14.08.2024
-	 */
-	private final Object2IntMap<String> CAT_KAT = Util.make(new Object2IntOpenHashMap<>(), map -> {
-		map.put("KAT_FLOWER", 500);
-		map.put("KAT_BOUQUET", 2500);
-	});
-
-	private final Object2IntMap<String> CAT_UPGRADE_COMPONENTS = Util.make(new Object2IntOpenHashMap<>(), map -> {
-		map.put("HEAT_CORE", 3000);
-		map.put("HYPER_CATALYST_UPGRADE", 300);
-		map.put("ULTIMATE_CARROT_CANDY_UPGRADE", 8000);
-		map.put("COLOSSAL_EXP_BOTTLE_UPGRADE", 1200);
-		map.put("JUMBO_BACKPACK_UPGRADE", 4000);
-		map.put("MINION_STORAGE_EXPANDER", 1500);
-	});
-
-	private final Object2IntMap<String> CAT_SACKS = Util.make(new Object2IntOpenHashMap<>(), map -> {
-		map.put("POCKET_SACK_IN_A_SACK", 8000);
-		map.put("LARGE_DUNGEON_SACK", 14000);
-		map.put("RUNE_SACK", 14000);
-		map.put("FLOWER_SACK", 14000);
-		map.put("DWARVEN_MINES_SACK", 14000);
-		map.put("CRYSTAL_HOLLOWS_SACK", 14000);
-	});
-
-	private final Object2IntMap<String> CAT_ABIPHONE = Util.make(new Object2IntOpenHashMap<>(), map -> {
-		map.put("TRIO_CONTACTS_ADDON", 6450);
-		map.put("ABICASE_SUMSUNG_1", 15000);
-		map.put("ABICASE_SUMSUNG_2", 25000);
-		map.put("ABICASE_REZAR", 26000);
-		map.put("ABICASE_BLUE_AQUA", 17000);
-		map.put("ABICASE_BLUE_BLUE", 17000);
-		map.put("ABICASE_BLUE_GREEN", 17000);
-		map.put("ABICASE_BLUE_RED", 17000);
-		map.put("ABICASE_BLUE_YELLOW", 17000);
-	});
-
-	private final Object2IntMap<String> CAT_DYES = Util.make(new Object2IntOpenHashMap<>(), map -> {
-		map.put("DYE_PURE_WHITE", 250000);
-		map.put("DYE_PURE_BLACK", 250000);
-	});
-
-	private final Object2IntMap<String> CAT_ENCHANTS = Util.make(new Object2IntOpenHashMap<>(), map -> {
-		map.put("ENCHANTMENT_HECATOMB_1", 6000);
-		map.put("ENCHANTMENT_EXPERTISE_1", 4000);
-		map.put("ENCHANTMENT_COMPACT_1", 4000);
-		map.put("ENCHANTMENT_CULTIVATING_1", 4000);
-		map.put("ENCHANTMENT_CHAMPION_1", 4000);
-		map.put("ENCHANTMENT_TOXOPHILITE_1", 4000);
-	});
-
-	private final Object2IntMap<String> CAT_ENRICHMENTS = Util.make(new Object2IntOpenHashMap<>(), map -> {
-		map.put("TALISMAN_ENRICHMENT_SWAPPER", 200);
-		map.put("TALISMAN_ENRICHMENT_WALK_SPEED", 5000);
-		map.put("TALISMAN_ENRICHMENT_INTELLIGENCE", 5000);
-		map.put("TALISMAN_ENRICHMENT_CRITICAL_DAMAGE", 5000);
-		map.put("TALISMAN_ENRICHMENT_CRITICAL_CHANCE", 5000);
-		map.put("TALISMAN_ENRICHMENT_STRENGTH", 5000);
-		map.put("TALISMAN_ENRICHMENT_DEFENSE", 5000);
-		map.put("TALISMAN_ENRICHMENT_HEALTH", 5000);
-		map.put("TALISMAN_ENRICHMENT_MAGIC_FIND", 5000);
-		map.put("TALISMAN_ENRICHMENT_FEROCITY", 5000);
-		map.put("TALISMAN_ENRICHMENT_SEA_CREATURE_CHANCE", 5000);
-		map.put("TALISMAN_ENRICHMENT_ATTACK_SPEED", 5000);
-	});
-
-
-	private final Map<String, Map<String, Integer>> categories = Util.make(new HashMap<>(), map -> {
-		map.put("Kat Items", CAT_KAT);
-		map.put("Upgrade Components", CAT_UPGRADE_COMPONENTS);
-		map.put("Sacks", CAT_SACKS);
-		map.put("Abiphone Supershop", CAT_ABIPHONE);
-		map.put("Dyes", CAT_DYES);
-		map.put("Stacking Enchants", CAT_ENCHANTS);
-		map.put("Enrichments", CAT_ENRICHMENTS);
-	});
 
 	private record BestItemsResult(
 			int bestSlotIndexSelling,
