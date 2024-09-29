ea2840572e340f14a903c9558a5983ae6bd7502f
olim <bobq4582@gmail.com>
2024-09-29 19:55:29 +0100

    add check to see if smoothing is enabled

--- src/main/java/de/hysky/skyblocker/mixins/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/de/hysky/skyblocker/mixins/ClientPlayNetworkHandlerMixin.java
@@ -2,11 +2,10 @@ package de.hysky.skyblocker.mixins;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import de.hysky.skyblocker.config.SkyblockerConfigManager;
 import de.hysky.skyblocker.config.configs.SlayersConfig;
+import de.hysky.skyblocker.config.configs.UIAndVisualsConfig;
 import de.hysky.skyblocker.skyblock.CompactDamage;
 import de.hysky.skyblocker.skyblock.FishingHelper;
 import de.hysky.skyblocker.skyblock.SmoothAOTE;
@@ -24,7 +23,6 @@ import de.hysky.skyblocker.skyblock.slayers.SlayerEntitiesGlow;
 import de.hysky.skyblocker.skyblock.waypoint.MythologicalRitual;
 import de.hysky.skyblocker.utils.Utils;
 import net.minecraft.block.Blocks;
-import net.minecraft.client.gui.hud.DebugHud;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
@@ -47,9 +45,9 @@ public abstract class ClientPlayNetworkHandlerMixin {
 	@Shadow
 	private ClientWorld world;
 
-    @Shadow
-    @Final
-    private static Logger LOGGER;
+	@Shadow
+	@Final
+	private static Logger LOGGER;
 
     @Inject(method = "method_64896", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;removeEntity(ILnet/minecraft/entity/Entity$RemovalReason;)V"))
     private void skyblocker$onItemDestroy(int entityId, CallbackInfo ci) {
@@ -58,105 +56,106 @@ public abstract class ClientPlayNetworkHandlerMixin {
         }
     }
 
-    @ModifyVariable(method = "onItemPickupAnimation", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;removeEntity(ILnet/minecraft/entity/Entity$RemovalReason;)V", ordinal = 0))
-    private ItemEntity skyblocker$onItemPickup(ItemEntity itemEntity) {
-        DungeonManager.onItemPickup(itemEntity);
-        return itemEntity;
-    }
+	@ModifyVariable(method = "onItemPickupAnimation", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;removeEntity(ILnet/minecraft/entity/Entity$RemovalReason;)V", ordinal = 0))
+	private ItemEntity skyblocker$onItemPickup(ItemEntity itemEntity) {
+		DungeonManager.onItemPickup(itemEntity);
+		return itemEntity;
+	}
 
-    @WrapWithCondition(method = "onEntityPassengersSet", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;)V", remap = false))
-    private boolean skyblocker$cancelEntityPassengersWarning(Logger instance, String msg) {
-        return !Utils.isOnHypixel();
-    }
+	@WrapWithCondition(method = "onEntityPassengersSet", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;)V", remap = false))
+	private boolean skyblocker$cancelEntityPassengersWarning(Logger instance, String msg) {
+		return !Utils.isOnHypixel();
+	}
 
-    @WrapWithCondition(method = "onPlayerList", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V", remap = false))
-    private boolean skyblocker$cancelPlayerListWarning(Logger instance, String format, Object arg1, Object arg2) {
-        return !Utils.isOnHypixel();
-    }
+	@WrapWithCondition(method = "onPlayerList", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V", remap = false))
+	private boolean skyblocker$cancelPlayerListWarning(Logger instance, String format, Object arg1, Object arg2) {
+		return !Utils.isOnHypixel();
+	}
 
-    @Inject(method = "onPlaySound", at = @At("RETURN"))
-    private void skyblocker$onPlaySound(PlaySoundS2CPacket packet, CallbackInfo ci) {
-        FishingHelper.onSound(packet);
-        CrystalsChestHighlighter.onSound(packet);
-    }
+	@Inject(method = "onPlaySound", at = @At("RETURN"))
+	private void skyblocker$onPlaySound(PlaySoundS2CPacket packet, CallbackInfo ci) {
+		FishingHelper.onSound(packet);
+		CrystalsChestHighlighter.onSound(packet);
+	}
 
-    @WrapWithCondition(method = "warnOnUnknownPayload", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;)V", remap = false))
-    private boolean skyblocker$dropBadlionPacketWarnings(Logger instance, String message, Object identifier) {
-        return !(Utils.isOnHypixel() && ((Identifier) identifier).getNamespace().equals("badlion"));
-    }
+	@WrapWithCondition(method = "warnOnUnknownPayload", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;)V", remap = false))
+	private boolean skyblocker$dropBadlionPacketWarnings(Logger instance, String message, Object identifier) {
+		return !(Utils.isOnHypixel() && ((Identifier) identifier).getNamespace().equals("badlion"));
+	}
 
-    @WrapWithCondition(method = {"onScoreboardScoreUpdate", "onScoreboardScoreReset"}, at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;)V", remap = false))
-    private boolean skyblocker$cancelUnknownScoreboardObjectiveWarnings(Logger instance, String message, Object objectiveName) {
-        return !Utils.isOnHypixel();
-    }
+	@WrapWithCondition(method = {"onScoreboardScoreUpdate", "onScoreboardScoreReset"}, at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;)V", remap = false))
+	private boolean skyblocker$cancelUnknownScoreboardObjectiveWarnings(Logger instance, String message, Object objectiveName) {
+		return !Utils.isOnHypixel();
+	}
 
-    @WrapWithCondition(method = "onTeam", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;[Ljava/lang/Object;)V", remap = false))
-    private boolean skyblocker$cancelTeamWarning(Logger instance, String format, Object... arg) {
-        return !Utils.isOnHypixel();
-    }
+	@WrapWithCondition(method = "onTeam", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;[Ljava/lang/Object;)V", remap = false))
+	private boolean skyblocker$cancelTeamWarning(Logger instance, String format, Object... arg) {
+		return !Utils.isOnHypixel();
+	}
 
-    @Inject(method = "onParticle", at = @At("RETURN"))
-    private void skyblocker$onParticle(ParticleS2CPacket packet, CallbackInfo ci) {
-        MythologicalRitual.onParticle(packet);
-        DojoManager.onParticle(packet);
-        CrystalsChestHighlighter.onParticle(packet);
-        EnderNodes.onParticle(packet);
-        WishingCompassSolver.onParticle(packet);
-    }
+	@Inject(method = "onParticle", at = @At("RETURN"))
+	private void skyblocker$onParticle(ParticleS2CPacket packet, CallbackInfo ci) {
+		MythologicalRitual.onParticle(packet);
+		DojoManager.onParticle(packet);
+		CrystalsChestHighlighter.onParticle(packet);
+		EnderNodes.onParticle(packet);
+		WishingCompassSolver.onParticle(packet);
+	}
 
-    @ModifyExpressionValue(method = "onEntityStatus", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/packet/s2c/play/EntityStatusS2CPacket;getEntity(Lnet/minecraft/world/World;)Lnet/minecraft/entity/Entity;"))
-    private Entity skyblocker$onEntityDeath(Entity entity, @Local(argsOnly = true) EntityStatusS2CPacket packet) {
-        if (packet.getStatus() == EntityStatuses.PLAY_DEATH_SOUND_OR_ADD_PROJECTILE_HIT_PARTICLES) {
-            DungeonScore.handleEntityDeath(entity);
-            TheEnd.onEntityDeath(entity);
-            SlayerEntitiesGlow.onEntityDeath(entity);
-        }
-        return entity;
-    }
+	@ModifyExpressionValue(method = "onEntityStatus", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/packet/s2c/play/EntityStatusS2CPacket;getEntity(Lnet/minecraft/world/World;)Lnet/minecraft/entity/Entity;"))
+	private Entity skyblocker$onEntityDeath(Entity entity, @Local(argsOnly = true) EntityStatusS2CPacket packet) {
+		if (packet.getStatus() == EntityStatuses.PLAY_DEATH_SOUND_OR_ADD_PROJECTILE_HIT_PARTICLES) {
+			DungeonScore.handleEntityDeath(entity);
+			TheEnd.onEntityDeath(entity);
+			SlayerEntitiesGlow.onEntityDeath(entity);
+		}
+		return entity;
+	}
 
-    @Inject(method = "onEntityTrackerUpdate", at = @At("TAIL"))
-    private void skyblocker$onEntityTrackerUpdate(EntityTrackerUpdateS2CPacket packet, CallbackInfo ci, @Local Entity entity) {
-        if (!(entity instanceof ArmorStandEntity armorStandEntity)) return;
+	@Inject(method = "onEntityTrackerUpdate", at = @At("TAIL"))
+	private void skyblocker$onEntityTrackerUpdate(EntityTrackerUpdateS2CPacket packet, CallbackInfo ci, @Local Entity entity) {
+		if (!(entity instanceof ArmorStandEntity armorStandEntity)) return;
 
-        if (SkyblockerConfigManager.get().slayers.highlightMinis == SlayersConfig.HighlightSlayerEntities.GLOW && SlayerEntitiesGlow.isSlayerMiniMob(armorStandEntity)
-                || SkyblockerConfigManager.get().slayers.highlightBosses == SlayersConfig.HighlightSlayerEntities.GLOW && SlayerEntitiesGlow.isSlayer(armorStandEntity)) {
-            if (armorStandEntity.isDead()) {
-                SlayerEntitiesGlow.cleanupArmorstand(armorStandEntity);
-            } else {
-                SlayerEntitiesGlow.setSlayerMobGlow(armorStandEntity);
-            }
-        }
+		if (SkyblockerConfigManager.get().slayers.highlightMinis == SlayersConfig.HighlightSlayerEntities.GLOW && SlayerEntitiesGlow.isSlayerMiniMob(armorStandEntity)
+				|| SkyblockerConfigManager.get().slayers.highlightBosses == SlayersConfig.HighlightSlayerEntities.GLOW && SlayerEntitiesGlow.isSlayer(armorStandEntity)) {
+			if (armorStandEntity.isDead()) {
+				SlayerEntitiesGlow.cleanupArmorstand(armorStandEntity);
+			} else {
+				SlayerEntitiesGlow.setSlayerMobGlow(armorStandEntity);
+			}
+		}
 
-        if (SkyblockerConfigManager.get().slayers.blazeSlayer.firePillarCountdown != SlayersConfig.BlazeSlayer.FirePillar.OFF)
-            FirePillarAnnouncer.checkFirePillar(entity);
+		if (SkyblockerConfigManager.get().slayers.blazeSlayer.firePillarCountdown != SlayersConfig.BlazeSlayer.FirePillar.OFF)
+			FirePillarAnnouncer.checkFirePillar(entity);
 
-        EggFinder.checkIfEgg(armorStandEntity);
-        try { //Prevent packet handling fails if something goes wrong so that entity trackers still update, just without compact damage numbers
-            CompactDamage.compactDamage(armorStandEntity);
-        } catch (Exception e) {
-            LOGGER.error("[Skyblocker Compact Damage] Failed to compact damage number", e);
-        }
-    }
+		EggFinder.checkIfEgg(armorStandEntity);
+		try { //Prevent packet handling fails if something goes wrong so that entity trackers still update, just without compact damage numbers
+			CompactDamage.compactDamage(armorStandEntity);
+		} catch (Exception e) {
+			LOGGER.error("[Skyblocker Compact Damage] Failed to compact damage number", e);
+		}
+	}
 
-    @Inject(method = "onEntityEquipmentUpdate", at = @At(value = "TAIL"))
-    private void skyblocker$onEntityEquip(EntityEquipmentUpdateS2CPacket packet, CallbackInfo ci, @Local Entity entity) {
-        EggFinder.checkIfEgg(entity);
-    }
+	@Inject(method = "onEntityEquipmentUpdate", at = @At(value = "TAIL"))
+	private void skyblocker$onEntityEquip(EntityEquipmentUpdateS2CPacket packet, CallbackInfo ci, @Local Entity entity) {
+		EggFinder.checkIfEgg(entity);
+	}
 
-    @Inject(method = "onPlayerPositionLook", at = @At("TAIL"))
-    private void onPlayerTeleported(PlayerPositionLookS2CPacket packet, CallbackInfo ci) {
-        //player has been teleported by the server tell the smooth AOTE this
-        SmoothAOTE.playerTeleported();
-    }
+	@Inject(method = "onPlayerPositionLook", at = @At("TAIL"))
+	private void onPlayerTeleported(PlayerPositionLookS2CPacket packet, CallbackInfo ci) {
+		//player has been teleported by the server tell the smooth AOTE this
+		SmoothAOTE.playerTeleported();
+	}
 
-    @ModifyExpressionValue(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/DebugHud;shouldShowPacketSizeAndPingCharts()Z"))
-    private boolean shouldShowPacketSizeAndPingCharts(boolean original) {
-        //make the f3+3 screen always send ping packets even when closed
-        //this is needed to make smooth AOTE work
-        if (Utils.isOnSkyblock()) {
-            return true;
-        }
-        return original;
+	@ModifyExpressionValue(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/DebugHud;shouldShowPacketSizeAndPingCharts()Z"))
+	private boolean shouldShowPacketSizeAndPingCharts(boolean original) {
+		//make the f3+3 screen always send ping packets even when closed
+		//this is needed to make smooth AOTE work so check if its enabled
+		UIAndVisualsConfig.SmoothAOTE options = SkyblockerConfigManager.get().uiAndVisuals.smoothAOTE;
+		if (Utils.isOnSkyblock() && !SmoothAOTE.teleportDisabled && (options.enableWeirdTransmission || options.enableEtherTransmission || options.enableInstantTransmission || options.enableSinrecallTransmission || options.enableWitherImpact)) {
+			return true;
+		}
+		return original;
 
-    }
+	}
 }
