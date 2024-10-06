38d258fea0325f0c46e1ef99ef1b93c98f5a49af
viciscat <51047087+viciscat@users.noreply.github.com>
2024-10-06 20:47:52 +0200

    improve method name

--- src/main/java/de/hysky/skyblocker/mixins/ClientPlayerInteractionManagerMixin.java
+++ src/main/java/de/hysky/skyblocker/mixins/ClientPlayerInteractionManagerMixin.java
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ClientPlayerInteractionManagerMixin {
 
 	@Inject(method = "clickSlot", at = @At("HEAD"), cancellable = true)
-	private void beans(int syncId, int slotId, int button, SlotActionType actionType, PlayerEntity player, CallbackInfo ci) {
+	private void kickPreventer9000Version2GimmeGameBreaker(int syncId, int slotId, int button, SlotActionType actionType, PlayerEntity player, CallbackInfo ci) {
 		if (Utils.isOnSkyblock() && slotId == -1) {
 			MinecraftClient.getInstance().getToastManager().add(new SystemToast(SystemToast.Type.PERIODIC_NOTIFICATION, Text.literal("Slot -1 click prevented!"), Text.literal("Hypixel would have probably kicked you for that! Can you believe it?")));
 			ci.cancel();
