072a4463fa28bc8e67ee97bd54fad411b0d5706c
GatienDoesStuff <GatienDoesStuff@users.noreply.github.com>
2024-10-15 23:02:23 +0200

    Missing import

--- src/main/java/de/hysky/skyblocker/skyblock/dungeon/GoldorWaypointsManager.java
+++ src/main/java/de/hysky/skyblocker/skyblock/dungeon/GoldorWaypointsManager.java
@@ -30,6 +30,7 @@ import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
 import java.io.BufferedReader;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
