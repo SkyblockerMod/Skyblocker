9b0f50232080853c6584b738d8aec89a676840bd
Rime <81419447+Emirlol@users.noreply.github.com>
2024-10-21 12:49:50 +0300

    Fix NewYearCakeAdder and change its color as well

--- src/main/java/de/hysky/skyblocker/skyblock/item/slottext/adders/NewYearCakeAdder.java
+++ src/main/java/de/hysky/skyblocker/skyblock/item/slottext/adders/NewYearCakeAdder.java
@@ -4,6 +4,7 @@ import de.hysky.skyblocker.skyblock.item.slottext.SimpleSlotTextAdder;
 import de.hysky.skyblocker.skyblock.item.slottext.SlotText;
 import de.hysky.skyblocker.utils.ItemUtils;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.text.Text;
 import org.jetbrains.annotations.NotNull;
@@ -14,9 +15,9 @@ import java.util.List;
 public class NewYearCakeAdder extends SimpleSlotTextAdder {
 	@Override
 	public @NotNull List<SlotText> getText(@Nullable Slot slot, @NotNull ItemStack stack, int slotId) {
-		if (stack.getSkyblockId().equals("NEW_YEAR_CAKE")) {
-			return SlotText.bottomLeftList(Text.literal(String.valueOf(ItemUtils.getCustomData(stack).getInt("new_years_cake"))).withColor(0xCBA6F7));
-		}
-		return List.of();
+		if (!stack.isOf(Items.CAKE)) return List.of();
+		int year = ItemUtils.getCustomData(stack).getInt("new_years_cake");
+		if (year <= 0) return List.of();
+		return SlotText.bottomLeftList(Text.literal(String.valueOf(year)).withColor(0x74c7ec));
 	}
 }
