6a1543f66d5e17ec38c71abeb52fbdc674be860c
Rime <81419447+Emirlol@users.noreply.github.com>
2024-10-21 07:02:20 +0300

    Add EvolvingItemAdder

--- src/main/java/de/hysky/skyblocker/skyblock/item/slottext/SlotTextManager.java
+++ src/main/java/de/hysky/skyblocker/skyblock/item/slottext/SlotTextManager.java
@@ -47,7 +47,8 @@ public class SlotTextManager {
 			new PowerStonesGuideAdder(),
 			new BazaarHelper(),
 			new StatsTuningAdder(),
-			ChocolateFactorySolver.INSTANCE
+			ChocolateFactorySolver.INSTANCE,
+			new EvolvingItemAdder()
 	};
 	private static final ArrayList<SlotTextAdder> currentScreenAdders = new ArrayList<>();
 	private static final KeyBinding keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.skyblocker.slottext", GLFW.GLFW_KEY_LEFT_ALT, "key.categories.skyblocker"));
