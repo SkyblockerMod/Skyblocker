e4add25b320925f6253dcc3072834e71696c5d3a
Rime <81419447+Emirlol@users.noreply.github.com>
2024-10-22 18:37:38 +0300

    Update EMI and change Inject to ModifyReceiver in FakeScreenMixin

--- src/main/java/de/hysky/skyblocker/mixins/emi/FakeScreenMixin.java
+++ src/main/java/de/hysky/skyblocker/mixins/emi/FakeScreenMixin.java
@@ -1,5 +1,7 @@
 package de.hysky.skyblocker.mixins.emi;
 
+import com.llamalad7.mixinextras.injector.ModifyReceiver;
+import com.llamalad7.mixinextras.sugar.Local;
 import de.hysky.skyblocker.utils.render.gui.AlignedTooltipComponent;
 import dev.emi.emi.screen.FakeScreen;
 import net.minecraft.client.MinecraftClient;
@@ -10,22 +12,21 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.text.MutableText;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Stream;
 
-@Mixin(value = FakeScreen.class, remap = false)
+@Mixin(value = FakeScreen.class)
 public class FakeScreenMixin {
-	@Inject(method = "getTooltipComponentListFromItem", at = @At(target = "Ljava/util/stream/Stream;collect(Ljava/util/stream/Collector;)Ljava/lang/Object;", value = "INVOKE", shift = At.Shift.AFTER), require = 0, cancellable = true)
-	private void skyblocker$alignedTooltip(ItemStack stack, CallbackInfoReturnable<List<TooltipComponent>> cir) {
-		cir.setReturnValue(Screen.getTooltipFromItem(MinecraftClient.getInstance(), stack).stream().map(text -> {
+	@ModifyReceiver(method = "getTooltipComponentListFromItem", at = @At(target = "Ljava/util/stream/Stream;collect(Ljava/util/stream/Collector;)Ljava/lang/Object;", value = "INVOKE"), require = 0)
+	private <A> Stream<TooltipComponent> skyblocker$alignedTooltip(Stream<TooltipComponent> instance, Collector<? super TooltipComponent, A, List<TooltipComponent>> arCollector, @Local(argsOnly = true) ItemStack stack) {
+		return Screen.getTooltipFromItem(MinecraftClient.getInstance(), stack).stream().map(text -> {
 			if (text instanceof MutableText mutableText) {
 				MutableText firstOfChain = mutableText.getFirstOfChain();
 				if (firstOfChain != null) return new AlignedTooltipComponent(firstOfChain);
 				else if (mutableText.getAlignedText() != null) return new AlignedTooltipComponent(mutableText);
-				else return new OrderedTextTooltipComponent(mutableText.asOrderedText());
-			} else return new OrderedTextTooltipComponent(text.asOrderedText());
-		}).toList());
+			} return new OrderedTextTooltipComponent(text.asOrderedText());
+		});
 	}
 }
