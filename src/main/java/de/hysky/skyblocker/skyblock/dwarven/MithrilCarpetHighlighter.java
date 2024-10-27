e75deab7ac40f99dff6a7a218a6f1a210b88e6b0
Rime <81419447+Emirlol@users.noreply.github.com>
2024-10-28 00:08:23 +0300

    Make mithril carpet highlighter configurable

--- src/main/java/de/hysky/skyblocker/skyblock/dwarven/MithrilCarpetHighlighter.java
+++ src/main/java/de/hysky/skyblocker/skyblock/dwarven/MithrilCarpetHighlighter.java
@@ -1,14 +1,17 @@
 package de.hysky.skyblocker.skyblock.dwarven;
 
 import de.hysky.skyblocker.annotations.Init;
+import de.hysky.skyblocker.config.SkyblockerConfigManager;
 import de.hysky.skyblocker.events.SkyblockEvents;
 import de.hysky.skyblocker.utils.Boxes;
 import de.hysky.skyblocker.utils.Location;
+import de.hysky.skyblocker.utils.Resettable;
 import de.hysky.skyblocker.utils.Tickable;
 import de.hysky.skyblocker.utils.render.RenderHelper;
 import de.hysky.skyblocker.utils.render.Renderable;
 import it.unimi.dsi.fastutil.objects.ObjectArraySet;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import net.minecraft.block.BlockState;
@@ -18,52 +21,77 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 
-public final class MithrilCarpetHighlighter implements Renderable, Tickable {
+import java.awt.*;
+
+public final class MithrilCarpetHighlighter implements Renderable, Tickable, Resettable {
+	public static final MithrilCarpetHighlighter INSTANCE = new MithrilCarpetHighlighter();
+
 	private static final Vec3d CARPET_BOUNDING_BOX = Boxes.getLengthVec(CarpetBlock.SHAPE.getBoundingBox());
-	private static final float[] RED_COLOR_COMPONENTS = {1.0F, 0.0F, 0.0F};
-	private static final float BOX_ALPHA = 0.3F;
 	private static final int SEARCH_RADIUS = 15;
+	private static float[] colorComponents;
 	private static final int TICK_INTERVAL = 15;
 	private static int tickCounter = 0;
-	private static final MithrilCarpetHighlighter INSTANCE = new MithrilCarpetHighlighter();
 	private static final ObjectArraySet<BlockPos> CARPET_LOCATIONS = new ObjectArraySet<>();
 	private static boolean isLocationValid = false;
 
 	@Init
 	public static void init() {
+		INSTANCE.configCallback(SkyblockerConfigManager.get().mining.dwarvenMines.carpetHighlightColor);
 		WorldRenderEvents.AFTER_TRANSLUCENT.register(INSTANCE::render);
 		SkyblockEvents.LOCATION_CHANGE.register(INSTANCE::onLocationChange);
 		ClientTickEvents.END_CLIENT_TICK.register(INSTANCE::tick);
+		ClientPlayConnectionEvents.JOIN.register(INSTANCE);
 	}
 
 	@Override
 	public void render(WorldRenderContext context) {
-		if (!isLocationValid) return;
+		if (!isLocationValid || !SkyblockerConfigManager.get().mining.dwarvenMines.enableCarpetHighlighter) return;
 		for (BlockPos carpetLocation : CARPET_LOCATIONS) {
-			RenderHelper.renderFilled(context, carpetLocation, CARPET_BOUNDING_BOX, RED_COLOR_COMPONENTS, BOX_ALPHA, false);
+			RenderHelper.renderFilled(context, carpetLocation, CARPET_BOUNDING_BOX, colorComponents, colorComponents[3], false);
 		}
 	}
 
 	public void onLocationChange(Location location) {
 		isLocationValid = location == Location.DWARVEN_MINES;
-		CARPET_LOCATIONS.clear();
 	}
 
 	@Override
 	public void tick(MinecraftClient client) {
 		if (!isLocationValid || MinecraftClient.getInstance().world == null || MinecraftClient.getInstance().player == null) return;
-		if (tickCounter++ < TICK_INTERVAL) return; // Only search for carpets every 15 ticks
+		if (tickCounter++ < TICK_INTERVAL) return; // Only search for carpets every TICK_INTERVAL ticks
 		Iterable<BlockPos> iterable = BlockPos.iterateOutwards(MinecraftClient.getInstance().player.getBlockPos(), SEARCH_RADIUS, SEARCH_RADIUS, SEARCH_RADIUS);
 		for (BlockPos blockPos : iterable) {
+			//The iterator contains a BlockPos.Mutable that it changes the position of to iterate over blocks,
+			// so it has to be converted to an immutable BlockPos or the position will change based on the player's position && the search radius
 			if (checkForCarpet(blockPos)) CARPET_LOCATIONS.add(blockPos.toImmutable());
 		}
 		tickCounter = 0;
 	}
 
+	/**
+	 * @param blockPos The position to check for a carpet
+	 * @return Whether the block at the given position is a gray carpet with a sea lantern below it, which is how all mithril carpets are placed
+	 * @implNote <p>getBlockState is a heavy method, so this method will become a hot spot as the search radius increases || the tick interval decreases.</p>
+	 * 	         <p>Consider profiling this method if either of those values are changed.</p>
+	 */
 	private boolean checkForCarpet(BlockPos blockPos) {
 		@SuppressWarnings("DataFlowIssue") // Null check is already done in the run method
 		BlockState actualBlock = MinecraftClient.getInstance().world.getBlockState(blockPos);
 		BlockState blockBelow = MinecraftClient.getInstance().world.getBlockState(blockPos.down());
 		return actualBlock.isOf(Blocks.GRAY_CARPET) && blockBelow.isOf(Blocks.SEA_LANTERN);
+	}
+
+	/**
+	 * <p>Caches the color components from the given color for rendering to avoid recalculating them every frame.</p>
+	 * <p>Called by the {@link de.hysky.skyblocker.config.categories.MiningCategory MiningCategory} > carpetHighlightColor when the color is updated.</p>
+	 */
+	public void configCallback(Color color) {
+		colorComponents = color.getRGBComponents(null);
+	}
+
+	@Override
+	public void reset() {
+		isLocationValid = false;
+		CARPET_LOCATIONS.clear();
 	}
 }
