b43c951d1e1119dd0a17e409019f77fd4df41a20
Rime <81419447+Emirlol@users.noreply.github.com>
2024-11-01 13:25:03 +0300

    Fix TITLE related crash

--- src/main/java/de/hysky/skyblocker/skyblock/slayers/hud/SlayerHudWidget.java
+++ src/main/java/de/hysky/skyblocker/skyblock/slayers/hud/SlayerHudWidget.java
@@ -6,7 +6,6 @@ import de.hysky.skyblocker.skyblock.slayers.SlayerManager;
 import de.hysky.skyblocker.skyblock.tabhud.util.Ico;
 import de.hysky.skyblocker.skyblock.tabhud.widget.Widget;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 
@@ -14,12 +13,11 @@ import java.text.NumberFormat;
 
 public class SlayerHudWidget extends Widget {
 	public static final SlayerHudWidget INSTANCE = new SlayerHudWidget();
-	private static final MutableText TITLE = Text.literal("Slayer").formatted(Formatting.DARK_PURPLE, Formatting.BOLD);
 	private final MinecraftClient client = MinecraftClient.getInstance();
 	private final NumberFormat numberFormat = NumberFormat.getInstance();
 
 	public SlayerHudWidget() {
-		super(TITLE, Formatting.DARK_PURPLE.getColorValue());
+		super(Text.literal("Slayer").formatted(Formatting.DARK_PURPLE, Formatting.BOLD), Formatting.DARK_PURPLE.getColorValue());
 		setX(SkyblockerConfigManager.get().slayers.slayerHud.x);
 		setY(SkyblockerConfigManager.get().slayers.slayerHud.y);
 		update();
