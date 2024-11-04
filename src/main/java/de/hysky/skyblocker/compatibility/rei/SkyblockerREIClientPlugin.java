8819f79cea66d5845579ee8f44f56951c1749f26
Rime <81419447+Emirlol@users.noreply.github.com>
2024-11-04 14:57:26 +0300

    Add supercraft button to REI

--- src/main/java/de/hysky/skyblocker/compatibility/rei/SkyblockerREIClientPlugin.java
+++ src/main/java/de/hysky/skyblocker/compatibility/rei/SkyblockerREIClientPlugin.java
@@ -6,6 +6,7 @@ import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
 import me.shedaniel.rei.api.client.registry.entry.EntryRegistry;
+import me.shedaniel.rei.api.client.registry.transfer.TransferHandlerRegistry;
 import me.shedaniel.rei.api.common.category.CategoryIdentifier;
 import me.shedaniel.rei.api.common.util.EntryStacks;
 import net.minecraft.item.Items;
@@ -14,21 +15,26 @@ import net.minecraft.item.Items;
  * REI integration
  */
 public class SkyblockerREIClientPlugin implements REIClientPlugin {
-    public static final CategoryIdentifier<SkyblockCraftingDisplay> SKYBLOCK = CategoryIdentifier.of(SkyblockerMod.NAMESPACE, "skyblock");
+	public static final CategoryIdentifier<SkyblockCraftingDisplay> SKYBLOCK = CategoryIdentifier.of(SkyblockerMod.NAMESPACE, "skyblock");
 
-    @Override
-    public void registerCategories(CategoryRegistry categoryRegistry) {
-        categoryRegistry.addWorkstations(SKYBLOCK, EntryStacks.of(Items.CRAFTING_TABLE));
-        categoryRegistry.add(new SkyblockCategory());
-    }
+	@Override
+	public void registerCategories(CategoryRegistry categoryRegistry) {
+		categoryRegistry.addWorkstations(SKYBLOCK, EntryStacks.of(Items.CRAFTING_TABLE));
+		categoryRegistry.add(new SkyblockCategory());
+	}
 
-    @Override
-    public void registerDisplays(DisplayRegistry displayRegistry) {
-        displayRegistry.registerDisplayGenerator(SKYBLOCK, new SkyblockCraftingDisplayGenerator());
-    }
+	@Override
+	public void registerDisplays(DisplayRegistry displayRegistry) {
+		displayRegistry.registerDisplayGenerator(SKYBLOCK, new SkyblockCraftingDisplayGenerator());
+	}
 
-    @Override
-    public void registerEntries(EntryRegistry entryRegistry) {
-        entryRegistry.addEntries(ItemRepository.getItemsStream().map(EntryStacks::of).toList());
-    }
+	@Override
+	public void registerTransferHandlers(TransferHandlerRegistry registry) {
+		registry.register(new SkyblockerTransferHandler());
+	}
+
+	@Override
+	public void registerEntries(EntryRegistry entryRegistry) {
+		entryRegistry.addEntries(ItemRepository.getItemsStream().map(EntryStacks::of).toList());
+	}
 }
