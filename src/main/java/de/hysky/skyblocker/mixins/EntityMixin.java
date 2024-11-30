e83394c83e17cb85df9bbb1a9cefc5a08d70aa64
7azeemm <hazemfarah26@gmail.com>
2024-11-30 14:51:20 +0100

    LazerTimer rewrite

--- src/main/java/de/hysky/skyblocker/mixins/EntityMixin.java
+++ src/main/java/de/hysky/skyblocker/mixins/EntityMixin.java
@@ -1,14 +1,14 @@
 package de.hysky.skyblocker.mixins;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import de.hysky.skyblocker.config.SkyblockerConfigManager;
 import de.hysky.skyblocker.debug.Debug;
 import de.hysky.skyblocker.skyblock.slayers.SlayerManager;
+import de.hysky.skyblocker.skyblock.slayers.SlayerType;
 import de.hysky.skyblocker.skyblock.slayers.boss.voidgloom.LazerTimer;
 import de.hysky.skyblocker.utils.Utils;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.mob.EndermanEntity;
-import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
@@ -17,8 +17,6 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -61,8 +59,6 @@ public abstract class EntityMixin {
 				Entity slayer = SlayerManager.getSlayerBoss();
 				if (slayer != null) {
 					if (slayer.getUuid().equals(getUuid()) && !LazerTimer.isRiding()) {
-						LazerTimer.bossUUID = getUuid();
-						LazerTimer.bossLocation = getPos();
 						LazerTimer.resetTimer();
 						LazerTimer.setRiding(true);
 					}
@@ -73,12 +69,16 @@ public abstract class EntityMixin {
 
 	@Inject(method = "tick", at = @At("TAIL"))
 	private void onTick(CallbackInfo ci) {
-		if (this.getType() == EntityType.ENDERMAN) {
-			if (LazerTimer.isRiding() && LazerTimer.bossUUID.equals(getUuid()) && getVehicle() == null) {
-				if (LazerTimer.remainingTime > 5.0) return;
-				LazerTimer.bossUUID = null;
-				LazerTimer.bossLocation = null;
-				LazerTimer.setRiding(false);
+		if (this.getType() == EntityType.ENDERMAN && SkyblockerConfigManager.get().slayers.endermanSlayer.lazerTimer && SlayerManager.isInSlayerType(SlayerType.VOIDGLOOM)) {
+			if (SlayerManager.getSlayerBoss() != null && getUuid().equals(SlayerManager.getSlayerBoss().getUuid())) {
+				if (LazerTimer.isRiding()) {
+					if (getVehicle() == null) {
+						if (LazerTimer.remainingTime > 5.0) return;
+						LazerTimer.setRiding(false);
+					} else {
+						LazerTimer.updateTimer();
+					}
+				}
 			}
 		}
 	}
--- src/main/java/de/hysky/skyblocker/skyblock/slayers/boss/voidgloom/LazerTimer.java
+++ src/main/java/de/hysky/skyblocker/skyblock/slayers/boss/voidgloom/LazerTimer.java
@@ -3,19 +3,13 @@ package de.hysky.skyblocker.skyblock.slayers.boss.voidgloom;
 import de.hysky.skyblocker.annotations.Init;
 import de.hysky.skyblocker.skyblock.slayers.SlayerManager;
 import de.hysky.skyblocker.utils.render.RenderHelper;
-import de.hysky.skyblocker.utils.scheduler.Scheduler;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
+import net.minecraft.entity.Entity;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
-import net.minecraft.util.math.Vec3d;
-
-import java.util.UUID;
 
 public class LazerTimer {
-
-	public static UUID bossUUID = null;
-	public static Vec3d bossLocation = null;
 
 	public static double remainingTime = 0;
 	private static boolean isRiding = false;
@@ -23,23 +17,18 @@ public class LazerTimer {
 	@Init
 	public static void init() {
 		WorldRenderEvents.AFTER_TRANSLUCENT.register(LazerTimer::render);
-		Scheduler.INSTANCE.scheduleCyclic(LazerTimer::updateTimer, 1);
 	}
 
-	private static void updateTimer() {
+	public static void updateTimer() {
 		if (isRiding) {
 			if (!SlayerManager.isBossSpawned()) {//in case player died in lazer phase
 				isRiding = false;
-				remainingTime = 0;
 				return;
 			}
 
 			remainingTime -= 0.05;
 			if (remainingTime <= 0) {
-				remainingTime = 0;
 				isRiding = false;
-				bossUUID = null;
-				bossLocation = null;
 			}
 		}
 	}
@@ -58,11 +47,15 @@ public class LazerTimer {
 
 	private static void render(WorldRenderContext context) {
 		if (isRiding) {
-			String timeText = String.format("%.2fsc", remainingTime);
-			Text renderText = Text.literal("Lazer: ").formatted(Formatting.WHITE)
-					.append(Text.literal(timeText).formatted(Formatting.GREEN).formatted(Formatting.BOLD));
+			Entity boss = SlayerManager.getSlayerBoss();
+			if (boss != null) {
+				String timeText = String.format("%.2fsc", remainingTime);
+				Text renderText = Text.literal("Lazer: ").formatted(Formatting.WHITE)
+						.append(Text.literal(timeText).formatted(Formatting.GREEN).formatted(Formatting.BOLD));
 
-			RenderHelper.renderText(context, renderText, bossLocation.add(0, 2, 0), true);
+				RenderHelper.renderText(context, renderText, boss.getPos().add(0, 2, 0), true);
+
+			}
 		}
 	}
 }
