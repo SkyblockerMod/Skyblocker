028200fe3d934b0b6d08bcf6c7647f7072df91dc
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2024-12-23 16:27:33 -0800

    Add mob glow cache

--- src/main/java/de/hysky/skyblocker/mixins/WorldRendererMixin.java
+++ src/main/java/de/hysky/skyblocker/mixins/WorldRendererMixin.java
@@ -3,7 +3,6 @@ package de.hysky.skyblocker.mixins;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -12,8 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.Share;
-import com.llamalad7.mixinextras.sugar.ref.LocalBooleanRef;
 
 import de.hysky.skyblocker.injected.CustomGlowInfo;
 import de.hysky.skyblocker.skyblock.dungeon.LividColor;
@@ -35,16 +32,12 @@ public class WorldRendererMixin implements CustomGlowInfo {
 	@Shadow
 	@Final
 	private DefaultFramebufferSet framebufferSet;
-	@Unique
-	private boolean atLeastOneMobHasCustomGlow;
 
-	@ModifyExpressionValue(method = "getEntitiesToRender", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;hasOutline(Lnet/minecraft/entity/Entity;)Z"))
-	private boolean skyblocker$setupEntityOutlineFramebufferIfCustomGlow(boolean original, @Local Entity entity) {
-		boolean hasCustomGlow = MobGlow.shouldMobGlow(entity);
-
-		if (hasCustomGlow) atLeastOneMobHasCustomGlow = true;
-
-		return original || hasCustomGlow;
+	@ModifyExpressionValue(method = {"getEntitiesToRender", "renderEntities"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;hasOutline(Lnet/minecraft/entity/Entity;)Z"))
+	private boolean skyblocker$shouldMobGlow(boolean original, @Local Entity entity) {
+		boolean allowGlow = LividColor.allowGlow();
+		boolean customGlow = MobGlow.hasOrComputeMobGlow(entity);
+		return allowGlow && original || customGlow;
 	}
 
 	@Inject(method = "method_62214",
@@ -52,27 +45,19 @@ public class WorldRendererMixin implements CustomGlowInfo {
 			at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gl/Framebuffer;clear()V", ordinal = 0, shift = At.Shift.AFTER)
 	)
 	private void skyblocker$copyFramebufferDepth2AdjustGlowVisibility(CallbackInfo ci) {
-		if (atLeastOneMobHasCustomGlow) framebufferSet.entityOutlineFramebuffer.get().copyDepthFrom(client.getFramebuffer());
-	}
-
-	@ModifyExpressionValue(method = "renderEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;hasOutline(Lnet/minecraft/entity/Entity;)Z"))
-	private boolean skyblocker$shouldMobGlow(boolean original, @Local Entity entity, @Share("hasCustomGlow") LocalBooleanRef hasCustomGlow) {
-		boolean allowGlow = LividColor.allowGlow();
-		boolean shouldGlow = MobGlow.shouldMobGlow(entity);
-		hasCustomGlow.set(shouldGlow);
-		return allowGlow && original || shouldGlow;
+		if (MobGlow.atLeastOneMobHasCustomGlow()) framebufferSet.entityOutlineFramebuffer.get().copyDepthFrom(client.getFramebuffer());
 	}
 
 	@ModifyVariable(method = "renderEntities",
 			slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;hasOutline(Lnet/minecraft/entity/Entity;)Z"), to = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/OutlineVertexConsumerProvider;setColor(IIII)V")),
 			at = @At("STORE"), ordinal = 0
 	)
-	private int skyblocker$modifyGlowColor(int color, @Local Entity entity, @Share("hasCustomGlow") LocalBooleanRef hasCustomGlow) {
-		return hasCustomGlow.get() ? MobGlow.getGlowColor(entity) : color;
+	private int skyblocker$modifyGlowColor(int color, @Local Entity entity) {
+		return MobGlow.getMobGlowOrDefault(entity, color);
 	}
 
 	@Inject(method = "renderEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/WorldRenderer;renderEntity(Lnet/minecraft/entity/Entity;DDDFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;)V"))
-	private void skyblocker$beforeEntityIsRendered(CallbackInfo ci, @Local Entity entity) {
+	private void skyblocker$renderMobBoundingBox(CallbackInfo ci, @Local Entity entity) {
 		boolean shouldShowBoundingBox = MobBoundingBoxes.shouldDrawMobBoundingBox(entity);
 
 		if (shouldShowBoundingBox) {
@@ -83,13 +68,8 @@ public class WorldRendererMixin implements CustomGlowInfo {
 		}
 	}
 
-	@Override
-	public boolean atLeastOneMobHasCustomGlow() {
-		return atLeastOneMobHasCustomGlow;
-	}
-
-	@Inject(method = "render", at = @At("TAIL"))
-	private void skyblocker$resetCustomGlowBool(CallbackInfo ci) {
-		atLeastOneMobHasCustomGlow = false;
+	@Inject(method = "render", at = @At("RETURN"))
+	private void skyblocker$clearCustomGlowCache(CallbackInfo ci) {
+		MobGlow.clearCache();
 	}
 }
