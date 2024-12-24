fbbd3aefe2c3ae456a9a1d28415ef19410c10a76
Aaron <51387595+AzureAaron@users.noreply.github.com>
2024-12-23 22:44:19 -0500

    Fix Simon Says Solver (#1100)

--- src/main/java/de/hysky/skyblocker/skyblock/dungeon/device/SimonSays.java
+++ src/main/java/de/hysky/skyblocker/skyblock/dungeon/device/SimonSays.java
@@ -32,6 +32,8 @@ import net.minecraft.world.World;
 
 import java.util.Objects;
 
+import org.jetbrains.annotations.Nullable;
+
 public class SimonSays {
 	private static final Box BOARD_AREA = Box.enclosing(new BlockPos(111, 123, 92), new BlockPos(111, 120, 95));
 	private static final Box BUTTONS_AREA = Box.enclosing(new BlockPos(110, 123, 92), new BlockPos(110, 120, 95));
@@ -71,14 +73,14 @@ public class SimonSays {
 	//If the player goes out of the range required to receive block/chunk updates then their solver won't detect stuff but that
 	//doesn't matter because if they're doing pre-4 or something they won't be doing the ss, and if they end up needing to they can
 	//just reset it or have the other person finish the current sequence first then let them do it.
-	public static void onBlockUpdate(BlockPos pos, BlockState state) {
+	public static void onBlockUpdate(BlockPos pos, BlockState newState, @Nullable BlockState oldState) {
 		if (shouldProcess()) {
 			Vec3d posVec = Vec3d.of(pos);
-			Block block = state.getBlock();
+			Block newBlock = newState.getBlock();
 
-			if (BOARD_AREA.contains(posVec) && block.equals(Blocks.SEA_LANTERN)) {
+			if (BOARD_AREA.contains(posVec) && newBlock.equals(Blocks.OBSIDIAN) && oldState != null && oldState.getBlock().equals(Blocks.SEA_LANTERN)) {
 				SIMON_PATTERN.add(pos.toImmutable()); //Convert to immutable because chunk delta updates use the mutable variant
-			} else if (BUTTONS_AREA.contains(posVec) && block.equals(Blocks.AIR)) {
+			} else if (BUTTONS_AREA.contains(posVec) && newBlock.equals(Blocks.AIR)) {
 				//Upon reaching the showing of the next sequence we need to reset the state so that we don't show old data
 				//Otherwise, the nextIndex will go beyond 5 and that can cause bugs, it also helps with the other case noted above
 				reset();
