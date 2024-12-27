56b3b84ef18d271713b26829a5f5bb3e14d2940f
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2024-12-26 17:24:43 -0800

    Add seen waypoint

---
+++ src/main/java/de/hysky/skyblocker/utils/waypoint/SeenWaypoint.java
@@ -0,0 +1,38 @@
+package de.hysky.skyblocker.utils.waypoint;
+
+import de.hysky.skyblocker.utils.Tickable;
+import de.hysky.skyblocker.utils.render.FrustumUtils;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.RaycastContext;
+
+public class SeenWaypoint extends Waypoint implements Tickable {
+	private boolean seen = false;
+
+	public SeenWaypoint(BlockPos pos, Type type, float[] colorComponents) {
+		super(pos, type, colorComponents);
+	}
+
+	public boolean isSeen() {
+		return seen;
+	}
+
+	@Override
+	public Type getRenderType() {
+		return seen ? super.getRenderType() : super.getRenderType().withoutBeacon();
+	}
+
+	@Override
+	public boolean shouldRenderThroughWalls() {
+		return seen;
+	}
+
+	@Override
+	public void tick(MinecraftClient client) {
+		if (!seen && client.world != null && client.player != null && FrustumUtils.isVisible(pos.getX(), pos.getY(), pos.getZ(), pos.getX() + 1, pos.getY() + 1, pos.getZ() + 1) && client.world.raycast(new RaycastContext(client.player.getEyePos(), Vec3d.ofCenter(pos), RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, client.player)).getType() == HitResult.Type.MISS) {
+			seen = true;
+		}
+	}
+}
