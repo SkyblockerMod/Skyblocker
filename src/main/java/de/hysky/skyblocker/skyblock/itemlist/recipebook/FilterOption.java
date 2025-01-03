4ce7d1369ab0f2b9381ba6bda3b005fe9f85601a
Manchick0 <manchicking@gmail.com>
2025-01-03 22:46:46 +0100

    Clean up the code

--- src/main/java/de/hysky/skyblocker/skyblock/itemlist/recipebook/FilterOption.java
+++ src/main/java/de/hysky/skyblocker/skyblock/itemlist/recipebook/FilterOption.java
@@ -1,5 +1,6 @@
 package de.hysky.skyblocker.skyblock.itemlist.recipebook;
 
+import de.hysky.skyblocker.SkyblockerMod;
 import net.minecraft.util.Identifier;
 
 import java.util.function.Predicate;
@@ -7,25 +8,26 @@ import java.util.function.Supplier;
 
 public enum FilterOption implements Supplier<Identifier>, Predicate<String> {
 
-	ALL(query -> true, Identifier.of("skyblocker", "textures/gui/filter/all.png")),
+	ALL(query -> true, Identifier.of(SkyblockerMod.NAMESPACE, "textures/gui/filter/all.png")),
 	ENTITIES(query -> query.endsWith("(monster)") || query.endsWith("(miniboss)") || query.endsWith("(boss)")
 			|| query.endsWith("(animal)") || query.endsWith("(pest)") || query.endsWith("(sea creature)"),
-			Identifier.of("skyblocker", "textures/gui/filter/entities.png")),
-	NPCS(query -> query.endsWith("(npc)") || query.endsWith("(rift npc)"), Identifier.of("skyblocker", "textures/gui/filter/npcs.png")),
-	MAYORS(query -> query.endsWith("(mayor)") || query.endsWith("(retired mayor)"), Identifier.of("skyblocker", "textures/gui/filter/mayors.png")),
+			Identifier.of(SkyblockerMod.NAMESPACE, "textures/gui/filter/entities.png")),
+	NPCS(query -> query.endsWith("(npc)") || query.endsWith("(rift npc)"), Identifier.of(SkyblockerMod.NAMESPACE, "textures/gui/filter/npcs.png")),
+	MAYORS(query -> query.endsWith("(mayor)") || query.endsWith("(retired mayor)"), Identifier.of(SkyblockerMod.NAMESPACE, "textures/gui/filter/mayors.png")),
 
 	// Basically a negation on everything else.
 	ITEMS(query -> !ENTITIES.test(query) && !NPCS.test(query) && !MAYORS.test(query),
-			Identifier.of("skyblocker", "textures/gui/filter/items.png"));
+			Identifier.of(SkyblockerMod.NAMESPACE, "textures/gui/filter/items.png"));
 
-	final Predicate<String> matchingPredicate;
-	final Identifier texture;
+	private	final Predicate<String> matchingPredicate;
+	private final Identifier texture;
 
 	FilterOption(Predicate<String> matchingPredicate, Identifier texture) {
 		this.matchingPredicate = matchingPredicate;
 		this.texture = texture;
 	}
 
+	@Override
 	public boolean test(String query) {
 		return matchingPredicate.test(query);
 	}
