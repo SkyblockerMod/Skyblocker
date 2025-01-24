e40797e01557ee54d6ba247321f4340d93611236
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2025-01-23 21:50:12 -0500

    Refactor and clean up

--- src/main/java/de/hysky/skyblocker/skyblock/item/slottext/adders/EnchantmentLevelAdder.java
+++ src/main/java/de/hysky/skyblocker/skyblock/item/slottext/adders/EnchantmentLevelAdder.java
@@ -1,8 +1,8 @@
 package de.hysky.skyblocker.skyblock.item.slottext.adders;
 
 import de.hysky.skyblocker.debug.Debug;
-import de.hysky.skyblocker.skyblock.item.slottext.SlotText;
 import de.hysky.skyblocker.skyblock.item.slottext.SimpleSlotTextAdder;
+import de.hysky.skyblocker.skyblock.item.slottext.SlotText;
 import de.hysky.skyblocker.utils.ItemUtils;
 import de.hysky.skyblocker.utils.RomanNumerals;
 import it.unimi.dsi.fastutil.objects.Object2ObjectMap;
@@ -19,151 +19,153 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public class EnchantmentLevelAdder extends SimpleSlotTextAdder {
-	private static final Object2ObjectMap<String, String> ID_ABBREVIATION_MAP = new Object2ObjectOpenHashMap<>();
+	private static final Object2ObjectMap<String, String> ENCHANTMENT_ABBREVIATION_MAP = new Object2ObjectOpenHashMap<>();
+	private static final Map<String, String> ULTIMATE_ENCHANTMENT_ABBREVIATION_MAP = new Object2ObjectOpenHashMap<>();
 
 	static {
 		// Normal enchants (A - Z)
-		ID_ABBREVIATION_MAP.put("angler", "AN");
-		ID_ABBREVIATION_MAP.put("aqua_affinity", "AA");
-		ID_ABBREVIATION_MAP.put("aiming", "DT"); // Dragon tracer
+		ENCHANTMENT_ABBREVIATION_MAP.put("angler", "AN");
+		ENCHANTMENT_ABBREVIATION_MAP.put("aqua_affinity", "AA");
+		ENCHANTMENT_ABBREVIATION_MAP.put("aiming", "DT"); // Dragon tracer
 
-		ID_ABBREVIATION_MAP.put("bane_of_arthropods", "BA");
-		ID_ABBREVIATION_MAP.put("big_brain", "BB");
-		ID_ABBREVIATION_MAP.put("blast_protection", "BP");
-		ID_ABBREVIATION_MAP.put("blessing", "BL");
+		ENCHANTMENT_ABBREVIATION_MAP.put("bane_of_arthropods", "BA");
+		ENCHANTMENT_ABBREVIATION_MAP.put("big_brain", "BB");
+		ENCHANTMENT_ABBREVIATION_MAP.put("blast_protection", "BP");
+		ENCHANTMENT_ABBREVIATION_MAP.put("blessing", "BL");
 
-		ID_ABBREVIATION_MAP.put("caster", "CA");
-		ID_ABBREVIATION_MAP.put("cayenne", "CY");
-		ID_ABBREVIATION_MAP.put("champion", "CH");
-		ID_ABBREVIATION_MAP.put("chance", "CN");
-		ID_ABBREVIATION_MAP.put("charm", "CM");
-		ID_ABBREVIATION_MAP.put("cleave", "CL");
-		ID_ABBREVIATION_MAP.put("compact", "CO");
-		ID_ABBREVIATION_MAP.put("corruption", "CP");
-		ID_ABBREVIATION_MAP.put("counter_strike", "CS");
-		ID_ABBREVIATION_MAP.put("critical", "CR");
-		ID_ABBREVIATION_MAP.put("cubism", "CU");
+		ENCHANTMENT_ABBREVIATION_MAP.put("caster", "CA");
+		ENCHANTMENT_ABBREVIATION_MAP.put("cayenne", "CY");
+		ENCHANTMENT_ABBREVIATION_MAP.put("champion", "CH");
+		ENCHANTMENT_ABBREVIATION_MAP.put("chance", "CN");
+		ENCHANTMENT_ABBREVIATION_MAP.put("charm", "CM");
+		ENCHANTMENT_ABBREVIATION_MAP.put("cleave", "CL");
+		ENCHANTMENT_ABBREVIATION_MAP.put("compact", "CO");
+		ENCHANTMENT_ABBREVIATION_MAP.put("corruption", "CP");
+		ENCHANTMENT_ABBREVIATION_MAP.put("counter_strike", "CS");
+		ENCHANTMENT_ABBREVIATION_MAP.put("critical", "CR");
+		ENCHANTMENT_ABBREVIATION_MAP.put("cubism", "CU");
 
-		ID_ABBREVIATION_MAP.put("cultivating", "CT");
-		ID_ABBREVIATION_MAP.put("dedication", "DD");
-		ID_ABBREVIATION_MAP.put("delicate", "DE");
+		ENCHANTMENT_ABBREVIATION_MAP.put("cultivating", "CT");
+		ENCHANTMENT_ABBREVIATION_MAP.put("dedication", "DD");
+		ENCHANTMENT_ABBREVIATION_MAP.put("delicate", "DE");
 
-		ID_ABBREVIATION_MAP.put("depth_strider", "DS");
-		ID_ABBREVIATION_MAP.put("divine_gift", "DG");
-		ID_ABBREVIATION_MAP.put("dragon_hunter", "DH");
+		ENCHANTMENT_ABBREVIATION_MAP.put("depth_strider", "DS");
+		ENCHANTMENT_ABBREVIATION_MAP.put("divine_gift", "DG");
+		ENCHANTMENT_ABBREVIATION_MAP.put("dragon_hunter", "DH");
 
-		ID_ABBREVIATION_MAP.put("efficiency", "EF");
-		ID_ABBREVIATION_MAP.put("ender_slayer", "ES");
-		ID_ABBREVIATION_MAP.put("execute", "EX");
-		ID_ABBREVIATION_MAP.put("experience", "XP");
-		ID_ABBREVIATION_MAP.put("expertise", "EP");
+		ENCHANTMENT_ABBREVIATION_MAP.put("efficiency", "EF");
+		ENCHANTMENT_ABBREVIATION_MAP.put("ender_slayer", "ES");
+		ENCHANTMENT_ABBREVIATION_MAP.put("execute", "EX");
+		ENCHANTMENT_ABBREVIATION_MAP.put("experience", "XP");
+		ENCHANTMENT_ABBREVIATION_MAP.put("expertise", "EP");
 
-		ID_ABBREVIATION_MAP.put("feather_falling", "FF");
-		ID_ABBREVIATION_MAP.put("ferocious_mana", "FM");
-		ID_ABBREVIATION_MAP.put("fire_aspect", "FA");
-		ID_ABBREVIATION_MAP.put("fire_protection", "FP");
-		ID_ABBREVIATION_MAP.put("first_strike", "FS");
-		ID_ABBREVIATION_MAP.put("flame", "FL");
-		ID_ABBREVIATION_MAP.put("fortune", "FO");
-		ID_ABBREVIATION_MAP.put("frail", "FR");
-		ID_ABBREVIATION_MAP.put("frost_walker", "FW");
+		ENCHANTMENT_ABBREVIATION_MAP.put("feather_falling", "FF");
+		ENCHANTMENT_ABBREVIATION_MAP.put("ferocious_mana", "FM");
+		ENCHANTMENT_ABBREVIATION_MAP.put("fire_aspect", "FA");
+		ENCHANTMENT_ABBREVIATION_MAP.put("fire_protection", "FP");
+		ENCHANTMENT_ABBREVIATION_MAP.put("first_strike", "FS");
+		ENCHANTMENT_ABBREVIATION_MAP.put("flame", "FL");
+		ENCHANTMENT_ABBREVIATION_MAP.put("fortune", "FO");
+		ENCHANTMENT_ABBREVIATION_MAP.put("frail", "FR");
+		ENCHANTMENT_ABBREVIATION_MAP.put("frost_walker", "FW");
 
-		ID_ABBREVIATION_MAP.put("giant_killer", "GK");
-		ID_ABBREVIATION_MAP.put("great_spook", "GS");
-		ID_ABBREVIATION_MAP.put("green_thumb", "GT");
-		ID_ABBREVIATION_MAP.put("growth", "GR");
+		ENCHANTMENT_ABBREVIATION_MAP.put("giant_killer", "GK");
+		ENCHANTMENT_ABBREVIATION_MAP.put("great_spook", "GS");
+		ENCHANTMENT_ABBREVIATION_MAP.put("green_thumb", "GT");
+		ENCHANTMENT_ABBREVIATION_MAP.put("growth", "GR");
 
-		ID_ABBREVIATION_MAP.put("hardened_mana", "HM");
-		ID_ABBREVIATION_MAP.put("harvesting", "HV");
-		ID_ABBREVIATION_MAP.put("hecatomb", "HE");
+		ENCHANTMENT_ABBREVIATION_MAP.put("hardened_mana", "HM");
+		ENCHANTMENT_ABBREVIATION_MAP.put("harvesting", "HV");
+		ENCHANTMENT_ABBREVIATION_MAP.put("hecatomb", "HE");
 
-		ID_ABBREVIATION_MAP.put("ice_cold", "IC");
-		ID_ABBREVIATION_MAP.put("impaling", "IM");
-		ID_ABBREVIATION_MAP.put("infinite_quiver", "IQ");
+		ENCHANTMENT_ABBREVIATION_MAP.put("ice_cold", "IC");
+		ENCHANTMENT_ABBREVIATION_MAP.put("impaling", "IM");
+		ENCHANTMENT_ABBREVIATION_MAP.put("infinite_quiver", "IQ");
 
-		ID_ABBREVIATION_MAP.put("knockback", "KB");
+		ENCHANTMENT_ABBREVIATION_MAP.put("knockback", "KB");
 
-		ID_ABBREVIATION_MAP.put("lapidary", "LP");
-		ID_ABBREVIATION_MAP.put("lethality", "LE");
-		ID_ABBREVIATION_MAP.put("life_steal", "LS");
-		ID_ABBREVIATION_MAP.put("looting", "LO");
-		ID_ABBREVIATION_MAP.put("luck", "LU");
-		ID_ABBREVIATION_MAP.put("luck_of_the_sea", "LA");
-		ID_ABBREVIATION_MAP.put("lure", "LR");
+		ENCHANTMENT_ABBREVIATION_MAP.put("lapidary", "LP");
+		ENCHANTMENT_ABBREVIATION_MAP.put("lethality", "LE");
+		ENCHANTMENT_ABBREVIATION_MAP.put("life_steal", "LS");
+		ENCHANTMENT_ABBREVIATION_MAP.put("looting", "LO");
+		ENCHANTMENT_ABBREVIATION_MAP.put("luck", "LU");
+		ENCHANTMENT_ABBREVIATION_MAP.put("luck_of_the_sea", "LA");
+		ENCHANTMENT_ABBREVIATION_MAP.put("lure", "LR");
 
-		ID_ABBREVIATION_MAP.put("magnet", "MG");
-		ID_ABBREVIATION_MAP.put("mana_steal", "MS");
-		ID_ABBREVIATION_MAP.put("mana_vampire", "MV");
+		ENCHANTMENT_ABBREVIATION_MAP.put("magnet", "MG");
+		ENCHANTMENT_ABBREVIATION_MAP.put("mana_steal", "MS");
+		ENCHANTMENT_ABBREVIATION_MAP.put("mana_vampire", "MV");
 
-		ID_ABBREVIATION_MAP.put("overload", "OV");
+		ENCHANTMENT_ABBREVIATION_MAP.put("overload", "OV");
 
-		ID_ABBREVIATION_MAP.put("paleontologist", "PA");
-		ID_ABBREVIATION_MAP.put("pesterminator", "PS");
-		ID_ABBREVIATION_MAP.put("piercing", "PI");
-		ID_ABBREVIATION_MAP.put("piscary", "PC");
-		ID_ABBREVIATION_MAP.put("power", "PW");
-		ID_ABBREVIATION_MAP.put("pristine", "PM"); // Prismatic
-		ID_ABBREVIATION_MAP.put("projectile_protection", "PP");
-		ID_ABBREVIATION_MAP.put("prosecute", "PO");
-		ID_ABBREVIATION_MAP.put("prosperity", "PE");
-		ID_ABBREVIATION_MAP.put("protection", "PR");
-		ID_ABBREVIATION_MAP.put("punch", "PU");
+		ENCHANTMENT_ABBREVIATION_MAP.put("paleontologist", "PA");
+		ENCHANTMENT_ABBREVIATION_MAP.put("pesterminator", "PS");
+		ENCHANTMENT_ABBREVIATION_MAP.put("piercing", "PI");
+		ENCHANTMENT_ABBREVIATION_MAP.put("piscary", "PC");
+		ENCHANTMENT_ABBREVIATION_MAP.put("power", "PW");
+		ENCHANTMENT_ABBREVIATION_MAP.put("pristine", "PM"); // Prismatic
+		ENCHANTMENT_ABBREVIATION_MAP.put("projectile_protection", "PP");
+		ENCHANTMENT_ABBREVIATION_MAP.put("prosecute", "PO");
+		ENCHANTMENT_ABBREVIATION_MAP.put("prosperity", "PE");
+		ENCHANTMENT_ABBREVIATION_MAP.put("protection", "PR");
+		ENCHANTMENT_ABBREVIATION_MAP.put("punch", "PU");
 
-		ID_ABBREVIATION_MAP.put("quantum", "QU");
+		ENCHANTMENT_ABBREVIATION_MAP.put("quantum", "QU");
 
-		ID_ABBREVIATION_MAP.put("rainbow", "RA");
-		ID_ABBREVIATION_MAP.put("reflection", "RF");
-		ID_ABBREVIATION_MAP.put("rejuvenate", "RJ");
-		ID_ABBREVIATION_MAP.put("replenish", "RP");
-		ID_ABBREVIATION_MAP.put("respiration", "RE");
-		ID_ABBREVIATION_MAP.put("respite", "RS");
+		ENCHANTMENT_ABBREVIATION_MAP.put("rainbow", "RA");
+		ENCHANTMENT_ABBREVIATION_MAP.put("reflection", "RF");
+		ENCHANTMENT_ABBREVIATION_MAP.put("rejuvenate", "RJ");
+		ENCHANTMENT_ABBREVIATION_MAP.put("replenish", "RP");
+		ENCHANTMENT_ABBREVIATION_MAP.put("respiration", "RE");
+		ENCHANTMENT_ABBREVIATION_MAP.put("respite", "RS");
 
-		ID_ABBREVIATION_MAP.put("scavenger", "SC");
-		ID_ABBREVIATION_MAP.put("sharpness", "SH");
-		ID_ABBREVIATION_MAP.put("silk_touch", "ST");
-		ID_ABBREVIATION_MAP.put("smarty_pants", "SP");
-		ID_ABBREVIATION_MAP.put("smelting_touch", "SE");
-		ID_ABBREVIATION_MAP.put("smite", "SI");
-		ID_ABBREVIATION_MAP.put("smoldering", "SL");
-		ID_ABBREVIATION_MAP.put("snipe", "SN");
-		ID_ABBREVIATION_MAP.put("spiked_hook", "SK");
-		ID_ABBREVIATION_MAP.put("strong_mana", "SM");
-		ID_ABBREVIATION_MAP.put("sugar_rush", "SR");
-		ID_ABBREVIATION_MAP.put("sunder", "SU");
-		ID_ABBREVIATION_MAP.put("syphon", "SY");
+		ENCHANTMENT_ABBREVIATION_MAP.put("scavenger", "SC");
+		ENCHANTMENT_ABBREVIATION_MAP.put("sharpness", "SH");
+		ENCHANTMENT_ABBREVIATION_MAP.put("silk_touch", "ST");
+		ENCHANTMENT_ABBREVIATION_MAP.put("smarty_pants", "SP");
+		ENCHANTMENT_ABBREVIATION_MAP.put("smelting_touch", "SE");
+		ENCHANTMENT_ABBREVIATION_MAP.put("smite", "SI");
+		ENCHANTMENT_ABBREVIATION_MAP.put("smoldering", "SL");
+		ENCHANTMENT_ABBREVIATION_MAP.put("snipe", "SN");
+		ENCHANTMENT_ABBREVIATION_MAP.put("spiked_hook", "SK");
+		ENCHANTMENT_ABBREVIATION_MAP.put("strong_mana", "SM");
+		ENCHANTMENT_ABBREVIATION_MAP.put("sugar_rush", "SR");
+		ENCHANTMENT_ABBREVIATION_MAP.put("sunder", "SU");
+		ENCHANTMENT_ABBREVIATION_MAP.put("syphon", "SY");
 
-		ID_ABBREVIATION_MAP.put("tabasco", "TA");
-		ID_ABBREVIATION_MAP.put("thorns", "TN");
-		ID_ABBREVIATION_MAP.put("thunderbolt", "TB");
-		ID_ABBREVIATION_MAP.put("thunderlord", "TL");
-		ID_ABBREVIATION_MAP.put("titan_killer", "TK");
-		ID_ABBREVIATION_MAP.put("toxophilite", "TX");
-		ID_ABBREVIATION_MAP.put("transylvanian", "TY");
-		ID_ABBREVIATION_MAP.put("triple_strike", "TS");
-		ID_ABBREVIATION_MAP.put("true_protection", "TP");
+		ENCHANTMENT_ABBREVIATION_MAP.put("tabasco", "TA");
+		ENCHANTMENT_ABBREVIATION_MAP.put("thorns", "TN");
+		ENCHANTMENT_ABBREVIATION_MAP.put("thunderbolt", "TB");
+		ENCHANTMENT_ABBREVIATION_MAP.put("thunderlord", "TL");
+		ENCHANTMENT_ABBREVIATION_MAP.put("titan_killer", "TK");
+		ENCHANTMENT_ABBREVIATION_MAP.put("toxophilite", "TX");
+		ENCHANTMENT_ABBREVIATION_MAP.put("transylvanian", "TY");
+		ENCHANTMENT_ABBREVIATION_MAP.put("triple_strike", "TS");
+		ENCHANTMENT_ABBREVIATION_MAP.put("true_protection", "TP");
 
 		// Turbo books
-		ID_ABBREVIATION_MAP.put("turbo_cacti", "TI");
-		ID_ABBREVIATION_MAP.put("turbo_cane", "TC");
-		ID_ABBREVIATION_MAP.put("turbo_carrot", "TR");
-		ID_ABBREVIATION_MAP.put("turbo_coco", "TO");
-		ID_ABBREVIATION_MAP.put("turbo_melon", "TE");
-		ID_ABBREVIATION_MAP.put("turbo_mushrooms", "TM");
-		ID_ABBREVIATION_MAP.put("turbo_potato", "TT");
-		ID_ABBREVIATION_MAP.put("turbo_pumpkin", "TU");
-		ID_ABBREVIATION_MAP.put("turbo_warts", "TW");
-		ID_ABBREVIATION_MAP.put("turbo_wheat", "TH");
+		ENCHANTMENT_ABBREVIATION_MAP.put("turbo_cacti", "TI");
+		ENCHANTMENT_ABBREVIATION_MAP.put("turbo_cane", "TC");
+		ENCHANTMENT_ABBREVIATION_MAP.put("turbo_carrot", "TR");
+		ENCHANTMENT_ABBREVIATION_MAP.put("turbo_coco", "TO");
+		ENCHANTMENT_ABBREVIATION_MAP.put("turbo_melon", "TE");
+		ENCHANTMENT_ABBREVIATION_MAP.put("turbo_mushrooms", "TM");
+		ENCHANTMENT_ABBREVIATION_MAP.put("turbo_potato", "TT");
+		ENCHANTMENT_ABBREVIATION_MAP.put("turbo_pumpkin", "TU");
+		ENCHANTMENT_ABBREVIATION_MAP.put("turbo_warts", "TW");
+		ENCHANTMENT_ABBREVIATION_MAP.put("turbo_wheat", "TH");
 
-		ID_ABBREVIATION_MAP.put("vampirism", "VP");
-		ID_ABBREVIATION_MAP.put("venomous", "VE");
-		ID_ABBREVIATION_MAP.put("vicious", "VI");
+		ENCHANTMENT_ABBREVIATION_MAP.put("vampirism", "VP");
+		ENCHANTMENT_ABBREVIATION_MAP.put("venomous", "VE");
+		ENCHANTMENT_ABBREVIATION_MAP.put("vicious", "VI");
 
 		// check if all abbreviations are unique, if not report duplicates
-		if (Debug.debugEnabled() && ID_ABBREVIATION_MAP.size() != (int) ID_ABBREVIATION_MAP.values().stream().distinct().count()) {
+		if (Debug.debugEnabled() && ENCHANTMENT_ABBREVIATION_MAP.size() != (int) ENCHANTMENT_ABBREVIATION_MAP.values().stream().distinct().count()) {
 			Map<String, String> otherDirection = new Object2ObjectOpenHashMap<>();
-			for (Map.Entry<String, String> entry : ID_ABBREVIATION_MAP.entrySet()) {
+			for (Map.Entry<String, String> entry : ENCHANTMENT_ABBREVIATION_MAP.entrySet()) {
 				final String put = otherDirection.put(entry.getValue(), entry.getKey());
 				if (put != null) {
 					throw new IllegalArgumentException("Duplicate key [%s, %s] for value %s".formatted(put, entry.getKey(), entry.getValue()));
@@ -172,28 +174,28 @@ public class EnchantmentLevelAdder extends SimpleSlotTextAdder {
 		}
 
 		// Ultimate enchants - Added after check since some of these are duplicated, tho due to color difference this is fine
-		ID_ABBREVIATION_MAP.put("ultimate_bank", "B");
-		ID_ABBREVIATION_MAP.put("ultimate_bobbin_time", "BT");
-		ID_ABBREVIATION_MAP.put("ultimate_chimera", "CH");
-		ID_ABBREVIATION_MAP.put("ultimate_combo", "CO");
-		ID_ABBREVIATION_MAP.put("ultimate_reiterate", "D"); // Duplex
-		ID_ABBREVIATION_MAP.put("ultimate_fatal_tempo", "FT");
-		ID_ABBREVIATION_MAP.put("ultimate_flash", "F");
-		ID_ABBREVIATION_MAP.put("ultimate_flowstate", "FL");
-		ID_ABBREVIATION_MAP.put("ultimate_habanero_tactics", "HT");
-		ID_ABBREVIATION_MAP.put("ultimate_inferno", "I");
-		ID_ABBREVIATION_MAP.put("ultimate_last_stand", "LS");
-		ID_ABBREVIATION_MAP.put("ultimate_legion", "L");
-		ID_ABBREVIATION_MAP.put("ultimate_no_pain_no_gain", "NP");
-		ID_ABBREVIATION_MAP.put("ultimate_one_for_all", "O");
-		ID_ABBREVIATION_MAP.put("ultimate_refrigerate", "RF");
-		ID_ABBREVIATION_MAP.put("ultimate_rend", "R");
-		ID_ABBREVIATION_MAP.put("ultimate_soul_eater", "SE");
-		ID_ABBREVIATION_MAP.put("ultimate_swarm", "SW");
-		ID_ABBREVIATION_MAP.put("the_one", "TO");
-		ID_ABBREVIATION_MAP.put("ultimate_jerry", "UJ");
-		ID_ABBREVIATION_MAP.put("ultimate_wise", "UW");
-		ID_ABBREVIATION_MAP.put("ultimate_wisdom", "W");
+		ULTIMATE_ENCHANTMENT_ABBREVIATION_MAP.put("ultimate_bank", "B");
+		ULTIMATE_ENCHANTMENT_ABBREVIATION_MAP.put("ultimate_bobbin_time", "BT");
+		ULTIMATE_ENCHANTMENT_ABBREVIATION_MAP.put("ultimate_chimera", "CH");
+		ULTIMATE_ENCHANTMENT_ABBREVIATION_MAP.put("ultimate_combo", "CO");
+		ULTIMATE_ENCHANTMENT_ABBREVIATION_MAP.put("ultimate_reiterate", "D"); // Duplex
+		ULTIMATE_ENCHANTMENT_ABBREVIATION_MAP.put("ultimate_fatal_tempo", "FT");
+		ULTIMATE_ENCHANTMENT_ABBREVIATION_MAP.put("ultimate_flash", "F");
+		ULTIMATE_ENCHANTMENT_ABBREVIATION_MAP.put("ultimate_flowstate", "FL");
+		ULTIMATE_ENCHANTMENT_ABBREVIATION_MAP.put("ultimate_habanero_tactics", "HT");
+		ULTIMATE_ENCHANTMENT_ABBREVIATION_MAP.put("ultimate_inferno", "I");
+		ULTIMATE_ENCHANTMENT_ABBREVIATION_MAP.put("ultimate_last_stand", "LS");
+		ULTIMATE_ENCHANTMENT_ABBREVIATION_MAP.put("ultimate_legion", "L");
+		ULTIMATE_ENCHANTMENT_ABBREVIATION_MAP.put("ultimate_no_pain_no_gain", "NP");
+		ULTIMATE_ENCHANTMENT_ABBREVIATION_MAP.put("ultimate_one_for_all", "O");
+		ULTIMATE_ENCHANTMENT_ABBREVIATION_MAP.put("ultimate_refrigerate", "RF");
+		ULTIMATE_ENCHANTMENT_ABBREVIATION_MAP.put("ultimate_rend", "R");
+		ULTIMATE_ENCHANTMENT_ABBREVIATION_MAP.put("ultimate_soul_eater", "SE");
+		ULTIMATE_ENCHANTMENT_ABBREVIATION_MAP.put("ultimate_swarm", "SW");
+		ULTIMATE_ENCHANTMENT_ABBREVIATION_MAP.put("the_one", "TO");
+		ULTIMATE_ENCHANTMENT_ABBREVIATION_MAP.put("ultimate_jerry", "UJ");
+		ULTIMATE_ENCHANTMENT_ABBREVIATION_MAP.put("ultimate_wise", "UW");
+		ULTIMATE_ENCHANTMENT_ABBREVIATION_MAP.put("ultimate_wisdom", "W");
 	}
 
 	public EnchantmentLevelAdder() {
@@ -211,40 +213,27 @@ public class EnchantmentLevelAdder extends SimpleSlotTextAdder {
 			if (enchantments.getSize() != 1) return List.of(); //Only makes sense to display the level when there's one enchant.
 			final String enchantmentId = enchantments.getKeys().iterator().next();
 			int level = enchantments.getInt(enchantmentId);
-
 			final SlotText enchantmentLevel = SlotText.bottomLeft(Text.literal(String.valueOf(level)).withColor(0xFFDDC1));
 
-			if (ID_ABBREVIATION_MAP.containsKey(enchantmentId)) {
-				return List.of(
-						SlotText.topRight(getAbbreviation(enchantmentId)),
-						enchantmentLevel
-				);
-			}
-			return List.of(enchantmentLevel);
+			return getAbbreviation(enchantmentId)
+					.map(text -> List.of(SlotText.topRight(text), enchantmentLevel))
+					.orElseGet(() -> List.of(enchantmentLevel));
 		} else { //In bazaar, the books have the enchantment level in the name
 			int level = getEnchantLevelFromString(name);
-			if (level == 0) return List.of();
-			return SlotText.bottomLeftList(Text.literal(String.valueOf(level)).withColor(0xFFDDC1));
+			return level != 0 ? SlotText.bottomLeftList(Text.literal(String.valueOf(level)).withColor(0xFFDDC1)) : List.of();
 		}
 	}
 
-	private Text getAbbreviation(String enchantmentId) {
-		if (!ID_ABBREVIATION_MAP.containsKey(enchantmentId)) {
-			return Text.empty();
+	private Optional<Text> getAbbreviation(String enchantmentId) {
+		if (ENCHANTMENT_ABBREVIATION_MAP.containsKey(enchantmentId)) {
+			return Optional.of(Text.literal(ENCHANTMENT_ABBREVIATION_MAP.get(enchantmentId)).withColor(Formatting.BLUE.getColorValue()));
+		} else if (ULTIMATE_ENCHANTMENT_ABBREVIATION_MAP.containsKey(enchantmentId)) {
+			return Optional.of(Text.literal(ULTIMATE_ENCHANTMENT_ABBREVIATION_MAP.get(enchantmentId)).withColor(Formatting.LIGHT_PURPLE.getColorValue()));
 		}
-
-		final int color;
-		if (enchantmentId.startsWith("ultimate_") || enchantmentId.equalsIgnoreCase("the_one")) {
-			color = Formatting.LIGHT_PURPLE.getColorValue();
-		} else {
-			color = Formatting.BLUE.getColorValue();
-		}
-
-		return Text.literal(ID_ABBREVIATION_MAP.get(enchantmentId)).withColor(color);
+		return Optional.empty();
 	}
 
 	private static int getEnchantLevelFromString(String str) {
-		String romanNumeral = str.substring(str.lastIndexOf(' ') + 1); //+1 because we don't need the space itself
-		return RomanNumerals.romanToDecimal(romanNumeral); //Temporary line. The method will be moved out later.
+		return RomanNumerals.romanToDecimal(str.substring(str.lastIndexOf(' ') + 1)); //+1 because we don't need the space itself
 	}
 }
