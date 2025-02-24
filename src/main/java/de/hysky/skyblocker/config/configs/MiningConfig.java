eac4314fac3dc12929269b478583ea34f1561bbe
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2025-02-24 14:20:57 -0500

    Fix config location

--- src/main/java/de/hysky/skyblocker/config/configs/MiningConfig.java
+++ src/main/java/de/hysky/skyblocker/config/configs/MiningConfig.java
@@ -165,6 +165,9 @@ public class MiningConfig {
         @SerialEntry
         public boolean coldOverlay = true;
 
+		@SerialEntry
+		public boolean fossilSolver = true;
+
         @SerialEntry
         public boolean enableCorpseFinder = true;
 
@@ -173,9 +176,6 @@ public class MiningConfig {
 
 		@SerialEntry
 	    public boolean autoShareCorpses = false;
-        
-		@SerialEntry
-		public boolean fossilSolver = true;
     }
 
 	/**
