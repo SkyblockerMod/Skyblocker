3d03bfd1c11c2f04cdfebe2268f4d61f683c884d
Yasin <19829407+LifeIsAParadox@users.noreply.github.com>
2025-05-12 00:48:20 +0200

    Glowing Mushroom

--- src/main/java/de/hysky/skyblocker/config/configs/OtherLocationsConfig.java
+++ src/main/java/de/hysky/skyblocker/config/configs/OtherLocationsConfig.java
@@ -4,79 +4,82 @@ import dev.isxander.yacl3.config.v2.api.SerialEntry;
 
 public class OtherLocationsConfig {
 
-    @SerialEntry
-    public Barn barn = new Barn();
+	@SerialEntry
+	public Barn barn = new Barn();
 
-    @SerialEntry
-    public Rift rift = new Rift();
+	@SerialEntry
+	public Rift rift = new Rift();
 
-    @SerialEntry
-    public TheEnd end = new TheEnd();
+	@SerialEntry
+	public TheEnd end = new TheEnd();
 
-    @SerialEntry
-    public SpidersDen spidersDen = new SpidersDen();
+	@SerialEntry
+	public SpidersDen spidersDen = new SpidersDen();
 
-    public static class Barn {
-        @SerialEntry
-        public boolean solveHungryHiker = true;
+	public static class Barn {
+		@SerialEntry
+		public boolean enableGlowingMushroomHelper = true;
 
-        @SerialEntry
-        public boolean solveTreasureHunter = true;
-    }
+		@SerialEntry
+		public boolean solveHungryHiker = true;
 
-    public static class Rift {
-        @SerialEntry
-        public boolean mirrorverseWaypoints = true;
+		@SerialEntry
+		public boolean solveTreasureHunter = true;
+	}
 
-        @SerialEntry
-        public boolean blobbercystGlow = true;
+	public static class Rift {
+		@SerialEntry
+		public boolean mirrorverseWaypoints = true;
 
-        @SerialEntry
-        public boolean enigmaSoulWaypoints = false;
+		@SerialEntry
+		public boolean blobbercystGlow = true;
 
-        @SerialEntry
-        public boolean highlightFoundEnigmaSouls = true;
+		@SerialEntry
+		public boolean enigmaSoulWaypoints = false;
 
-        @SerialEntry
-        public int mcGrubberStacks = 0;
-    }
+		@SerialEntry
+		public boolean highlightFoundEnigmaSouls = true;
 
-    public static class TheEnd {
-        @SerialEntry
-        public boolean enableEnderNodeHelper = true;
+		@SerialEntry
+		public int mcGrubberStacks = 0;
+	}
 
-        @SerialEntry
-        public boolean hudEnabled = true;
+	public static class TheEnd {
+		@SerialEntry
+		public boolean enableEnderNodeHelper = true;
 
-        @SerialEntry
-        public boolean zealotKillsEnabled = true;
+		@SerialEntry
+		public boolean hudEnabled = true;
 
-        @SerialEntry
-        public boolean protectorLocationEnabled = true;
+		@SerialEntry
+		public boolean zealotKillsEnabled = true;
 
-        @SerialEntry
-        public boolean waypoint = true;
+		@SerialEntry
+		public boolean protectorLocationEnabled = true;
+
+		@SerialEntry
+		public boolean waypoint = true;
 
 		@SerialEntry
 		public boolean muteEndermanSounds = false;
 
-        @SerialEntry
-        public int x = 10;
+		@SerialEntry
+		public int x = 10;
 
-        @SerialEntry
-        public int y = 10;
-    }
+		@SerialEntry
+		public int y = 10;
+	}
 
-    public static class SpidersDen {
-        @SerialEntry
-        public Relics relics = new Relics();
-    }
+	public static class SpidersDen {
+		@SerialEntry
+		public Relics relics = new Relics();
+	}
 
-    public static class Relics {
-        @SerialEntry
-        public boolean enableRelicsHelper = false;
+	public static class Relics {
+		@SerialEntry
+		public boolean enableRelicsHelper = false;
 
-        @SerialEntry
-        public boolean highlightFoundRelics = true;
-    }
+		@SerialEntry
+		public boolean highlightFoundRelics = true;
+	}
 }
