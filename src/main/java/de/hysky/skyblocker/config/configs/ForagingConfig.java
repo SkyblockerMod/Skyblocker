b84dd664e4656ee0c8fd82fc1caa10cbbccf6c21
Peyton Brown <81496880+PeytonBrown@users.noreply.github.com>
2025-06-10 04:50:11 -0400

    Add waypoints to forest nodes (#1325)
    * Initial Implementation
    
    * Remove waypoint if block is right clicked
    
    * Changed to HIGHLIGHT waypoint type
    
    * Remove from list if not seen for 5 seconds
    
    * resolve pr comments

--- src/main/java/de/hysky/skyblocker/config/configs/ForagingConfig.java
+++ src/main/java/de/hysky/skyblocker/config/configs/ForagingConfig.java
@@ -5,7 +5,15 @@ import dev.isxander.yacl3.config.v2.api.SerialEntry;
 public class ForagingConfig {
 
 	@SerialEntry
+	public Galatea galatea = new Galatea();
+
+	@SerialEntry
 	public Hunting hunting = new Hunting();
+
+	public static class Galatea {
+		@SerialEntry
+		public boolean enableForestNodeHelper = true;
+	}
 
 	public static class Hunting {
 		
