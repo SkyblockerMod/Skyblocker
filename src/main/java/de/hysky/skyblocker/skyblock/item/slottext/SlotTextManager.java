17931aea342ce7a15fcb9fa15d4f29546a41f24b
Rime <81419447+Emirlol@users.noreply.github.com>
2025-06-24 14:46:27 +0300

    Add attribute level helper

--- src/main/java/de/hysky/skyblocker/skyblock/item/slottext/SlotTextManager.java
+++ src/main/java/de/hysky/skyblocker/skyblock/item/slottext/SlotTextManager.java
@@ -5,6 +5,7 @@ import de.hysky.skyblocker.config.SkyblockerConfigManager;
 import de.hysky.skyblocker.skyblock.WardrobeKeybinds;
 import de.hysky.skyblocker.skyblock.bazaar.BazaarHelper;
 import de.hysky.skyblocker.skyblock.chocolatefactory.ChocolateFactorySolver;
+import de.hysky.skyblocker.skyblock.hunting.AttributeLevelHelper;
 import de.hysky.skyblocker.skyblock.item.slottext.adders.*;
 import de.hysky.skyblocker.skyblock.profileviewer.ProfileViewerScreen;
 import de.hysky.skyblocker.utils.Utils;
@@ -58,7 +59,8 @@ public class SlotTextManager {
 			new EvolvingItemAdder(),
 			new NewYearCakeAdder(),
 			WardrobeKeybinds.INSTANCE,
-			new SkyblockGuideAdder()
+			new SkyblockGuideAdder(),
+			AttributeLevelHelper.INSTANCE
 	};
 	private static final ArrayList<SlotTextAdder> currentScreenAdders = new ArrayList<>();
 	private static final KeyBinding keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.skyblocker.slottext", GLFW.GLFW_KEY_LEFT_ALT, "key.categories.skyblocker"));
