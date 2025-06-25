9eb231847fce2ef1190ddf60a4bced3d5d3b1fd2
alex <8379108+Alex33856@users.noreply.github.com>
2025-06-24 22:09:27 -0400

    Add ItemStack icons, add text in Widgets Config Screen

--- src/main/java/de/hysky/skyblocker/skyblock/galatea/SweepDetailsHudWidget.java
+++ src/main/java/de/hysky/skyblocker/skyblock/galatea/SweepDetailsHudWidget.java
@@ -1,10 +1,14 @@
 package de.hysky.skyblocker.skyblock.galatea;
 
 import de.hysky.skyblocker.annotations.RegisterWidget;
+import de.hysky.skyblocker.skyblock.tabhud.config.WidgetsConfigurationScreen;
 import de.hysky.skyblocker.skyblock.tabhud.widget.ComponentBasedWidget;
+import de.hysky.skyblocker.skyblock.tabhud.widget.component.IcoTextComponent;
 import de.hysky.skyblocker.skyblock.tabhud.widget.component.PlainTextComponent;
 import de.hysky.skyblocker.utils.Location;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.text.Text;
 
 import java.util.Set;
@@ -21,7 +25,12 @@ public class SweepDetailsHudWidget extends ComponentBasedWidget {
     // todo: localization, combine toughness with tree name maybe? indent / add emoji for penalty text + test
     @Override
     public void updateContent() {
-        if (CLIENT.player == null) return;
+        if (CLIENT.player == null || CLIENT.currentScreen instanceof WidgetsConfigurationScreen) {
+            addComponent(new IcoTextComponent(new ItemStack(Items.STRIPPED_SPRUCE_LOG), Text.literal("Fig Tree")));
+            addComponent(new PlainTextComponent(Text.literal("Toughness: 3.5")));
+            addComponent(new PlainTextComponent(Text.literal("Sweep: 314.15")));
+            return;
+        }
 
         if (!SweepDetailsListener.active || System.currentTimeMillis() > SweepDetailsListener.lastMatch + 1_000) {
             SweepDetailsListener.active = false;
@@ -29,8 +38,18 @@ public class SweepDetailsHudWidget extends ComponentBasedWidget {
             return;
         }
 
-        addComponent(new PlainTextComponent(Text.literal(SweepDetailsListener.lastTreeType + " Tree")));
+        ItemStack logItemStack;
+        if (SweepDetailsListener.lastTreeType == null) {
+            logItemStack = new ItemStack(Items.BARRIER);
+        } else if (SweepDetailsListener.lastTreeType.equals("Fig")) {
+            logItemStack = new ItemStack(Items.STRIPPED_SPRUCE_LOG);
+        } else {
+            logItemStack = new ItemStack(Items.MANGROVE_LOG);
+        }
+
+        addComponent(new IcoTextComponent(logItemStack, Text.literal(SweepDetailsListener.lastTreeType + " Tree")));
         addComponent(new PlainTextComponent(Text.literal("Toughness: " + SweepDetailsListener.toughness)));
+
         Text sweepAmount;
         if (SweepDetailsListener.maxSweep > SweepDetailsListener.lastSweep) {
             sweepAmount = Text.literal(SweepDetailsListener.lastSweep + " (" + SweepDetailsListener.maxSweep + ")");
@@ -40,11 +59,11 @@ public class SweepDetailsHudWidget extends ComponentBasedWidget {
         addComponent(new PlainTextComponent(Text.literal("Sweep: ").append(sweepAmount)));
 
         if (SweepDetailsListener.axePenalty) {
-            addComponent(new PlainTextComponent(Text.literal("Axe Throw Penalty (" + SweepDetailsListener.axePenaltyAmount + "%)")));
+            addComponent(new IcoTextComponent(new ItemStack(Items.BARRIER), Text.literal("Axe Throw Penalty (" + SweepDetailsListener.axePenaltyAmount + "%)")));
         }
 
         if (SweepDetailsListener.stylePenalty) {
-            addComponent(new PlainTextComponent(Text.literal("Style Penalty (" + SweepDetailsListener.stylePenaltyAmount + "%)")));
+            addComponent(new IcoTextComponent(new ItemStack(Items.BARRIER), Text.literal("Style Penalty (" + SweepDetailsListener.stylePenaltyAmount + "%)")));
             addComponent(new PlainTextComponent(Text.literal("Correct Style: " + SweepDetailsListener.correctStyle)));
         }
     }
