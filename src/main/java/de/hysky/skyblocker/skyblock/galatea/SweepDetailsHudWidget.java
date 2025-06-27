27880b2d2ba3a04a0d1da41f52de310481d49c31
alex <8379108+Alex33856@users.noreply.github.com>
2025-06-27 17:38:24 -0400

    Reformat

--- src/main/java/de/hysky/skyblocker/skyblock/galatea/SweepDetailsHudWidget.java
+++ src/main/java/de/hysky/skyblocker/skyblock/galatea/SweepDetailsHudWidget.java
@@ -17,13 +17,13 @@ import java.util.Set;
 
 @RegisterWidget
 public class SweepDetailsHudWidget extends ComponentBasedWidget {
-    private final MinecraftClient CLIENT = MinecraftClient.getInstance();
-	public static final Set<Location> locations = Set.of(Location.GALATEA, Location.THE_PARK);
+	private final MinecraftClient CLIENT = MinecraftClient.getInstance();
+	public static final Set<Location> LOCATIONS = Set.of(Location.GALATEA, Location.THE_PARK);
 
-    public SweepDetailsHudWidget() {
-        super(Text.translatable("skyblocker.galatea.hud.sweepDetails"), 0xFF6E37CC, "sweepDetails");
-        update();
-    }
+	public SweepDetailsHudWidget() {
+		super(Text.translatable("skyblocker.galatea.hud.sweepDetails"), 0xFF6E37CC, "sweepDetails");
+		update();
+	}
 
 	private ItemStack getLogItem(String logName) {
 		return switch (logName) {
@@ -38,68 +38,68 @@ public class SweepDetailsHudWidget extends ComponentBasedWidget {
 		};
 	}
 
-    @Override
-    public void updateContent() {
-        if (CLIENT.player == null || CLIENT.currentScreen instanceof WidgetsConfigurationScreen) {
-            addComponent(new IcoTextComponent(new ItemStack(Items.STRIPPED_SPRUCE_LOG), Text.translatable("skyblocker.galatea.hud.sweepDetails.treeType", "Fig")));
-            addComponent(new PlainTextComponent(Text.translatable("skyblocker.galatea.hud.sweepDetails.toughness", 3.5)));
-            addComponent(new PlainTextComponent(Text.translatable("skyblocker.galatea.hud.sweepDetails.sweep", 314.15)));
-            return;
-        }
+	@Override
+	public void updateContent() {
+		if (CLIENT.player == null || CLIENT.currentScreen instanceof WidgetsConfigurationScreen) {
+			addComponent(new IcoTextComponent(new ItemStack(Items.STRIPPED_SPRUCE_LOG), Text.translatable("skyblocker.galatea.hud.sweepDetails.treeType", "Fig")));
+			addComponent(new PlainTextComponent(Text.translatable("skyblocker.galatea.hud.sweepDetails.toughness", 3.5)));
+			addComponent(new PlainTextComponent(Text.translatable("skyblocker.galatea.hud.sweepDetails.sweep", 314.15)));
+			return;
+		}
 
-        if (!SweepDetailsListener.active || System.currentTimeMillis() > SweepDetailsListener.lastMatch + 1_000) {
-            SweepDetailsListener.active = false;
-            addComponent(new IcoTextComponent(new ItemStack(Items.STONE_AXE), Text.translatable("skyblocker.galatea.hud.sweepDetails.inactive")));
-            return;
-        }
+		if (!SweepDetailsListener.active || System.currentTimeMillis() > SweepDetailsListener.lastMatch + 1_000) {
+			SweepDetailsListener.active = false;
+			addComponent(new IcoTextComponent(new ItemStack(Items.STONE_AXE), Text.translatable("skyblocker.galatea.hud.sweepDetails.inactive")));
+			return;
+		}
 
-        addComponent(new IcoTextComponent(getLogItem(SweepDetailsListener.lastTreeType), Text.translatable("skyblocker.galatea.hud.sweepDetails.treeType", SweepDetailsListener.lastTreeType)));
-        addComponent(new PlainTextComponent(Text.translatable("skyblocker.galatea.hud.sweepDetails.toughness", SweepDetailsListener.toughness)));
+		addComponent(new IcoTextComponent(getLogItem(SweepDetailsListener.lastTreeType), Text.translatable("skyblocker.galatea.hud.sweepDetails.treeType", SweepDetailsListener.lastTreeType)));
+		addComponent(new PlainTextComponent(Text.translatable("skyblocker.galatea.hud.sweepDetails.toughness", SweepDetailsListener.toughness)));
 
-        Text sweepAmount;
-        final int greenColor = 0xFF00FF00;
-        final int redColor = 0xFFFF5555;
-        final int defaultColor = 0xFFFFFFFF;
-        if (SweepDetailsListener.maxSweep > SweepDetailsListener.lastSweep) {
-            MutableText lastSweep = Text.literal(Float.toString(SweepDetailsListener.lastSweep)).withColor(redColor);
-            Text thisSweep = Text.literal(Float.toString(SweepDetailsListener.maxSweep)).withColor(greenColor);
-            sweepAmount = lastSweep.append(Text.literal(" (").withColor(defaultColor)).append(thisSweep).append(Text.literal(")").withColor(defaultColor));
-        } else {
-            sweepAmount = Text.literal(Float.toString(SweepDetailsListener.maxSweep)).withColor(greenColor);
-        }
-        addComponent(new PlainTextComponent(Text.translatable("skyblocker.galatea.hud.sweepDetails.sweep", sweepAmount)));
+		Text sweepAmount;
+		final int greenColor = 0xFF00FF00;
+		final int redColor = 0xFFFF5555;
+		final int defaultColor = 0xFFFFFFFF;
+		if (SweepDetailsListener.maxSweep > SweepDetailsListener.lastSweep) {
+			MutableText lastSweep = Text.literal(Float.toString(SweepDetailsListener.lastSweep)).withColor(redColor);
+			Text thisSweep = Text.literal(Float.toString(SweepDetailsListener.maxSweep)).withColor(greenColor);
+			sweepAmount = lastSweep.append(Text.literal(" (").withColor(defaultColor)).append(thisSweep).append(Text.literal(")").withColor(defaultColor));
+		} else {
+			sweepAmount = Text.literal(Float.toString(SweepDetailsListener.maxSweep)).withColor(greenColor);
+		}
+		addComponent(new PlainTextComponent(Text.translatable("skyblocker.galatea.hud.sweepDetails.sweep", sweepAmount)));
 
 		addComponent(new PlainTextComponent(Text.translatable("skyblocker.galatea.hud.sweepDetails.logs", Text.literal(SweepDetailsListener.logs).withColor(greenColor))));
 
-        if (SweepDetailsListener.axePenalty) {
-            addComponent(new IcoTextComponent(new ItemStack(Items.BARRIER), Text.translatable("skyblocker.galatea.hud.sweepDetails.throwPenalty", SweepDetailsListener.axePenaltyAmount + "%")));
-        }
+		if (SweepDetailsListener.axePenalty) {
+			addComponent(new IcoTextComponent(new ItemStack(Items.BARRIER), Text.translatable("skyblocker.galatea.hud.sweepDetails.throwPenalty", SweepDetailsListener.axePenaltyAmount + "%")));
+		}
 
-        if (SweepDetailsListener.stylePenalty) {
-            addComponent(new IcoTextComponent(new ItemStack(Items.BARRIER), Text.translatable("skyblocker.galatea.hud.sweepDetails.stylePenalty", SweepDetailsListener.stylePenaltyAmount + "%")));
-            addComponent(new PlainTextComponent(Text.translatable("skyblocker.galatea.hud.sweepDetails.correctStyle", SweepDetailsListener.correctStyle)));
-        }
-    }
+		if (SweepDetailsListener.stylePenalty) {
+			addComponent(new IcoTextComponent(new ItemStack(Items.BARRIER), Text.translatable("skyblocker.galatea.hud.sweepDetails.stylePenalty", SweepDetailsListener.stylePenaltyAmount + "%")));
+			addComponent(new PlainTextComponent(Text.translatable("skyblocker.galatea.hud.sweepDetails.correctStyle", SweepDetailsListener.correctStyle)));
+		}
+	}
 
-    @Override
-    public Set<Location> availableLocations() {
-        return locations;
-    }
+	@Override
+	public Set<Location> availableLocations() {
+		return LOCATIONS;
+	}
 
-    @Override
-    public void setEnabledIn(Location location, boolean enabled) {
-        if (!availableLocations().contains(location)) return;
-        SkyblockerConfigManager.get().foraging.galatea.enableSweepDetailsWidget = enabled;
-    }
+	@Override
+	public void setEnabledIn(Location location, boolean enabled) {
+		if (!availableLocations().contains(location)) return;
+		SkyblockerConfigManager.get().foraging.galatea.enableSweepDetailsWidget = enabled;
+	}
 
-    @Override
-    public boolean isEnabledIn(Location location) {
-        if (!availableLocations().contains(location)) return false;
-        return SkyblockerConfigManager.get().foraging.galatea.enableSweepDetailsWidget;
-    }
+	@Override
+	public boolean isEnabledIn(Location location) {
+		if (!availableLocations().contains(location)) return false;
+		return SkyblockerConfigManager.get().foraging.galatea.enableSweepDetailsWidget;
+	}
 
-    @Override
-    public boolean shouldUpdateBeforeRendering() {
-        return true;
-    }
+	@Override
+	public boolean shouldUpdateBeforeRendering() {
+		return true;
+	}
 }
--- src/main/java/de/hysky/skyblocker/skyblock/galatea/SweepDetailsListener.java
+++ src/main/java/de/hysky/skyblocker/skyblock/galatea/SweepDetailsListener.java
@@ -12,104 +12,104 @@ import java.util.regex.Pattern;
 
 @SuppressWarnings("RegExpRepeatedSpace") // followup messages have 2 leading spaces
 public class SweepDetailsListener implements ChatMessageListener {
-    protected static final Pattern SWEEP_DETAILS = Pattern.compile("Sweep Details: ([\\d.]+)∮ Sweep");
-    protected static final Pattern TREE_TOUGHNESS = Pattern.compile("  (.+?) Tree Toughness: ([\\d.]+) ([\\d.]+) Logs");
-    protected static final Pattern AXE_THROW_PENALTY = Pattern.compile("  Axe throw: (-\\d+)% Sweep ([\\d.]+) Logs");
-    protected static final Pattern WRONG_STYLE_PENALTY = Pattern.compile("  Wrong Style: (-\\d+)% Sweep ([\\d.]+) Logs ([a-zA-Z ]*)!!");
+	protected static final Pattern SWEEP_DETAILS = Pattern.compile("Sweep Details: ([\\d.]+)∮ Sweep");
+	protected static final Pattern TREE_TOUGHNESS = Pattern.compile("  (.+?) Tree Toughness: ([\\d.]+) ([\\d.]+) Logs");
+	protected static final Pattern AXE_THROW_PENALTY = Pattern.compile("  Axe throw: (-\\d+)% Sweep ([\\d.]+) Logs");
+	protected static final Pattern WRONG_STYLE_PENALTY = Pattern.compile("  Wrong Style: (-\\d+)% Sweep ([\\d.]+) Logs ([a-zA-Z ]*)!!");
 
-    public static boolean active = false;
-    public static float lastMatch = -1;
+	public static boolean active = false;
+	public static float lastMatch = -1;
 
-    public static float maxSweep = -1;
-    public static float lastSweep = -1;
-    public static String lastTreeType = "Unknown";
-    public static String toughness;
-    public static String logs;
-    public static boolean axePenalty;
-    public static float axePenaltyAmount;
-    public static boolean stylePenalty;
-    public static float stylePenaltyAmount;
-    public static String correctStyle;
+	public static float maxSweep = -1;
+	public static float lastSweep = -1;
+	public static String lastTreeType = "Unknown";
+	public static String toughness;
+	public static String logs;
+	public static boolean axePenalty;
+	public static float axePenaltyAmount;
+	public static boolean stylePenalty;
+	public static float stylePenaltyAmount;
+	public static String correctStyle;
 
-    private static void resetStats() {
-        active = false;
-        lastMatch = -1;
-        maxSweep = -1;
-        lastSweep = -1;
+	private static void resetStats() {
+		active = false;
+		lastMatch = -1;
+		maxSweep = -1;
+		lastSweep = -1;
 		lastTreeType = "Unknown";
-        axePenalty = false;
-        stylePenalty = false;
-    }
+		axePenalty = false;
+		stylePenalty = false;
+	}
 
-    @Override
-    public ChatFilterResult onMessage(Text message, String asString) {
-        if (!SweepDetailsHudWidget.locations.contains(Utils.getLocation())) return ChatFilterResult.PASS;
-        if (!SkyblockerConfigManager.get().foraging.galatea.enableSweepDetailsWidget) return ChatFilterResult.PASS;
-        String msg = message.getString();
+	@Override
+	public ChatFilterResult onMessage(Text message, String asString) {
+		if (!SweepDetailsHudWidget.LOCATIONS.contains(Utils.getLocation())) return ChatFilterResult.PASS;
+		if (!SkyblockerConfigManager.get().foraging.galatea.enableSweepDetailsWidget) return ChatFilterResult.PASS;
+		String msg = message.getString();
 
-        Matcher sweepDetails = SWEEP_DETAILS.matcher(msg);
-        if (sweepDetails.matches() && sweepDetails.groupCount() == 1) {
-            resetStats();
-            active = true;
-            lastMatch = System.currentTimeMillis();
+		Matcher sweepDetails = SWEEP_DETAILS.matcher(msg);
+		if (sweepDetails.matches() && sweepDetails.groupCount() == 1) {
+			resetStats();
+			active = true;
+			lastMatch = System.currentTimeMillis();
 
-            String rawMaxSweep = sweepDetails.group(1);
-            if (NumberUtils.isCreatable(rawMaxSweep)) {
-                maxSweep = Float.parseFloat(rawMaxSweep);
-            } else {
-                maxSweep = -1;
-            }
-            lastSweep = maxSweep;
+			String rawMaxSweep = sweepDetails.group(1);
+			if (NumberUtils.isCreatable(rawMaxSweep)) {
+				maxSweep = Float.parseFloat(rawMaxSweep);
+			} else {
+				maxSweep = -1;
+			}
+			lastSweep = maxSweep;
 
-            return ChatFilterResult.FILTER;
-        }
+			return ChatFilterResult.FILTER;
+		}
 
 		if (active && System.currentTimeMillis() > lastMatch + 1_000) active = false;
 		if (!active) return ChatFilterResult.PASS;
 
-        Matcher treeToughness = TREE_TOUGHNESS.matcher(msg);
-        if (treeToughness.matches() && treeToughness.groupCount() == 3) {
+		Matcher treeToughness = TREE_TOUGHNESS.matcher(msg);
+		if (treeToughness.matches() && treeToughness.groupCount() == 3) {
 
-            lastTreeType = treeToughness.group(1);
-            toughness = treeToughness.group(2);
-            logs = treeToughness.group(3);
-            return ChatFilterResult.FILTER;
-        }
+			lastTreeType = treeToughness.group(1);
+			toughness = treeToughness.group(2);
+			logs = treeToughness.group(3);
+			return ChatFilterResult.FILTER;
+		}
 
-        Matcher axeThrow = AXE_THROW_PENALTY.matcher(msg);
-        if (axeThrow.matches() && axeThrow.groupCount() == 2) {
-            axePenalty = true;
+		Matcher axeThrow = AXE_THROW_PENALTY.matcher(msg);
+		if (axeThrow.matches() && axeThrow.groupCount() == 2) {
+			axePenalty = true;
 
-            String rawAxePenaltyAmount = axeThrow.group(1);
-            if (NumberUtils.isCreatable(rawAxePenaltyAmount)) {
-                axePenaltyAmount = Float.parseFloat(rawAxePenaltyAmount);
-                lastSweep *= -axePenaltyAmount / 100;
-            } else {
-                axePenaltyAmount = -1;
-            }
+			String rawAxePenaltyAmount = axeThrow.group(1);
+			if (NumberUtils.isCreatable(rawAxePenaltyAmount)) {
+				axePenaltyAmount = Float.parseFloat(rawAxePenaltyAmount);
+				lastSweep *= -axePenaltyAmount / 100;
+			} else {
+				axePenaltyAmount = -1;
+			}
 
-            logs = axeThrow.group(2);
-            return ChatFilterResult.FILTER;
-        }
+			logs = axeThrow.group(2);
+			return ChatFilterResult.FILTER;
+		}
 
-        Matcher wrongStyle = WRONG_STYLE_PENALTY.matcher(msg);
-        if (wrongStyle.matches() && wrongStyle.groupCount() == 3) {
-            stylePenalty = true;
+		Matcher wrongStyle = WRONG_STYLE_PENALTY.matcher(msg);
+		if (wrongStyle.matches() && wrongStyle.groupCount() == 3) {
+			stylePenalty = true;
 
-            String rawStylePenaltyAmount = wrongStyle.group(1);
-            if (NumberUtils.isCreatable(rawStylePenaltyAmount)) {
-                stylePenaltyAmount = Float.parseFloat(rawStylePenaltyAmount);
-                lastSweep *= -stylePenaltyAmount / 100;
-                System.out.println(lastSweep);
-            } else {
-                stylePenaltyAmount = -1;
-            }
+			String rawStylePenaltyAmount = wrongStyle.group(1);
+			if (NumberUtils.isCreatable(rawStylePenaltyAmount)) {
+				stylePenaltyAmount = Float.parseFloat(rawStylePenaltyAmount);
+				lastSweep *= -stylePenaltyAmount / 100;
+				System.out.println(lastSweep);
+			} else {
+				stylePenaltyAmount = -1;
+			}
 
-            logs = wrongStyle.group(2);
-            correctStyle = wrongStyle.group(3);
-            return ChatFilterResult.FILTER;
-        }
+			logs = wrongStyle.group(2);
+			correctStyle = wrongStyle.group(3);
+			return ChatFilterResult.FILTER;
+		}
 
-        return ChatFilterResult.PASS;
-    }
+		return ChatFilterResult.PASS;
+	}
 }
