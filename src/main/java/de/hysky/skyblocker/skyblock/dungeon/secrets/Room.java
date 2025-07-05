ce173e7d98afe8814f55eebdc82a7febad1a8ca1
daroche <gatiendr@gmail.com>
2025-07-05 12:10:25 +0200

    Avoid secrets getting wrongfully marked as found Now disables secret rendering as long as chat reports all secrets found. If it changes (meaning the current room is not cleared), it will re-enable rendering.

--- src/main/java/de/hysky/skyblocker/skyblock/dungeon/secrets/Room.java
+++ src/main/java/de/hysky/skyblocker/skyblock/dungeon/secrets/Room.java
@@ -57,6 +57,7 @@ public class Room implements Tickable, Renderable {
     private final Type type;
     @NotNull
     final Set<Vector2ic> segments;
+    private boolean isCleared = false;
 
     /**
      * The shape of the room. See {@link #getShape(IntSortedSet, IntSortedSet)}.
@@ -520,7 +521,7 @@ public class Room implements Tickable, Renderable {
         }
 
         synchronized (this) {
-            if (SkyblockerConfigManager.get().dungeons.secretWaypoints.enableSecretWaypoints && isMatched()) {
+            if (SkyblockerConfigManager.get().dungeons.secretWaypoints.enableSecretWaypoints && isMatched() && !isCleared) {
                 for (SecretWaypoint secretWaypoint : secretWaypoints.values()) {
                     if (secretWaypoint.shouldRender()) {
                         secretWaypoint.render(context);
@@ -535,8 +536,9 @@ public class Room implements Tickable, Renderable {
      */
     protected void onChatMessage(String message) {
         if (isAllSecretsFound(message)) {
-            markAllSecrets(true);
+            isCleared = true;
         } else if (LOCKED_CHEST.equals(message) && lastChestSecretTime + 1000 > System.currentTimeMillis() && lastChestSecret != null) {
+            isCleared = false;
             secretWaypoints.column(lastChestSecret).values().stream().filter(SecretWaypoint::needsInteraction).findAny()
                     .ifPresent(secretWaypoint -> markSecretsAndLogInfo(secretWaypoint, false, "[Skyblocker Dungeon Secrets] Detected locked chest interaction, setting secret #{} as missing", secretWaypoint.secretIndex));
         }
