27fb44600edef0cd58a4993d79ccbf935e73a64f
ninjaguardian <cjfairchild51@gmail.com>
2025-07-12 13:34:04 -0500

    Annotate environment according to PopupScreen

--- src/main/java/de/hysky/skyblocker/mixins/accessors/PopupBackgroundAccessor.java
+++ src/main/java/de/hysky/skyblocker/mixins/accessors/PopupBackgroundAccessor.java
@@ -1,10 +1,13 @@
 package de.hysky.skyblocker.mixins.accessors;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.screen.PopupScreen;
 import net.minecraft.client.gui.screen.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
+@Environment(EnvType.CLIENT)
 @Mixin(PopupScreen.class)
 public interface PopupBackgroundAccessor {
 	@Accessor("backgroundScreen")
