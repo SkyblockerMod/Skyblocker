6e1deb0bc3d3fbb8c5768e116762de3f6770f953
Alex <8379108+Alex33856@users.noreply.github.com>
2025-07-13 00:16:52 -0400

    Add custom color option, localization changes (#1486)

--- src/main/java/de/hysky/skyblocker/config/configs/DungeonsConfig.java
+++ src/main/java/de/hysky/skyblocker/config/configs/DungeonsConfig.java
@@ -4,6 +4,8 @@ import de.hysky.skyblocker.utils.waypoint.Waypoint;
 import net.minecraft.client.resource.language.I18n;
 import net.minecraft.util.Formatting;
 
+import java.awt.Color;
+
 public class DungeonsConfig {
 	public boolean fancyPartyFinder = true;
 
@@ -100,6 +102,8 @@ public class DungeonsConfig {
 
 	public static class Livid {
 		public boolean enableSolidColor = false;
+
+		public Color customColor = Color.RED;
 
 		public boolean enableLividColorGlow = false;
 
