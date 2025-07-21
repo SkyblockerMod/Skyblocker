acd22c9fcff73f457ccce04b079cf89aca4a5c64
Internet Yamero <119892303+AC19970@users.noreply.github.com>
2025-07-22 03:51:04 +0800

    [WIP] Standardized text style (#1380)
    * Standardized text style
    
    - Unified spelling and punctuation
    - Rewrote ambiguous sentences
    
    * Update en_us.json
    
    * Update DungeonsConfig.java
    
    * Update en_us.json
    
    * Update DungeonsConfig.java

--- src/main/java/de/hysky/skyblocker/config/configs/DungeonsConfig.java
+++ src/main/java/de/hysky/skyblocker/config/configs/DungeonsConfig.java
@@ -221,7 +221,7 @@ public class DungeonsConfig {
 
 		public int dungeonCryptsMessageThreshold = 250;
 
-		public String dungeonCryptsMessage = "We only have [crypts] crypts out of 5, we need more!";
+		public String dungeonCryptsMessage = "Crypts: [crypts]/5";
 
 		public boolean enableScoreHUD = true;
 
