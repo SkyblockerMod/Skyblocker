1721d3195d82196ba4f2baab2c538c979bf1c40d
617excelle <109393182+617excelle@users.noreply.github.com>
2025-07-25 16:55:44 +0200

    javadoc merge removal fix

--- src/main/java/de/hysky/skyblocker/config/configs/UIAndVisualsConfig.java
+++ src/main/java/de/hysky/skyblocker/config/configs/UIAndVisualsConfig.java
@@ -18,57 +18,57 @@ public class UIAndVisualsConfig {
 
 	public int nightVisionStrength = 100;
 
-    public boolean compactorDeletorPreview = true;
+	public boolean compactorDeletorPreview = true;
 
-    public boolean dontStripSkinAlphaValues = true;
+	public boolean dontStripSkinAlphaValues = true;
 
-    public boolean backpackPreviewWithoutShift = false;
+	public boolean backpackPreviewWithoutShift = false;
 
-    public boolean hideEmptyTooltips = true;
+	public boolean hideEmptyTooltips = true;
 
-    public boolean fancyCraftingTable = true;
+	public boolean fancyCraftingTable = true;
 
-    public boolean hideStatusEffectOverlay = true;
+	public boolean hideStatusEffectOverlay = true;
 
-    public boolean showEquipmentInInventory = true;
+	public boolean showEquipmentInInventory = true;
 
-    public boolean cancelComponentUpdateAnimation = true;
+	public boolean cancelComponentUpdateAnimation = true;
 
 	public boolean showCustomizeButton = true;
 
 	public boolean showConfigButton = false;
 
-    public ChestValue chestValue = new ChestValue();
+	public ChestValue chestValue = new ChestValue();
 
-    public ItemCooldown itemCooldown = new ItemCooldown();
+	public ItemCooldown itemCooldown = new ItemCooldown();
 
 	public SlotText slotText = new SlotText();
 
-    public InventorySearchConfig inventorySearch = new InventorySearchConfig();
+	public InventorySearchConfig inventorySearch = new InventorySearchConfig();
 
-    public TitleContainer titleContainer = new TitleContainer();
+	public TitleContainer titleContainer = new TitleContainer();
 
-    public TabHudConf tabHud = new TabHudConf();
+	public TabHudConf tabHud = new TabHudConf();
 
-    public FancyAuctionHouse fancyAuctionHouse = new FancyAuctionHouse();
+	public FancyAuctionHouse fancyAuctionHouse = new FancyAuctionHouse();
 
-    public Bars bars = new Bars();
+	public Bars bars = new Bars();
 
-    public Waypoints waypoints = new Waypoints();
+	public Waypoints waypoints = new Waypoints();
 
-    public TeleportOverlay teleportOverlay = new TeleportOverlay();
+	public TeleportOverlay teleportOverlay = new TeleportOverlay();
 
-    public SmoothAOTE smoothAOTE = new SmoothAOTE();
+	public SmoothAOTE smoothAOTE = new SmoothAOTE();
 
-    public SearchOverlay searchOverlay = new SearchOverlay();
+	public SearchOverlay searchOverlay = new SearchOverlay();
 
 	public BazaarQuickQuantities bazaarQuickQuantities = new BazaarQuickQuantities();
 
-    public InputCalculator inputCalculator = new InputCalculator();
+	public InputCalculator inputCalculator = new InputCalculator();
 
-    public FlameOverlay flameOverlay = new FlameOverlay();
+	public FlameOverlay flameOverlay = new FlameOverlay();
 
-    public CompactDamage compactDamage = new CompactDamage();
+	public CompactDamage compactDamage = new CompactDamage();
 
 	public HealthBars healthBars = new HealthBars();
 
@@ -76,17 +76,17 @@ public class UIAndVisualsConfig {
 
 	public ItemPickup itemPickup = new ItemPickup();
 
-    public static class ChestValue {
-        public boolean enableChestValue = true;
+	public static class ChestValue {
+		public boolean enableChestValue = true;
 
-        public Formatting color = Formatting.DARK_GREEN;
+		public Formatting color = Formatting.DARK_GREEN;
 
-        public Formatting incompleteColor = Formatting.BLUE;
-    }
+		public Formatting incompleteColor = Formatting.BLUE;
+	}
 
-    public static class ItemCooldown {
-        public boolean enableItemCooldowns = true;
-    }
+	public static class ItemCooldown {
+		public boolean enableItemCooldowns = true;
+	}
 
 	public static class SlotText {
 		public SlotTextMode slotTextMode = SlotTextMode.ENABLED;
@@ -97,93 +97,107 @@ public class UIAndVisualsConfig {
 
 	}
 
-    public static class InventorySearchConfig {
-        public EnableState enabled = EnableState.SKYBLOCK;
+	public static class InventorySearchConfig {
+		public EnableState enabled = EnableState.SKYBLOCK;
 
-        public boolean ctrlK = false;
+		public boolean ctrlK = false;
 
-        public boolean clickableText = false;
+		public boolean clickableText = false;
 
-        public enum EnableState {
-            OFF,
-            SKYBLOCK,
-            EVERYWHERE;
+		public enum EnableState {
+			OFF,
+			SKYBLOCK,
+			EVERYWHERE;
 
-            @Override
-            public String toString() {
-                return I18n.translate("skyblocker.config.uiAndVisuals.inventorySearch.state." + this.name());
-            }
+			@Override
+			public String toString() {
+				return I18n.translate("skyblocker.config.uiAndVisuals.inventorySearch.state." + this.name());
+			}
 
-            public boolean isEnabled() {
-                return switch (this) {
-                    case OFF -> false;
-                    case SKYBLOCK -> de.hysky.skyblocker.utils.Utils.isOnSkyblock();
-                    case EVERYWHERE -> true;
-                };
-            }
-        }
-    }
+			public boolean isEnabled() {
+				return switch (this) {
+					case OFF -> false;
+					case SKYBLOCK -> de.hysky.skyblocker.utils.Utils.isOnSkyblock();
+					case EVERYWHERE -> true;
+				};
+			}
+		}
+	}
 
-    public static class TitleContainer {
-        public float titleContainerScale = 100;
+	public static class TitleContainer {
+		public float titleContainerScale = 100;
 
-        public int x = 540;
+		public int x = 540;
 
-        public int y = 10;
+		public int y = 10;
 
-        public Direction direction = Direction.VERTICAL;
+		public Direction direction = Direction.VERTICAL;
 
-        public Alignment alignment = Alignment.MIDDLE;
+		public Alignment alignment = Alignment.MIDDLE;
 
-        public float getRenderScale() {
-            return titleContainerScale * 0.03f;
-        }
-    }
+		public float getRenderScale() {
+			return titleContainerScale * 0.03f;
+		}
+	}
 
-    public enum Direction {
-        HORIZONTAL, VERTICAL;
+	public enum Direction {
+		HORIZONTAL, VERTICAL;
 
-        @Override
-        public String toString() {
-            return I18n.translate("skyblocker.config.uiAndVisuals.titleContainer.direction." + name());
-        }
-    }
+		@Override
+		public String toString() {
+			return I18n.translate("skyblocker.config.uiAndVisuals.titleContainer.direction." + name());
+		}
+	}
 
-    public enum Alignment {
-        LEFT, MIDDLE, RIGHT;
+	public enum Alignment {
+		LEFT, MIDDLE, RIGHT;
 
-        @Override
-        public String toString() {
-            return I18n.translate("skyblocker.config.uiAndVisuals.titleContainer.alignment." + name());
-        }
-    }
+		@Override
+		public String toString() {
+			return I18n.translate("skyblocker.config.uiAndVisuals.titleContainer.alignment." + name());
+		}
+	}
 
-    public static class TabHudConf {
-        public boolean tabHudEnabled = true;
+	public static class TabHudConf {
+		public boolean tabHudEnabled = true;
 
-        public int tabHudScale = 100;
+		public int tabHudScale = 100;
 
 		public boolean showVanillaTabByDefault = false;
 
 		public TabHudStyle style = TabHudStyle.FANCY;
 
-        public boolean enableHudBackground = true;
+		public boolean enableHudBackground = true;
 
-        public boolean effectsFromFooter = false;
+		public boolean effectsFromFooter = false;
 
-        public ScreenBuilder.DefaultPositioner defaultPositioning = ScreenBuilder.DefaultPositioner.CENTERED;
+		public ScreenBuilder.DefaultPositioner defaultPositioning = ScreenBuilder.DefaultPositioner.CENTERED;
 
-        @Deprecated
-        public transient boolean plainPlayerNames = false;
+		@Deprecated
+		public transient boolean plainPlayerNames = false;
 
-        @Deprecated
-        public transient NameSorting nameSorting = NameSorting.DEFAULT;
-    }
+		@Deprecated
+		public transient NameSorting nameSorting = NameSorting.DEFAULT;
+	}
 
 	public enum TabHudStyle {
+		/**
+		 * The minimal style, with no decorations, icons, or custom components,
+		 * rendered in a minimal rectangle background,
+		 * or no background at all if {@link TabHudConf#enableHudBackground} is false.
+		 */
 		MINIMAL,
+		/**
+		 * The simple style, with no decorations, icons, or custom components.
+		 */
 		SIMPLE,
+		/**
+		 * The classic style, with decorations such as icons but no custom components.
+		 */
 		CLASSIC,
+		/**
+		 * The default style, with all custom components and decorations in use.
+		 */
 		FANCY;
 
 		public boolean isMinimal() {
@@ -196,34 +210,35 @@ public class UIAndVisualsConfig {
 		}
 	}
 
-    @Deprecated
-    public enum NameSorting {
-        DEFAULT, ALPHABETICAL;
+	@Deprecated
+	public enum NameSorting {
+		DEFAULT, ALPHABETICAL;
 
-        @Override
-        public String toString() {
-            return switch (this) {
-                case DEFAULT -> "Default";
-                case ALPHABETICAL -> "Alphabetical";
-            };
-        }
-    }
+		@Override
+		public String toString() {
+			return switch (this) {
+				case DEFAULT -> "Default";
+				case ALPHABETICAL -> "Alphabetical";
+			};
+		}
+	}
 
-    public static class FancyAuctionHouse {
-        public boolean enabled = false;
+	public static class FancyAuctionHouse {
+		public boolean enabled = false;
 
-        public boolean highlightCheapBIN = true;
-    }
+		public boolean highlightCheapBIN = true;
+	}
 
-    public static class Bars {
-        public boolean enableBars = true;
+	public static class Bars {
+		public boolean enableBars = true;
 
 		public IntelligenceDisplay intelligenceDisplay = IntelligenceDisplay.ORIGINAL;
 
-        @SuppressWarnings("DeprecatedIsStillUsed")
-        @Deprecated
-        public LegacyBarPositions barPositions = new LegacyBarPositions();
-    }
+		// Kept in for backwards compatibility, remove if needed
+		@SuppressWarnings("DeprecatedIsStillUsed")
+		@Deprecated
+		public LegacyBarPositions barPositions = new LegacyBarPositions();
+	}
 
 	public enum IntelligenceDisplay {
 		ORIGINAL,
@@ -231,75 +246,84 @@ public class UIAndVisualsConfig {
 		IN_FRONT;
 	}
 
-    @SuppressWarnings("DeprecatedIsStillUsed")
-    @Deprecated
-    public static class LegacyBarPositions {
-        public LegacyBarPosition healthBarPosition = LegacyBarPosition.LAYER1;
+	/**
+	 * Backwards compat.
+	 * <p>
+	 * Used to load the legacy bar positions, which will not have an effect once the bars are saved in the new format at {@code /skyblocker/status_bars.json}.
+	 * New bars do not need to be added here.
+	 */
+	@SuppressWarnings("DeprecatedIsStillUsed")
+	@Deprecated
+	public static class LegacyBarPositions {
+		public LegacyBarPosition healthBarPosition = LegacyBarPosition.LAYER1;
 
-        public LegacyBarPosition manaBarPosition = LegacyBarPosition.LAYER1;
+		public LegacyBarPosition manaBarPosition = LegacyBarPosition.LAYER1;
 
-        public LegacyBarPosition defenceBarPosition = LegacyBarPosition.RIGHT;
+		public LegacyBarPosition defenceBarPosition = LegacyBarPosition.RIGHT;
 
-        public LegacyBarPosition experienceBarPosition = LegacyBarPosition.LAYER2;
-    }
+		public LegacyBarPosition experienceBarPosition = LegacyBarPosition.LAYER2;
+	}
 
-    public enum LegacyBarPosition {
-        LAYER1, LAYER2, RIGHT, NONE
-    }
+	/**
+	 * Backwards compat
+	 */
+	public enum LegacyBarPosition {
+		LAYER1, LAYER2, RIGHT, NONE
+	}
 
-    public static class Waypoints {
-        public boolean enableWaypoints = true;
+	public static class Waypoints {
+		public boolean enableWaypoints = true;
 
-        public Waypoint.Type waypointType = Waypoint.Type.WAYPOINT;
-    }
+		public Waypoint.Type waypointType = Waypoint.Type.WAYPOINT;
+	}
 
-    public static class TeleportOverlay {
-        public boolean enableTeleportOverlays = true;
+	public static class TeleportOverlay {
+		public boolean enableTeleportOverlays = true;
 
-        public Color teleportOverlayColor = new Color(0x7F761594, true);
+		public Color teleportOverlayColor = new Color(0x7F761594, true);
 
-        public boolean enableWeirdTransmission = false;
+		public boolean enableWeirdTransmission = false;
 
-        public boolean enableInstantTransmission = false;
+		public boolean enableInstantTransmission = false;
 
-        public boolean enableEtherTransmission = true;
+		public boolean enableEtherTransmission = true;
 
-        public boolean enableSinrecallTransmission = false;
+		public boolean enableSinrecallTransmission = false;
 
-        public boolean enableWitherImpact = false;
-    }
+		public boolean enableWitherImpact = false;
+	}
 
-    public static class SmoothAOTE {
-        public boolean enableWeirdTransmission = false;
+	public static class SmoothAOTE {
+		public boolean enableWeirdTransmission = false;
 
-        public boolean enableInstantTransmission = false;
+		public boolean enableInstantTransmission = false;
 
-        public boolean enableEtherTransmission = false;
+		public boolean enableEtherTransmission = false;
 
-        public boolean enableSinrecallTransmission = false;
+		public boolean enableSinrecallTransmission = false;
 
-        public boolean enableWitherImpact = false;
+		public boolean enableWitherImpact = false;
 
 		public int maximumAddedLag = 100;
-    }
+	}
 
-    public static class SearchOverlay {
-        public boolean enableBazaar = true;
+	public static class SearchOverlay {
+		public boolean enableBazaar = true;
 
-        public boolean enableAuctionHouse = true;
+		public boolean enableAuctionHouse = true;
 
-        public boolean keepPreviousSearches = false;
+		public boolean keepPreviousSearches = false;
 
-        public int maxSuggestions = 3;
+		public int maxSuggestions = 3;
 
-        public int historyLength = 3;
+		public int historyLength = 3;
 
-        public boolean enableCommands = false;
+		public boolean enableCommands = false;
 
-        public List<String> bazaarHistory = new ArrayList<>();
+		public List<String> bazaarHistory = new ArrayList<>();
 
-        public List<String> auctionHistory = new ArrayList<>();
-    }
+		public List<String> auctionHistory = new ArrayList<>();
+	}
 
 	public static class BazaarQuickQuantities {
 		public boolean enabled = false;
@@ -313,31 +337,31 @@ public class UIAndVisualsConfig {
 		public int slot3Quantity = 256;
 	}
 
-    public static class InputCalculator {
-        public boolean enabled = true;
+	public static class InputCalculator {
+		public boolean enabled = true;
 
-        public boolean requiresEquals = false;
+		public boolean requiresEquals = false;
 
 		public boolean closeSignsWithEnter = true;
-    }
+	}
 
-    public static class FlameOverlay {
-        public int flameHeight = 100;
+	public static class FlameOverlay {
+		public int flameHeight = 100;
 
-        public int flameOpacity = 100;
-    }
+		public int flameOpacity = 100;
+	}
 
-    public static class CompactDamage {
-        public boolean enabled = true;
+	public static class CompactDamage {
+		public boolean enabled = true;
 
-        public int precision = 1;
+		public int precision = 1;
 
-        public Color normalDamageColor = new Color(0xFFFFFF);
+		public Color normalDamageColor = new Color(0xFFFFFF);
 
-        public Color critDamageGradientStart = new Color(0xFFFF55);
+		public Color critDamageGradientStart = new Color(0xFFFF55);
 
-        public Color critDamageGradientEnd = new Color(0xFF5555);
-    }
+		public Color critDamageGradientEnd = new Color(0xFF5555);
+	}
 
 	public static class HealthBars {
 		public boolean enabled = false;
