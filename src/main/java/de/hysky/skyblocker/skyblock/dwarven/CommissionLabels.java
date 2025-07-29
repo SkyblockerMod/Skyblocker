038b44afd263150fed68eb8385c6f72cb035e4d7
viciscat <51047087+viciscat@users.noreply.github.com>
2025-07-30 00:52:37 +0200

    fix AttributeLevelHelper not showing in config and make CommissionLabels work when CommsWidget is disabled.

--- src/main/java/de/hysky/skyblocker/skyblock/dwarven/CommissionLabels.java
+++ src/main/java/de/hysky/skyblocker/skyblock/dwarven/CommissionLabels.java
@@ -3,10 +3,14 @@ package de.hysky.skyblocker.skyblock.dwarven;
 import de.hysky.skyblocker.annotations.Init;
 import de.hysky.skyblocker.config.SkyblockerConfigManager;
 import de.hysky.skyblocker.config.configs.MiningConfig;
+import de.hysky.skyblocker.skyblock.tabhud.util.PlayerListManager;
 import de.hysky.skyblocker.utils.Location;
 import de.hysky.skyblocker.utils.Utils;
+import de.hysky.skyblocker.utils.scheduler.Scheduler;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
+import net.minecraft.client.network.PlayerListEntry;
+import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
 
 import java.util.ArrayList;
@@ -14,6 +18,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class CommissionLabels {
@@ -23,11 +29,53 @@ public class CommissionLabels {
     private static final Map<String, MiningLocationLabel.GlaciteCategory> GLACITE_LOCATIONS = Arrays.stream(MiningLocationLabel.GlaciteCategory.values()).collect(Collectors.toMap(MiningLocationLabel.GlaciteCategory::toString, Function.identity()));
 
     protected static List<MiningLocationLabel> activeWaypoints = new ArrayList<>();
+	private static List<String> commissions = List.of();
+	private static boolean commissionDone = false;
+	private static final Pattern COMM_PATTERN = Pattern.compile("(?<name>.*): (?<progress>.*)%?");
+
 
     @Init
     public static void init() {
         WorldRenderEvents.AFTER_TRANSLUCENT.register(CommissionLabels::render);
+		Scheduler.INSTANCE.scheduleCyclic(CommissionLabels::tick, 20);
     }
+
+	public static boolean enabled() {
+		return SkyblockerConfigManager.get().mining.commissionWaypoints.mode != MiningConfig.CommissionWaypointMode.OFF;
+	}
+
+	private static void tick() {
+		if (!Utils.isInDwarvenMines() || !enabled()) return;
+		boolean foundCommissions = false;
+
+		List<String> newCommissions = new ArrayList<>();
+		boolean newCommissionDone = false;
+
+		for (int i = 0; i < PlayerListManager.getPlayerList().size(); i++) {
+			PlayerListEntry entry = PlayerListManager.getPlayerList().get(i);
+			Text displayName = entry.getDisplayName();
+			if (displayName == null) continue;
+			String string = displayName.getString();
+			if (foundCommissions) {
+				if (!string.startsWith(" ")) break;
+				string = string.substring(1);
+				Matcher matcher = COMM_PATTERN.matcher(string);
+				if (matcher.matches()) {
+					String name = matcher.group("name");
+					String progress = matcher.group("progress");
+					newCommissionDone = newCommissionDone || "DONE".equals(progress);
+					newCommissions.add(name);
+				}
+			} else if (string.startsWith("Commissions")) {
+				foundCommissions = true;
+			}
+		}
+		if (!newCommissions.equals(commissions) || newCommissionDone != commissionDone) {
+			commissions = newCommissions;
+			commissionDone = newCommissionDone;
+			update(commissions, commissionDone);
+		}
+	}
 
     /**
      * update the activeWaypoints when there is a change in commissions
@@ -36,11 +84,11 @@ public class CommissionLabels {
      * @param completed      if there is a commission completed
      */
     public static void update(List<String> newCommissions, boolean completed) {
-        MiningConfig.CommissionWaypointMode currentMode = SkyblockerConfigManager.get().mining.commissionWaypoints.mode;
-        if (currentMode == MiningConfig.CommissionWaypointMode.OFF) {
-            return;
-        }
-        activeWaypoints.clear();
+        if (!enabled()) {
+			return;
+		}
+		MiningConfig.CommissionWaypointMode currentMode = SkyblockerConfigManager.get().mining.commissionWaypoints.mode;
+		activeWaypoints.clear();
         String location = Utils.getIslandArea().substring(2);
         //find commission locations in glacite
         if (location.equals("Dwarven Base Camp") || location.equals("Glacite Tunnels") || location.equals("Glacite Mineshafts") || location.equals("Glacite Lake")) {
@@ -93,7 +141,7 @@ public class CommissionLabels {
      */
     private static void render(WorldRenderContext context) {
         // Only render in the dwarven mines and not the mineshaft.
-        if (Location.DWARVEN_MINES != Utils.getLocation() || SkyblockerConfigManager.get().mining.commissionWaypoints.mode == MiningConfig.CommissionWaypointMode.OFF) {
+        if (Location.DWARVEN_MINES != Utils.getLocation() || !enabled()) {
             return;
         }
         for (MiningLocationLabel MiningLocationLabel : activeWaypoints) {
