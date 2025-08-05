941067bdcbdea3770ec36dded785537123536d92
DaysSky <88261516+DaysSky@users.noreply.github.com>
2025-08-05 21:20:06 +0900

    checkstyle fix

--- src/main/java/de/hysky/skyblocker/skyblock/galatea/TreeBreakProgressHud.java
+++ src/main/java/de/hysky/skyblocker/skyblock/galatea/TreeBreakProgressHud.java
@@ -18,11 +18,6 @@ import net.minecraft.util.Formatting;
 import net.minecraft.util.math.Vec3d;
 
 import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 import java.util.List;
 import java.util.Set;
 
