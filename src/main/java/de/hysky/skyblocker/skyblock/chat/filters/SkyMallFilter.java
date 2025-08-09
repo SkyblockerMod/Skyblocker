ba79b1d06fe88778bd3c351652ab1a9af07e9eac
Aaron <51387595+AzureAaron@users.noreply.github.com>
2025-08-09 02:18:01 -0400

    Fix Sky Mall regex

--- src/main/java/de/hysky/skyblocker/skyblock/chat/filters/SkyMallFilter.java
+++ src/main/java/de/hysky/skyblocker/skyblock/chat/filters/SkyMallFilter.java
@@ -6,8 +6,8 @@ import de.hysky.skyblocker.utils.chat.ChatFilterResult;
 
 public class SkyMallFilter extends SimpleChatFilter {
 	private static final String SKY_MALL_BUFF_CHANGED = "^New day! Your Sky Mall buff changed!$";
-	//^New buff: Gain (?:.+) (?:Mining Speed|Mining Fortune|more Powder while mining|Pickaxe Ability cooldowns|chance to find Golden and Diamond Goblins|Titanium drops)\.$
-	private static final String SKY_MALL_BUFF = "^New buff: Gain (?:.+) (?:Mining Speed|Mining Fortune|more Powder while mining|Pickaxe Ability cooldowns|chance to find Golden and Diamond Goblins|Titanium drops)\\.$";
+	//^New buff: (?:Gain )?(?:.+) (?:Mining Speed|Mining Fortune|more Powder while mining|Pickaxe Ability cooldowns|chance to find Golden and Diamond Goblins|Titanium drops)\.$
+	private static final String SKY_MALL_BUFF = "^New buff: (?:Gain )?(?:.+) (?:Mining Speed|Mining Fortune|more Powder while mining|Pickaxe Ability cooldowns|chance to find Golden and Diamond Goblins|Titanium drops)\\.$";
 	private static final String TOGGLE_SKY_MALL = "^You can disable this messaging by toggling Sky Mall in your /hotm!$";
 
 	public SkyMallFilter() {
