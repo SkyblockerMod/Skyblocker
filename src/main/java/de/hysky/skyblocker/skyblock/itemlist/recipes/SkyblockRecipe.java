1f24b62b1415ebcb876b8a858bb6afe54e9135af
alex <8379108+Alex33856@users.noreply.github.com>
2025-08-16 22:02:48 -0400

    Add fallback stack for item recipes to avoid showing empty recipes

--- src/main/java/de/hysky/skyblocker/skyblock/itemlist/recipes/SkyblockRecipe.java
+++ src/main/java/de/hysky/skyblocker/skyblock/itemlist/recipes/SkyblockRecipe.java
@@ -20,79 +20,80 @@ import java.util.List;
 import java.util.Locale;
 
 public interface SkyblockRecipe {
+	Logger LOGGER = LoggerFactory.getLogger(SkyblockRecipe.class);
+	NumberFormat NUMBER_FORMAT = NumberFormat.getInstance(Locale.US);
 
-    Logger LOGGER = LoggerFactory.getLogger(SkyblockRecipe.class);
-    NumberFormat NUMBER_FORMAT = NumberFormat.getInstance(Locale.US);
+	static ItemStack getItemStack(NEUIngredient input) {
+		if (input == NEUIngredient.SENTINEL_EMPTY) return Items.AIR.getDefaultStack();
 
+		ItemStack stack = ItemRepository.getItemStack(input.getItemId());
+		if (stack != null) {
+			return stack.copyWithCount((int) input.getAmount());
+		} else if (input.getItemId().equals("SKYBLOCK_COIN")) {
+			ItemStack itemStack = new ItemStack(Items.GOLD_NUGGET);
+			itemStack.set(DataComponentTypes.ITEM_NAME, Text.literal("Skyblock Coins").formatted(Formatting.GOLD));
+			itemStack.set(DataComponentTypes.ENCHANTMENT_GLINT_OVERRIDE, true);
+			String format = NUMBER_FORMAT.format(input.getAmount());
+			itemStack.set(DataComponentTypes.LORE, new LoreComponent(List.of(Text.literal(format).formatted(Formatting.GOLD).append(Text.literal(" coins.")))));
+			return itemStack;
+		}
 
+		LOGGER.warn("[Skyblocker Recipe] Unable to find item {}", input.getItemId());
+		ItemStack fallbackStack = new ItemStack(Items.BARRIER);
+		fallbackStack.set(DataComponentTypes.ITEM_NAME, Text.literal(input.getItemId()));
+		return fallbackStack;
+	}
 
-    static ItemStack getItemStack(NEUIngredient input) {
-        if (input != NEUIngredient.SENTINEL_EMPTY) {
-            ItemStack stack = ItemRepository.getItemStack(input.getItemId());
-            if (stack != null) {
-                return stack.copyWithCount((int) input.getAmount());
-            } else if (input.getItemId().equals("SKYBLOCK_COIN")) {
-                ItemStack itemStack = new ItemStack(Items.GOLD_NUGGET);
-                itemStack.set(DataComponentTypes.ITEM_NAME, Text.literal("Skyblock Coins").formatted(Formatting.GOLD));
-                itemStack.set(DataComponentTypes.ENCHANTMENT_GLINT_OVERRIDE, true);
-                String format = NUMBER_FORMAT.format(input.getAmount());
-                itemStack.set(DataComponentTypes.LORE, new LoreComponent(List.of(Text.literal(format).formatted(Formatting.GOLD).append(Text.literal(" coins.")))));
-                return itemStack;
-            } else {
-                LOGGER.warn("[Skyblocker Recipe] Unable to find item {}", input.getItemId());
-            }
-        }
-        return Items.AIR.getDefaultStack();
-    }
+	/**
+	 * @param width  the available area's width
+	 * @param height the available area's height
+	 * @return list of slots
+	 */
+	List<RecipeSlot> getInputSlots(int width, int height);
 
-    /**
-     * @param width the available area's width
-     * @param height the available area's height
-     * @return list of slots
-     */
-    List<RecipeSlot> getInputSlots(int width, int height);
+	/**
+	 * @param width  the available area's width
+	 * @param height the available area's height
+	 * @return list of slots
+	 */
+	List<RecipeSlot> getOutputSlots(int width, int height);
 
-    /**
-     * @param width the available area's width
-     * @param height the available area's height
-     * @return list of slots
-     */
-    List<RecipeSlot> getOutputSlots(int width, int height);
+	default @Nullable ScreenPos getArrowLocation(int width, int height) {
+		return null;
+	}
 
-    default @Nullable ScreenPos getArrowLocation(int width, int height) {
-        return null;
-    }
+	List<ItemStack> getInputs();
 
-    List<ItemStack> getInputs();
-    List<ItemStack> getOutputs();
+	List<ItemStack> getOutputs();
 
-    /**
-     * Render some extra things, i.e an entity
-     * @param width available width
-     * @param height available height
-     * @param mouseX mouse x
-     * @param mouseY mouse y
-     */
-    default void render(DrawContext context, int width, int height, double mouseX, double mouseY) {}
+	/**
+	 * Render some extra things, i.e an entity
+	 *
+	 * @param width  available width
+	 * @param height available height
+	 * @param mouseX mouse x
+	 * @param mouseY mouse y
+	 */
+	default void render(DrawContext context, int width, int height, double mouseX, double mouseY) {}
 
-    /**
-     * Extra text like collection requirements
-     */
-    Text getExtraText();
+	/**
+	 * Extra text like collection requirements
+	 */
+	Text getExtraText();
 
 	/**
 	 * Identifier used for REI, EMI. Also used in the recipe book for the name
 	 */
-    Identifier getCategoryIdentifier();
+	Identifier getCategoryIdentifier();
 
 	/**
 	 * Used for EMI.
 	 */
-    Identifier getRecipeIdentifier();
+	Identifier getRecipeIdentifier();
 
-    record RecipeSlot(int x, int y, ItemStack stack, boolean showBackground) {
-        public RecipeSlot(int x, int y, ItemStack stack) {
-            this(x, y, stack, true);
-        }
-    }
+	record RecipeSlot(int x, int y, ItemStack stack, boolean showBackground) {
+		public RecipeSlot(int x, int y, ItemStack stack) {
+			this(x, y, stack, true);
+		}
+	}
 }
