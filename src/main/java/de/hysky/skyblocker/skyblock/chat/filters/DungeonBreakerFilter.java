883892df216e42d1da7b4fa15d5522944502b997
alex <8379108+Alex33856@users.noreply.github.com>
2025-08-22 13:51:38 -0400

    Add DungeonBreakerFilterTest

--- src/main/java/de/hysky/skyblocker/skyblock/chat/filters/DungeonBreakerFilter.java
+++ src/main/java/de/hysky/skyblocker/skyblock/chat/filters/DungeonBreakerFilter.java
@@ -4,12 +4,6 @@ import de.hysky.skyblocker.config.SkyblockerConfigManager;
 import de.hysky.skyblocker.utils.chat.ChatFilterResult;
 
 public class DungeonBreakerFilter extends SimpleChatFilter {
-	/* Messages:
-	 * A mystical force prevents you from digging that block!
-	 * A mystical force prevents you digging in this room!
-	 * A mystical force prevents you digging there!
-	 * You don't have enough charges to break this block right now!
-	 */
 	public DungeonBreakerFilter() {
 		super("^(A mystical force prevents you (from digging that block|digging in this room|digging there)|You don't have enough charges to break this block right now)!$");
 	}
