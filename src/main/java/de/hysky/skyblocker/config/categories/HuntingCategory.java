f514be6cf697bf81c9958362bd544c6288e1d2de
Jordan Bentkowski <109393182+617excelle@users.noreply.github.com>
2025-09-08 02:53:57 +0200

    hunting added coralot & shellwise highlight (#1572)
    * hunting added coralot & shellwise highlight
    
    * added RGB pickers
    
    * changes to hunting config layout
    
    * back to old way of organizing config
    
    * tooltip changes
    
    imply on default color, and inform on color changing possibility
    
    * hideonleaf tooltip
    
    * typo

--- src/main/java/de/hysky/skyblocker/config/categories/HuntingCategory.java
+++ src/main/java/de/hysky/skyblocker/config/categories/HuntingCategory.java
@@ -6,8 +6,11 @@ import de.hysky.skyblocker.config.SkyblockerConfig;
 import net.azureaaron.dandelion.systems.ConfigCategory;
 import net.azureaaron.dandelion.systems.Option;
 import net.azureaaron.dandelion.systems.OptionGroup;
+import net.azureaaron.dandelion.systems.controllers.ColourController;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+
+import java.awt.*;
 
 public class HuntingCategory {
 
@@ -23,8 +26,6 @@ public class HuntingCategory {
 						.controller(ConfigUtils.createBooleanController())
 						.description(Text.translatable("skyblocker.config.hunting.huntingBoxHelper.@Tooltip"))
 						.build())
-
-				//Hunting Mob Features
 				.group(OptionGroup.createBuilder()
 						.name(Text.translatable("skyblocker.config.hunting.huntingMobs"))
 						.collapsed(false)
@@ -43,6 +44,49 @@ public class HuntingCategory {
 										() -> config.hunting.huntingMobs.highlightHideonleaf,
 										newValue -> config.hunting.huntingMobs.highlightHideonleaf = newValue)
 								.controller(ConfigUtils.createBooleanController())
+								.build())
+						.option(Option.<Color>createBuilder()
+								.name(Text.translatable("skyblocker.config.hunting.huntingMobs.colorPicker.Hideonleaf"))
+								.binding(defaults.hunting.huntingMobs.hideonleafGlowColor,
+										() -> config.hunting.huntingMobs.hideonleafGlowColor,
+										newValue -> {
+											config.hunting.huntingMobs.hideonleafGlowColor = newValue;
+										})
+								.controller(ColourController.createBuilder().hasAlpha(false).build())
+								.build())
+						.option(Option.<Boolean>createBuilder()
+								.name(Text.translatable("skyblocker.config.hunting.huntingMobs.highlightShellwise"))
+								.description(Text.translatable("skyblocker.config.hunting.huntingMobs.highlightShellwise.@Tooltip"))
+								.binding(defaults.hunting.huntingMobs.highlightShellwise,
+										() -> config.hunting.huntingMobs.highlightShellwise,
+										newValue -> config.hunting.huntingMobs.highlightShellwise = newValue)
+								.controller(ConfigUtils.createBooleanController())
+								.build())
+						.option(Option.<Color>createBuilder()
+								.name(Text.translatable("skyblocker.config.hunting.huntingMobs.colorPicker.Shellwise"))
+								.binding(defaults.hunting.huntingMobs.shellwiseGlowColor,
+										() -> config.hunting.huntingMobs.shellwiseGlowColor,
+										newValue -> {
+											config.hunting.huntingMobs.shellwiseGlowColor = newValue;
+										})
+								.controller(ColourController.createBuilder().hasAlpha(false).build())
+								.build())
+						.option(Option.<Boolean>createBuilder()
+								.name(Text.translatable("skyblocker.config.hunting.huntingMobs.highlightCoralot"))
+								.description(Text.translatable("skyblocker.config.hunting.huntingMobs.highlightCoralot.@Tooltip"))
+								.binding(defaults.hunting.huntingMobs.highlightCoralot,
+										() -> config.hunting.huntingMobs.highlightCoralot,
+										newValue -> config.hunting.huntingMobs.highlightCoralot = newValue)
+								.controller(ConfigUtils.createBooleanController())
+								.build())
+						.option(Option.<Color>createBuilder()
+								.name(Text.translatable("skyblocker.config.hunting.huntingMobs.colorPicker.Coralot"))
+								.binding(defaults.hunting.huntingMobs.coralotGlowColor,
+										() -> config.hunting.huntingMobs.coralotGlowColor,
+										newValue -> {
+											config.hunting.huntingMobs.coralotGlowColor = newValue;
+										})
+								.controller(ColourController.createBuilder().hasAlpha(false).build())
 								.build())
 						.build())
 				.build();
