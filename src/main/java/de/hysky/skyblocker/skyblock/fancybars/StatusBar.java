2c2b7fe80ef6b104ee91864ee09e48a89cd8e853
viciscat <51047087+viciscat@users.noreply.github.com>
2025-09-18 21:15:23 +0200

    fix rendering order

--- src/main/java/de/hysky/skyblocker/skyblock/fancybars/StatusBar.java
+++ src/main/java/de/hysky/skyblocker/skyblock/fancybars/StatusBar.java
@@ -111,6 +111,11 @@ public class StatusBar implements Widget, Drawable, Element, Selectable {
 
 	@Override
 	public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+		renderBar(context);
+		if (enabled) renderText(context);
+	}
+
+	public void renderBar(DrawContext context) {
 		if (renderWidth <= 0) return;
 		int transparency = transparency(-1);
 		switch (iconPosition) {
@@ -123,9 +128,6 @@ public class StatusBar implements Widget, Drawable, Element, Selectable {
 		context.drawGuiTexture(RenderPipelines.GUI_TEXTURED, BAR_BACK, barX, renderY + 1, barWidth, 7, transparency);
 		drawBarFill(context, barX, barWidth);
 		//context.drawText(MinecraftClient.getInstance().textRenderer, gridX + " " + gridY + " s:" + size , x, y-9, Colors.WHITE, true);
-		if (showText() && enabled) {
-			renderText(context);
-		}
 	}
 
 	protected void drawBarFill(DrawContext context, int barX, int barWith) {
