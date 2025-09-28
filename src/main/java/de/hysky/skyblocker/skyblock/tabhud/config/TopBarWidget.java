00414fbf2aaefef42632c27ba5297ed8a8a66d45
viciscat <51047087+viciscat@users.noreply.github.com>
2025-09-28 14:48:26 +0200

    fix overlap on auto GUI scale (not if you have a 4:3 monitor tho)

--- src/main/java/de/hysky/skyblocker/skyblock/tabhud/config/TopBarWidget.java
+++ src/main/java/de/hysky/skyblocker/skyblock/tabhud/config/TopBarWidget.java
@@ -18,21 +18,28 @@ import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.ColorHelper;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 class TopBarWidget extends ContainerWidget {
 	private static final Identifier TEXTURE = Identifier.of(SkyblockerMod.NAMESPACE, "menu_outer_space");
 	private static final int HEIGHT = 15;
 	private final CustomDropdownWidget<Location> locationDropdown;
 	private final CustomDropdownWidget<WidgetManager.ScreenLayer> screenLayerDropdown;
-	private final SimplePositioningWidget layout;
+	private final Layout layout;
 	private final List<ClickableWidget> widgets;
 
 	TopBarWidget(int width, WidgetsConfigScreen parent) {
 		super(0, 0, width, HEIGHT, Text.literal("hi"));
 
-		layout = new SimplePositioningWidget(getWidth(), HEIGHT);
-		layout.getMainPositioner().alignTop();
+		layout = new Layout();
+
+		StyledButtonWidget helpButton = new StyledButtonWidget(60, HEIGHT, Text.literal("Help"), button -> parent.openPopup(screen -> new PopupScreen.Builder(screen, Text.literal("Help"))
+				.message(Text.literal("Use right click to add widgets and edit their options.\nYou can delete a widget by pressing the delete key.\n\nWidgets are per island. To have a widget show up everywhere select \"Everywhere\" in the island dropdown (there's only 2 dropdowns you should be able to find it). Widget options apply to every location with a few exceptions."))
+				.button(ScreenTexts.OK, PopupScreen::close)
+				.build()));
+		layout.add(helpButton);
 
 		List<Location> locations = Lists.newArrayList(Location.values());
 		// move UNKNOWN to be first
@@ -42,8 +49,7 @@ class TopBarWidget extends ContainerWidget {
 		locationDropdown.setFormatter(location -> location == Location.UNKNOWN ? Text.literal("Everywhere").formatted(Formatting.YELLOW) : Text.literal(location.toString()));
 		screenLayerDropdown = new CustomDropdownWidget<>(width / 2 + 5, 0, 100, 200, List.of(WidgetManager.ScreenLayer.values()), parent::setCurrentScreenLayer, WidgetManager.ScreenLayer.HUD);
 
-		DirectionalLayoutWidget dropdownsLayout = DirectionalLayoutWidget.horizontal();
-		dropdownsLayout.getMainPositioner().marginX(5);
+		DirectionalLayoutWidget dropdownsLayout = DirectionalLayoutWidget.horizontal().spacing(2);
 		dropdownsLayout.add(locationDropdown);
 		dropdownsLayout.add(screenLayerDropdown);
 		layout.add(dropdownsLayout);
@@ -58,13 +64,7 @@ class TopBarWidget extends ContainerWidget {
 		DirectionalLayoutWidget togglesLayout = DirectionalLayoutWidget.horizontal();
 		togglesLayout.add(snappingToggle);
 		togglesLayout.add(autoAnchorToggle);
-		layout.add(togglesLayout, Positioner::alignRight);
-
-		StyledButtonWidget helpButton = new StyledButtonWidget(60, HEIGHT, Text.literal("Help"), button -> parent.openPopup(screen -> new PopupScreen.Builder(screen, Text.literal("Help"))
-				.message(Text.literal("Use right click to add widgets and edit their options.\nYou can delete a widget by pressing the delete key.\n\nWidgets are per island. To have a widget show up everywhere select \"Everywhere\" in the island dropdown (there's only 2 dropdowns you should be able to find it). Widget options apply to every location with a few exceptions."))
-				.button(ScreenTexts.OK, PopupScreen::close)
-				.build()));
-		layout.add(helpButton, Positioner::alignLeft);
+		layout.add(togglesLayout);
 
 		widgets = List.of(helpButton, locationDropdown, screenLayerDropdown, snappingToggle, autoAnchorToggle);
 		layout.refreshPositions();
@@ -83,7 +83,6 @@ class TopBarWidget extends ContainerWidget {
 	@Override
 	public void setWidth(int width) {
 		super.setWidth(width);
-		layout.setMinWidth(width);
 		layout.refreshPositions();
 	}
 
@@ -119,5 +118,61 @@ class TopBarWidget extends ContainerWidget {
 		context.drawVerticalLine(x - 1, y, y2, ColorHelper.withAlpha(15, -1));
 		context.drawVerticalLine(x, y, y2, ColorHelper.withAlpha(100, 0));
 		context.drawVerticalLine(x + 1, y, y2, ColorHelper.withAlpha(15, -1));
+	}
+
+	private class Layout extends WrapperWidget {
+		private final List<Widget> widgets = new ArrayList<>(4);
+
+		Layout() {
+			super(0, 0, 0, 0);
+		}
+
+		private void add(Widget widget) {
+			widgets.add(widget);
+		}
+
+		@Override
+		public void forEachElement(Consumer<Widget> consumer) {
+			widgets.forEach(consumer);
+		}
+
+		@Override
+		public int getX() {
+			return TopBarWidget.this.getX();
+		}
+
+		@Override
+		public int getY() {
+			return TopBarWidget.this.getY();
+		}
+
+		@Override
+		public int getWidth() {
+			return TopBarWidget.this.getWidth();
+		}
+
+		@Override
+		public int getHeight() {
+			return TopBarWidget.this.getHeight();
+		}
+
+		@Override
+		public void refreshPositions() {
+			super.refreshPositions();
+			Widget first = widgets.getFirst();
+			first.setPosition(getX(), getY());
+			int low = first.getX() + first.getWidth();
+			Widget last = widgets.getLast();
+			last.setPosition(getX() + getWidth() - last.getWidth(), getY());
+			int high = last.getX();
+			for (int i = 1; i < widgets.size() - 1; i++) {
+				Widget widget = widgets.get(i);
+				widget.setY(getY());
+				SimplePositioningWidget.setPos(0, getWidth(), widget.getWidth(), widget::setX, 0.5f);
+				if (widget.getX() + widget.getWidth() > high) {
+					SimplePositioningWidget.setPos(low, high - low, widget.getWidth(), widget::setX, 0.5f);
+				}
+			}
+		}
 	}
 }
