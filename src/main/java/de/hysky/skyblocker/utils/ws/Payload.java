c0015acd5f5740f46c84ac9e63b9b174a3a6533d
alex <8379108+Alex33856@users.noreply.github.com>
2025-10-04 21:08:26 -0400

    Make serverId optional

--- src/main/java/de/hysky/skyblocker/utils/ws/Payload.java
+++ src/main/java/de/hysky/skyblocker/utils/ws/Payload.java
@@ -6,11 +6,15 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.Dynamic;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
-record Payload(Type type, Service service, String serverId, Optional<Dynamic<?>> message) {
+record Payload(Type type, Service service, Optional<String> serverId, Optional<Dynamic<?>> message) {
 	static final Codec<Payload> CODEC = RecordCodecBuilder.create(instance -> instance.group(
 			Type.CODEC.fieldOf("type").forGetter(Payload::type),
 			Service.CODEC.fieldOf("service").forGetter(Payload::service),
-			Codec.STRING.fieldOf("serverId").forGetter(Payload::serverId),
+			Codec.STRING.optionalFieldOf("serverId").forGetter(Payload::serverId),
 			Codec.PASSTHROUGH.optionalFieldOf("message").forGetter(Payload::message))
 			.apply(instance, Payload::new));
+
+	Payload(Type type, Service service, String serverId, Optional<Dynamic<?>> message) {
+		this(type, service, Optional.of(serverId), message);
+	}
 }
