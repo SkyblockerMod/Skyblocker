bb913a74934d2ea7da4d601e1ded47d96bd035d4
alex <8379108+Alex33856@users.noreply.github.com>
2025-10-05 11:07:12 -0400

    Strip formatting from Plot Names for the Garden Plots Widget

--- src/main/java/de/hysky/skyblocker/skyblock/garden/GardenPlotsWidget.java
+++ src/main/java/de/hysky/skyblocker/skyblock/garden/GardenPlotsWidget.java
@@ -105,7 +105,8 @@ public class GardenPlotsWidget extends ContainerWidget {
 						ItemStack stack = slot.getStack();
 						if (stack.isEmpty() || stack.isOf(Items.RED_STAINED_GLASS_PANE) || stack.isOf(Items.OAK_BUTTON) || stack.isOf(Items.BLACK_STAINED_GLASS_PANE))
 							continue;
-						String name = stack.getName().getString();
+						// SkyHanni adds formatting codes to the plot names when using their custom plot icons.
+						String name = Formatting.strip(stack.getName().getString());
 						String[] parts = name.split("-", 2);
 						if (parts.length < 2) {
 							LOGGER.warn("Invalid plot name: {}", name);
