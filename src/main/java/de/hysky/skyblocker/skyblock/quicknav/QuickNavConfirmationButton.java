713ee52c3a330e980215314a1210128ce35436f5
alex <8379108+Alex33856@users.noreply.github.com>
2025-10-14 20:04:32 -0400

    Remove texture, change sound for first click

--- src/main/java/de/hysky/skyblocker/skyblock/quicknav/QuickNavConfirmationButton.java
+++ src/main/java/de/hysky/skyblocker/skyblock/quicknav/QuickNavConfirmationButton.java
@@ -1,12 +1,12 @@
 package de.hysky.skyblocker.skyblock.quicknav;
 
-import de.hysky.skyblocker.SkyblockerMod;
-import net.minecraft.client.gl.RenderPipelines;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.tooltip.Tooltip;
+import net.minecraft.client.sound.PositionedSoundInstance;
+import net.minecraft.client.sound.SoundManager;
 import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
 
 /**
  * A special type of {@link QuickNavButton} that requires double-clicking to open.
@@ -16,7 +16,6 @@ public class QuickNavConfirmationButton extends QuickNavButton {
 	private static final long DOUBLE_CLICK_TIME = 1000;
 	private long lastClicked = 0;
 	private boolean showingConfirmTooltip;
-	private static final Identifier DOUBLE_CLICK_ICON = Identifier.of(SkyblockerMod.NAMESPACE, "textures/gui/quick_nav_double_click.png");
 
 	protected final int index;
 
@@ -31,27 +30,33 @@ public class QuickNavConfirmationButton extends QuickNavButton {
 	@Override
 	public void renderWidget(DrawContext context, int mouseX, int mouseY, float delta) {
 		super.renderWidget(context, mouseX, mouseY, delta);
-		if (!toggled()) {
-			context.drawTexture(RenderPipelines.GUI_TEXTURED, DOUBLE_CLICK_ICON,
-					getX() + 5, getY() + 8 + (index < 7 ? 1 : -1),
-					0, 0, 16, 16, 16, 16);
-			long now = System.currentTimeMillis();
-			long timeSinceClick = now - lastClicked;
-			if (timeSinceClick < DOUBLE_CLICK_TIME != showingConfirmTooltip) {
-				showingConfirmTooltip = !showingConfirmTooltip;
-				setTooltip(showingConfirmTooltip ? CONFIRM_TOOLTIP : tooltip);
-			}
+		if (toggled()) return;
+		if (isDoubleClick() == showingConfirmTooltip) return;
+		showingConfirmTooltip = !showingConfirmTooltip;
+		setTooltip(showingConfirmTooltip ? CONFIRM_TOOLTIP : tooltip);
+	}
+
+	@Override
+	public void playDownSound(SoundManager soundManager) {
+		if (isDoubleClick()) {
+			super.playDownSound(soundManager);
+			return;
 		}
+		soundManager.play(PositionedSoundInstance.master(SoundEvents.BLOCK_NOTE_BLOCK_CHIME, 1.0F));
 	}
 
 	@Override
 	public void onClick(double mouseX, double mouseY) {
-		long now = System.currentTimeMillis();
-		if (now - lastClicked > DOUBLE_CLICK_TIME) {
+		if (!isDoubleClick()) {
 			lastClicked = System.currentTimeMillis();
 			return;
 		}
 
 		super.onClick(mouseX, mouseY);
+	}
+
+	private boolean isDoubleClick() {
+		long now = System.currentTimeMillis();
+		return now - lastClicked < DOUBLE_CLICK_TIME;
 	}
 }
