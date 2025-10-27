d3d8cf79a4bc25467880f45ce83bcd22bd2e9cc3
alex <8379108+Alex33856@users.noreply.github.com>
2025-10-27 14:24:46 -0400

    Use ItemTooltipCallback instead of modifying item data components

--- src/main/java/de/hysky/skyblocker/skyblock/bazaar/FixBazaarGraphs.java
+++ src/main/java/de/hysky/skyblocker/skyblock/bazaar/FixBazaarGraphs.java
@@ -1,20 +1,24 @@
 package de.hysky.skyblocker.skyblock.bazaar;
 
+import de.hysky.skyblocker.SkyblockerMod;
 import de.hysky.skyblocker.config.SkyblockerConfigManager;
 import de.hysky.skyblocker.utils.Constants;
-import de.hysky.skyblocker.utils.ItemUtils;
 import de.hysky.skyblocker.utils.container.SimpleContainerSolver;
 import de.hysky.skyblocker.utils.render.gui.ColorHighlight;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
+import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
+import net.fabricmc.fabric.api.event.Event;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.GenericContainerScreen;
-import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.LoreComponent;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.StringVisitable;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,41 +26,57 @@ import java.util.Optional;
 import java.util.regex.Pattern;
 
 public class FixBazaarGraphs extends SimpleContainerSolver {
-	boolean hasRun = false;
+	private static final MinecraftClient CLIENT = MinecraftClient.getInstance();
+	private static final Identifier FIX_BAZAAR_GRAPHS_PHASE = SkyblockerMod.id("fix-bazaar-graphs");
+	/**
+	 * Temporarily holds/caches the fixed lore for the graphs on screen.
+	 * Cleared when marked dirty (items are changed), or on reset (screen is closed).
+	 */
+	private static final Object2ObjectOpenHashMap<ItemStack, List<Text>> FIXED_LORE_MAP = new Object2ObjectOpenHashMap<>();
+
+	@Nullable
+	private GenericContainerScreen screen = null;
 
 	@Override
-	public void start(GenericContainerScreen screen) {
-		hasRun = false;
+	public void start(GenericContainerScreen newScreen) {
+		screen = newScreen;
 	}
 
 	@Override
 	public void markDirty() {
-		hasRun = false;
+		FIXED_LORE_MAP.clear();
+	}
+
+	@Override
+	public void reset() {
+		screen = null;
+		FIXED_LORE_MAP.clear();
 	}
 
 	public FixBazaarGraphs() {
-		super(Pattern.compile("^([\\w ])+➜ Graphs$"));
+		super(Pattern.compile("^.* ➜ Graphs$"));
+		// Register this before the default phase since it clears all the lore lines.
+		ItemTooltipCallback.EVENT.addPhaseOrdering(FIX_BAZAAR_GRAPHS_PHASE, Event.DEFAULT_PHASE);
+		// This runs on every frame that the item is hovered :(
+		ItemTooltipCallback.EVENT.register(FIX_BAZAAR_GRAPHS_PHASE, (stack, tooltipContext, tooltipType, lines) -> {
+			if (CLIENT.currentScreen == null || CLIENT.currentScreen != screen) return;
+			if (!stack.isOf(Items.PAPER)) return;
+			List<Text> newLines = FIXED_LORE_MAP.computeIfAbsent(stack, (_s) -> fixLore(lines));
+			lines.clear();
+			lines.addAll(newLines);
+		});
 	}
 
 	@Override
 	public List<ColorHighlight> getColors(Int2ObjectMap<ItemStack> slots) {
-		if (hasRun) return List.of();
-		hasRun = true;
-		slots.int2ObjectEntrySet().stream()
-				.filter(entry -> entry.getValue().isOf(Items.PAPER))
-				.forEach(entry -> fixLore(entry.getValue()));
 		return List.of();
 	}
 
-	private void fixLore(ItemStack stack) {
-		List<Text> lore = ItemUtils.getLore(stack);
-		if (lore.getLast().getString().contains(Constants.PREFIX.get().getString())) return;
-
+	private List<Text> fixLore(List<Text> loreLines) {
 		List<Text> fixedLore = new ArrayList<>();
-		lore.stream().map(this::fixLine).forEach(fixedLore::add);
+		loreLines.stream().map(this::fixLine).forEach(fixedLore::add);
 		fixedLore.add(Constants.PREFIX.get().append(Text.translatable("skyblocker.config.helpers.bazaar.fixBazaarGraphs.loreMessage")).styled(style -> style.withItalic(false).withColor(Formatting.WHITE)));
-
-		stack.set(DataComponentTypes.LORE, new LoreComponent(fixedLore));
+		return fixedLore;
 	}
 
 	private Text fixLine(Text line) {
