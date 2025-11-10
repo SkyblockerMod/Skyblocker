2635d957b6a4b98e7b0f9a3da05ff87740cce3a6
alex <8379108+Alex33856@users.noreply.github.com>
2025-11-10 17:02:15 -0500

    Codec Test

--- src/gametest/java/de/hysky/skyblocker/RoomDataTest.java
+++ src/gametest/java/de/hysky/skyblocker/RoomDataTest.java
@@ -1,12 +1,16 @@
 package de.hysky.skyblocker;
 
+import com.google.gson.JsonParser;
 import com.mojang.logging.LogUtils;
+import com.mojang.serialization.JsonOps;
 import de.hysky.skyblocker.skyblock.dungeon.secrets.DungeonManager;
 import net.fabricmc.fabric.api.client.gametest.v1.FabricClientGameTest;
 import net.fabricmc.fabric.api.client.gametest.v1.context.ClientGameTestContext;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 
+import java.io.BufferedReader;
 import java.util.List;
 
 import static de.hysky.skyblocker.skyblock.dungeon.secrets.DungeonManager.DUNGEONS_PATH;
@@ -27,13 +31,19 @@ public class RoomDataTest implements FabricClientGameTest {
 		List<String> roomFiles = getRoomFilesByType(client, ".json");
 		LOGGER.info("Found {} .skeleton files and {} .json files!", skeletonFiles.size(), roomFiles.size());
 
-		boolean isValid = checkRooms(skeletonFiles, roomFiles);
-		if (!isValid) throw new AssertionError("One or more rooms have an issue!");
+		if (!checkRooms(skeletonFiles, roomFiles))
+			throw new AssertionError("There are missing .skeleton or .json files!");
+
+		if (!checkRoomJson(client))
+			throw new AssertionError("There are invalid room .json files!");
 
 		int roomCount = skeletonFiles.size();
 		checkIfLoadedCorrectly(roomCount);
 	}
 
+	/**
+	 * Ensures that for each .skeleton file there is an associated .json file, and vice versa.
+	 */
 	public boolean checkRooms(List<String> skeletonFiles, List<String> roomFiles) {
 		boolean isValid = true;
 		for (String roomName : skeletonFiles) {
@@ -41,25 +51,30 @@ public class RoomDataTest implements FabricClientGameTest {
 				isValid = false;
 				LOGGER.error("{} is missing a .json file!", roomName);
 			}
-
-			// TODO: Move these to be separate tests using the Codec directly
-			/*
-			if (DungeonManager.getRoomMetadata(roomName) == null) {
-				isValid = false;
-				LOGGER.error("{} is missing room metadata!", roomName);
-			}
-
-			if (DungeonManager.getRoomWaypoints(roomName) == null) {
-				isValid = false;
-				LOGGER.error("{} is missing waypoints!", roomName);
-			}
-			 */
 		}
 
 		for (String roomName : roomFiles) {
 			if (!skeletonFiles.contains(roomName)) {
 				isValid = false;
 				LOGGER.error("{} is mising a .skeleton file!", roomName);
+			}
+		}
+		return isValid;
+	}
+
+	/**
+	 * Ensures every room .json is parsable
+	 */
+	public boolean checkRoomJson(MinecraftClient client) {
+		boolean isValid = true;
+		for (Identifier filePath : getRoomJson(client)) {
+			try (BufferedReader reader = client.getResourceManager().openAsReader(filePath)) {
+				DungeonManager.RoomData.CODEC.parse(JsonOps.INSTANCE, JsonParser.parseReader(reader)).getOrThrow();
+			} catch (Exception ex) {
+				String[] parts = filePath.getPath().split("/");
+				String roomName = parts[2] + "-" + parts[3];
+				LOGGER.error("Failed to load room: {}", roomName, ex);
+				isValid = false;
 			}
 		}
 		return isValid;
@@ -76,5 +91,10 @@ public class RoomDataTest implements FabricClientGameTest {
 		return client.getResourceManager().findResources(DUNGEONS_PATH, id -> id.getPath().endsWith(fileType))
 				.keySet().stream().map(identifier -> identifier.getPath().split("/"))
 				.filter(path -> path.length == 4).map(path -> path[3].replace(fileType, "")).toList();
+	}
+
+	List<Identifier> getRoomJson(MinecraftClient client) {
+		return client.getResourceManager().findResources(DUNGEONS_PATH, id ->
+				id.getPath().split("/").length == 4 && id.getPath().endsWith(".json")).keySet().stream().toList();
 	}
 }
