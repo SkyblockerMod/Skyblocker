d762b14726dfab8577da3d7883d239b20615704a
alex <8379108+Alex33856@users.noreply.github.com>
2025-11-12 14:34:33 -0500

    Review changes + minor refactor

--- src/main/java/de/hysky/skyblocker/skyblock/dungeon/DungeonMapLabels.java
+++ src/main/java/de/hysky/skyblocker/skyblock/dungeon/DungeonMapLabels.java
@@ -1,14 +1,8 @@
 package de.hysky.skyblocker.skyblock.dungeon;
 
-import de.hysky.skyblocker.annotations.Init;
-import de.hysky.skyblocker.config.SkyblockerConfigManager;
-import de.hysky.skyblocker.events.DungeonEvents;
-import de.hysky.skyblocker.skyblock.dungeon.secrets.DungeonManager;
-import de.hysky.skyblocker.skyblock.dungeon.secrets.DungeonMapUtils;
-import de.hysky.skyblocker.skyblock.dungeon.secrets.Room;
-import de.hysky.skyblocker.utils.Utils;
-import de.hysky.skyblocker.utils.render.HudHelper;
-import de.hysky.skyblocker.utils.scheduler.Scheduler;
+import java.util.List;
+import java.util.Set;
+
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
@@ -22,11 +16,18 @@ import org.joml.Vector2dc;
 import org.joml.Vector2i;
 import org.joml.Vector2ic;
 
-import java.util.List;
-import java.util.Set;
+import de.hysky.skyblocker.annotations.Init;
+import de.hysky.skyblocker.config.SkyblockerConfigManager;
+import de.hysky.skyblocker.events.DungeonEvents;
+import de.hysky.skyblocker.skyblock.dungeon.secrets.DungeonManager;
+import de.hysky.skyblocker.skyblock.dungeon.secrets.DungeonMapUtils;
+import de.hysky.skyblocker.skyblock.dungeon.secrets.Room;
+import de.hysky.skyblocker.utils.Utils;
+import de.hysky.skyblocker.utils.render.HudHelper;
+import de.hysky.skyblocker.utils.scheduler.Scheduler;
 
 public class DungeonMapLabels {
-	private static final Object2ObjectOpenHashMap<String, RoomLabel> LABELS = new Object2ObjectOpenHashMap<String, RoomLabel>();
+	private static final Object2ObjectOpenHashMap<String, RoomLabel> LABELS = new Object2ObjectOpenHashMap<>();
 	private static final float LABEL_SCALE = 0.55f;
 	private static final float MAX_WIDTH_SCALAR = 1.5f;
 
@@ -72,38 +73,44 @@ public class DungeonMapLabels {
 		if (roomInfo == null) return;
 		String roomName = roomInfo.name();
 
-		int color = Colors.GRAY;
-		if (room.greenChecked) {
-			color = Colors.GREEN;
-		} else if (room.whiteChecked) {
-			color = Colors.WHITE;
-		}
+		final int color = switch (room) {
+			case Room r when r.greenChecked -> Colors.GREEN;
+			case Room r when r.whiteChecked -> Colors.WHITE;
+			default -> Colors.GRAY;
+		};
 
+		float width = getMaxWidth(room, mapRoomSize) / LABEL_SCALE;
 		Text text = Text.literal(roomName);
-		LABELS.put(room.getName(), new RoomLabel(text, (int) mapPos.x(), (int) mapPos.y(), getMaxWidth(room, mapRoomSize), color));
+		List<OrderedText> lines = textRenderer.wrapLines(text, (int) width);
+		LABELS.put(room.getName(), new RoomLabel(lines, (int) mapPos.x(), (int) mapPos.y(), color));
 	}
 
 	protected static void renderRoomNames(DrawContext context) {
 		if (!SkyblockerConfigManager.get().dungeons.dungeonMap.showRoomLabels) return;
 
 		TextRenderer textRenderer = MinecraftClient.getInstance().textRenderer;
-		context.getMatrices().pushMatrix();
 		for (RoomLabel label : LABELS.values()) {
 			context.getMatrices().pushMatrix();
 			context.getMatrices().translate(label.x, label.y);
 			context.getMatrices().scale(LABEL_SCALE);
-			drawText(context, textRenderer, label.text, 0, 0, (int) (label.width / LABEL_SCALE), label.color);
+			drawText(context, textRenderer, label.textLines, label.color);
 			context.getMatrices().popMatrix();
 		}
-		context.getMatrices().popMatrix();
 	}
 
+	/**
+	 * The general idea is to put the text in the center of the room, this can vary on the room rotation.<br>
+	 * For 1x1 rooms, this is very simple.<br>
+	 * For L-shaped rooms, the text is centered on the wide part of the room.<br>
+	 * For the remaining room types, we take the average of the x and y.<br>
+	 */
 	private static Vec3d getPosForLabel(Room room, int mapRoomSize, int fontHeight) {
 		switch (room.getType()) {
 			case BLOOD, MINIBOSS, FAIRY:
 				return null;
 		}
 
+		// Each room is made up of one or more segments, depending on the size of the room.
 		Set<Vector2ic> segments = room.getSegments();
 		int[] segmentsX = segments.stream().mapToInt(Vector2ic::x).sorted().toArray();
 		int[] segmentsY = segments.stream().mapToInt(Vector2ic::y).sorted().toArray();
@@ -143,10 +150,15 @@ public class DungeonMapLabels {
 		return new Vec3d(result.x(), 0, result.y());
 	}
 
+	/**
+	 * Determines how long a room name can be before wrapping onto the next line.<br>
+	 * Generally, it is the size of the room multiplied by mapRoomSize.<br>
+	 * For 1x2s, 1x3s, and 1x4s, the rotation of the room is important, since we only draw text horizontally.
+	 */
 	private static int getMaxWidth(Room room, int mapRoomSize) {
 		int maxWidth = switch (room.getShape()) {
 			case ONE_BY_ONE -> {
-				if (mapRoomSize == 16) yield mapRoomSize + 2;
+				if (mapRoomSize == 16) yield mapRoomSize + 2; // minor nudging so most names can fit on one line
 				else yield mapRoomSize;
 			}
 			case TRAP, PUZZLE -> mapRoomSize;
@@ -160,16 +172,14 @@ public class DungeonMapLabels {
 		return (int) (maxWidth * MAX_WIDTH_SCALAR);
 	}
 
-	private static void drawText(DrawContext context, TextRenderer textRenderer, Text text, int x, int y, int width, int color) {
-		List<OrderedText> lines = textRenderer.wrapLines(text, width);
-		if (lines.size() > 1) y -= (textRenderer.fontHeight / 2) * (lines.size() - 1);
+	private static void drawText(DrawContext context, TextRenderer textRenderer, List<OrderedText> lines, int color) {
+		int y = lines.size() > 1 ? -(textRenderer.fontHeight / 2) * (lines.size() - 1) : 0;
 		for (OrderedText orderedText : lines) {
 			int textWidth = textRenderer.getWidth(orderedText) / 2;
-//			if (Screen.hasControlDown()) context.fill(x - textWidth, y - textRenderer.fontHeight / 2 + 4, x + textWidth, y + textRenderer.fontHeight - 1, Colors.RED);
-			HudHelper.drawOutlinedText(context, orderedText, x - textWidth, y, color, Colors.BLACK);
+			HudHelper.drawOutlinedText(context, orderedText, -textWidth, y, color, Colors.BLACK);
 			y += 9;
 		}
 	}
 
-	record RoomLabel(Text text, int x, int y, int width, int color) {}
+	record RoomLabel(List<OrderedText> textLines, int x, int y, int color) {}
 }
