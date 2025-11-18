f5369a8db0411ae74755c3eca800a06313b1625c
alex <8379108+Alex33856@users.noreply.github.com>
2025-11-17 19:24:41 -0500

    Add New Feature Config Search Tags

---
+++ src/main/java/de/hysky/skyblocker/config/CommonTags.java
@@ -0,0 +1,9 @@
+package de.hysky.skyblocker.config;
+
+import net.minecraft.text.Text;
+
+public final class CommonTags {
+	private static final String ADDED_IN_PREFIX = "added_in:";
+
+	public static final Text ADDED_IN_v5_9_0 = Text.of(ADDED_IN_PREFIX + "v5.9.0");
+}
