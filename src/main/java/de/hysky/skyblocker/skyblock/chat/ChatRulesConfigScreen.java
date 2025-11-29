3a8abe2ca0f2c5083cdfd6de0586e945031fa1eb
alex <8379108+Alex33856@users.noreply.github.com>
2025-11-28 19:07:11 -0500

    Minor Chat Rules Screen Changes

--- src/main/java/de/hysky/skyblocker/skyblock/chat/ChatRulesConfigScreen.java
+++ src/main/java/de/hysky/skyblocker/skyblock/chat/ChatRulesConfigScreen.java
@@ -1,70 +1,67 @@
 package de.hysky.skyblocker.skyblock.chat;
 
-import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ConfirmScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.gui.widget.GridWidget;
-import net.minecraft.client.gui.widget.SimplePositioningWidget;
+import net.minecraft.client.gui.widget.DirectionalLayoutWidget;
+import net.minecraft.client.gui.widget.ThreePartsLayoutWidget;
 import net.minecraft.screen.ScreenTexts;
 import net.minecraft.text.Text;
 
 public class ChatRulesConfigScreen extends Screen {
+	private ChatRulesConfigListWidget chatRulesConfigListWidget;
+	private ThreePartsLayoutWidget layout;
+	private final Screen parent;
 
-    private ChatRulesConfigListWidget chatRulesConfigListWidget;
-    private final Screen parent;
+	public ChatRulesConfigScreen(Screen parent) {
+		super(Text.translatable("skyblocker.config.chat.chatRules.screen.ruleScreen"));
+		this.parent = parent;
+	}
 
-    public ChatRulesConfigScreen(Screen parent) {
-        super(Text.translatable("skyblocker.config.chat.chatRules.screen.ruleScreen"));
-        this.parent = parent;
-    }
+	@Override
+	protected void init() {
+		layout = new ThreePartsLayoutWidget(this);
+		layout.addHeader(this.title, textRenderer);
+		chatRulesConfigListWidget = layout.addBody(new ChatRulesConfigListWidget(client, this, width, layout.getContentHeight(), layout.getHeaderHeight()));
+		DirectionalLayoutWidget footerLayout = layout.addFooter(new DirectionalLayoutWidget(0, 0, DirectionalLayoutWidget.DisplayAxis.HORIZONTAL));
+		footerLayout.getMainPositioner().marginX(5).marginY(2);
+		footerLayout.add(ButtonWidget.builder(ScreenTexts.CANCEL, button -> {
+			if (client != null) close();
+		}).build());
+		footerLayout.add(ButtonWidget.builder(Text.translatable("skyblocker.config.chat.chatRules.screen.new"),
+				buttonNew -> chatRulesConfigListWidget.addRuleAfterSelected()
+		).build());
+		footerLayout.add(ButtonWidget.builder(ScreenTexts.DONE, button -> {
+			chatRulesConfigListWidget.saveRules();
+			if (client != null) {
+				close();
+			}
+		}).build());
 
-    @Override
-    protected void init() {
-        super.init();
-        chatRulesConfigListWidget = new ChatRulesConfigListWidget(client, this, width, height - 96, 32, 25);
-        addDrawableChild(chatRulesConfigListWidget);
-        GridWidget gridWidget = new GridWidget();
-        gridWidget.getMainPositioner().marginX(5).marginY(2);
-        GridWidget.Adder adder = gridWidget.createAdder(3);
-        adder.add(ButtonWidget.builder(ScreenTexts.CANCEL, button -> {
-            if (client != null) {
-                close();
-            }
-        }).build());
-        ButtonWidget buttonNew1 = ButtonWidget.builder(Text.translatable("skyblocker.config.chat.chatRules.screen.new"), buttonNew -> chatRulesConfigListWidget.addRuleAfterSelected()).build();
-        adder.add(buttonNew1);
-        ButtonWidget buttonDone = ButtonWidget.builder(ScreenTexts.DONE, button -> {
-            chatRulesConfigListWidget.saveRules();
-            if (client != null) {
-                close();
-            }
-        }).build();
-        adder.add(buttonDone);
-        gridWidget.refreshPositions();
-        SimplePositioningWidget.setPos(gridWidget, 0, this.height - 64, this.width, 64);
-        gridWidget.forEachChild(this::addDrawableChild);
+		layout.refreshPositions();
+		layout.forEachChild(this::addDrawableChild);
+	}
 
-    }
+	@Override
+	protected void refreshWidgetPositions() {
+		layout.refreshPositions();
+		chatRulesConfigListWidget.setDimensionsAndPosition(width, layout.getContentHeight(), 0, layout.getHeaderHeight());
+		chatRulesConfigListWidget.refreshScroll();
+	}
 
-    @Override
-    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        super.render(context, mouseX, mouseY, delta);
-        context.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, 16, 0xFFFFFFFF);
-    }
-
-    @Override
-    public void close() {
-        if (client != null && chatRulesConfigListWidget.hasChanges()) {
-            client.setScreen(new ConfirmScreen(confirmedAction -> {
-                if (confirmedAction) {
-                    this.client.setScreen(parent);
-                } else {
-                    client.setScreen(this);
-                }
-            }, Text.translatable("text.skyblocker.quit_config"), Text.translatable("text.skyblocker.quit_config_sure"), Text.translatable("text.skyblocker.quit_discard"), ScreenTexts.CANCEL));
-        } else {
-            this.client.setScreen(parent);
-        }
-    }
+	@Override
+	public void close() {
+		assert client != null;
+		if (!chatRulesConfigListWidget.hasChanges()) {
+			this.client.setScreen(parent);
+			return;
+		}
+		client.setScreen(new ConfirmScreen(confirmedAction -> {
+			if (confirmedAction) {
+				this.client.setScreen(parent);
+			} else {
+				client.setScreen(this);
+			}
+		}, Text.translatable("text.skyblocker.quit_config"), Text.translatable("text.skyblocker.quit_config_sure"), Text.translatable("text.skyblocker.quit_discard"), ScreenTexts.CANCEL));
+	}
 }
