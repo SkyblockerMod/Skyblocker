cb1a129692156a1e051e9737bc7473269a7a7ed1
alex <8379108+Alex33856@users.noreply.github.com>
2025-12-03 13:20:33 -0500

    Add friendly name to all Locations

--- src/main/java/de/hysky/skyblocker/utils/Location.java
+++ src/main/java/de/hysky/skyblocker/utils/Location.java
@@ -2,7 +2,6 @@ package de.hysky.skyblocker.utils;
 
 import com.mojang.serialization.Codec;
 import net.minecraft.util.StringIdentifiable;
-import org.apache.commons.text.WordUtils;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
@@ -12,38 +11,38 @@ import java.util.EnumSet;
  * All Skyblock locations
  */
 public enum Location implements StringIdentifiable {
-	PRIVATE_ISLAND("dynamic"),
-	GARDEN("garden"),
-	HUB("hub"),
-	THE_FARMING_ISLAND("farming_1"),
-	THE_PARK("foraging_1", true),
+	PRIVATE_ISLAND("dynamic", "Private Island"),
+	GARDEN("garden", "The Garden"),
+	HUB("hub", "Hub"),
+	THE_FARMING_ISLAND("farming_1", "The Farming Islands"),
+	THE_PARK("foraging_1", "The Park", true),
 	SPIDERS_DEN("combat_1", "Spider's Den"),
-	BLAZING_FORTRESS("combat_2"),
-	THE_END("combat_3"),
-	CRIMSON_ISLE("crimson_isle"),
-	GOLD_MINE("mining_1"),
-	DEEP_CAVERNS("mining_2"),
-	DWARVEN_MINES("mining_3"),
-	BACKWATER_BAYOU("fishing_1"),
-	DUNGEON_HUB("dungeon_hub"),
+	@Deprecated BLAZING_FORTRESS("combat_2", "Blazing Fortress"),
+	THE_END("combat_3", "The End"),
+	CRIMSON_ISLE("crimson_isle", "Crimson Isle"),
+	GOLD_MINE("mining_1", "Gold Mine"),
+	DEEP_CAVERNS("mining_2", "Deep Caverns"),
+	DWARVEN_MINES("mining_3", "Dwarven Mines"),
+	BACKWATER_BAYOU("fishing_1", "Backwater Bayou"),
+	DUNGEON_HUB("dungeon_hub", "Dungeon Hub"),
 	WINTER_ISLAND("winter", "Jerry's Workshop"),
-	THE_RIFT("rift"),
-	DARK_AUCTION("dark_auction"),
-	CRYSTAL_HOLLOWS("crystal_hollows"),
+	THE_RIFT("rift", "The Rift"),
+	DARK_AUCTION("dark_auction", "Dark Auction"),
+	CRYSTAL_HOLLOWS("crystal_hollows", "Crystal Hollows"),
 	DUNGEON("dungeon", "Dungeons"),
 	KUUDRAS_HOLLOW("kuudra", "Kuudra's Hollow"),
 	/**
 	 * The freezing cold Glacite Mineshafts! *brr... so cold... :(*
 	 */
-	GLACITE_MINESHAFT("mineshaft"),
+	GLACITE_MINESHAFT("mineshaft", "Glacite Mineshaft"),
 	/**
 	 * <p>Goodbye 1.8 hello 1.21 (and foraging 50 for all)!</p>
 	 */
-	GALATEA("foraging_2", true),
+	GALATEA("foraging_2", "Galatea", true),
 	/**
 	 * Unknown Skyblock location
 	 */
-	UNKNOWN("unknown");
+	UNKNOWN("unknown", "Unknown");
 
 	public static final Codec<Location> CODEC = StringIdentifiable.createCodec(Location::values);
 	public static final Codec<EnumSet<Location>> SET_CODEC = CodecUtils.enumSetCodec(CODEC, Location.class);
@@ -51,13 +50,8 @@ public enum Location implements StringIdentifiable {
 	/**
 	 * location id from <a href="https://api.hypixel.net/v2/resources/games">Hypixel API</a>
 	 */
-	@NotNull
 	private final String id;
 
-	/**
-	 * friendly name from <a href="https://api.hypixel.net/v2/resources/games">Hypixel API</a>
-	 */
-	@NotNull
 	private final String friendlyName;
 
 	/**
@@ -69,20 +63,9 @@ public enum Location implements StringIdentifiable {
 	 * @param id           location id from <a href="https://api.hypixel.net/v2/resources/games">Hypixel API</a>
 	 * @param friendlyName friendly name from <a href="https://api.hypixel.net/v2/resources/games">Hypixel API</a>
 	 */
-	Location(@NotNull String id, @NotNull String friendlyName) {
+	Location(String id, String friendlyName) {
 		this.id = id;
 		this.friendlyName = friendlyName;
-		this.modern = false;
-	}
-
-	/**
-	 * Alternative constructor to avoid replicating simple friendlyNames that can be obtained with manipulating the enum's name.
-	 *
-	 * @param id location id from <a href="https://api.hypixel.net/v2/resources/games">Hypixel API</a>
-	 */
-	Location(@NotNull String id) {
-		this.id = id;
-		this.friendlyName = WordUtils.capitalizeFully(name().replace('_', ' '));
 		this.modern = false;
 	}
 
@@ -92,9 +75,9 @@ public enum Location implements StringIdentifiable {
 	 * @param id     location id from <a href="https://api.hypixel.net/v2/resources/games">Hypixel API</a>
 	 * @param modern the location is 1.21+ server
 	 */
-	Location(@NotNull String id, boolean modern) {
+	Location(String id, String friendlyName, boolean modern) {
 		this.id = id;
-		this.friendlyName = WordUtils.capitalizeFully(name().replace('_', ' '));
+		this.friendlyName = friendlyName;
 		this.modern = modern;
 	}
 
