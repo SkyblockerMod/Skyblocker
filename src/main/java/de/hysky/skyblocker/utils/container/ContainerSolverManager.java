4d2a1e056f7daf360f3084823e65089b3ce9789e
Kevin <92656833+kevinthegreat1@users.noreply.github.com>
2025-12-10 14:31:59 -0500

    Add anvil helper (#1916)
    (cherry picked from commit 7e4e73a050649726401d024c700cebf6d7619d52)

--- src/main/java/de/hysky/skyblocker/utils/container/ContainerSolverManager.java
+++ src/main/java/de/hysky/skyblocker/utils/container/ContainerSolverManager.java
@@ -8,24 +8,25 @@ import de.hysky.skyblocker.skyblock.accessories.newyearcakes.NewYearCakesHelper;
 import de.hysky.skyblocker.skyblock.auction.CopyUnderbidPrice;
 import de.hysky.skyblocker.skyblock.bazaar.ReorderHelper;
 import de.hysky.skyblocker.skyblock.chocolatefactory.ChocolateFactorySolver;
-import de.hysky.skyblocker.skyblock.dungeon.SellableItemsHighlighter;
-import de.hysky.skyblocker.skyblock.end.EndStatsBestiaryUpdater;
-import de.hysky.skyblocker.skyblock.galatea.TunerSolver;
 import de.hysky.skyblocker.skyblock.dungeon.CroesusHelper;
 import de.hysky.skyblocker.skyblock.dungeon.CroesusProfit;
 import de.hysky.skyblocker.skyblock.dungeon.SalvageHelper;
+import de.hysky.skyblocker.skyblock.dungeon.SellableItemsHighlighter;
 import de.hysky.skyblocker.skyblock.dungeon.terminal.ColorTerminal;
 import de.hysky.skyblocker.skyblock.dungeon.terminal.LightsOnTerminal;
 import de.hysky.skyblocker.skyblock.dungeon.terminal.OrderTerminal;
-import de.hysky.skyblocker.skyblock.dungeon.terminal.StartsWithTerminal;
 import de.hysky.skyblocker.skyblock.dungeon.terminal.SameColorTerminal;
+import de.hysky.skyblocker.skyblock.dungeon.terminal.StartsWithTerminal;
 import de.hysky.skyblocker.skyblock.dwarven.CommissionHighlight;
 import de.hysky.skyblocker.skyblock.dwarven.fossil.FossilSolver;
+import de.hysky.skyblocker.skyblock.end.EndStatsBestiaryUpdater;
 import de.hysky.skyblocker.skyblock.experiment.ChronomatronSolver;
 import de.hysky.skyblocker.skyblock.experiment.SuperpairsSolver;
 import de.hysky.skyblocker.skyblock.experiment.UltrasequencerSolver;
+import de.hysky.skyblocker.skyblock.galatea.TunerSolver;
 import de.hysky.skyblocker.skyblock.garden.StereoHarmonyHelper;
 import de.hysky.skyblocker.skyblock.hunting.HuntingBoxHelper;
+import de.hysky.skyblocker.skyblock.item.AnvilHelper;
 import de.hysky.skyblocker.skyblock.item.tooltip.adders.BitsHelper;
 import de.hysky.skyblocker.utils.Utils;
 import de.hysky.skyblocker.utils.render.gui.ColorHighlight;
@@ -38,7 +39,7 @@ import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.GenericContainerScreenHandler;
 import net.minecraft.screen.slot.Slot;
-import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
@@ -72,8 +73,11 @@ public class ContainerSolverManager {
 			new SellableItemsHighlighter(),
 			StereoHarmonyHelper.INSTANCE,
 			new EndStatsBestiaryUpdater(),
+			new AnvilHelper(),
 	};
+	@Nullable
 	private static ContainerSolver currentSolver = null;
+	@Nullable
 	private static List<ColorHighlight> highlights;
 	/**
 	 * Useful for keeping track of a solver's state in a Screen instance, such as if Hypixel closes & reopens a screen after every click (as they do with terminals).
@@ -82,6 +86,7 @@ public class ContainerSolverManager {
 
 	private ContainerSolverManager() {}
 
+	@Nullable
 	public static ContainerSolver getCurrentSolver() {
 		return currentSolver;
 	}
@@ -99,7 +104,7 @@ public class ContainerSolverManager {
 	}
 
 	@SuppressWarnings({"ConstantValue", "java:S1066"})
-	public static void onSetScreen(@NotNull GenericContainerScreen screen) {
+	public static void onSetScreen(GenericContainerScreen screen) {
 		String screenName = screen.getTitle().getString();
 		for (ContainerSolver solver : solvers) {
 			if (solver.isEnabled()) {
