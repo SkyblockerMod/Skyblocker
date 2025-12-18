4c01abf09dee68580a53fd6f2c908950996b3cf3
viciscat <51047087+viciscat@users.noreply.github.com>
2025-12-18 14:13:01 -0500

    use HudElementRegistry instead of mixins
    Cherry-picked from: 7b26e87d

--- src/main/java/de/hysky/skyblocker/skyblock/tabhud/TabHud.java
+++ src/main/java/de/hysky/skyblocker/skyblock/tabhud/TabHud.java
@@ -2,7 +2,12 @@ package de.hysky.skyblocker.skyblock.tabhud;
 
 import de.hysky.skyblocker.annotations.Init;
 import de.hysky.skyblocker.config.SkyblockerConfigManager;
+import de.hysky.skyblocker.skyblock.tabhud.config.WidgetsConfigurationScreen;
+import de.hysky.skyblocker.utils.Utils;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.hud.VanillaHudElements;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
@@ -23,6 +28,11 @@ public class TabHud {
 						InputUtil.Type.KEYSYM,
 						GLFW.GLFW_KEY_M,
 						"key.categories.skyblocker"));
+
+		HudElementRegistry.replaceElement(VanillaHudElements.PLAYER_LIST, hudElement -> {
+			if (!Utils.isOnSkyblock() || !SkyblockerConfigManager.get().uiAndVisuals.tabHud.tabHudEnabled || TabHud.shouldRenderVanilla() || MinecraftClient.getInstance().currentScreen instanceof WidgetsConfigurationScreen) return hudElement;
+			return (context, tickCounter) -> {};
+		});
 	}
 
 	public static boolean shouldRenderVanilla() {
