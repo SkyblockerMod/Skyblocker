5d8ffbdbb0280d1aa2de206561fff1b961c24365
viciscat <51047087+viciscat@users.noreply.github.com>
2025-12-19 18:31:03 +0100

    remove empty line

--- src/main/java/de/hysky/skyblocker/skyblock/StatusBarTracker.java
+++ src/main/java/de/hysky/skyblocker/skyblock/StatusBarTracker.java
@@ -19,7 +19,6 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-
 import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
