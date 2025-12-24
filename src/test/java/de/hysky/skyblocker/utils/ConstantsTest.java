5bb069739d1ff1b2c028b57cc3941f472dec20b5
alex <8379108+Alex33856@users.noreply.github.com>
2025-12-24 12:12:13 -0500

    Fix Constants Test

--- src/test/java/de/hysky/skyblocker/utils/ConstantsTest.java
+++ src/test/java/de/hysky/skyblocker/utils/ConstantsTest.java
@@ -10,8 +10,8 @@ public class ConstantsTest {
 	void testPrefix() {
 		LocalDate time = LocalDate.now();
 		// Yes, this test won't work on April 1st. I'm sorry.
-		if (time.getMonthValue() != 4 || time.getDayOfMonth() != 1) {
-			Assertions.assertEquals("empty[siblings=[literal{[}[style={color=gray}], literal{S}[style={color=#00FF4C}], literal{k}[style={color=#02FA60}], literal{y}[style={color=#04F574}], literal{b}[style={color=#07EF88}], literal{l}[style={color=#09EA9C}], literal{o}[style={color=#0BE5AF}], literal{c}[style={color=#0DE0C3}], literal{k}[style={color=#10DAD7}], literal{e}[style={color=#12D5EB}], literal{r}[style={color=#14D0FF}], literal{] }[style={color=gray}]]]", Constants.PREFIX.get().toString());
-		}
+		if (time.getMonthValue() == 4 && time.getDayOfMonth() == 1) return;
+		if (time.getMonthValue() == 12 && (time.getDayOfMonth() == 24 || time.getDayOfMonth() == 25)) return;
+		Assertions.assertEquals("empty[siblings=[literal{[}[style={color=gray}], literal{S}[style={color=#00FF4C}], literal{k}[style={color=#02FA60}], literal{y}[style={color=#04F574}], literal{b}[style={color=#07EF88}], literal{l}[style={color=#09EA9C}], literal{o}[style={color=#0BE5AF}], literal{c}[style={color=#0DE0C3}], literal{k}[style={color=#10DAD7}], literal{e}[style={color=#12D5EB}], literal{r}[style={color=#14D0FF}], literal{] }[style={color=gray}]]]", Constants.PREFIX.get().toString());
 	}
 }
