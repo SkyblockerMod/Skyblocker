f506ec2deb156396ca1a52ee86bfd42b6fb81019
alex <8379108+Alex33856@users.noreply.github.com>
2025-12-26 17:59:15 -0500

    Fix Stereo Harmony Helper not working with SkyHanni

--- src/main/java/de/hysky/skyblocker/skyblock/garden/StereoHarmonyHelper.java
+++ src/main/java/de/hysky/skyblocker/skyblock/garden/StereoHarmonyHelper.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import de.hysky.skyblocker.utils.RegexListUtils;
+import net.minecraft.ChatFormatting;
 import net.minecraft.world.item.ItemStack;
 import de.hysky.skyblocker.config.SkyblockerConfigManager;
 import de.hysky.skyblocker.utils.ItemUtils;
@@ -29,13 +32,14 @@ public class StereoHarmonyHelper extends SimpleContainerSolver {
 	public List<ColorHighlight> getColors(Int2ObjectMap<ItemStack> slots) {
 		List<ColorHighlight> highlights = new ArrayList<>();
 		for (Int2ObjectMap.Entry<ItemStack> entry : slots.int2ObjectEntrySet()) {
-			Matcher matcher = ItemUtils.getLoreLineIfMatch(entry.getValue(), PEST_NAME_PATTERN);
+			//noinspection DataFlowIssue - stripFormatting will not return null (!null to !null)
+			Matcher matcher = RegexListUtils.findInList(entry.getValue().skyblocker$getLoreStrings(), ChatFormatting::stripFormatting, PEST_NAME_PATTERN);
 
 			if (matcher != null) {
 				String pestName = matcher.group("name").trim();
 				String crop = GardenConstants.CROP_BY_PEST.get(pestName);
 				if (crop == null) continue;
-				boolean isPlaying = ItemUtils.getLoreLineIf(entry.getValue(), text -> text.equals("PLAYING")) != null;
+				boolean isPlaying = ItemUtils.getLoreLineIf(entry.getValue(), text -> text.contains("PLAYING")) != null;
 
 				if (Objects.equals(crop, CurrentJacobCrop.CURRENT_CROP_CONTEST)) {
 					highlights.add(isPlaying ? ColorHighlight.yellow(entry.getIntKey()) : ColorHighlight.green(entry.getIntKey()));
