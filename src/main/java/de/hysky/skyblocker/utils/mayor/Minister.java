a1586b928867ce805e606191c98aa6347cc85b9e
Aaron <51387595+AzureAaron@users.noreply.github.com>
2025-12-28 05:24:43 -0500

    Convert Mayor stuff to Codecs

--- src/main/java/de/hysky/skyblocker/utils/mayor/Minister.java
+++ src/main/java/de/hysky/skyblocker/utils/mayor/Minister.java
@@ -1,5 +1,8 @@
 package de.hysky.skyblocker.utils.mayor;
 
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
 /**
  * Represents a minister as retrieved from the API.
  * @param key The key of the minister.
@@ -11,6 +14,11 @@ public record Minister(String key, String name, Perk perk) {
 	 * An empty minister. Allows for better null safety.
 	 */
 	public static final Minister EMPTY = new Minister("", "", Perk.EMPTY);
+	public static final Codec<Minister> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+			Codec.STRING.fieldOf("key").forGetter(Minister::key),
+			Codec.STRING.fieldOf("name").forGetter(Minister::name),
+			Perk.CODEC.fieldOf("perk").forGetter(Minister::perk)
+			).apply(instance, Minister::new));
 
 	/**
 	 * For formatting purposes when printing out the result of the API call.
