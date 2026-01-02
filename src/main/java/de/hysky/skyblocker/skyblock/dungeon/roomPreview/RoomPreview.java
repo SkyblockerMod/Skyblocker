0ccb574b72a3f33e664ee60258db07542f0c935d
alex <8379108+Alex33856@users.noreply.github.com>
2026-01-02 14:45:28 -0500

    Fix double rendering

--- src/main/java/de/hysky/skyblocker/skyblock/dungeon/roomPreview/RoomPreview.java
+++ src/main/java/de/hysky/skyblocker/skyblock/dungeon/roomPreview/RoomPreview.java
@@ -6,25 +6,17 @@ import com.mojang.brigadier.context.CommandContext;
 import de.hysky.skyblocker.SkyblockerMod;
 import de.hysky.skyblocker.annotations.Init;
 import de.hysky.skyblocker.skyblock.dungeon.secrets.DungeonManager;
-import de.hysky.skyblocker.skyblock.dungeon.secrets.Room;
-import de.hysky.skyblocker.utils.render.WorldRenderExtractionCallback;
-import de.hysky.skyblocker.utils.render.primitive.PrimitiveCollector;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
-import org.jetbrains.annotations.Nullable;
 
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument;
 
 public class RoomPreview {
 	private static final Minecraft CLIENT = Minecraft.getInstance();
-
-	private static boolean isActive = false;
-	private static @Nullable Room previewRoom = null;
 
 	@Init
 	public static void init() {
@@ -33,8 +25,6 @@ public class RoomPreview {
 						then(ClientCommandManager.literal("previewRoom").then(argument("type", StringArgumentType.string()).suggests(DungeonManager::suggestRoomTypes)
 								.then(argument("room", StringArgumentType.string()).suggests((ctx, sB) -> DungeonManager.suggestRooms(ctx.getArgument("type", String.class), sB))
 										.executes(RoomPreview::startPreview)))))));
-		ClientPlayConnectionEvents.JOIN.register((nH, pS, client) -> reset());
-		WorldRenderExtractionCallback.EVENT.register(RoomPreview::renderWorld);
 	}
 
 	private static int startPreview(CommandContext<FabricClientCommandSource> ctx) {
@@ -50,19 +40,8 @@ public class RoomPreview {
 		return Command.SINGLE_SUCCESS;
 	}
 
-	private static void reset() {
-		isActive = false;
-		previewRoom = null;
-	}
-
-	private static void renderWorld(PrimitiveCollector primitiveCollector) {
-		if (!isActive || previewRoom == null) return;
-		previewRoom.extractRendering(primitiveCollector);
-	}
-
 	static void onJoin() {
-		previewRoom = new PreviewRoom(RoomPreviewServer.selectedRoom);
+		PreviewRoom previewRoom = new PreviewRoom(RoomPreviewServer.selectedRoom);
 		DungeonManager.startFromRoomPreview(previewRoom);
-		isActive = true;
 	}
 }
