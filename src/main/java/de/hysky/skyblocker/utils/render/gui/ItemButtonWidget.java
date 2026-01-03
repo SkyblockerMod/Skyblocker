2ee3e977d76aee870889df3a4f4d1d3b267f9a6c
Alex <8379108+Alex33856@users.noreply.github.com>
2026-01-02 23:01:55 -0500

    Forwardport some changes + Fix WaypointsTest (#2006)
    * Fix WaypointsTest with DFU update
    
    * Forwardport some changes
    
    * Fix TexturePool JavaDoc

--- src/main/java/de/hysky/skyblocker/utils/render/gui/ItemButtonWidget.java
+++ src/main/java/de/hysky/skyblocker/utils/render/gui/ItemButtonWidget.java
@@ -4,24 +4,25 @@ import java.time.Duration;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.Tooltip;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 
 public class ItemButtonWidget extends Button {
 
 	private final ItemStack item;
 
-	public ItemButtonWidget(int x, int y, int width, int height, ItemStack item, net.minecraft.network.chat.Component message, OnPress onPress, CreateNarration narrationSupplier) {
+	public ItemButtonWidget(int x, int y, int width, int height, ItemStack item, Component message, OnPress onPress, CreateNarration narrationSupplier) {
 		super(x, y, width, height, message, onPress, narrationSupplier);
 		this.item = item.copy();
 		setTooltip(Tooltip.create(message));
 		setTooltipDelay(Duration.ofMillis(250));
 	}
 
-	public ItemButtonWidget(int x, int y, int width, int height, ItemStack item, net.minecraft.network.chat.Component message, OnPress onPress) {
+	public ItemButtonWidget(int x, int y, int width, int height, ItemStack item, Component message, OnPress onPress) {
 		this(x, y, width, height, item, message, onPress, DEFAULT_NARRATION);
 	}
 
-	public ItemButtonWidget(int x, int y, ItemStack item, net.minecraft.network.chat.Component message, OnPress onPress) {
+	public ItemButtonWidget(int x, int y, ItemStack item, Component message, OnPress onPress) {
 		this(x, y, 20, 20, item, message, onPress, DEFAULT_NARRATION);
 	}
 
