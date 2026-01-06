6bfc0197095fa1c40af2df92e9367d43daf17d3b
Kazzyyyyyyyy <kazzybdw.yt@gmail.com>
2026-01-06 15:44:20 +0100

    #1

--- src/main/java/de/hysky/skyblocker/skyblock/profileviewer/ProfileViewerScreen.java
+++ src/main/java/de/hysky/skyblocker/skyblock/profileviewer/ProfileViewerScreen.java
@@ -58,7 +58,7 @@ public class ProfileViewerScreen extends Screen {
 	private static final String HYPIXEL_COLLECTIONS = "https://api.hypixel.net/v2/resources/skyblock/collections";
 	private static final Identifier TEXTURE = SkyblockerMod.id("textures/gui/profile_viewer/base_plate.png");
 	private static final int GUI_WIDTH = 322;
-	private static final int GUI_HEIGHT = 188;
+	private static final int GUI_HEIGHT = 189;
 	private static Map<String, String[]> COLLECTIONS;
 	private static Map<String, IntList> TIER_REQUIREMENTS;
 
