2913cbc02f0e5b4735236256c6e7b07c69fd9969
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2026-01-09 21:15:30 +0100

    Update config game test

--- src/gametest/java/de/hysky/skyblocker/SkyblockerConfigTest.java
+++ src/gametest/java/de/hysky/skyblocker/SkyblockerConfigTest.java
@@ -3,16 +3,16 @@ package de.hysky.skyblocker;
 import de.hysky.skyblocker.config.SkyblockerConfigManager;
 import net.fabricmc.fabric.api.client.gametest.v1.FabricClientGameTest;
 import net.fabricmc.fabric.api.client.gametest.v1.context.ClientGameTestContext;
-import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screens.Screen;
 
 @SuppressWarnings("UnstableApiUsage")
 public class SkyblockerConfigTest implements FabricClientGameTest {
 	@Override
 	public void runTest(ClientGameTestContext clientGameTestContext) {
-		Screen currentScreen = clientGameTestContext.computeOnClient(client -> client.currentScreen);
-		clientGameTestContext.runOnClient(client -> client.setScreen(SkyblockerConfigManager.createGUI(client.currentScreen)));
+		Screen currentScreen = clientGameTestContext.computeOnClient(client -> client.screen);
+		clientGameTestContext.runOnClient(client -> client.setScreen(SkyblockerConfigManager.createGUI(client.screen)));
 		clientGameTestContext.waitTicks(20);
-		clientGameTestContext.takeScreenshot("skyblocker_config"); // TODO assert the screenshot once we update to 1.21.11, which prevents the panorama from rotating
+		clientGameTestContext.assertScreenshotEquals("skyblocker_config");
 		clientGameTestContext.runOnClient(client -> client.setScreen(currentScreen));
 	}
 }
