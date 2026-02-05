1fa5a1b0ab1f0373c8cef56144917b858c48c4c7
viciscat <51047087+viciscat@users.noreply.github.com>
2026-02-05 17:06:46 +0100

    use ModifyVariable instead of MEV because aaron hates fun

--- src/main/java/de/hysky/skyblocker/mixins/ContainerScreenMixin.java
+++ src/main/java/de/hysky/skyblocker/mixins/ContainerScreenMixin.java
@@ -1,8 +1,5 @@
 package de.hysky.skyblocker.mixins;
 
-import com.llamalad7.mixinextras.expression.Definition;
-import com.llamalad7.mixinextras.expression.Expression;
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.gui.screens.inventory.ContainerScreen;
 import net.minecraft.network.chat.Component;
@@ -10,6 +7,7 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.ChestMenu;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(ContainerScreen.class)
 public abstract class ContainerScreenMixin extends AbstractContainerScreen<ChestMenu> {
@@ -18,18 +16,18 @@ public abstract class ContainerScreenMixin extends AbstractContainerScreen<Chest
 		super(handler, inventory, title);
 	}
 
-	@Definition(id = "width", field = "Lnet/minecraft/client/gui/screens/inventory/ContainerScreen;width:I")
-	@Definition(id = "imageWidth", field = "Lnet/minecraft/client/gui/screens/inventory/ContainerScreen;imageWidth:I")
-	@Expression("(this.width - this.imageWidth) / ?")
-	@ModifyExpressionValue(method = "renderBg", at = @At("MIXINEXTRAS:EXPRESSION"))
+	@ModifyVariable(
+			method = "renderBg",
+			at = @At("STORE"),
+			index = 5)
 	public int x(int ignored) {
 		return leftPos;
 	}
 
-	@Definition(id = "height", field = "Lnet/minecraft/client/gui/screens/inventory/ContainerScreen;height:I")
-	@Definition(id = "imageHeight", field = "Lnet/minecraft/client/gui/screens/inventory/ContainerScreen;imageHeight:I")
-	@Expression("(this.height - this.imageHeight) / ?")
-	@ModifyExpressionValue(method = "renderBg", at = @At("MIXINEXTRAS:EXPRESSION"))
+	@ModifyVariable(
+			method = "renderBg",
+			at = @At("STORE"),
+			index = 6)
 	public int y(int ignored) {
 		return topPos;
 	}
