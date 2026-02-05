be64b9eb530ccef2b4542d97d6957f07e493ad8c
Aaron <51387595+azureaaron@users.noreply.github.com>
2026-02-05 00:17:44 -0500

    Only apply glow during level rendering
    (cherry picked from commit d6f611367b71861ee215ac8ee3e92792fa19e954)

--- src/main/java/de/hysky/skyblocker/mixins/EntityRenderDispatcherMixin.java
+++ src/main/java/de/hysky/skyblocker/mixins/EntityRenderDispatcherMixin.java
@@ -3,41 +3,18 @@ package de.hysky.skyblocker.mixins;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import de.hysky.skyblocker.config.SkyblockerConfigManager;
-import de.hysky.skyblocker.injected.EntityRenderMarker;
 import de.hysky.skyblocker.skyblock.item.HeadTextures;
 import de.hysky.skyblocker.utils.ItemUtils;
 import de.hysky.skyblocker.utils.Utils;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
-import net.minecraft.client.renderer.entity.state.EntityRenderState;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.decoration.ArmorStand;
-import org.jspecify.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityRenderDispatcher.class)
-public class EntityRenderDispatcherMixin implements EntityRenderMarker {
-	@Unique
-	private EntityRenderState currentStateBeingRendered;
-
-	@Override
-	public @Nullable EntityRenderState skyblocker$getEntityStateBeingRendered() {
-		return this.currentStateBeingRendered;
-	}
-
-	@Inject(method = "submit", at = @At("HEAD"))
-	private void skyblocker$markEntityStateBeingRendered(CallbackInfo ci, @Local(argsOnly = true) EntityRenderState state) {
-		this.currentStateBeingRendered = state;
-	}
-
-	@Inject(method = "submit", at = @At("RETURN"))
-	private void skyblocker$clearEntityStateBeingRendered(CallbackInfo ci) {
-		this.currentStateBeingRendered = null;
-	}
+public class EntityRenderDispatcherMixin {
 
 	@ModifyReturnValue(method = "shouldRender", at = @At("RETURN"))
 	private <E extends Entity> boolean skyblocker$shouldRender(boolean original, @Local(argsOnly = true) E entity) {
