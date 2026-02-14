82241ca00a4929454777bc8e3bd361703af7de1b
viciscat <51047087+viciscat@users.noreply.github.com>
2026-02-14 19:00:38 +0100

    aaron HATES providers

--- src/main/java/de/hysky/skyblocker/utils/render/primitive/TextPrimitiveRenderer.java
+++ src/main/java/de/hysky/skyblocker/utils/render/primitive/TextPrimitiveRenderer.java
@@ -15,21 +15,25 @@ import net.minecraft.client.gui.render.TextureSetup;
 import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.client.renderer.state.CameraRenderState;
+import org.jspecify.annotations.Nullable;
 
 public final class TextPrimitiveRenderer implements PrimitiveRenderer<TextRenderState> {
 	protected static final TextPrimitiveRenderer INSTANCE = new TextPrimitiveRenderer();
-	private static final PipelineProvider SEE_THROUGH = CaxtonCompatibility.getSeeThroughTextPipeline()
-			.<PipelineProvider>map(p -> ignored -> p)
-			.orElse(intensityBased -> intensityBased ? RenderPipelines.TEXT_INTENSITY_SEE_THROUGH : RenderPipelines.TEXT_SEE_THROUGH);
-	private static final PipelineProvider NORMAL = CaxtonCompatibility.getTextPipeline()
-			.<PipelineProvider>map(p -> ignored -> p)
-			.orElse(intensityBased -> intensityBased ? RenderPipelines.TEXT_INTENSITY : RenderPipelines.TEXT);
+	private static final @Nullable RenderPipeline CAXTON_SEE_THROUGH = CaxtonCompatibility.getSeeThroughTextPipeline().orElse(null);
+	private static final @Nullable RenderPipeline CAXTON_NORMAL = CaxtonCompatibility.getTextPipeline().orElse(null);
+
+	private static RenderPipeline getPipeline(boolean seeThrough, boolean intensity) {
+		if (seeThrough) {
+			return CAXTON_SEE_THROUGH != null ? CAXTON_SEE_THROUGH : (intensity ? RenderPipelines.TEXT_INTENSITY_SEE_THROUGH : RenderPipelines.TEXT_SEE_THROUGH);
+		} else {
+			return CAXTON_NORMAL != null ? CAXTON_NORMAL : (intensity ? RenderPipelines.TEXT_INTENSITY : RenderPipelines.TEXT);
+		}
+	}
 
 	private TextPrimitiveRenderer() {}
 
 	@Override
 	public void submitPrimitives(TextRenderState state, CameraRenderState cameraState) {
-		PipelineProvider pipeline = state.throughWalls ? SEE_THROUGH : NORMAL;
 		Matrix4f positionMatrix = new Matrix4f()
 				.translate((float) (state.pos.x() - cameraState.pos.x()), (float) (state.pos.y() - cameraState.pos.y()), (float) (state.pos.z() - cameraState.pos.z()))
 				.rotate(cameraState.orientation)
@@ -50,14 +54,10 @@ public final class TextPrimitiveRenderer implements PrimitiveRenderer<TextRender
 				TextureSetup textureSetup = TextureSetup.singleTextureWithLightmap(glyph.textureView(), RenderSystem.getSamplerCache().getClampToEdge(FilterMode.NEAREST));
 				// This is a bit of a weird workaround to know if the intensity pipelines should be used instead of the normal ones.
 				// Normally GlyphBitmap#isColored should be used to figure that out, but we don't have access to it here
-				BufferBuilder buffer = Renderer.getBuffer(pipeline.get(glyph.guiPipeline() == RenderPipelines.GUI_TEXT_INTENSITY), textureSetup);
+				BufferBuilder buffer = Renderer.getBuffer(getPipeline(state.throughWalls, glyph.guiPipeline() == RenderPipelines.GUI_TEXT_INTENSITY), textureSetup);
 
 				glyph.render(positionMatrix, buffer, LightTexture.FULL_BRIGHT, false);
 			}
 		});
-	}
-
-	private interface PipelineProvider {
-		RenderPipeline get(boolean intensityBased);
 	}
 }
