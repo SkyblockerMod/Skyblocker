463c29c2009d2a8fa0b65963930f29bd8c2275f8
alex <8379108+Alex33856@users.noreply.github.com>
2026-03-31 20:18:56 -0400

    Fix Constants Test again

--- src/test/java/de/hysky/skyblocker/utils/ConstantsTest.java
+++ src/test/java/de/hysky/skyblocker/utils/ConstantsTest.java
@@ -11,11 +11,6 @@ public class ConstantsTest {
 		System.setProperty("skyblocker.iAmABoringPersonAndHateFun", "true");
 	}
 
-	@AfterAll
-	static void afterAll() {
-		System.setProperty("skyblocker.iAmABoringPersonAndHateFun", "");
-	}
-
 	@Test
 	void testPrefix() {
 		Assertions.assertEquals("empty[siblings=[literal{[}[style={color=gray}], empty[siblings=[literal{S}[style={color=#00FF4C}], literal{k}[style={color=#00FB6D}], literal{y}[style={color=#00F686}], literal{b}[style={color=#00F29C}], literal{l}[style={color=#00EDAF}], literal{o}[style={color=#00E8C0}], literal{c}[style={color=#00E2D1}], literal{k}[style={color=#00DCE1}], literal{e}[style={color=#02D6F0}], literal{r}[style={color=#14D0FF}]]], literal{] }[style={color=gray}]]]", Constants.PREFIX.get().toString());
