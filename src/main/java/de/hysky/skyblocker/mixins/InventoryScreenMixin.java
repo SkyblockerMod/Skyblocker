13569fed175a2f4c0cfb5ada4f1599426cd49e19
alex <8379108+Alex33856@users.noreply.github.com>
2026-04-21 23:30:26 -0400

    Improve Compatibility with FSR (#2336)
    * Remove ResourcePackCompatibility
    
    * Fix Equipment Items not rendering if labels are hidden
    
    * Simplify rendering
    
    * spotless
    
    (cherry picked from commit d8cf0d1a3f7440d59bdb002a285682f8f869a0df)

--- src/main/java/de/hysky/skyblocker/mixins/InventoryScreenMixin.java
+++ src/main/java/de/hysky/skyblocker/mixins/InventoryScreenMixin.java
@@ -2,9 +2,7 @@ package de.hysky.skyblocker.mixins;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
-import de.hysky.skyblocker.compatibility.ResourcePackCompatibility;
 import de.hysky.skyblocker.injected.RecipeBookHolder;
-import de.hysky.skyblocker.mixins.accessors.ScreenAccessor;
 
 import de.hysky.skyblocker.skyblock.item.SkyblockInventoryScreen;
 import net.minecraft.resources.Identifier;
@@ -87,13 +85,6 @@ public abstract class InventoryScreenMixin extends AbstractContainerScreen<Inven
 	@Inject(method = "init", at = @At("HEAD"))
 	private void skyblocker$clearRecipeToggleCallbacks(CallbackInfo ci) {
 		recipeBookToggleCallbacks.clear();
-	}
-
-	@Inject(method = "<init>", at = @At("TAIL"), order = 900) // run it a little earlier in case firmament do stuff
-	private void skyblocker$furfskyCompat(CallbackInfo ci) {
-		if (Utils.isOnSkyblock() && ResourcePackCompatibility.options.renameInventoryScreen().orElse(false)) {
-			((ScreenAccessor) this).setTitle(Component.literal(SkyblockerConfigManager.get().quickNav.enableQuickNav ? "InventoryScreenQuickNavSkyblocker" : "InventoryScreenSkyblocker"));
-		}
 	}
 
 	@Override
