b18de6c93342c4b54b7ee9236da22409b0072178
Alex <8379108+Alex33856@users.noreply.github.com>
2026-05-06 04:26:38 -0400

    Add support for Kat Pet Upgrade recipes (#2327)
    * Add Support for Kat Upgrade Recipes
    
    * 26.1

--- src/main/java/de/hysky/skyblocker/compatibility/jei/SkyblockKatUpgradeRecipeCategory.java
+++ src/main/java/de/hysky/skyblocker/compatibility/jei/SkyblockKatUpgradeRecipeCategory.java
@@ -1,6 +1,6 @@
 package de.hysky.skyblocker.compatibility.jei;
 
-import de.hysky.skyblocker.skyblock.itemlist.recipes.SkyblockNpcShopRecipe;
+import de.hysky.skyblocker.skyblock.itemlist.recipes.SkyblockKatUpgradeRecipe;
 import de.hysky.skyblocker.skyblock.tabhud.util.Ico;
 import de.hysky.skyblocker.utils.render.GuiHelper;
 import mezz.jei.api.gui.drawable.IDrawableStatic;
@@ -10,28 +10,29 @@ import mezz.jei.api.recipe.types.IRecipeType;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.network.chat.Component;
 
-public final class SkyblockNpcShopRecipeCategory extends AbstractSkyblockRecipeCategory<SkyblockNpcShopRecipe> {
-	private static final IRecipeType<SkyblockNpcShopRecipe> RECIPE_TYPE = IRecipeType.create(SkyblockNpcShopRecipe.ID, SkyblockNpcShopRecipe.class);
-	private static final Component TITLE = Component.translatable("emi.category.skyblocker.skyblock_npc_shop");
+public final class SkyblockKatUpgradeRecipeCategory extends AbstractSkyblockRecipeCategory<SkyblockKatUpgradeRecipe> {
+	private static final IRecipeType<SkyblockKatUpgradeRecipe> RECIPE_TYPE = IRecipeType.create(SkyblockKatUpgradeRecipe.ID, SkyblockKatUpgradeRecipe.class);
+	private static final Component TITLE = Component.translatable("emi.category.skyblocker.skyblock_kat_upgrade");
 	private static final int ITEM_SIZE = 16;
 	private static final int NPC_ITEM_PADDING = 2;
 
-	protected SkyblockNpcShopRecipeCategory(IGuiHelper guiHelper) {
+	protected SkyblockKatUpgradeRecipeCategory(IGuiHelper guiHelper) {
 		super(guiHelper, RECIPE_TYPE, TITLE, Ico.GOLD_NUGGET);
 	}
 
 	@Override
-	public void draw(SkyblockNpcShopRecipe recipe, IRecipeSlotsView recipeSlotsView, GuiGraphicsExtractor context, double mouseX, double mouseY) {
+	public void draw(SkyblockKatUpgradeRecipe recipe, IRecipeSlotsView recipeSlotsView, GuiGraphicsExtractor graphics, double mouseX, double mouseY) {
 		IDrawableStatic recipeArrow = this.guiHelper.getRecipeArrow();
 		int arrowYOffset = (this.getHeight() - recipeArrow.getHeight()) / 2;
-		recipeArrow.draw(context, 61, arrowYOffset);
+		recipeArrow.draw(graphics, 61, arrowYOffset);
 
 		int itemX = 61 + ((recipeArrow.getWidth() - ITEM_SIZE) / 2);
 		int itemY = arrowYOffset - ITEM_SIZE - NPC_ITEM_PADDING;
-		context.item(recipe.getRepresentative().getStackOrThrow(), itemX, itemY);
+		if (recipe.getRepresentative() == null) return;
+		graphics.item(recipe.getRepresentative().getStackOrThrow(), itemX, itemY);
 
 		if (GuiHelper.pointIsInArea(mouseX, mouseY, itemX, itemY, itemX + ITEM_SIZE, itemY + ITEM_SIZE)) {
-			this.drawTooltip(context, recipe.getRepresentative().getStackOrThrow().getHoverName(), mouseX, mouseY);
+			this.drawTooltip(graphics, recipe.getRepresentative().getStackOrThrow().getHoverName(), mouseX, mouseY);
 		}
 	}
 }
