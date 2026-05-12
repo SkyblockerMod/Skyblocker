954986283af0c48e7129d4e7cbca48d831812067
Alex <8379108+Alex33856@users.noreply.github.com>
2026-05-11 22:49:09 -0400

    Fix Price Tooltip in Attribute Menu (#2380)
    * Fix Bazaar Price Tooltip in Attribute Menu
    
    * remove old comment

--- src/main/java/de/hysky/skyblocker/utils/ItemUtils.java
+++ src/main/java/de/hysky/skyblocker/utils/ItemUtils.java
@@ -269,6 +269,13 @@ public final class ItemUtils {
 					};
 				}
 
+				if (currentScreen instanceof ContainerScreen container && container.getTitle().getString().equals("Attribute Menu")) {
+					Component stackName = stack.getOrDefault(DataComponents.CUSTOM_NAME, Component.empty());
+					Attribute attribute = Attributes.getAttributeFromAbilityName(stackName.getString());
+					if (attribute != null) return attribute.apiId();
+					return id;
+				}
+
 				if (stack instanceof ItemStack realStack && realStack.has(DataComponents.CUSTOM_NAME)) {
 					Component stackName = stack.getOrDefault(DataComponents.CUSTOM_NAME, Component.empty());
 					// Enchanted Books in the Bazaar
