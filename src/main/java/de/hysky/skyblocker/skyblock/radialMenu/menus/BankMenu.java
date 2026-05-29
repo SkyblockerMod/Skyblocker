8429105d630729c7cd178d209ea0d47a51ad154a
Aaron <51387595+AzureAaron@users.noreply.github.com>
2026-05-29 15:02:57 -0400

    Fix missing @Overrides
    (cherry picked from commit 20a2e9f7d3ad86e8b35d52f9f758fdce75a2f183)

--- src/main/java/de/hysky/skyblocker/skyblock/radialMenu/menus/BankMenu.java
+++ src/main/java/de/hysky/skyblocker/skyblock/radialMenu/menus/BankMenu.java
@@ -9,6 +9,7 @@ public class BankMenu extends RegexMenu {
 		super(".*bank.*", "bank");
 	}
 
+	@Override
 	public boolean itemMatches(int slotId, ItemStack stack) {
 		Item item = stack.getItem();
 		return !(item.equals(Items.BLACK_STAINED_GLASS_PANE) || item.equals(Items.GOLD_BLOCK) || item.equals(Items.REDSTONE_TORCH) || item.equals(Items.FILLED_MAP));
