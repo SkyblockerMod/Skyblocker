b283f542dec5eb1ac9f549959313c77a3c35b3a1
Aaron <51387595+AzureAaron@users.noreply.github.com>
2026-06-06 12:52:21 -0400

    Make Config Null Fields Fix work again

--- src/main/java/de/hysky/skyblocker/config/ConfigNullFieldsFix.java
+++ src/main/java/de/hysky/skyblocker/config/ConfigNullFieldsFix.java
@@ -6,8 +6,6 @@ import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
 
-import dev.isxander.yacl3.config.v2.api.SerialEntry;
-
 /**
  * While this sounds like a data fixer it isn't. - It's the only reasonable solution to deal with the mine field
  * that is YACL's null handling.
@@ -16,15 +14,13 @@ public class ConfigNullFieldsFix {
 	private static final Logger LOGGER = LogUtils.getLogger();
 
 	public static void init() {
-		return;
-		/*
 		SkyblockerConfigManager.update(config -> {
 			try {
 				fixNullFields(config, new SkyblockerConfig());
 			} catch (Exception e) {
 				LOGGER.error("[Skyblocker Config Null Fields Fixer] Failed to ensure that the config has no null fields! You may encounter crashes :(", e);
 			}
-		});*/
+		});
 	}
 
 	/**
@@ -32,17 +28,15 @@ public class ConfigNullFieldsFix {
 	 */
 	private static void fixNullFields(Object config, Object defaultConfig) throws Exception {
 		for (Field field : config.getClass().getDeclaredFields()) {
-			if (field.isAnnotationPresent(SerialEntry.class)) {
-				field.setAccessible(true);
+			field.setAccessible(true);
 
-				Object configValue = field.get(config);
-				Object defaultValue = field.get(defaultConfig);
+			Object configValue = field.get(config);
+			Object defaultValue = field.get(defaultConfig);
 
-				if (configValue == null && defaultValue != null) {
-					field.set(config, defaultValue);
-				} else if (configValue != null && defaultValue != null && SkyblockerConfigManager.isConfigClass(field.getType())) {
-					fixNullFields(configValue, defaultValue);
-				}
+			if (configValue == null && defaultValue != null) {
+				field.set(config, defaultValue);
+			} else if (configValue != null && defaultValue != null && SkyblockerConfigManager.isConfigClass(field.getType())) {
+				fixNullFields(configValue, defaultValue);
 			}
 		}
 	}
