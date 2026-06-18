683cd4fe19edddcf6879a6214135eaa019eb0e23
alex <8379108+Alex33856@users.noreply.github.com>
2026-06-18 18:00:46 -0400

    Replace Excluded Screen with Exclusion Zone for Side Buttons
    The bug preventing input was fixed.

--- src/main/java/de/hysky/skyblocker/compatibility/ItemListCompatibility.java
+++ src/main/java/de/hysky/skyblocker/compatibility/ItemListCompatibility.java
@@ -1,19 +1,18 @@
 package de.hysky.skyblocker.compatibility;
 
 import com.mojang.datafixers.util.Either;
-import com.operationpotato.itemlist.api.ExcludedScreensManager;
 import com.operationpotato.itemlist.api.ExclusionZoneManager;
 import com.operationpotato.itemlist.api.HoveredItemManager;
 import com.operationpotato.itemlist.api.Plugin;
 import de.hysky.skyblocker.config.SkyblockerConfigManager;
 import de.hysky.skyblocker.mixins.accessors.AbstractContainerScreenAccessor;
+import de.hysky.skyblocker.skyblock.auction.AuctionBrowserScreen;
 import de.hysky.skyblocker.skyblock.garden.visitor.VisitorHelper;
 import de.hysky.skyblocker.skyblock.item.ItemPrice;
 import de.hysky.skyblocker.skyblock.item.wikilookup.WikiLookupManager;
 import de.hysky.skyblocker.skyblock.museum.MuseumManager;
 import de.hysky.skyblocker.utils.Utils;
 import de.hysky.skyblocker.utils.hoveredItem.HoveredItemStackUtils;
-import de.hysky.skyblocker.utils.render.gui.AbstractCustomHypixelGUI;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.ContainerScreen;
@@ -22,7 +21,6 @@ import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.Rect2i;
 
 import java.util.List;
-import java.util.Optional;
 
 public class ItemListCompatibility implements Plugin {
 
@@ -47,14 +45,11 @@ public class ItemListCompatibility implements Plugin {
 					.map(rect -> new Rect2i(rect.position().x(), rect.position().y(), rect.width(), rect.height()))
 					.toList();
 		});
-	}
 
-	@Override
-	public void registerExcludedScreens(ExcludedScreensManager manager) {
-		if (!SkyblockerConfigManager.get().general.itemList.enableItemList) return;
-
-		// The Favorites Item Panel currently interferes with the side tabs of the Auction Browser Screen
-		manager.addProvider(AbstractCustomHypixelGUI.class, _ -> Optional.of("Skyblocker Fancy Auction House"));
+		zones.addProvider(AuctionBrowserScreen.class, screen -> {
+			AbstractContainerScreenAccessor accessor = (AbstractContainerScreenAccessor) screen;
+			return List.of(new Rect2i(accessor.getX() - 31, accessor.getY() + 2, 32, 28 * 6));
+		});
 	}
 
 	@Override
