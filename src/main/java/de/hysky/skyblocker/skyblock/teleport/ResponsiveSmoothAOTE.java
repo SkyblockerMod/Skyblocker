a69390fa2eca5ec4531c09516e6fff3212367a48
olim88 <bobq4582@gmail.com>
2026-06-22 20:25:16 +0100

    fix null import.

--- src/main/java/de/hysky/skyblocker/skyblock/teleport/ResponsiveSmoothAOTE.java
+++ src/main/java/de/hysky/skyblocker/skyblock/teleport/ResponsiveSmoothAOTE.java
@@ -9,7 +9,7 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.phys.Vec3;
 
-import javax.annotation.Nullable;
+import org.jspecify.annotations.Nullable;
 
 import static de.hysky.skyblocker.skyblock.teleport.PredictiveSmoothAOTE.getItemDistance;
 
