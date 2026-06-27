65f2c5902d4c3a928e8b0521f29516f8ec11f554
Aaron <51387595+AzureAaron@users.noreply.github.com>
2026-06-27 18:01:11 -0400

    Rename AbstractWidget to BasicWidget
    Avoids a pesky naming conflict with vanilla

--- src/main/java/de/hysky/skyblocker/utils/render/gui/EmptyWidget.java
+++ src/main/java/de/hysky/skyblocker/utils/render/gui/EmptyWidget.java
@@ -2,7 +2,7 @@ package de.hysky.skyblocker.utils.render.gui;
 
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 
-public class EmptyWidget extends AbstractWidget {
+public class EmptyWidget extends BasicWidget {
 	@Override
 	public void extractRenderState(GuiGraphicsExtractor context, int mouseX, int mouseY, float a) {}
 }
