9db1627eaa5c0311314b94cbda6c8dc32bf6b132
Aaron <51387595+AzureAaron@users.noreply.github.com>
2026-07-01 14:09:09 -0400

    Add Raffle Reward Highlighter
    (cherry picked from commit 1b15131e2f5cf648f32ff7d58494de17bea28024)

---
+++ src/main/java/de/hysky/skyblocker/skyblock/RaffleRewardHighlight.java
@@ -0,0 +1,48 @@
+package de.hysky.skyblocker.skyblock;
+
+import java.util.List;
+
+import de.hysky.skyblocker.utils.container.SimpleContainerSolver;
+import de.hysky.skyblocker.utils.render.gui.ColorHighlight;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+
+public class RaffleRewardHighlight extends SimpleContainerSolver {
+	public RaffleRewardHighlight() {
+		super("Previous Raffles");
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	@Override
+	public List<ColorHighlight> getColors(Int2ObjectMap<ItemStack> slots) {
+		ObjectArrayList<ColorHighlight> highlights = new ObjectArrayList<>();
+
+		for (Int2ObjectMap.Entry<ItemStack> entry : slots.int2ObjectEntrySet()) {
+			ItemStack itemStack = entry.getValue();
+
+			// Skip items that aren't valid
+			if (!itemStack.is(Items.FIREWORK_ROCKET) || !itemStack.is(Items.DIAMOND_BLOCK) || !itemStack.is(Items.GOLD_BLOCK)) {
+				continue;
+			}
+
+			List<String> lore = itemStack.skyblocker$getLoreStrings();
+
+			if (lore.isEmpty()) {
+				continue;
+			}
+
+			switch (lore.getLast()) {
+				case "You claimed your prize!" -> highlights.add(ColorHighlight.green(entry.getIntKey()));
+				case "Click to reveal your prize!" -> highlights.add(ColorHighlight.red(entry.getIntKey()));
+			}
+		}
+
+		return highlights;
+	}
+}
