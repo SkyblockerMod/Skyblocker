bbf38412ee7e98e2e92cbf2efce8185d059ccb94
LJNeon <23249107+LJNeon@users.noreply.github.com>
2026-07-04 23:44:07 -0700

    Improve autopet chat filter toasts

--- src/main/java/de/hysky/skyblocker/skyblock/chat/filters/AutopetFilter.java
+++ src/main/java/de/hysky/skyblocker/skyblock/chat/filters/AutopetFilter.java
@@ -5,28 +5,36 @@ import de.hysky.skyblocker.utils.chat.ChatFilterResult;
 import de.hysky.skyblocker.utils.chat.ChatPatternListener;
 import java.util.Objects;
 import java.util.regex.Matcher;
+
+import de.hysky.skyblocker.utils.datafixer.ItemStackComponentizationFixer;
+import de.hysky.skyblocker.utils.render.gui.BasicToast;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
 
 public class AutopetFilter extends ChatPatternListener {
+	private static ItemStack ICON = ItemStackComponentizationFixer.fromItemString("minecraft:player_head[minecraft:profile={id:[I;-7390193,1641495988,-1810636176,-1326826012],name:\"\",properties:[{name:\"textures\",signature:\"pxcnzvUMWZ5ESaviDIxUmfz3BEvz+wIUYkfsYM4ZJqtOhcgvsR/oC3/Gp+wgpRHBfTNTcOs/V+nlMdpyLPvXD7bhtdVqF5DiYO3O6UuCi30+InAlvPLKsUFEn0NSAH5oKjRr2ppVTwieZSUfoIvF/xXxgrSlZo+U", 1);
+
 	public AutopetFilter() {
 		super("^Autopet equipped your .*! VIEW RULE$");
 	}
 
 	@Override
-	public boolean onMatch(Component _message, Matcher matcher) {
+	public boolean onMatch(Component message, Matcher matcher) {
 		if (SkyblockerConfigManager.get().chat.hideAutopet == ChatFilterResult.ACTION_BAR) {
 			Objects.requireNonNull(Minecraft.getInstance().player).sendOverlayMessage(
 					Component.literal(
-							_message.getString().replace("VIEW RULE", "")
+							message.getString().replace("VIEW RULE", "")
 					));
+		} else if (SkyblockerConfigManager.get().chat.hideAutopet == ChatFilterResult.TOAST) {
+			Minecraft.getInstance().gui.toastManager().addToast(new BasicToast(Component.literal(message.getString().replace("VIEW RULE", "")), (long) (SkyblockerConfigManager.get().chat.toastDisplayDuration * 1000L), ICON));
 		}
 		return true;
 	}
 
 	@Override
 	public ChatFilterResult state() {
-		if (SkyblockerConfigManager.get().chat.hideAutopet == ChatFilterResult.ACTION_BAR)
+		if (SkyblockerConfigManager.get().chat.hideAutopet == ChatFilterResult.ACTION_BAR || SkyblockerConfigManager.get().chat.hideAutopet == ChatFilterResult.TOAST)
 			return ChatFilterResult.FILTER;
 		else
 			return SkyblockerConfigManager.get().chat.hideAutopet;
