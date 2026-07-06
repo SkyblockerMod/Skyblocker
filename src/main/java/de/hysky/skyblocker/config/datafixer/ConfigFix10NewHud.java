851bb856ee4eb8c5af84b32d44343efcb75db66e
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2026-07-05 17:38:46 -0700

    Fix a few hud ids

--- src/main/java/de/hysky/skyblocker/config/datafixer/ConfigFix10NewHud.java
+++ src/main/java/de/hysky/skyblocker/config/datafixer/ConfigFix10NewHud.java
@@ -9,6 +9,7 @@ import com.mojang.serialization.Dynamic;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.UnaryOperator;
 
 public class ConfigFix10NewHud extends ConfigDataFix {
 	public ConfigFix10NewHud(Schema outputSchema, boolean changesType) {
@@ -25,23 +26,55 @@ public class ConfigFix10NewHud extends ConfigDataFix {
 	}
 
 	private <T> Dynamic<T> fix(Dynamic<T> dynamic) {
-		return fixVersion(dynamic).renameAndFixField("positions", "configs", locations -> locations.updateMapValues(location -> {
-			Map<Dynamic<?>, Dynamic<?>> layers = new HashMap<>(Map.of(
-					dynamic.createString("hud"), dynamic.emptyMap(),
-					dynamic.createString("tab"), dynamic.emptyMap(),
-					dynamic.createString("secondary_tab"), dynamic.emptyMap()
-			));
-			location.getSecond().getMapValues().getOrThrow().forEach((widgetId, widget) -> layers.computeIfPresent(
-					fixWidgetLayer(widget, widgetId.asString("")),
-					(_, widgets) -> widgets.set(widgetId.asString(""), fixWidget(widget))
-			));
-			layers.replaceAll((_, widgets) -> dynamic.emptyMap().set("widgets", widgets));
-			return Pair.of(location.getFirst(), dynamic.createMap(layers));
-		})).set("copies", dynamic.createMap(Map.of(
+		return fixVersion(dynamic).renameAndFixField(
+				"positions",
+				"configs",
+				fixWidgets()
+		).set("copies", dynamic.createMap(Map.of(
 				dynamic.createString("hud"), dynamic.emptyMap(),
 				dynamic.createString("tab"), dynamic.emptyMap(),
 				dynamic.createString("secondary_tab"), dynamic.emptyMap()
 		)));
+	}
+
+	/**
+	 * Fixes the map of skyblock locations to widgets.
+	 */
+	private static UnaryOperator<Dynamic<?>> fixWidgets() {
+		return locations -> locations.updateMapValues(ConfigFix10NewHud::fixWidgetsForLocation);
+	}
+
+	/**
+	 * Fixes widgets in this skyblock location and returns a map of layers to widgets.
+	 */
+	private static Pair<Dynamic<?>, Dynamic<?>> fixWidgetsForLocation(Pair<Dynamic<?>, Dynamic<?>> location) {
+		Dynamic<?> locationId = location.getFirst();
+		Map<Dynamic<?>, Dynamic<?>> layers = new HashMap<>(Map.of(
+				locationId.createString("hud"), locationId.emptyMap(),
+				locationId.createString("tab"), locationId.emptyMap(),
+				locationId.createString("secondary_tab"), locationId.emptyMap()
+		));
+		location.getSecond().getMapValues().getOrThrow().forEach((widgetId, widget) -> fixWidgetAndLayer(widgetId, widget, layers));
+		layers.replaceAll((_, widgets) -> locationId.emptyMap().set("widgets", widgets));
+		return Pair.of(locationId, locationId.createMap(layers));
+	}
+
+	private static void fixWidgetAndLayer(Dynamic<?> widgetId, Dynamic<?> widget, Map<Dynamic<?>, Dynamic<?>> layers) {
+		Dynamic<?> widgetIdNew = fixWidgetId(widgetId);
+		layers.computeIfPresent(
+				fixWidgetLayer(widget, widgetIdNew.asString("")),
+				(_, widgets) -> widgets.set(widgetIdNew.asString(""), fixWidget(widget))
+		);
+	}
+
+	private static Dynamic<?> fixWidgetId(Dynamic<?> widgetId) {
+		return switch (widgetId.asString("")) {
+			case "sweepDetails" -> widgetId.createString("sweep_details");
+			case "Lasso HUD" -> widgetId.createString("hud_lasso");
+			case "Dungeon Splits" -> widgetId.createString("dungeon_splits");
+			case "Item Pickup" -> widgetId.createString("item_pickup");
+			default -> widgetId;
+		};
 	}
 
 	/**
@@ -52,7 +85,7 @@ public class ConfigFix10NewHud extends ConfigDataFix {
 			case "HUD" -> "hud";
 			case "MAIN_TAB" -> "tab";
 			case "SECONDARY_TAB" -> "secondary_tab";
-			default -> widgetId.contains("hud") ? "hud" : "tab";
+			default -> widgetId.contains("hud") || widgetId.equals("sweep_details") || widgetId.equals("powder_mining_tracker") || widgetId.equals("dungeon_splits") || widgetId.equals("item_pickup") ? "hud" : "tab";
 		};
 		return widget.createString(layer);
 	}
