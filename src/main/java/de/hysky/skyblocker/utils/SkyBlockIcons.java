8c48bc408b6d78d3b0c29aebf11c0d22cc89b7df
Alex <8379108+Alex33856@users.noreply.github.com>
2026-07-08 10:56:56 -0400

    Add support for new Resource Pack icons (#2499)
    * Add support for new Resource Pack Status Icons
    
    * just in case
    
    (cherry picked from commit 23c8762e25041667194ad311c7246732574e526a)

---
+++ src/main/java/de/hysky/skyblocker/utils/SkyBlockIcons.java
@@ -0,0 +1,9 @@
+package de.hysky.skyblocker.utils;
+
+public final class SkyBlockIcons {
+	// Action Bar Stats
+	public static final String HEALTH = "\uE010";
+	public static final String DEFENSE = "\uE008";
+	public static final String MANA = "\uE003";
+	public static final String RIFT_TIME = "\uE020";
+}
