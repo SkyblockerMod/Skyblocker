4c24ec925d5d487866fa3c6a53ccf1f9aa619965
LJNeon <23249107+LJNeon@users.noreply.github.com>
2026-07-07 22:02:25 -0700

    Correctly handle autopet head

--- src/main/java/de/hysky/skyblocker/skyblock/chat/filters/AutopetFilter.java
+++ src/main/java/de/hysky/skyblocker/skyblock/chat/filters/AutopetFilter.java
@@ -1,19 +1,20 @@
 package de.hysky.skyblocker.skyblock.chat.filters;
 
 import de.hysky.skyblocker.config.SkyblockerConfigManager;
+import de.hysky.skyblocker.skyblock.item.HeadTextures;
+import de.hysky.skyblocker.utils.FlexibleItemStack;
+import de.hysky.skyblocker.utils.ItemUtils;
 import de.hysky.skyblocker.utils.chat.ChatFilterResult;
 import de.hysky.skyblocker.utils.chat.ChatPatternListener;
 import java.util.Objects;
 import java.util.regex.Matcher;
 
-import de.hysky.skyblocker.utils.datafixer.ItemStackComponentizationFixer;
 import de.hysky.skyblocker.utils.render.gui.BasicToast;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.ItemStack;
 
 public class AutopetFilter extends ChatPatternListener {
-	private static ItemStack ICON = ItemStackComponentizationFixer.fromItemString("minecraft:player_head[minecraft:profile={id:[I;-7390193,1641495988,-1810636176,-1326826012],name:\"\",properties:[{name:\"textures\",signature:\"pxcnzvUMWZ5ESaviDIxUmfz3BEvz+wIUYkfsYM4ZJqtOhcgvsR/oC3/Gp+wgpRHBfTNTcOs/V+nlMdpyLPvXD7bhtdVqF5DiYO3O6UuCi30+InAlvPLKsUFEn0NSAH5oKjRr2ppVTwieZSUfoIvF/xXxgrSlZo+U", 1);
+	private static final FlexibleItemStack ICON = ItemUtils.createSkull(HeadTextures.AUTOPET_RULES_2);
 
 	public AutopetFilter() {
 		super("^Autopet equipped your .*! VIEW RULE$");
@@ -27,7 +28,7 @@ public class AutopetFilter extends ChatPatternListener {
 							message.getString().replace("VIEW RULE", "")
 					));
 		} else if (SkyblockerConfigManager.get().chat.hideAutopet == ChatFilterResult.TOAST) {
-			Minecraft.getInstance().gui.toastManager().addToast(new BasicToast(Component.literal(message.getString().replace("VIEW RULE", "")), (long) (SkyblockerConfigManager.get().chat.toastDisplayDuration * 1000L), ICON));
+			Minecraft.getInstance().gui.toastManager().addToast(new BasicToast(Component.literal(message.getString().replace("VIEW RULE", "")), (long) (SkyblockerConfigManager.get().chat.toastDisplayDuration * 1000L), ICON.getStack()));
 		}
 		return true;
 	}
