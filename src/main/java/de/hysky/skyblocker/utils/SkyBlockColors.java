235605171450256d10c5275aa9801581fe2f0050
Aaron <51387595+azureaaron@users.noreply.github.com>
2026-07-09 03:31:53 -0400

    Update rarity background colours to match SkyBlock
    (cherry picked from commit 21607ee86f205ee2bc0afc54c6d26cda3af5b922)

---
+++ src/main/java/de/hysky/skyblocker/utils/SkyBlockColors.java
@@ -0,0 +1,14 @@
+package de.hysky.skyblocker.utils;
+
+import net.minecraft.network.chat.TextColor;
+
+public class SkyBlockColors {
+	public static final TextColor DARK_RED = TextColor.fromRgb(0xD13228);
+	public static final TextColor GOLD = TextColor.fromRgb(0xFF9000);
+	public static final TextColor DARK_BLUE = TextColor.fromRgb(0x353FCE);
+	public static final TextColor BLUE = TextColor.fromRgb(0x459BFF);
+	public static final TextColor DARK_PURPLE = TextColor.fromRgb(0xA335EE);
+	public static final TextColor LIGHT_GRAY = TextColor.fromRgb(0xA8BFD2);
+	public static final TextColor GRAY = TextColor.fromRgb(0x707592);
+	public static final TextColor YELLOW = TextColor.fromRgb(0xFFDE2F);
+}
