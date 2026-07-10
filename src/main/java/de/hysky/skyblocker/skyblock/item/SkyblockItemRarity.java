83c55714b45ace733e0761875a018ffa74158754
Aaron <51387595+AzureAaron@users.noreply.github.com>
2026-07-10 00:03:25 -0400

    Move SkyblockItemRarity away from ChatFormatting

--- src/main/java/de/hysky/skyblocker/skyblock/item/SkyblockItemRarity.java
+++ src/main/java/de/hysky/skyblocker/skyblock/item/SkyblockItemRarity.java
@@ -2,45 +2,40 @@ package de.hysky.skyblocker.skyblock.item;
 
 import java.util.Arrays;
 import java.util.Optional;
-import net.minecraft.ChatFormatting;
-import net.minecraft.network.chat.TextColor;
-import net.minecraft.util.ARGB;
-import net.minecraft.util.StringRepresentable;
+
 import com.google.common.collect.Streams;
 import com.mojang.serialization.Codec;
+
 import de.hysky.skyblocker.utils.EnumUtils;
 import de.hysky.skyblocker.utils.SkyBlockColors;
 import io.github.moulberry.repo.data.Rarity;
+import net.minecraft.network.chat.TextColor;
+import net.minecraft.util.ARGB;
+import net.minecraft.util.StringRepresentable;
 
 public enum SkyblockItemRarity implements StringRepresentable {
-	COMMON(ChatFormatting.WHITE),
-	UNCOMMON(ChatFormatting.GREEN),
-	RARE(ChatFormatting.BLUE, SkyBlockColors.BLUE),
-	EPIC(ChatFormatting.DARK_PURPLE, SkyBlockColors.DARK_PURPLE),
-	LEGENDARY(ChatFormatting.GOLD, SkyBlockColors.GOLD),
-	MYTHIC(ChatFormatting.LIGHT_PURPLE),
-	DIVINE(ChatFormatting.AQUA),
-	SPECIAL(ChatFormatting.RED),
-	VERY_SPECIAL(ChatFormatting.RED),
-	ULTIMATE(ChatFormatting.DARK_RED, SkyBlockColors.DARK_RED),
-	ADMIN(ChatFormatting.DARK_RED, SkyBlockColors.DARK_RED),
-	UNKNOWN(ChatFormatting.DARK_GRAY);
+	COMMON(TextColor.WHITE),
+	UNCOMMON(TextColor.GREEN),
+	RARE(SkyBlockColors.BLUE),
+	EPIC(SkyBlockColors.DARK_PURPLE),
+	LEGENDARY(SkyBlockColors.GOLD),
+	MYTHIC(TextColor.LIGHT_PURPLE),
+	DIVINE(TextColor.AQUA),
+	SPECIAL(TextColor.RED),
+	VERY_SPECIAL(TextColor.RED),
+	ULTIMATE(SkyBlockColors.DARK_RED),
+	ADMIN(SkyBlockColors.DARK_RED),
+	UNKNOWN(TextColor.DARK_GRAY);
 
 	public static final Codec<SkyblockItemRarity> CODEC = StringRepresentable.fromEnum(SkyblockItemRarity::values);
 	public final String name;
-	public final ChatFormatting formatting;
 	public final int color;
 	public final float r;
 	public final float g;
 	public final float b;
 
-	SkyblockItemRarity(ChatFormatting formatting) {
-		this(formatting, TextColor.fromLegacyFormat(formatting));
-	}
-
-	SkyblockItemRarity(ChatFormatting formatting, TextColor textColor) {
-		this.name = name().replace("_", " ");
-		this.formatting = formatting;
+	SkyblockItemRarity(TextColor textColor) {
+		this.name = this.name().replace("_", " ");
 		//noinspection DataFlowIssue
 		this.color = textColor.getValue();
 
@@ -96,7 +91,7 @@ public enum SkyblockItemRarity implements StringRepresentable {
 
 	@Override
 	public String getSerializedName() {
-		return name();
+		return this.name();
 	}
 
 	@Override
