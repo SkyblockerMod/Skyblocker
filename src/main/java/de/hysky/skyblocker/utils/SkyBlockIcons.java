17adffd7308c9a9178a40231438dec9c4f38c37e
Aaron <51387595+AzureAaron@users.noreply.github.com>
2026-07-09 22:18:20 -0400

    Fix sack auto complete symbol filtering

--- src/main/java/de/hysky/skyblocker/utils/SkyBlockIcons.java
+++ src/main/java/de/hysky/skyblocker/utils/SkyBlockIcons.java
@@ -1,17 +1,38 @@
 package de.hysky.skyblocker.utils;
 
 public final class SkyBlockIcons {
-	// Action Bar Stats
+	// Combat Stats
 	public static final String HEALTH = "\uE010";
 	public static final String DEFENSE = "\uE008";
-	public static final String MANA = "\uE003";
+	public static final String TRUE_DEFENSE = "\uE027";
+	public static final String STRENGTH = "\uE00D";
+	public static final String CRIT_DAMAGE = "\uE007";
+	public static final String INTELLIGENCE = "\uE003";
+	public static final String MANA = INTELLIGENCE;
 	public static final String OVERFLOW_MANA = "\uE017";
-	public static final String RIFT_TIME = "\uE020";
-	// Other stats
+
+	// Mining Stats
+	public static final String MINING_FORTUNE = "\uE053";
+	public static final String MINING_SPEED = "\uE015";
+	public static final String PRISTINE = "\uE01C";
+
+	// Farming Stats
+	public static final String FARMING_FORTUNE = "\uE051";
+
+	// Foraging stats
 	public static final String SWEEP = "\uE023";
+	public static final String FORAGING_FORTUNE = "\uE054";
+
+	// Fishing Stats
+	public static final String FISHING_SPEED = "\uE00C";
+
+	// Rift Stats
+	public static final String RIFT_TIME = "\uE020";
+
 	// Scoreboard Icons
 	public static final String AREA = "\uE067";
 	public static final String RIFT_AREA = "\uE020";
+
 	// Mob Types
 	public static final String UNDEAD = "\uE084";
 }
