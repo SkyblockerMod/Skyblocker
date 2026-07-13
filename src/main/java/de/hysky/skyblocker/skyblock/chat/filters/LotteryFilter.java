ca222241241813cd1e38a7f78fcd1f8f20eba534
Internet Yamero <119892303+AC19970@users.noreply.github.com>
2026-07-13 20:45:55 +0900

    Update LotteryFilter.java
    +Helix Fortune

--- src/main/java/de/hysky/skyblocker/skyblock/chat/filters/LotteryFilter.java
+++ src/main/java/de/hysky/skyblocker/skyblock/chat/filters/LotteryFilter.java
@@ -6,8 +6,8 @@ import de.hysky.skyblocker.utils.chat.ChatFilterResult;
 
 public class LotteryFilter extends SimpleChatFilter {
 	private static final String LOTTERY_BUFF_CHANGED = "^New day! Your Lottery buff changed!$";
-	//^New buff: Gain (?:.+) (?:Fig Fortune|Mangrove Fortune|Sweep)\.$
-	private static final String LOTTERY_BUFF = "^New buff: Gain (?:.+) (?:Fig Fortune|Mangrove Fortune|Sweep)\\.$";
+	//^New buff: Gain (?:.+) (?:Fig Fortune|Mangrove Fortune|Helix Fortune|Sweep)\.$
+	private static final String LOTTERY_BUFF = "^New buff: Gain (?:.+) (?:Fig Fortune|Mangrove Fortune|Helix Fortune|Sweep)\\.$";
 	private static final String TOGGLE_LOTTERY = "^You can disable this messaging by toggling Lottery in your /hotf!$";
 
 	public LotteryFilter() {
