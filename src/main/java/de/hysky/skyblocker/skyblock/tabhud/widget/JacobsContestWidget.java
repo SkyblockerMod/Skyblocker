e413461f7516c6bfe8871c4d7a17e364a9b75ad6
viciscat <51047087+viciscat@users.noreply.github.com>
2026-07-16 16:28:16 +0200

    fix anita crop symbol (#2551)

--- src/main/java/de/hysky/skyblocker/skyblock/tabhud/widget/JacobsContestWidget.java
+++ src/main/java/de/hysky/skyblocker/skyblock/tabhud/widget/JacobsContestWidget.java
@@ -12,6 +12,7 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import de.hysky.skyblocker.utils.SkyBlockIcons;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
@@ -26,7 +27,7 @@ public class JacobsContestWidget extends TabHudWidget {
 	private static final MutableComponent TITLE = Component.literal("Jacob's Contest").withStyle(ChatFormatting.YELLOW,
 			ChatFormatting.BOLD);
 
-	private static final Pattern CROP_PATTERN = Pattern.compile("(?<fortune>[☘○]) (?<crop>.+?)(?: ◆ )?(?<percentage>Top [\\d.]+%)?");
+	private static final Pattern CROP_PATTERN = Pattern.compile(String.format("(?<fortune>[%s○]) (?<crop>.+?)(?: ◆ )?(?<percentage>Top [\\d.]+%%)?", SkyBlockIcons.FARMING_FORTUNE));
 
 	// Ordered the same as "Unique Brackets Reached" in Anita NPC shop
 	public static final Map<String, FlexibleItemStack> FARM_DATA = Map.ofEntries(
@@ -60,7 +61,7 @@ public class JacobsContestWidget extends TabHudWidget {
 					String crop = matcher.group("crop");
 					String percentage = matcher.group("percentage");
 					MutableComponent cropText = Component.empty().append(crop);
-					if (matcher.group("fortune").equals("☘")) cropText.append(Component.literal(" ☘").withStyle(ChatFormatting.GOLD));
+					if (matcher.group("fortune").equals(SkyBlockIcons.FARMING_FORTUNE)) cropText.append(Component.literal(" " + SkyBlockIcons.FARMING_FORTUNE).withStyle(ChatFormatting.GOLD));
 
 					this.addComponent(Elements.iconTextComponent(FARM_DATA.get(crop), cropText));
 					if (percentage != null) this.addComponent(new PlainTextElement(Component.literal(percentage)));
