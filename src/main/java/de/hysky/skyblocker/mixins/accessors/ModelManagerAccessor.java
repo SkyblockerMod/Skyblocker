b9d4a24403098ae714f0735e4f765a30df9990de
Aaron <51387595+AzureAaron@users.noreply.github.com>
2026-07-18 22:58:24 -0400

    Add Item Model Selector (#2566)
    * Skyblock Model Selector beginning
    
    * button
    
    * any model mwahahahaha
    
    * Add model data fixing & fix compile problem
    
    * Even out the size of the item selection box
    
    * Fix being unable to select item models
    
    * Tweak position of item selection item
    
    * Fix mixin file formatting & move ModelManagerAccessor
    
    * Add back end line
    
    ---------
    
    Co-authored-by: viciscat <51047087+viciscat@users.noreply.github.com>

---
+++ src/main/java/de/hysky/skyblocker/mixins/accessors/ModelManagerAccessor.java
@@ -0,0 +1,16 @@
+package de.hysky.skyblocker.mixins.accessors;
+
+import net.minecraft.client.renderer.item.ItemModel;
+import net.minecraft.client.resources.model.ModelManager;
+import net.minecraft.resources.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.Map;
+
+@Mixin(ModelManager.class)
+public interface ModelManagerAccessor {
+
+	@Accessor("bakedItemStackModels")
+	Map<Identifier, ItemModel> getBakedItemStackModels();
+}
