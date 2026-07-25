d0ac5b1de8634d307609a46aaf8698470b04cb2d
Aaron <51387595+AzureAaron@users.noreply.github.com>
2026-07-25 01:47:12 -0400

    Fix the order of operations not appearing to be what it seemed
    (cherry picked from commit cf2dc1d47c8548d7362dc87d700e244e45debc29)

--- src/main/java/de/hysky/skyblocker/skyblock/dwarven/CrystalsLocationsManager.java
+++ src/main/java/de/hysky/skyblocker/skyblock/dwarven/CrystalsLocationsManager.java
@@ -7,7 +7,9 @@ import com.mojang.logging.LogUtils;
 import de.hysky.skyblocker.SkyblockerMod;
 import de.hysky.skyblocker.annotations.Init;
 import de.hysky.skyblocker.config.SkyblockerConfigManager;
+import de.hysky.skyblocker.events.SkyblockEvents;
 import de.hysky.skyblocker.utils.Constants;
+import de.hysky.skyblocker.utils.Location;
 import de.hysky.skyblocker.utils.Utils;
 import de.hysky.skyblocker.utils.command.argumenttypes.blockpos.ClientBlockPosArgumentType;
 import de.hysky.skyblocker.utils.command.argumenttypes.blockpos.ClientPosArgument;
@@ -74,7 +76,8 @@ public class CrystalsLocationsManager {
 
 	protected static Map<String, MiningLocationLabel> activeWaypoints = new HashMap<>();
 	protected static List<String> verifiedWaypoints = new ArrayList<>();
-	private static boolean sentWaypointSubscribe = false;
+	private static boolean updatedTime = false;
+	private static boolean updatedLocation = false;
 	private static final List<MiningLocationLabel.CrystalHollowsLocationsCategory> waypointsSent2Socket = new ArrayList<>();
 
 	@Init
@@ -84,6 +87,7 @@ public class CrystalsLocationsManager {
 		LevelRenderExtractionCallback.EVENT.register(CrystalsLocationsManager::extractRendering);
 		ClientReceiveMessageEvents.ALLOW_GAME.register(CrystalsLocationsManager::extractLocationFromMessage);
 		ClientCommandRegistrationCallback.EVENT.register(CrystalsLocationsManager::registerWaypointLocationCommands);
+		SkyblockEvents.LOCATION_CHANGE.register(CrystalsLocationsManager::onLocationChange);
 		ClientPlayConnectionEvents.JOIN.register((_, _, _) -> reset());
 
 		// Nucleus Waypoints
@@ -388,25 +392,36 @@ public class CrystalsLocationsManager {
 		}
 	}
 
-	/// The WebSocket needs an accurate world time which only gets sent after the location is updated so we "defer" the
-	/// sending of the subscribe packet until the time is updated.
-	public static void onTimeUpdate() {
-		if (!sentWaypointSubscribe && Utils.isInCrystalHollows()) {
-			WsStateManager.subscribeServer(Service.CRYSTAL_WAYPOINTS, Optional.of(CrystalsWaypointSubscribeMessage.create(CLIENT.level)));
-			sentWaypointSubscribe = true;
+	private static void onLocationChange(Location newLocation) {
+		if (newLocation == Location.CRYSTAL_HOLLOWS) {
+			updatedLocation = true;
 		}
+	}
+
+	public static void onTimeUpdate() {
+		updatedTime = true;
 	}
 
 	private static void reset() {
 		activeWaypoints.clear();
 		verifiedWaypoints.clear();
 		waypointsSent2Socket.clear();
-		sentWaypointSubscribe = false;
+		updatedTime = false;
+		updatedLocation = false;
 	}
 
 	private static void update() {
 		if (CLIENT.player == null || CLIENT.getConnection() == null || !SkyblockerConfigManager.get().mining.crystalsWaypoints.enabled || !Utils.isInCrystalHollows()) {
 			return;
+		}
+
+		// Try connect to socket - this must happen after the location & time are updated since those two updates
+		// can happen in any order.
+		if (updatedTime && updatedLocation) {
+			WsStateManager.subscribeServer(Service.CRYSTAL_WAYPOINTS, Optional.of(CrystalsWaypointSubscribeMessage.create(CLIENT.level)));
+
+			updatedTime = false;
+			updatedLocation = false;
 		}
 
 		//get if the player is in the crystals
