57c35ca380e25fb727d7eeede5c29f15be36fada
Aaron <51387595+AzureAaron@users.noreply.github.com>
2026-07-26 19:29:19 -0400

    Add option gating & fix Beekeeper spelling

---
+++ src/main/java/de/hysky/skyblocker/skyblock/foraging/ForagingUtils.java
@@ -0,0 +1,7 @@
+package de.hysky.skyblocker.skyblock.foraging;
+
+import de.hysky.skyblocker.debug.Debug;
+
+public class ForagingUtils {
+	public static final boolean SHOW_TORRHUS_SAFARI_OPTIONS = Debug.debugEnabled();
+}
