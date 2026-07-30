73de77e7ffade953d894819696496688a7852e7f
Aaron <51387595+AzureAaron@users.noreply.github.com>
2026-07-30 00:11:26 -0400

    Add Biome Detection

---
+++ src/main/java/de/hysky/skyblocker/utils/SkyBlockBiomes.java
@@ -0,0 +1,11 @@
+package de.hysky.skyblocker.utils;
+
+import net.minecraft.resources.Identifier;
+
+public final class SkyBlockBiomes {
+	// Critter Safari
+	public static final Identifier HAUNTED = Identifier.fromNamespaceAndPath(Utils.HYPIXEL_NAMESPACE, "haunted");
+	public static final Identifier FOREST = Identifier.fromNamespaceAndPath(Utils.HYPIXEL_NAMESPACE, "forest");
+	public static final Identifier CANYON = Identifier.fromNamespaceAndPath(Utils.HYPIXEL_NAMESPACE, "canyon");
+	public static final Identifier ICY = Identifier.fromNamespaceAndPath(Utils.HYPIXEL_NAMESPACE, "icy");
+}
