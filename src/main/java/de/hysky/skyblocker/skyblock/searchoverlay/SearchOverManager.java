1d1cf3d42221e83757d0e23d19d79b043f4a2f6d
Luna <lunaynx@gmail.com>
2026-08-01 21:39:44 +0000

    fix: /ahsearch -> /auctionsearch

--- src/main/java/de/hysky/skyblocker/skyblock/searchoverlay/SearchOverManager.java
+++ src/main/java/de/hysky/skyblocker/skyblock/searchoverlay/SearchOverManager.java
@@ -527,7 +527,7 @@ public class SearchOverManager {
 		if (search.isEmpty()) return;
 		String command;
 		switch (location) {
-			case AUCTION -> command = "/ahsearch " + search;
+			case AUCTION -> command = "/auctionsearch " + search;
 			case BAZAAR -> command = "/bz " + search;
 			default -> {
 				return;
