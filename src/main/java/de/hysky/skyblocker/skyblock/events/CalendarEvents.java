4ec3d259ab35bccb1baf4e03adb97cb25cdb6346
viciscat <51047087+viciscat@users.noreply.github.com>
2026-08-01 13:25:53 +0200

    spotless

--- src/main/java/de/hysky/skyblocker/skyblock/events/CalendarEvents.java
+++ src/main/java/de/hysky/skyblocker/skyblock/events/CalendarEvents.java
@@ -18,7 +18,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executors;
 
 /**
  * Handles events that are parsed from the in-game calendar
