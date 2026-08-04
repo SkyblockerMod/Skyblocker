9967b15fd020420ffc28e8a080bd65d99fd371ae
Aaron <51387595+AzureAaron@users.noreply.github.com>
2026-08-03 21:45:14 -0400

    Make Hunting Box Price Tooltips aligned
    (cherry picked from commit d7b57cacb7f72684d2b94fa4b4b0eb571dfbbc88)

--- src/main/java/de/hysky/skyblocker/skyblock/item/tooltip/adders/HuntingBoxPriceTooltip.java
+++ src/main/java/de/hysky/skyblocker/skyblock/item/tooltip/adders/HuntingBoxPriceTooltip.java
@@ -17,6 +17,7 @@ import de.hysky.skyblocker.skyblock.item.tooltip.info.TooltipInfoType;
 import de.hysky.skyblocker.utils.BazaarProduct;
 import de.hysky.skyblocker.utils.ItemUtils;
 import de.hysky.skyblocker.utils.render.GuiHelper;
+import de.hysky.skyblocker.utils.render.text.GridComponent;
 
 public class HuntingBoxPriceTooltip extends SimpleTooltipAdder {
 	public HuntingBoxPriceTooltip(int priority) {
@@ -34,11 +35,12 @@ public class HuntingBoxPriceTooltip extends SimpleTooltipAdder {
 			boolean holdingShift = GuiHelper.hasShiftDown();
 			String shardText = count > 1 ? "Shards" : "Shard";
 
-			lines.add(Component.literal(shardText + " Sell Price: ")
-					.withStyle(ChatFormatting.GOLD)
-					.append(product.sellPrice().isEmpty()
+			lines.add(GridComponent.of(
+					Component.literal(shardText + " Sell Price:").withStyle(ChatFormatting.GOLD),
+					product.sellPrice().isEmpty()
 							? Component.literal("No data").withStyle(ChatFormatting.RED)
-							: ItemTooltip.getCoinsMessage(product.sellPrice().getAsDouble() * count, holdingShift ? count : 1, true)));
+							: ItemTooltip.getCoinsMessage(product.sellPrice().getAsDouble() * count, holdingShift ? count : 1, true)
+					));
 		}
 	}
 
