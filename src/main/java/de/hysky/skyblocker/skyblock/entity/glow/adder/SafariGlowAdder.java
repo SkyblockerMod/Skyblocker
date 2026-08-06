a5771d63b041453e70386aaa9db54112f8119ec0
Aaron <51387595+AzureAaron@users.noreply.github.com>
2026-08-05 21:19:35 -0400

    Fix moving Hideonfloors not highlighting
    (cherry picked from commit 3e382e766d005cc47f6d2615d266c96b904fd763)

--- src/main/java/de/hysky/skyblocker/skyblock/entity/glow/adder/SafariGlowAdder.java
+++ src/main/java/de/hysky/skyblocker/skyblock/entity/glow/adder/SafariGlowAdder.java
@@ -11,6 +11,7 @@ import de.hysky.skyblocker.utils.Utils;
 import net.minecraft.world.entity.Display.ItemDisplay;
 import net.minecraft.world.entity.monster.Shulker;
 import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.entity.Entity;
 
 public class SafariGlowAdder extends MobGlowAdder {
@@ -30,6 +31,7 @@ public class SafariGlowAdder extends MobGlowAdder {
 
 			// Forest Biome
 			case Shulker shulker when huntingConfig.forestBiome.highlightHideonfloor && SafariUtils.isInForestBiome() && shulker.getColor() == DyeColor.GREEN -> huntingConfig.forestBiome.hideonfloorHighlightColor.getRGB();
+			case ItemDisplay display when huntingConfig.forestBiome.highlightHideonfloor && SafariUtils.isInForestBiome() && display.getItemStack().is(Items.DYED_SHULKER_BOX.green()) -> huntingConfig.forestBiome.hideonfloorHighlightColor.getRGB();
 
 			// Haunted Biome
 			case ItemDisplay display when huntingConfig.hauntedBiome.highlightDuplico && SafariUtils.isInHauntedBiome() && display.getPosRotInterpolationDuration() == 3 -> huntingConfig.hauntedBiome.duplicoHighlightColor.getRGB();
