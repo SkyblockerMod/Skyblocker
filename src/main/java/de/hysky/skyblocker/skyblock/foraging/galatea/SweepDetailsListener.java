c422e69ee02c8dce440d475367a1c000cf59450c
LJ <23249107+LJNeon@users.noreply.github.com>
2026-08-12 23:30:54 -0700

    Correctly detect sweep values above 1,000 (#2658)

--- src/main/java/de/hysky/skyblocker/skyblock/foraging/galatea/SweepDetailsListener.java
+++ src/main/java/de/hysky/skyblocker/skyblock/foraging/galatea/SweepDetailsListener.java
@@ -17,7 +17,7 @@ import net.minecraft.network.chat.Component;
 public class SweepDetailsListener implements ChatMessageListener {
 	// Used to keep cancelled sweep messages in logs
 	private static final Logger LOGGER = LogUtils.getLogger();
-	protected static final Pattern SWEEP_DETAILS = Pattern.compile(String.format("Sweep Details: ([\\d.]+)[∮%s] Sweep", SkyBlockIcons.SWEEP));
+	protected static final Pattern SWEEP_DETAILS = Pattern.compile(String.format("Sweep Details: ([\\d,.]+)[∮%s] Sweep", SkyBlockIcons.SWEEP));
 	protected static final Pattern TREE_TOUGHNESS = Pattern.compile("  (.+?) Tree Toughness: ([\\d.]+) ([\\d.]+) Logs");
 	protected static final Pattern AXE_THROW_PENALTY = Pattern.compile("  Axe throw: (-\\d+)% Sweep ([\\d.]+) Logs");
 	protected static final Pattern WRONG_STYLE_PENALTY = Pattern.compile("  Wrong Style: (-\\d+)% Sweep ([\\d.]+) Logs ([a-zA-Z ]*)!!");
@@ -79,7 +79,7 @@ public class SweepDetailsListener implements ChatMessageListener {
 			active = true;
 			lastMatch = System.currentTimeMillis();
 
-			String rawMaxSweep = sweepDetails.group(1);
+			String rawMaxSweep = sweepDetails.group(1).replace(",", "");
 			if (NumberUtils.isCreatable(rawMaxSweep)) {
 				maxSweep = Float.parseFloat(rawMaxSweep);
 			} else {
