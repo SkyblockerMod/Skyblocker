d08f0f5c51c815c521007e704bf703c8a590a0d5
Kevin <92656833+kevinthegreat1@users.noreply.github.com>
2026-08-13 01:45:37 -0400

    Use foraging check

--- src/main/java/de/hysky/skyblocker/skyblock/foraging/galatea/TunerSolver.java
+++ src/main/java/de/hysky/skyblocker/skyblock/foraging/galatea/TunerSolver.java
@@ -326,7 +326,7 @@ public class TunerSolver extends SimpleContainerSolver implements SlotTextAdder 
 
 	private void onSound(ClientboundSoundPacket packet) {
 		if (!SkyblockerConfigManager.get().foraging.moongladeMarsh.enableTunerSolver
-				|| pitchSolved || (!Utils.isInGalatea() && !Utils.isInTorrhusCanyon()) || !isInMenu
+				|| pitchSolved || !Utils.isInForagingIsland() || !isInMenu
 				|| !packet.getSound().value().location().equals(SoundEvents.NOTE_BLOCK_BASS.value().location())) {
 			return;
 		}
