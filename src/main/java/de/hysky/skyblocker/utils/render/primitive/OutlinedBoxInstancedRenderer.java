bf7549d063c50e50074215188c3d5f7956fee3f6
Aaron <51387595+AzureAaron@users.noreply.github.com>
2026-08-20 11:03:27 -0400

    Use vertex colour multiplication

--- src/main/java/de/hysky/skyblocker/utils/render/primitive/OutlinedBoxInstancedRenderer.java
+++ src/main/java/de/hysky/skyblocker/utils/render/primitive/OutlinedBoxInstancedRenderer.java
@@ -38,13 +38,13 @@ public final class OutlinedBoxInstancedRenderer implements AutoCloseable {
 
 		if (!normalStates.isEmpty()) {
 			Renderer.UniformBinding normalUniform = new Renderer.UniformBinding("OutlinedBoxData", this.normalOutlinedBoxData.update(normalStates, cameraState));
-			VertexConsumer normalBuffer = Renderer.getBuffer(SkyblockerRenderPipelines.OUTLINED_BOX_INSTANCED, TextureSetup.noTexture(), 1f, normalStates.size(), normalUniform);
+			VertexConsumer normalBuffer = Renderer.getBuffer(SkyblockerRenderPipelines.OUTLINED_BOX_INSTANCED, TextureSetup.noTexture(), normalStates.size(), normalUniform);
 			buildUnitBox((float) UNIT_BOX.minX, (float) UNIT_BOX.minY, (float) UNIT_BOX.minZ, (float) UNIT_BOX.maxX, (float) UNIT_BOX.maxY, (float) UNIT_BOX.maxZ, normalBuffer);
 		}
 
 		if (!throughWallsStates.isEmpty()) {
 			Renderer.UniformBinding throughWallsUniform = new Renderer.UniformBinding("OutlinedBoxData", this.throughWallsOutlinedBoxData.update(throughWallsStates, cameraState));
-			VertexConsumer throughWallsBuffer = Renderer.getBuffer(SkyblockerRenderPipelines.OUTLINED_BOX_THROUGH_WALLS_INSTANCED, TextureSetup.noTexture(), 1f, throughWallsStates.size(), throughWallsUniform);
+			VertexConsumer throughWallsBuffer = Renderer.getBuffer(SkyblockerRenderPipelines.OUTLINED_BOX_THROUGH_WALLS_INSTANCED, TextureSetup.noTexture(), throughWallsStates.size(), throughWallsUniform);
 			buildUnitBox((float) UNIT_BOX.minX, (float) UNIT_BOX.minY, (float) UNIT_BOX.minZ, (float) UNIT_BOX.maxX, (float) UNIT_BOX.maxY, (float) UNIT_BOX.maxZ, throughWallsBuffer);
 		}
 	}
