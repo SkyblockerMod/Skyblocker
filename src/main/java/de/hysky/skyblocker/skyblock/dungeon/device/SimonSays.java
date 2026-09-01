c45d06538fe400d044c3b1d138c925cabcb68168
Aaron <51387595+AzureAaron@users.noreply.github.com>
2026-09-01 12:23:54 -0400

    Revert some device solver changes that broke them

--- src/main/java/de/hysky/skyblocker/skyblock/dungeon/device/SimonSays.java
+++ src/main/java/de/hysky/skyblocker/skyblock/dungeon/device/SimonSays.java
@@ -2,6 +2,8 @@ package de.hysky.skyblocker.skyblock.dungeon.device;
 
 import java.util.Objects;
 
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import it.unimi.dsi.fastutil.objects.ObjectList;
 import org.jspecify.annotations.Nullable;
 
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
@@ -40,7 +42,7 @@ public class SimonSays {
 	private static final float[] GREEN = ColorUtils.getFloatComponents(DyeColor.LIME);
 	private static final float[] YELLOW = ColorUtils.getFloatComponents(DyeColor.YELLOW);
 	private static final BlockPosSet CLICKED_BUTTONS = new BlockPosSet();
-	private static final BlockPosSet SIMON_PATTERN = new BlockPosSet();
+	private static final ObjectList<BlockPos> SIMON_PATTERN = new ObjectArrayList<>();
 
 	@Init
 	public static void init() {
@@ -59,7 +61,7 @@ public class SimonSays {
 
 			if (block.equals(Blocks.STONE_BUTTON)) {
 				if (BUTTONS_AREA.contains(Vec3.atLowerCornerOf(pos))) {
-					CLICKED_BUTTONS.add(pos);
+					CLICKED_BUTTONS.add(new BlockPos(pos)); //Copy just in case it becomes mutable in the future
 				} else if (pos.equals(START_BUTTON)) {
 					reset();
 				}
@@ -79,7 +81,7 @@ public class SimonSays {
 			Block newBlock = newState.getBlock();
 
 			if (BOARD_AREA.contains(posVec) && newBlock.equals(Blocks.OBSIDIAN) && oldState != null && oldState.getBlock().equals(Blocks.SEA_LANTERN)) {
-				SIMON_PATTERN.add(pos);
+				SIMON_PATTERN.add(pos.immutable()); //Convert to immutable because chunk delta updates use the mutable variant
 			} else if (BUTTONS_AREA.contains(posVec) && newBlock.equals(Blocks.AIR)) {
 				//Upon reaching the showing of the next sequence we need to reset the state so that we don't show old data
 				//Otherwise, the nextIndex will go beyond 5 and that can cause bugs, it also helps with the other case noted above
@@ -92,7 +94,7 @@ public class SimonSays {
 		if (shouldProcess()) {
 			int buttonsRendered = 0;
 
-			for (BlockPos pos : SIMON_PATTERN.iterateMut()) {
+			for (BlockPos pos : SIMON_PATTERN) {
 				//Offset to west (x - 1) to get the position of the button from the sea lantern block
 				BlockPos buttonPos = pos.west();
 				ClientLevel world = Objects.requireNonNull(Minecraft.getInstance().level); //Should never be null here
