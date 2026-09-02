aa82ff93817c81288b897f4d4e09872221f44231
Kevin <92656833+kevinthegreat1@users.noreply.github.com>
2026-09-02 09:58:39 -0700

    Fix numerous fancy hud bugs (#2720)
    Co-authored-by: Aaron <51387595+AzureAaron@users.noreply.github.com>

--- src/test/java/de/hysky/skyblocker/config/datafixer/HudDataFixerTest.java
+++ src/test/java/de/hysky/skyblocker/config/datafixer/HudDataFixerTest.java
@@ -1,6 +1,13 @@
 package de.hysky.skyblocker.config.datafixer;
 
+import java.io.InputStreamReader;
+
+import com.google.gson.JsonParser;
+import com.mojang.serialization.JsonOps;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import de.hysky.skyblocker.skyblock.tabhud.screenbuilder.WidgetManager;
 
 public class HudDataFixerTest {
 	@Test
@@ -13,5 +20,16 @@ public class HudDataFixerTest {
 	void testDataFixerWidgetOptions() {
 		ConfigDataFixerTest.testDataFix(9, 13);
 		ConfigDataFixerTest.testDataFix(12, 13, ConfigDataFixer.HUD_WIDGETS_TYPE, "/assets/skyblocker/config/skyblocker/hud_widgets_v");
+	}
+
+	@Test
+	void testDataFixerTabWidgets() {
+		ConfigDataFixerTest.testDataFix(9, 13);
+		ConfigDataFixerTest.testDataFix(1, 12, ConfigDataFixer.HUD_WIDGETS_TYPE, "/assets/skyblocker/config/skyblocker/hud_widgets_tab_v");
+	}
+
+	@Test
+	void testDataFixerHudWidgetCopies() {
+		Assertions.assertDoesNotThrow(() -> WidgetManager.Config.DATA_FIXING_CODEC.decode(JsonOps.INSTANCE, JsonParser.parseReader(new InputStreamReader(ConfigDataFixerTest.class.getResourceAsStream("/assets/skyblocker/config/skyblocker/hud_widgets_copies_v13.json")))).getOrThrow().getFirst());
 	}
 }
